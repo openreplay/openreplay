85041ac278bca1e8648117410b7cb5a11c230606
KRAIEM Taha Yassine <tahayk2@gmail.com>
2021-05-11 01:37:18 +0200

    Changes: - Alerts worker use PG only

--- backend/services/alerts/manager.go
+++ backend/services/alerts/manager.go
@@ -11,16 +11,16 @@ import (
 	"openreplay/backend/pkg/db/postgres"
 )
 
-const CHParallelLimit = 2
+const PGParallelLimit = 2
 
-var chCount int64
+var pgCount int64
 
 type manager struct {
 	notificationsUrl   string
 	alertsCache        map[uint32]*postgres.Alert
 	cacheMutex         sync.Mutex
-	chParallel         chan bool
-	ch                 *sql.DB
+	pgParallel         chan bool
+	pgs                *sql.DB
 	pg                 *postgres.Conn
 	pgMutex            sync.Mutex
 	notifications      map[uint32]*postgres.TenantNotification
@@ -28,13 +28,13 @@ type manager struct {
 	notificationsMutex sync.Mutex
 }
 
-func NewManager(notificationsUrl string, ch *sql.DB, pg *postgres.Conn) *manager {
+func NewManager(notificationsUrl string, pg *sql.DB, pg *postgres.Conn) *manager {
 	return &manager{
 		notificationsUrl:   notificationsUrl,
 		alertsCache:        make(map[uint32]*postgres.Alert),
 		cacheMutex:         sync.Mutex{},
-		chParallel:         make(chan bool, CHParallelLimit),
-		ch:                 ch,
+		pgParallel:         make(chan bool, PGParallelLimit),
+		pgs:                pgs,
 		pg:                 pg,
 		pgMutex:            sync.Mutex{},
 		notifications:      make(map[uint32]*postgres.TenantNotification),
@@ -64,29 +64,23 @@ func (m *manager) Update(a *postgres.Alert) error {
 func (m *manager) processAlert(a *postgres.Alert) {
 	defer func() {
 		defer m.notificationsGo.Done()
-		<-m.chParallel
+		<-m.pgParallel
 	}()
 	if !a.CanCheck() {
-		//log.Printf("cannot check %+v", a)
-		//log.Printf("cannot check alertId %d", a.AlertID)
 		log.Printf("cannot check %s", a.Name)
 		return
 	}
 	//log.Printf("checking %+v", a)
 	log.Printf("quering %s", a.Name)
 	//--- For stats:
-	atomic.AddInt64(&chCount, 1)
+	atomic.AddInt64(&pgCount, 1)
 	q, err := a.Build()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	//sub1, args, _ := q.ToSql()
-	//log.Println(sub1)
-	//log.Println(args)
-	//return
-	rows, err := q.RunWith(m.ch).Query()
+	rows, err := q.RunWith(m.pgs).Query()
 
 	if err != nil {
 		log.Println(err)
@@ -103,8 +97,7 @@ func (m *manager) processAlert(a *postgres.Alert) {
 			log.Println(err)
 			continue
 		}
-		//log.Println(valid)
-		//log.Println(value.Valid)
+
 		if valid && value.Valid {
 			log.Printf("%s: valid", a.Name)
 			m.notificationsMutex.Lock()
@@ -115,7 +108,7 @@ func (m *manager) processAlert(a *postgres.Alert) {
 				ButtonText:  "Check metrics for more details",
 				ButtonUrl:   fmt.Sprintf("/%d/metrics", a.ProjectID),
 				ImageUrl:    nil,
-				Options:     map[string]interface{}{"source": "ALERT", "sourceId": a.AlertID, "sourceMeta": a.DetectionMethod, "message": a.Options.Message, "projectId": a.ProjectID},
+				Options:     map[string]interface{}{"source": "ALERT", "sourceId": a.AlertID, "sourceMeta": a.DetectionMethod, "message": a.Options.Message, "projectId": a.ProjectID, "data": map[string]interface{}{"title": a.Name, "limitValue": a.Query.Right, "actualValue": value.Float64, "operator": a.Query.Operator, "trigger": a.Query.Left, "alertId": a.AlertID, "detectionMethod": a.DetectionMethod, "currentPeriod": a.Options.CurrentPeriod, "previousPeriod": a.Options.PreviousPeriod, "createdAt": time.Now().Unix() * 1000}},
 			}
 			m.notificationsMutex.Unlock()
 		}
@@ -126,7 +119,7 @@ func (m *manager) RequestAll() {
 	now := time.Now().Unix()
 	m.cacheMutex.Lock()
 	for _, a := range m.alertsCache {
-		m.chParallel <- true
+		m.pgParallel <- true
 		m.notificationsGo.Add(1)
 		go m.processAlert(a)
 		//m.processAlert(a)
@@ -135,8 +128,8 @@ func (m *manager) RequestAll() {
 	m.cacheMutex.Unlock()
 	//log.Println("waiting for all alerts to finish")
 	m.notificationsGo.Wait()
-	log.Printf("done %d CH queries in: %ds", chCount, time.Now().Unix()-now)
-	chCount = 0
+	log.Printf("done %d PG queries in: %ds", pgCount, time.Now().Unix()-now)
+	pgCount = 0
 	//log.Printf("Processing %d Notifications", len(m.notifications))
 	m.notificationsMutex.Lock()
 	go m.ProcessNotifications(m.notifications)
@@ -146,7 +139,6 @@ func (m *manager) RequestAll() {
 }
 
 func (m *manager) ProcessNotifications(allNotifications map[uint32]*postgres.TenantNotification) {
-	//return
 	if len(allNotifications) == 0 {
 		log.Println("No notifications to process")
 		return
@@ -164,6 +156,14 @@ func (m *manager) ProcessNotifications(allNotifications map[uint32]*postgres.Ten
 	toSend.Send(m.notificationsUrl)
 	if err := m.pg.SaveLastNotification(allIds); err != nil {
 		log.Printf("Error saving LastNotification time: %v", err)
-		return
+		if err.Error() == "conn closed" {
+			m.pg, err = postgres.NewPostgres(POSTGRES_STRING)
+			if err != nil {
+				panic(fmt.Sprintf("Postgres renew notifications connection error: %v\n", err))
+			}
+			if err := m.pg.SaveLastNotification(allIds); err != nil {
+				panic(fmt.Sprintf("Error saving LastNotification time, suicide: %v", err))
+			}
+		}
 	}
 }
