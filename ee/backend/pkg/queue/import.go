bddb08bda74c1ffc598041801c826f5415082be9
ShiKhu <alex.kaminsky.11@gmail.com>
2021-05-10 19:44:28 +0200

    feat (backend ee): check license

--- ee/backend/pkg/queue/import.go
+++ ee/backend/pkg/queue/import.go
@@ -3,13 +3,16 @@ package queue
 import (
 	"openreplay/backend/pkg/kafka"
 	"openreplay/backend/pkg/queue/types"
+	"openreplay/backend/pkg/license"
 )
 
 func NewConsumer(group string, topics []string, handler types.MessageHandler) types.Consumer {
+	license.CheckLicense()
 	return kafka.NewConsumer(group, topics, handler)
 }
 
 func NewProducer() types.Producer {
+	license.CheckLicense()
 	return kafka.NewProducer()
 }
 
