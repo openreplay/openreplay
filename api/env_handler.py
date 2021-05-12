d6e9d5dc0a1f58e9ef121fb769c2c28b80ad96f6
KRAIEM Taha Yassine <tahayk2@gmail.com>
2021-05-12 19:05:15 +0200

    Changes: - changed env-handler logic

--- api/env_handler.py
+++ api/env_handler.py
@@ -1,20 +1,11 @@
 from os import environ
 import json
-import os
 
 with open('.chalice/config.json') as json_file:
     data = json.load(json_file)
     stages = data.get("stages", {})
     for s in stages.keys():
-        target = f'chalicelib/.configs/{stages[s].get("environment_variables", {}).get("stage", s)}.json'
-        data = {}
-        try:
-            with open(target) as stage_vars:
-                data = json.load(stage_vars)
-        except IOError:
-            if not os.path.exists('chalicelib/.configs'):
-                os.makedirs('chalicelib/.configs')
-        with open(target, 'w') as outfile:
-            json.dump({**stages[s].get("environment_variables", {}), **data, **environ},
-                      outfile, indent=2, sort_keys=True)
-            print(f"injected env-vars to {target}")
+        data["stages"][s]["environment_variables"] = {**stages[s].get("environment_variables", {}), **environ}
+with open('.chalice/config.json', 'w') as outfile:
+    json.dump(data, outfile, indent=2, sort_keys=True)
+    print("override config.json")
