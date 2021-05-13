9f076c0235b167ab94ec5daf5471890b24828ea0
Shekar Siri <sshekarsiri@gmail.com>
2021-05-13 17:05:41 +0530

    fix: uncompleted feature bg

--- frontend/app/components/Header/OnboardingExplore/FeatureItem.js
+++ frontend/app/components/Header/OnboardingExplore/FeatureItem.js
@@ -9,7 +9,7 @@ const FeatureItem = ({ label, completed = false, subText, onClick }) => {
       className={ cn(stl.wrapper, { [stl.activeLink]: onClick, [stl.completed]: !completed }) }
       onClick={onClick && onClick}
     >
-      <div className={ cn("h-6 w-6 flex-shrink-0 rounded-full flex items-center justify-center", { 'bg-white' : !completed, 'bg-teal' : completed })}>
+      <div className={ cn("h-6 w-6 flex-shrink-0 rounded-full flex items-center justify-center", { 'bg-gray-lightest border' : !completed, 'bg-teal' : completed })}>
         { completed && <Icon name="check" size="16" color="white" /> }
       </div>
       <div className="ml-3">{label}</div>     
