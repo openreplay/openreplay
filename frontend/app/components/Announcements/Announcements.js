6824afd72439fd43945ecca754ade7ff4a75834d
Shekar Siri <sshekarsiri@gmail.com>
2021-05-13 16:52:05 +0530

    fix: text change

--- frontend/app/components/Announcements/Announcements.js
+++ frontend/app/components/Announcements/Announcements.js
@@ -73,8 +73,8 @@ class Announcements extends React.Component {
           content={ 
             <div className="mx-4">
               <NoContent
-                title="No Alerts found!"
-                subtext="There are no alerts created."
+                title="No Announcements found!"
+                subtext="There are no announcements to show."
                 icon="exclamation-circle"
                 show={ !loading && announcements.size === 0 }
                 size="small"
