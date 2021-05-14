71135413779417968a426619d963d9b14b104f6c
Rajesh Rajendran <rjshrjndrn@gmail.com>
2021-05-14 22:29:36 +0530

    chore(script): update usage for build deploy.
    Signed-off-by: Rajesh Rajendran <rjshrjndrn@gmail.com>

--- scripts/helm/build_deploy.sh
+++ scripts/helm/build_deploy.sh
@@ -3,6 +3,8 @@ set -e
 
 # This script will build and push docker image to registry
 
+# Usage: IMAGE_TAG=latest DOCKER_REPO=rg.fr-par.scw.cloud/foss bash build_deploy.sh
+
 echo $DOCKER_REPO
 [[ -z DOCKER_REPO ]] && {
     echo Set DOCKER_REPO="your docker registry"
