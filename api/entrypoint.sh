6402333660a4356543d739267ea09f2ea5bba661
KRAIEM Taha Yassine <tahayk2@gmail.com>
2021-06-22 18:17:58 +0200

    feat(api): install assist_server dependencies and start it with the API

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd sourcemaps_reader
+cd /sourcemaps_reader
+nohup node server.js &> /tmp/sourcemaps_reader.log &
+cd /assist_server
 nohup node server.js &> /tmp/sourcemaps_reader.log &
 cd ..
 python env_handler.py
