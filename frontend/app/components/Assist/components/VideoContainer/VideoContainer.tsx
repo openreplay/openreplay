4fd018ca561039bbd0c2f6169933179283687348
Shekar Siri <sshekarsiri@gmail.com>
2021-06-30 12:08:49 +0530

    feature(ui) - assist ui changes

--- frontend/app/components/Assist/components/VideoContainer/VideoContainer.tsx
+++ frontend/app/components/Assist/components/VideoContainer/VideoContainer.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { Button, Icon } from 'UI'
+import { Button, Icon, IconButton } from 'UI'
+import cn from 'classnames'
+import stl from './VideoContainer.css'
 
 interface Props {
   stream: MediaStream | null
@@ -32,17 +34,21 @@ function VideoContainer({ stream, muted = false }: Props) {
   }
 
   return (
-    <div className="relative bg-gray-light-shade border p-1" style={{ height: '160px', width: '200px' }}>      
-      <div className="absolute inset-0 flex justify-center border border-gray-300 p-1 bg-white radius bg-opacity-25">
+    <div className="relative bg-gray-light-shade" style={{ height: '152px', width: '200px' }}>      
+      <div className="absolute inset-0 flex justify-center border border-gray-300 bg-white radius bg-opacity-25">
         <video autoPlay ref={ ref } muted={ muted } />
-        <div className="flex items-center absolute w-full justify-center bottom-0 bg-gray-lightest">
-          <Button plain size="small" onClick={toggleAudio}>
-            <Icon name={audioEnabled ? 'mic' : 'mic-mute'} size="16" />
-          </Button>
+        <div className={cn(stl.controls, "flex items-center absolute w-full justify-end bottom-0")}>
+          <div className={cn(stl.btnWrapper, { [stl.disabled]: !audioEnabled})}>
+            <Button plain size="small" onClick={toggleAudio} noPadding>
+              <Icon name={audioEnabled ? 'mic' : 'mic-mute'} size="14" />
+            </Button>
+          </div>
 
-          <Button plain size="small" onClick={toggleVideo}>
-            <Icon name={ videoEnabled ? 'camera-video' : 'camera-video-off' } size="16" />
-          </Button>
+          <div className={cn(stl.btnWrapper, { [stl.disabled]: !videoEnabled})}>
+            <Button plain size="small" onClick={toggleVideo} noPadding>
+              <Icon name={ videoEnabled ? 'camera-video' : 'camera-video-off' } size="14" />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
