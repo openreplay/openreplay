dc3823184da55c0145a40b1a1003fec0b91b5014
ShiKhu <alex.kaminsky.11@gmail.com>
2021-07-07 18:06:01 +0300

    fix (tracker-assist & frontend-assist): connection logic fix

--- tracker/tracker-assist/src/confirm.ts
+++ tracker/tracker-assist/src/confirm.ts
@@ -1,9 +1,10 @@
 
 const declineIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 128 128" ><g id="Circle_Grid" data-name="Circle Grid"><circle cx="64" cy="64" fill="#ef5261" r="64"/></g><g id="icon"><path d="m57.831 70.1c8.79 8.79 17.405 12.356 20.508 9.253l4.261-4.26a7.516 7.516 0 0 1 10.629 0l9.566 9.566a7.516 7.516 0 0 1 0 10.629l-7.453 7.453c-7.042 7.042-27.87-2.358-47.832-22.319-9.976-9.981-16.519-19.382-20.748-28.222s-5.086-16.091-1.567-19.61l7.453-7.453a7.516 7.516 0 0 1 10.629 0l9.566 9.563a7.516 7.516 0 0 1 0 10.629l-4.264 4.271c-3.103 3.1.462 11.714 9.252 20.5z" fill="#eeefee"/></g></svg>`;
 
-export default function confirm(text: string, styles?: Object): Promise<boolean> {
-  return new Promise(resolve => {
+export default class Confirm {
+  private wrapper: HTMLDivElement;
 
+  constructor(text: string, styles?: Object) {
     const wrapper = document.createElement('div');
     const popup = document.createElement('div');
     const p = document.createElement('p');
@@ -60,17 +61,30 @@ export default function confirm(text: string, styles?: Object): Promise<boolean>
       pointerEvents: "none",
     })
 
-
     wrapper.appendChild(popup);
-    document.body.appendChild(wrapper);
+    this.wrapper = wrapper;
 
     answerBtn.onclick = () => {
-      document.body.removeChild(wrapper);
-      resolve(true);
+      this.remove();
+      this.callback(true);
     }
     declineBtn.onclick = () => {
-      document.body.removeChild(wrapper);
-      resolve(false);
+      this.remove();
+      this.callback(false);
     }
-  })
+  }
+
+  mount() {
+    document.body.appendChild(this.wrapper);
+  }
+
+  private callback: (result: boolean) => void = ()=>{};
+  onAnswer(callback: (result: boolean) => void) {
+    this.callback = callback;
+  }
+
+  remove() {
+    if (!this.wrapper.parentElement) { return; }
+    document.body.removeChild(this.wrapper);
+  }
 }
