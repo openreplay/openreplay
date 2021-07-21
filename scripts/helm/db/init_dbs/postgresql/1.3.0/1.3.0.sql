a7de8763b1c1686927c5ba578987875c6a58ba20
ShiKhu <alex.kaminsky.11@gmail.com>
2021-07-22 00:58:10 +0800

    feat (db): clicks db schema for heatmaps

--- scripts/helm/db/init_dbs/postgresql/1.3.0/1.3.0.sql
+++ scripts/helm/db/init_dbs/postgresql/1.3.0/1.3.0.sql
@@ -1,5 +1,10 @@
 BEGIN ;
 
+ALTER TABLE events.clicks ADD COLUMN 
+  url text DEFAULT '' NOT NULL;
+ALTER TABLE events.clicks ADD COLUMN
+  selector text DEFAULT '' NOT NULL;
+
 CREATE INDEX clicks_url_idx ON events.clicks (url);
 CREATE INDEX clicks_url_gin_idx ON events.clicks USING GIN (url gin_trgm_ops);
 CREATE INDEX clicks_url_session_id_timestamp_selector_idx ON events.clicks (url, session_id, timestamp,selector);
