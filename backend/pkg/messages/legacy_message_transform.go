829fffcfb4070a7bc3a78f5f96f13d62e6d3c722
ShiKhu <alex.kaminsky.11@gmail.com>
2021-07-22 11:56:38 +0800

    fix(backend): legacy transform with meta

--- backend/pkg/messages/legacy_message_transform.go
+++ backend/pkg/messages/legacy_message_transform.go
@@ -4,7 +4,10 @@ package messages
 func transformDepricated(msg Message) Message {
 	switch m := msg.(type) {
 	case *MouseClickDepricated:
-		return &MouseClick {
+		meta :=  m.Meta()
+		meta.TypeID = 33
+		return &MouseClick{
+			meta: meta,
 			ID: m.ID,
 			HesitationTime: m.HesitationTime,
 			Label: m.Label,
