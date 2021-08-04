5b5ef424ce85f03f0548c561c15f17204f3d8c5f
Shekar Siri <sshekarsiri@gmail.com>
2021-08-04 16:42:39 +0530

    fix(ui) - heatmaps index

--- frontend/app/components/Session_/Player/Overlay/ElementsMarker.tsx
+++ frontend/app/components/Session_/Player/Overlay/ElementsMarker.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Marker from './ElementsMarker/Marker';
 
 export default function ElementsMarker({ targets, activeIndex }) {
-  return targets && targets.map(t => <Marker target={t} active={activeIndex === t.index}/>)
+  return targets && targets.map(t => <Marker target={t} active={activeIndex === t.index - 1}/>)
 }
 
 
