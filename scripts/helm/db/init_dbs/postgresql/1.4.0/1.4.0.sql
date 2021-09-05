5168b561ff976ac5a7cd430b27846d8280330110
Taha Yassine Kraiem <tahayk2@gmail.com>
2021-09-05 11:39:53 +0100

    feat(db): added new foreign-key indexes

--- scripts/helm/db/init_dbs/postgresql/1.4.0/1.4.0.sql
+++ scripts/helm/db/init_dbs/postgresql/1.4.0/1.4.0.sql
@@ -1,5 +1,9 @@
 BEGIN;
 CREATE INDEX pages_session_id_timestamp_idx ON events.pages (session_id, timestamp);
 
+CREATE INDEX projects_tenant_id_idx ON projects(tenant_id);
+CREATE INDEX webhooks_tenant_id_idx ON webhooks(tenant_id);
+CREATE INDEX issues_project_id_idx ON issues(project_id);
+
 
 COMMIT;
