7819d4b921c5867ff3fe4afa8084e1975ed1a913
ShiKhu <alex.kaminsky.11@gmail.com>
2021-09-23 19:32:42 +0200

    dev(sourcemap-uploader): upgrade lint dependencies

--- sourcemap-uploader/lib/uploadSourcemaps.js
+++ sourcemap-uploader/lib/uploadSourcemaps.js
@@ -9,43 +9,47 @@ const getUploadURLs = (api_key, project_key, js_file_urls, server) =>
     let serverURL;
     try {
       serverURL = new URL(server);
-    } catch(e) {
-      return reject(`Failed to parse server URL "${server}".`)
+    } catch (e) {
+      return reject(`Failed to parse server URL "${server}".`);
     }
 
-    const pathPrefix = (serverURL.pathname + "/").replace(/\/+/g, '/');
+    const pathPrefix = (serverURL.pathname + '/').replace(/\/+/g, '/');
     const options = {
       method: 'PUT',
       hostname: serverURL.host,
       path: pathPrefix + `${project_key}/sourcemaps/`,
       headers: { Authorization: api_key, 'Content-Type': 'application/json' },
-    }
+    };
     if (global._VERBOSE) {
-      console.log("Request: ", options, "\nFiles: ", js_file_urls);
+      console.log('Request: ', options, '\nFiles: ', js_file_urls);
     }
-    const req = https.request(
-      options,
-      res => {
+    const req = https.request(options, (res) => {
+      if (global._VERBOSE) {
+        console.log(
+          'Response Code: ',
+          res.statusCode,
+          '\nMessage: ',
+          res.statusMessage,
+        );
+      }
+      if (res.statusCode === 403) {
+        reject(
+          'Authorisation rejected. Please, check your API_KEY and/or PROJECT_KEY.',
+        );
+        return;
+      } else if (res.statusCode !== 200) {
+        reject('Server Error. Please, contact OpenReplay support.');
+        return;
+      }
+      let data = '';
+      res.on('data', (s) => (data += s));
+      res.on('end', () => {
         if (global._VERBOSE) {
-          console.log("Response Code: ", res.statusCode, "\nMessage: ", res.statusMessage);
+          console.log('Server Response: ', data);
         }
-        if (res.statusCode === 403) {
-          reject("Authorisation rejected. Please, check your API_KEY and/or PROJECT_KEY.")
-          return
-        } else if (res.statusCode !== 200) {
-          reject("Server Error. Please, contact OpenReplay support.");
-          return;
-        }
-        let data = '';
-        res.on('data', s => (data += s));
-        res.on('end', () => {
-          if (global._VERBOSE) {
-            console.log("Server Response: ", data)
-          }
-          resolve(JSON.parse(data).data)
-        });
-      },
-    );
+        resolve(JSON.parse(data).data);
+      });
+    });
     req.on('error', reject);
     req.write(JSON.stringify({ URL: js_file_urls }));
     req.end();
@@ -63,14 +67,19 @@ const uploadSourcemap = (upload_url, body) =>
           'Content-Type': 'application/json',
         },
       },
-      res => {
+      (res) => {
         if (res.statusCode !== 200) {
           if (global._VERBOSE) {
-            console.log("Response Code: ", res.statusCode, "\nMessage: ", res.statusMessage);
+            console.log(
+              'Response Code: ',
+              res.statusCode,
+              '\nMessage: ',
+              res.statusMessage,
+            );
           }
 
-          reject("Unable to upload. Please, contact OpenReplay support.");
-          return;  // TODO: report per-file errors.
+          reject('Unable to upload. Please, contact OpenReplay support.');
+          return; // TODO: report per-file errors.
         }
         resolve();
         //res.on('end', resolve);
@@ -86,12 +95,13 @@ module.exports = (api_key, project_key, sourcemaps, server) =>
     api_key,
     project_key,
     sourcemaps.map(({ js_file_url }) => js_file_url),
-    server || "https://api.openreplay.com",
-  ).then(upload_urls =>
+    server || 'https://api.openreplay.com',
+  ).then((upload_urls) =>
     Promise.all(
       upload_urls.map((upload_url, i) =>
-        uploadSourcemap(upload_url, sourcemaps[i].body)
-        .then(() => sourcemaps[i].js_file_url)
+        uploadSourcemap(upload_url, sourcemaps[i].body).then(
+          () => sourcemaps[i].js_file_url,
+        ),
       ),
     ),
   );
