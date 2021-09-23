dc2f3993e25aa915282d4375073edc3c6c6d52b5
ShiKhu <alex.kaminsky.11@gmail.com>
2021-09-23 19:16:04 +0200

    feat(tracker-assist):3.2.0: maintaining call resume on page reload

---
+++ tracker/tracker-assist/src/_slim.ts
@@ -0,0 +1,8 @@
+
+/**
+ * Hach for the issue of peerjs compilation on angular
+ * Mor info here: https://github.com/peers/peerjs/issues/552
+ */
+
+// @ts-ignore
+window.parcelRequire = window.parcelRequire || undefined;
