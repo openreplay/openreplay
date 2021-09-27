6c6289794202beb5404749c53f24182a56869a81
Taha Yassine Kraiem <tahayk2@gmail.com>
2021-09-27 16:32:03 +0200

    feat(api): optimized weekly report feat(db): changed indexes to optimize weekly report

--- ee/scripts/helm/db/init_dbs/postgresql/1.3.6/1.3.6.sql
+++ ee/scripts/helm/db/init_dbs/postgresql/1.3.6/1.3.6.sql
@@ -4,6 +4,7 @@ CREATE INDEX sessions_user_id_useridNN_idx ON sessions (user_id) WHERE user_id I
 CREATE INDEX sessions_uid_projectid_startts_sessionid_uidNN_durGTZ_idx ON sessions (user_id, project_id, start_ts, session_id) WHERE user_id IS NOT NULL AND duration > 0;
 CREATE INDEX pages_base_path_base_pathLNGT2_idx ON events.pages (base_path) WHERE length(base_path) > 2;
 
+CREATE INDEX users_tenant_id_deleted_at_N_idx ON users (tenant_id) WHERE deleted_at ISNULL;
 CREATE INDEX issues_issue_id_timestamp_idx ON events_common.issues(issue_id,timestamp);
 CREATE INDEX issues_timestamp_idx ON events_common.issues (timestamp);
 CREATE INDEX issues_project_id_issue_id_idx ON public.issues (project_id, issue_id);
