d70e0159a3e04aff9b96d26bdd7d775a06262066
Shekar Siri <sshekarsiri@gmail.com>
2021-10-01 19:02:02 +0530

    change(ui) - fetch headers, and heatmaps scroll

---
+++ frontend/app/components/Session_/Fetch/components/Headers/Headers.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { NoContent } from 'UI'
+import stl from './headers.css'
+
+function Headers(props) {
+  console.log('props', props);
+  return (
+    <div>
+      <NoContent
+        title="No data available."
+        size="small"
+        show={ !props.requestHeaders && !props.responseHeaders }
+        icon="exclamation-circle"
+      >
+        { props.requestHeaders && (
+          <>
+            <div className="mb-4 mt-4">
+              <div className="my-2 font-medium">Request Headers</div>
+              {
+                Object.keys(props.requestHeaders).map(h => (
+                  <div className={stl.row}>
+                    <div className="mr-2 font-medium">{h}:</div>
+                    <div>{props.requestHeaders[h]}</div>
+                  </div>
+                ))
+              }
+            </div>
+            <hr />
+          </>
+        )}
+        
+        { props.responseHeaders && (
+          <div className="mt-4">
+            <div className="my-2 font-medium">Response Headers</div>
+            {
+              Object.keys(props.responseHeaders).map(h => (
+                <div className={stl.row}>
+                  <div className="mr-2 font-medium">{h}:</div>
+                  <div>{props.responseHeaders[h]}</div>
+                </div>
+              ))
+            }
+          </div>
+        )}
+      </NoContent>
+    </div>
+  );
+}
+
+export default Headers;
