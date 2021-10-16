b8d57852e9be13a20babada2153b7e23eb1ef09f
ShiKhu <alex.kaminsky.11@gmail.com>
2021-10-16 02:37:08 +0200

    cherry pick changes from 240ce27681f6dddb7be76a8bf9ec8588ebe250a5

--- tracker/tracker-assist/src/_slim.ts
+++ tracker/tracker-assist/src/_slim.ts
@@ -1,7 +1,7 @@
 
 /**
- * Hach for the issue of peerjs compilation on angular
- * Mor info here: https://github.com/peers/peerjs/issues/552
+ * Hack for the peerjs compilation on angular
+ * About this issue: https://github.com/peers/peerjs/issues/552
  */
 
 // @ts-ignore
