4b9f8bdf8eaa03c9f3855825fc3a1aad9155d91a
Shekar Siri <sshekarsiri@gmail.com>
2021-11-21 23:17:04 +0530

    change(ui) - peerjs iceserver env config added transport

--- frontend/app/utils.js
+++ frontend/app/utils.js
@@ -221,6 +221,9 @@ export const iceServerConfigFromString = (str) => {
         server.username = arr[1]
         if (!!arr[2]) {
           server.password = arr[2]
+          if (!!arr[3]) {
+            server.transport = arr[3]
+          }
         }
       }
       return server
