91fb2544323eb45478006cbfcab51928dab72c1e
Taha Yassine Kraiem <tahayk2@gmail.com>
2021-11-23 16:31:48 +0100

    feat(api): code cleanup feat(api): removed sentry

--- api/app.py
+++ api/app.py
@@ -1,9 +1,6 @@
-import sentry_sdk
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from decouple import config
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from sentry_sdk import configure_scope
 from starlette.responses import StreamingResponse
 
 from chalicelib.utils import helper
@@ -13,42 +10,6 @@ from routers.app import v1_api
 from routers.crons import core_crons
 from routers.crons import core_dynamic_crons
 from routers.subs import dashboard
-# from routers.subs import  insights
-
-# # Monkey-patch print for DataDog hack
-# import sys
-# import traceback
-#
-# old_tb = traceback.print_exception
-# old_f = sys.stdout
-# old_e = sys.stderr
-# OR_SESSION_TOKEN = None
-#
-#
-# class F:
-#     def write(self, x):
-#         if OR_SESSION_TOKEN is not None and x != '\n' and not helper.is_local():
-#             old_f.write(f"[or_session_token={OR_SESSION_TOKEN}] {x}")
-#         else:
-#             old_f.write(x)
-#
-#     def flush(self):
-#         pass
-#
-#
-# def tb_print_exception(etype, value, tb, limit=None, file=None, chain=True):
-#     if OR_SESSION_TOKEN is not None and not helper.is_local():
-#         value = type(value)(f"[or_session_token={OR_SESSION_TOKEN}] " + str(value))
-#
-#     old_tb(etype, value, tb, limit, file, chain)
-#
-#
-# if helper.is_production():
-#     traceback.print_exception = tb_print_exception
-#
-# sys.stdout = F()
-# sys.stderr = F()
-# # ---End Monkey-patch
 
 app = FastAPI()
 
@@ -66,13 +27,6 @@ async def or_middleware(request: Request, call_next):
         if helper.TRACK_TIME:
             print(f"Execution time: {int(time.time() * 1000) - now} ms")
     except Exception as e:
-        if helper.allow_sentry() and OR_SESSION_TOKEN is not None and not helper.is_local():
-            with configure_scope() as scope:
-                scope.set_tag('stage', config["stage"])
-                scope.set_tag('openReplaySessionToken', OR_SESSION_TOKEN)
-                if hasattr(request.state, "currentContext"):
-                    scope.set_extra("context", request.state.currentContext.dict())
-            sentry_sdk.capture_exception(e)
         pg_client.close()
         raise e
     pg_client.close()
