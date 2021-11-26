1a79cda31b96c632df4bc1ad7c9702476eab3dc9
Shekar Siri <sshekarsiri@gmail.com>
2021-11-27 03:26:12 +0530

    feat(ui) - roles and permissions cleanup

--- frontend/app/duck/roles.js
+++ frontend/app/duck/roles.js
@@ -1,10 +1,7 @@
 import { List, Map } from 'immutable';
 import Role from 'Types/role';
 import crudDuckGenerator from './tools/crudDuck';
-import withRequestState, { RequestTypes } from 'Duck/requestStateCreator';
 import { reduceDucks } from 'Duck/tools';
-
-const GENERATE_LINK = new RequestTypes('roles/GENERATE_LINK');
 
 const crudDuck = crudDuckGenerator('client/role', Role, { idKey: 'roleId' });
 export const { fetchList, init, edit, remove, } = crudDuck.actions;
@@ -22,16 +19,12 @@ const initialState = Map({
 });
 
 const reducer = (state = initialState, action = {}) => {
-  switch (action.type) {
-    
-  }
   return state;
 };
 
 export function save(instance) {
   return {
     types: crudDuck.actionTypes.SAVE.toArray(),
-    // call: client => client.put( instance.roleId ? `/client/roles/${ instance.roleId }` : '/client/roles', instance.toData()),
     call: client => instance.roleId ? client.post(`/client/roles/${ instance.roleId }`, instance.toData()) : client.put(`/client/roles`, instance.toData()),
   };
 }
