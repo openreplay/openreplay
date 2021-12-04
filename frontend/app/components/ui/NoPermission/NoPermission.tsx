cd22e242fcbbabc77bb33088ccd7ac34aaf43690
Shekar Siri <sshekarsiri@gmail.com>
2021-12-05 02:36:50 +0530

    change(ui) - permissions go back for sessions

--- frontend/app/components/ui/NoPermission/NoPermission.tsx
+++ frontend/app/components/ui/NoPermission/NoPermission.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import stl from './noPermission.css'
-import { Icon } from 'UI';
+import { Icon, Button, Link } from 'UI';
 
-function NoPermission(props) {
+interface Props {
+  backLink: string
+}
+function NoPermission({ backLink }: Props) {
   return (
     <div className={stl.wrapper}>
       <Icon name="shield-lock" size="50" className="py-16"/>
       <div className={ stl.title }>Not allowed</div>
       You don’t have the necessary permissions to access this feature. Please check with your admin.
+      {backLink && (
+        <Link to={backLink}>
+          <Button primary className="mt-6">GO BACK</Button>
+        </Link>
+      )}
     </div>
   );
 }
