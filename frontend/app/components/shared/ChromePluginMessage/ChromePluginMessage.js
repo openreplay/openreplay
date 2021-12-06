3203a47722bcdef0db9cdbafaa73ec5db207878c
Shekar Siri <sshekarsiri@gmail.com>
2021-12-06 16:55:42 +0530

    fix(ui) - Potentially unsafe external link

--- frontend/app/components/shared/ChromePluginMessage/ChromePluginMessage.js
+++ frontend/app/components/shared/ChromePluginMessage/ChromePluginMessage.js
@@ -10,7 +10,9 @@ export default function ChromePluginMessage({ style }) {
     >
       <div className="flex items-center">
         <Icon name="info-circle" size="14" color="gray-darkest" />
-        <div className="ml-2 mr-2 color-gray-darkest">Finding it difficult to add steps? Try our Chrome <a className="color-teal" target="_blank" href={links['chrome-plugin']}>Test Recorder</a></div>
+        <div className="ml-2 mr-2 color-gray-darkest">
+          Finding it difficult to add steps? Try our Chrome <a className="color-teal" rel="noopener noreferrer" target="_blank" href={links['chrome-plugin']}>Test Recorder</a>
+        </div>
         <Icon name="external-link-alt" size="14" color="teal" />        
       </div>      
     </div>
