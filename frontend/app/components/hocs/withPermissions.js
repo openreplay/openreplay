71a66814d4b907fded85a6a4f382b8e443a80f8f
Shekar Siri <sshekarsiri@gmail.com>
2021-12-08 20:36:38 +0530

    change(ui) - live session check

--- frontend/app/components/hocs/withPermissions.js
+++ frontend/app/components/hocs/withPermissions.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
-import { NoPermission } from 'UI';
+import { NoPermission, NoSessionPermission } from 'UI';
 
-export default (requiredPermissions, className, backLink = '') => BaseComponent => 
+export default (requiredPermissions, className, isReplay = false) => BaseComponent => 
 @connect((state, props) => ({
   permissions: state.getIn([ 'user', 'account', 'permissions' ]) || [],
   isEnterprise: state.getIn([ 'user', 'client', 'edition' ]) === 'ee',
@@ -10,6 +10,12 @@ class extends React.PureComponent {
   render() {
     const hasPermission = requiredPermissions.every(permission => this.props.permissions.includes(permission));
 
-    return !this.props.isEnterprise || hasPermission ? <BaseComponent {...this.props} /> : <div className={className}><NoPermission backLink={backLink} /></div>
+    return (
+      (!this.props.isEnterprise || hasPermission) ?
+      <BaseComponent {...this.props} /> :
+      <div className={className}>
+        { isReplay ? <NoSessionPermission /> : <NoPermission /> }
+      </div>
+    )
   }
 }
