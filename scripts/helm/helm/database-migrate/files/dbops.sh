7961c9ddf546cecd146c1ddbd814eb82081ceeb2
Rajesh Rajendran <rjshrjndrn@gmail.com>
2021-12-12 17:57:46 +0530

    fix(uprade): version check
    Signed-off-by: Rajesh Rajendran <rjshrjndrn@gmail.com>

--- scripts/helm/helm/database-migrate/files/dbops.sh
+++ scripts/helm/helm/database-migrate/files/dbops.sh
@@ -5,16 +5,21 @@ cd $(dirname $0)
 function migration() {
     ls -la /opt/openreplay/openreplay
     db=$1
-    old_version=$2
 
-    if [[ old_version == $CHART_APP_VERSION ]]; then
+    # Checking if previous app version is set.
+    if [[ $PREVIOUS_APP_VERSION == "" ]]; then
+        echo "Previous app verision to be migrated is not set. Rerun using --set fromVersion=v1.3.5"
+        exit 100
+    fi
+
+    if [[ $PREVIOUS_APP_VERSION == $CHART_APP_VERSION ]]; then
         echo "No application version change. Not upgrading."
         exit 0
     fi
 
     # Checking migration versions
     cd /opt/openreplay/openreplay/scripts/helm
-    migration_versions=(`ls -l db/init_dbs/$db | grep -E ^d | awk -v number=${old_version} '$NF > number {print $NF}' | grep -v create`)
+    migration_versions=(`ls -l db/init_dbs/$db | grep -E ^d | awk -v number=${PREVIOUS_APP_VERSION} '$NF > number {print $NF}' | grep -v create`)
     echo "Migration version: $migration_versions"
     
     cd -
@@ -56,7 +61,7 @@ case "$1" in
         init $2
         ;;
     "true")
-        migration $2 $CHART_APP_VERSION
+        migration $2
         ;;
     *)
         echo "Unknown operation for db migration; exiting."
