1c8d145caee92da467a7077b55c8d2b007274439
Shekar Siri <sshekarsiri@gmail.com>
2021-12-15 17:57:04 +0530

    change(ui) - tracker version to 3.4.16

--- frontend/env.js
+++ frontend/env.js
@@ -21,7 +21,7 @@ const oss = {
 	MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY,
 	MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY,
 	ICE_SERVERS: process.env.ICE_SERVERS,
-	TRACKER_VERSION: '3.4.15', // trackerInfo.version,
+	TRACKER_VERSION: '3.4.16', // trackerInfo.version,
 }
 
 module.exports = {
