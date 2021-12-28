4209b5b1b9b87d7ddfec4038d7057311b208a1e8
Taha Yassine Kraiem <tahayk2@gmail.com>
2021-12-28 20:20:32 +0100

    feat(DB): UTM columns

--- ee/scripts/helm/db/init_dbs/postgresql/1.9.9/1.9.9.sql
+++ ee/scripts/helm/db/init_dbs/postgresql/1.9.9/1.9.9.sql
@@ -57,5 +57,13 @@ WHERE name ISNULL;
 ALTER TABLE tenants
     ALTER COLUMN name SET NOT NULL;
 
+ALTER TABLE sessions
+    ADD COLUMN utm_source   text NULL DEFAULT NULL,
+    ADD COLUMN utm_medium   text NULL DEFAULT NULL,
+    ADD COLUMN utm_campaign text NULL DEFAULT NULL;
+
+CREATE INDEX sessions_utm_source_gin_idx ON public.sessions USING GIN (utm_source gin_trgm_ops);
+CREATE INDEX sessions_utm_medium_gin_idx ON public.sessions USING GIN (utm_medium gin_trgm_ops);
+CREATE INDEX sessions_utm_campaign_gin_idx ON public.sessions USING GIN (utm_campaign gin_trgm_ops);
 
 COMMIT;
