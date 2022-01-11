99c31f2090fc9d51a2c215fa40350216ddfcde08
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-01-11 18:32:51 +0100

    feat(api): EE role-projects feat(api): EE project authorizer by user feat(api): EE projects list by user

--- ee/api/chalicelib/core/projects.py
+++ ee/api/chalicelib/core/projects.py
@@ -42,7 +42,7 @@ def __create(tenant_id, name):
 
 
 def get_projects(tenant_id, recording_state=False, gdpr=None, recorded=False, stack_integrations=False, version=False,
-                 last_tracker_version=None):
+                 last_tracker_version=None, user_id=None):
     with pg_client.PostgresClient() as cur:
         tracker_query = ""
         if last_tracker_version is not None and len(last_tracker_version) > 0:
@@ -54,6 +54,15 @@ def get_projects(tenant_id, recording_state=False, gdpr=None, recorded=False, st
         elif version:
             tracker_query = ",(SELECT tracker_version FROM public.sessions WHERE sessions.project_id = s.project_id ORDER BY start_ts DESC LIMIT 1) AS tracker_version"
 
+        role_query = """INNER JOIN LATERAL (SELECT 1
+                     FROM users
+                              INNER JOIN roles USING (role_id)
+                              LEFT JOIN roles_projects USING (role_id)
+                     WHERE users.user_id = %(user_id)s
+                       AND users.deleted_at ISNULL
+                       AND users.tenant_id = %(tenant_id)s
+                       AND (roles.all_projects OR roles_projects.project_id = s.project_id)
+                    ) AS role_project ON (TRUE)"""
         cur.execute(
             cur.mogrify(f"""\
                     SELECT
@@ -64,10 +73,11 @@ def get_projects(tenant_id, recording_state=False, gdpr=None, recorded=False, st
                             {tracker_query}
                     FROM public.projects AS s
                             {'LEFT JOIN LATERAL (SELECT COUNT(*) AS count FROM public.integrations WHERE s.project_id = integrations.project_id LIMIT 1) AS stack_integrations ON TRUE' if stack_integrations else ''}
+                            {role_query if user_id is not None else ""}
                     WHERE s.tenant_id =%(tenant_id)s
                         AND s.deleted_at IS NULL
                     ORDER BY s.project_id;""",
-                        {"tenant_id": tenant_id})
+                        {"tenant_id": tenant_id, "user_id": user_id})
         )
         rows = cur.fetchall()
         if recording_state:
@@ -124,10 +134,34 @@ def get_project(tenant_id, project_id, include_last_session=False, include_gdpr=
         return helper.dict_to_camel_case(row)
 
 
-def is_authorized(project_id, tenant_id):
+def is_authorized(project_id, tenant_id, user_id=None):
     if project_id is None or not str(project_id).isdigit():
         return False
-    return get_project(tenant_id=tenant_id, project_id=project_id) is not None
+    with pg_client.PostgresClient() as cur:
+        role_query = """INNER JOIN LATERAL (SELECT 1
+                             FROM users
+                                      INNER JOIN roles USING (role_id)
+                                      LEFT JOIN roles_projects USING (role_id)
+                             WHERE users.user_id = %(user_id)s 
+                                AND users.deleted_at ISNULL 
+                                AND users.tenant_id = %(tenant_id)s 
+                                AND (roles.all_projects OR roles_projects.project_id = %(project_id)s)
+                        ) AS role_project ON (TRUE)"""
+
+        query = cur.mogrify(f"""\
+                    SELECT project_id
+                    FROM public.projects AS s
+                    {role_query if user_id is not None else ""}
+                    where s.tenant_id =%(tenant_id)s 
+                        AND s.project_id =%(project_id)s
+                        AND s.deleted_at IS NULL
+                    LIMIT 1;""",
+                            {"tenant_id": tenant_id, "project_id": project_id, "user_id": user_id})
+        cur.execute(
+            query=query
+        )
+        row = cur.fetchone()
+    return row is not None
 
 
 def create(tenant_id, user_id, data: schemas.CreateProjectSchema, skip_authorization=False):
@@ -275,3 +309,22 @@ def get_project_by_key(tenant_id, project_key, include_last_session=False, inclu
         )
         row = cur.fetchone()
         return helper.dict_to_camel_case(row)
+
+
+def is_authorized_batch(project_ids, tenant_id):
+    if project_ids is None or not len(project_ids):
+        return False
+    with pg_client.PostgresClient() as cur:
+        query = cur.mogrify("""\
+                    SELECT project_id
+                    FROM public.projects
+                    where tenant_id =%(tenant_id)s 
+                        AND project_id IN %(project_ids)s
+                        AND deleted_at IS NULL;""",
+                            {"tenant_id": tenant_id, "project_ids": tuple(project_ids)})
+
+        cur.execute(
+            query=query
+        )
+        rows = cur.fetchall()
+        return [r["project_id"] for r in rows]
