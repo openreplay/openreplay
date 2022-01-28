4f83b9f3714c35e8e219cff7f6730b8cc0b24ff5
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-01-28 20:04:17 +0100

    feat(db): changed session structure to support null columns

--- backend/pkg/db/types/session.go
+++ backend/pkg/db/types/session.go
@@ -16,18 +16,18 @@ type Session struct {
 	PagesCount      int
 	EventsCount     int
 	ErrorsCount     int
-	UserID          string  // pointer??
+	UserID          string `db:"user_id" json:"user_id;omitempty"`
 	UserAnonymousID string `db:"user_anonymous_id" json:"user_anonymous_id;omitempty"`
-	Metadata1       string  `db:"metadata_1" json:"metadata_1;omitempty"`
-	Metadata2       string  `db:"metadata_2" json:"metadata_2;omitempty"`
-	Metadata3       string  `db:"metadata_3" json:"metadata_3;omitempty"`
-	Metadata4       string  `db:"metadata_4" json:"metadata_4;omitempty"`
-	Metadata5       string  `db:"metadata_5" json:"metadata_5;omitempty"`
-	Metadata6       string  `db:"metadata_6" json:"metadata_6;omitempty"`
-	Metadata7       string  `db:"metadata_7" json:"metadata_7;omitempty"`
-	Metadata8       string  `db:"metadata_8" json:"metadata_8;omitempty"`
-	Metadata9       string  `db:"metadata_9" json:"metadata_9;omitempty"`
-	Metadata10      string  `db:"metadata_10" json:"metadata_10;omitempty"`
+	Metadata1       string `db:"metadata_1" json:"metadata_1;omitempty"`
+	Metadata2       string `db:"metadata_2" json:"metadata_2;omitempty"`
+	Metadata3       string `db:"metadata_3" json:"metadata_3;omitempty"`
+	Metadata4       string `db:"metadata_4" json:"metadata_4;omitempty"`
+	Metadata5       string `db:"metadata_5" json:"metadata_5;omitempty"`
+	Metadata6       string `db:"metadata_6" json:"metadata_6;omitempty"`
+	Metadata7       string `db:"metadata_7" json:"metadata_7;omitempty"`
+	Metadata8       string `db:"metadata_8" json:"metadata_8;omitempty"`
+	Metadata9       string `db:"metadata_9" json:"metadata_9;omitempty"`
+	Metadata10      string `db:"metadata_10" json:"metadata_10;omitempty"`
 
 	Platform string
 	// Only-web properties
