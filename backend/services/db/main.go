5b256546ebda8a02b66edddfe2999e7705c71dde
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-01-28 17:29:11 +0100

    feat(db): log new events

--- backend/services/db/main.go
+++ backend/services/db/main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"log"
+	"reflect"
 	"time"
 
 	"os"
 	"os/signal"
 	"syscall"
 
+	"openreplay/backend/pkg/db/cache"
+	"openreplay/backend/pkg/db/postgres"
 	"openreplay/backend/pkg/env"
+	"openreplay/backend/pkg/messages"
 	"openreplay/backend/pkg/queue"
 	"openreplay/backend/pkg/queue/types"
-	"openreplay/backend/pkg/messages"
-	"openreplay/backend/pkg/db/postgres"
-	"openreplay/backend/pkg/db/cache"
 	"openreplay/backend/services/db/heuristics"
-) 
+)
 
 var pg *cache.PGCache
 
@@ -23,62 +24,66 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.LUTC | log.Llongfile)
 
 	initStats()
-	pg = cache.NewPGCache(postgres.NewConn(env.String("POSTGRES_STRING")), 1000 * 60 * 20)
+	pg = cache.NewPGCache(postgres.NewConn(env.String("POSTGRES_STRING")), 1000*60*20)
 	defer pg.Close()
 
 	heurFinder := heuristics.NewHandler()
 
 	consumer := queue.NewMessageConsumer(
 		env.String("GROUP_DB"),
-		[]string{ 
+		[]string{
 			env.String("TOPIC_RAW_IOS"),
 			env.String("TOPIC_TRIGGER"),
-	  },
-	  func(sessionID uint64, msg messages.Message, _ *types.Meta) {
-	  	if err := insertMessage(sessionID, msg); err != nil {
-	  		if !postgres.IsPkeyViolation(err) {
-		  		log.Printf("Message Insertion Error %v, SessionID: %v, Message: %v", err,sessionID, msg)
-	  		}
-	  		return
-	  	}
+		},
+		func(sessionID uint64, msg messages.Message, _ *types.Meta) {
+			log.Println(">>Insert message")
+			log.Println(reflect.TypeOf(msg))
+			if err := insertMessage(sessionID, msg); err != nil {
+				log.Println(">>Error")
+				log.Println(err)
+				if !postgres.IsPkeyViolation(err) {
+					log.Printf("Message Insertion Error %v, SessionID: %v, Message: %v", err, sessionID, msg)
+				}
+				return
+			}
 
-	  	session, err := pg.GetSession(sessionID)
+			session, err := pg.GetSession(sessionID)
 			if err != nil {
 				// Might happen due to the assets-related message TODO: log only if session is necessary for this kind of message
 				log.Printf("Error on session retrieving from cache: %v, SessionID: %v, Message: %v", err, sessionID, msg)
-				return;
+				return
 			}
 
-	  	err = insertStats(session, msg)
-	  	if err != nil {
-	  		log.Printf("Stats Insertion Error %v; Session: %v, Message: %v", err, session, msg)
-	  	}
+			err = insertStats(session, msg)
+			if err != nil {
+				log.Printf("Stats Insertion Error %v; Session: %v, Message: %v", err, session, msg)
+			}
 
 			heurFinder.HandleMessage(session, msg)
 			heurFinder.IterateSessionReadyMessages(sessionID, func(msg messages.Message) {
 				// TODO: DRY code (carefully with the return statement logic)
 				if err := insertMessage(sessionID, msg); err != nil {
-		  		if !postgres.IsPkeyViolation(err) {
-		  			log.Printf("Message Insertion Error %v; Session: %v,  Message %v", err, session, msg)
-		  		}
-		  		return
-		  	}
+					if !postgres.IsPkeyViolation(err) {
+						log.Printf("Message Insertion Error %v; Session: %v,  Message %v", err, session, msg)
+					}
+					return
+				}
 
-		  	err = insertStats(session, msg)
-		  	if err != nil {
-		  		log.Printf("Stats Insertion Error %v; Session: %v,  Message %v", err, session, msg)
-		  	}	
+				err = insertStats(session, msg)
+				if err != nil {
+					log.Printf("Stats Insertion Error %v; Session: %v,  Message %v", err, session, msg)
+				}
 			})
 		},
 	)
 	consumer.DisableAutoCommit()
 
 	sigchan := make(chan os.Signal, 1)
-  signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
-  tick := time.Tick(15 * time.Second)
+	tick := time.Tick(15 * time.Second)
 
-  log.Printf("Db service started\n")
+	log.Printf("Db service started\n")
 	for {
 		select {
 		case sig := <-sigchan:
@@ -88,11 +93,11 @@ func main() {
 		case <-tick:
 			if err := commitStats(); err != nil {
 				log.Printf("Error on stats commit: %v", err)
-			} 
+			}
 			// TODO?: separate stats & regular messages
 			if err := consumer.Commit(); err != nil {
 				log.Printf("Error on consumer commit: %v", err)
-			} 
+			}
 		default:
 			err := consumer.ConsumeNext()
 			if err != nil {
