8996a04965096f0715fc6658709ba693819dd9cc
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-02-09 19:12:21 +0100

    feat(db): create new indexes

--- ee/scripts/helm/db/init_dbs/postgresql/1.5.0/1.5.0.sql
+++ ee/scripts/helm/db/init_dbs/postgresql/1.5.0/1.5.0.sql
@@ -8,5 +8,6 @@ $$ LANGUAGE sql IMMUTABLE;
 
 ALTER TABLE public.metrics
     ADD COLUMN IF NOT EXISTS active boolean NOT NULL DEFAULT TRUE;
+CREATE INDEX IF NOT EXISTS resources_timestamp_duration_durationgt0NN_idx ON events.resources (timestamp, duration) WHERE duration > 0 AND duration IS NOT NULL;
 COMMIT;
 ALTER TYPE public.error_source ADD VALUE IF NOT EXISTS 'elasticsearch'; -- cannot add new value inside a transaction block
