2f734f8a6c9549cd3f9da5c84d74c9e46b51935a
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-02-09 18:02:51 +0100

    feat(db): changed delta files

--- scripts/helm/db/init_dbs/postgresql/1.5.0/1.5.0.sql
+++ scripts/helm/db/init_dbs/postgresql/1.5.0/1.5.0.sql
@@ -5,8 +5,8 @@ $$
 SELECT 'v1.5.0'
 $$ LANGUAGE sql IMMUTABLE;
 
-ALTER TYPE public.error_source ADD VALUE IF NOT EXISTS 'elasticsearch';
 
 ALTER TABLE public.metrics
     ADD COLUMN IF NOT EXISTS active boolean NOT NULL DEFAULT TRUE;
 COMMIT;
+ALTER TYPE public.error_source ADD VALUE IF NOT EXISTS 'elasticsearch'; -- cannot add new value inside a transaction block
