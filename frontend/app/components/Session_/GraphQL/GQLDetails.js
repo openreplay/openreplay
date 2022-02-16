cd73f374539b3c47a8ac59c70342a84b5c653766
Shekar Siri <sshekarsiri@gmail.com>
2022-02-16 16:57:43 +0100

    feat(ui) - graphql ui and navigation

--- frontend/app/components/Session_/GraphQL/GQLDetails.js
+++ frontend/app/components/Session_/GraphQL/GQLDetails.js
@@ -1,4 +1,4 @@
-import { JSONTree } from 'UI'
+import { JSONTree, Button } from 'UI'
 import cn from 'classnames';
 
 export default class GQLDetails extends React.PureComponent {
@@ -7,7 +7,14 @@ export default class GQLDetails extends React.PureComponent {
 			gql: {
 				variables,
 				response,
+				duration,
+				operationKind,
+				operationName,
 			},
+			nextClick,
+			prevClick,
+			first = false,
+			last = false,
 		} = this.props;
 
 		let jsonVars = undefined;
@@ -19,28 +26,52 @@ export default class GQLDetails extends React.PureComponent {
 			jsonResponse = JSON.parse(response);
 		} catch (e) {}
 		return (
-			<div className="ph-20" >
-				<div className="divider"/>
-				{ variables && variables !== "{}" &&
-					<div>
-						<div className="mt-6">
-							<h5>{ 'Variables'}</h5>
-							{ jsonVars === undefined 
-								? <div className="ml-3"> { variables } </div>
-								: <JSONTree src={ jsonVars } />
-							}
+			<div className="px-4 pb-16">
+				<h5 className="mb-2">{ 'Operation Name'}</h5>
+				<div className={ cn('p-2 bg-gray-lightest rounded color-gray-darkest')}>{ operationName }</div>
+
+				<div className="flex items-center mt-4">
+					<div className="w-4/12">
+						<div className="font-medium mb-2">Operation Kind</div>
+						<div>{operationKind}</div>
+					</div>
+					<div className="w-4/12">
+						<div className="font-medium mb-2">Duration</div>
+						<div>{parseInt(duration)} ms</div>
+					</div>					
+				</div>	
+
+				<div className="flex justify-between items-start mt-6">
+					<h5 className="mt-1 mr-1">{ 'Response' }</h5>
+				</div>
+				<div style={{ height: 'calc(100vh - 314px)', overflowY: 'auto' }}>
+					{ variables && variables !== "{}" &&
+						<div>
+							<div className="mt-2">
+								<h5>{ 'Variables'}</h5>
+								{ jsonVars === undefined 
+									? <div className="ml-3"> { variables } </div>
+									: <JSONTree src={ jsonVars } />
+								}
+							</div>
+							<div className="divider"/>
 						</div>
-						<div className="divider"/>
-					</div>
-				}
-				<div className="mt-6">
-					<div className="flex justify-between items-start">
-						<h5 className="mt-1 mr-1">{ 'Response' }</h5>
-					</div>
-					{ jsonResponse === undefined 
-						? <div className="ml-3"> { response } </div>
-						: <JSONTree src={ jsonResponse } />
 					}
+					<div className="mt-3">
+						{ jsonResponse === undefined 
+							? <div className="ml-3"> { response } </div>
+							: <JSONTree src={ jsonResponse } />
+						}
+					</div>
+				</div>
+
+				<div className="flex justify-between absolute bottom-0 left-0 right-0 p-3 border-t bg-white">
+					<Button primary plain onClick={prevClick} disabled={first}>
+						Prev
+					</Button>
+					<Button primary plain onClick={nextClick} disabled={last}>
+						Next
+					</Button>
 				</div>
 			</div>
 		);
