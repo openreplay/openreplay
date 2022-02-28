a9583db20f134a4c16e6c4c23fabedc026a4198e
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-02-28 22:07:35 +0100

    feat(DB): changed update script

--- scripts/helm/db/init_dbs/postgresql/1.5.X/1.5.X.sql
+++ scripts/helm/db/init_dbs/postgresql/1.5.X/1.5.X.sql
@@ -1,3 +1,5 @@
+-- !!!!NOT IN CREATE DB YET
+
 BEGIN;
 CREATE OR REPLACE FUNCTION openreplay_version()
     RETURNS text AS
@@ -8,13 +10,19 @@ $$ LANGUAGE sql IMMUTABLE;
 
 -- Split requests-URL:
 UPDATE events_common.requests
-SET schema=SUBSTRING(url, 1, POSITION('://' IN url) - 1),
+SET schema=CASE WHEN POSITION('://' IN url) > 0 THEN SUBSTRING(url, 1, POSITION('://' IN url) - 1) END,
     host=CASE
+             WHEN POSITION('://' IN url) = 0 THEN NULL
              WHEN POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) > 0 THEN SUBSTRING(
                      SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1), 1,
                      POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) - 1)
              ELSE SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1) END,
     base_path=CASE
+                  WHEN POSITION('://' IN url) = 0 THEN
+                      CASE
+                          WHEN POSITION('?' IN url) > 0 THEN
+                              SUBSTRING(url, 1, POSITION('?' IN url) - 1)
+                          ELSE url END
                   WHEN POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) > 0 THEN
                       CASE
                           WHEN POSITION('?' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) > 0 THEN
@@ -25,13 +33,10 @@ SET schema=SUBSTRING(url, 1, POSITION('://' IN url) - 1),
                           ELSE SUBSTRING(SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1),
                                          POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) +
                                          1) END
-                  ELSE '' END,
+        END,
     query_string=CASE
-                     WHEN POSITION('?' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) > 0 THEN SUBSTRING(
-                             SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1),
-                             POSITION('?' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) + 1)
+                     WHEN POSITION('?' IN url) > 0 THEN SUBSTRING(url, POSITION('?' IN url) + 1)
         END;
-
 
 
 
