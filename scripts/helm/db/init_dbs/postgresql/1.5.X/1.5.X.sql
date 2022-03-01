bc87d0302579b151924d56b2fbf026eae211945c
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-03-01 17:13:44 +0100

    feat(DB): changed delta file for request

--- scripts/helm/db/init_dbs/postgresql/1.5.X/1.5.X.sql
+++ scripts/helm/db/init_dbs/postgresql/1.5.X/1.5.X.sql
@@ -10,45 +10,44 @@ $$ LANGUAGE sql IMMUTABLE;
 CREATE TYPE events_common.http_method AS ENUM ('GET','HEAD','POST','PUT','DELETE','CONNECT','OPTIONS','TRACE','PATCH');
 
 ALTER TABLE events_common.requests
-    ADD COLUMN IF NOT EXISTS schema        text        NULL,
-    ADD COLUMN IF NOT EXISTS host          text        NULL,
-    ADD COLUMN IF NOT EXISTS base_path     text        NULL,
-    ADD COLUMN IF NOT EXISTS query_string  text        NULL,
-    ADD COLUMN IF NOT EXISTS request_body  text        NULL,
-    ADD COLUMN IF NOT EXISTS response_body text        NULL,
-    ADD COLUMN IF NOT EXISTS status_code   text        NULL,
-    ADD COLUMN IF NOT EXISTS method        http_method NULL;
+    ADD COLUMN IF NOT EXISTS schema        text                      NULL,
+    ADD COLUMN IF NOT EXISTS host          text                      NULL,
+    ADD COLUMN IF NOT EXISTS base_path     text                      NULL,
+    ADD COLUMN IF NOT EXISTS query_string  text                      NULL,
+    ADD COLUMN IF NOT EXISTS request_body  text                      NULL,
+    ADD COLUMN IF NOT EXISTS response_body text                      NULL,
+    ADD COLUMN IF NOT EXISTS status_code   text                      NULL,
+    ADD COLUMN IF NOT EXISTS method        events_common.http_method NULL;
 
--- Split requests-URL:
-UPDATE events_common.requests
-SET schema=CASE WHEN POSITION('://' IN url) > 0 THEN SUBSTRING(url, 1, POSITION('://' IN url) - 1) END,
-    host=CASE
-             WHEN POSITION('://' IN url) = 0 THEN NULL
-             WHEN POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) > 0 THEN SUBSTRING(
-                     SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1), 1,
-                     POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) - 1)
-             ELSE SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1) END,
-    base_path=CASE
-                  WHEN POSITION('://' IN url) = 0 THEN
-                      CASE
-                          WHEN POSITION('?' IN url) > 0 THEN
-                              SUBSTRING(url, 1, POSITION('?' IN url) - 1)
-                          ELSE url END
-                  WHEN POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) > 0 THEN
-                      CASE
-                          WHEN POSITION('?' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) > 0 THEN
-                              SUBSTRING(SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1),
-                                        POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) + 1,
-                                        POSITION('?' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) -
-                                        POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) - 1)
-                          ELSE SUBSTRING(SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1),
-                                         POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) +
-                                         1) END
-        END,
-    query_string=CASE
-                     WHEN POSITION('?' IN url) > 0 THEN SUBSTRING(url, POSITION('?' IN url) + 1)
-        END;
-
+-- Split requests-URL: Takes too long to use
+-- UPDATE events_common.requests
+-- SET schema=CASE WHEN POSITION('://' IN url) > 0 THEN SUBSTRING(url, 1, POSITION('://' IN url) - 1) END,
+--     host=CASE
+--              WHEN POSITION('://' IN url) = 0 THEN NULL
+--              WHEN POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) > 0 THEN SUBSTRING(
+--                      SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1), 1,
+--                      POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) - 1)
+--              ELSE SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1) END,
+--     base_path=CASE
+--                   WHEN POSITION('://' IN url) = 0 THEN
+--                       CASE
+--                           WHEN POSITION('?' IN url) > 0 THEN
+--                               SUBSTRING(url, 1, POSITION('?' IN url) - 1)
+--                           ELSE url END
+--                   WHEN POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) > 0 THEN
+--                       CASE
+--                           WHEN POSITION('?' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) > 0 THEN
+--                               SUBSTRING(SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1),
+--                                         POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) + 1,
+--                                         POSITION('?' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) -
+--                                         POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) - 1)
+--                           ELSE SUBSTRING(SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1),
+--                                          POSITION('/' IN SUBSTRING(SUBSTRING(url, POSITION('://' IN url) + 3), 1)) +
+--                                          1) END
+--         END,
+--     query_string=CASE
+--                      WHEN POSITION('?' IN url) > 0 THEN SUBSTRING(url, POSITION('?' IN url) + 1)
+--         END;
 
 
 COMMIT;
