24607df0fa5d05daa6642f48da72239f1d405d98
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-03-03 15:15:12 +0100

    feat(api): requests indexes feat(api): graphql indexes

--- scripts/helm/db/init_dbs/postgresql/1.5.X/1.5.X.sql
+++ scripts/helm/db/init_dbs/postgresql/1.5.X/1.5.X.sql
@@ -16,7 +16,7 @@ ALTER TABLE events_common.requests
     ADD COLUMN IF NOT EXISTS query_string  text        NULL,
     ADD COLUMN IF NOT EXISTS request_body  text        NULL,
     ADD COLUMN IF NOT EXISTS response_body text        NULL,
-    ADD COLUMN IF NOT EXISTS status_code   text        NULL,
+    ADD COLUMN IF NOT EXISTS status_code   smallint    NULL,
     ADD COLUMN IF NOT EXISTS method        http_method NULL;
 
 -- Split requests-URL: Takes too long to use
@@ -53,8 +53,26 @@ ALTER TABLE events_common.requests
 ALTER TABLE events.graphql
     ADD COLUMN IF NOT EXISTS request_body  text        NULL,
     ADD COLUMN IF NOT EXISTS response_body text        NULL,
-    ADD COLUMN IF NOT EXISTS status_code   text        NULL,
+    ADD COLUMN IF NOT EXISTS status_code   smallint    NULL,
     ADD COLUMN IF NOT EXISTS method        http_method NULL,
     ADD COLUMN IF NOT EXISTS duration      integer     NULL;
 
 COMMIT;
+
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_host_nn_idx ON events_common.requests (host) WHERE host IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_host_nn_gin_idx ON events_common.requests USING GIN (host gin_trgm_ops) WHERE host IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_base_path_nn_idx ON events_common.requests (base_path) WHERE base_path IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_base_path_nn_gin_idx ON events_common.requests USING GIN (base_path gin_trgm_ops) WHERE base_path IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_query_string_nn_idx ON events_common.requests (query_string) WHERE query_string IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_query_string_nn_gin_idx ON events_common.requests USING GIN (query_string gin_trgm_ops) WHERE query_string IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_request_body_nn_idx ON events_common.requests (request_body) WHERE request_body IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_request_body_nn_gin_idx ON events_common.requests USING GIN (request_body gin_trgm_ops) WHERE request_body IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_response_body_nn_idx ON events_common.requests (response_body) WHERE response_body IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_response_body_nn_gin_idx ON events_common.requests USING GIN (response_body gin_trgm_ops) WHERE response_body IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS requests_status_code_nn_idx ON events_common.requests (status_code) WHERE status_code IS NOT NULL;
+
+CREATE INDEX CONCURRENTLY IF NOT EXISTS graphql_request_body_nn_idx ON events.graphql (request_body) WHERE request_body IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS graphql_request_body_nn_gin_idx ON events.graphql USING GIN (request_body gin_trgm_ops) WHERE request_body IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS graphql_response_body_nn_idx ON events.graphql (response_body) WHERE response_body IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS graphql_response_body_nn_gin_idx ON events.graphql USING GIN (response_body gin_trgm_ops) WHERE response_body IS NOT NULL;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS graphql_status_code_nn_idx ON events.graphql (status_code) WHERE status_code IS NOT NULL;
