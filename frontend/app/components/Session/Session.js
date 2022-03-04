d57959199b3cdeef6270951427540cdc57d4d05f
Shekar Siri <sshekarsiri@gmail.com>
2022-03-04 10:53:12 +0100

    fix(ui) - session call made twice

--- frontend/app/components/Session/Session.js
+++ frontend/app/components/Session/Session.js
@@ -6,8 +6,6 @@ import { fetchList as fetchSlackList } from 'Duck/integrations/slack';
 import { Link, NoContent, Loader } from 'UI';
 import { sessions as sessionsRoute } from 'App/routes';
 import withPermissions from 'HOCs/withPermissions'
-
-import LivePlayer from './LivePlayer';
 import WebPlayer from './WebPlayer';
 import IOSPlayer from './IOSPlayer';
 
@@ -20,7 +18,6 @@ function Session({
 	session, 
 	fetchSession,
   	fetchSlackList,
-  	hasSessionsPath
  }) {
  	usePageTitle("OpenReplay Session Player");
  	useEffect(() => {
@@ -35,41 +32,38 @@ function Session({
 		return () => {
 			if (!session.exists()) return;
 		}
-	},[ sessionId, hasSessionsPath ]);
+	},[ sessionId ]);
 
 	return (
 		<NoContent
-      show={ hasErrors }
-      title="Session not found."
-      subtext={
-        <span>
-          {'Please check your data retention plan, or try '}
-          <Link to={ SESSIONS_ROUTE }>{'another one'}</Link>
-        </span>
-      }
-    >
-    	<Loader className="flex-1" loading={ loading || sessionId !== session.sessionId }> 
-    		{ session.isIOS 
-    			? <IOSPlayer session={session} />
-    			: <WebPlayer />
-      	}
-    	</Loader>
-    </NoContent>
+			show={ hasErrors }
+			title="Session not found."
+			subtext={
+				<span>
+				{'Please check your data retention plan, or try '}
+				<Link to={ SESSIONS_ROUTE }>{'another one'}</Link>
+				</span>
+			}
+		>
+			<Loader className="flex-1" loading={ loading || sessionId !== session.sessionId }> 
+				{ session.isIOS 
+					? <IOSPlayer session={session} />
+					: <WebPlayer />
+			}
+			</Loader>
+		</NoContent>
 	);
 }
 
 export default withPermissions(['SESSION_REPLAY'], '', true)(connect((state, props) => {
 	const { match: { params: { sessionId } } } = props;
-  const isAssist = state.getIn(['sessions', 'activeTab']).type === 'live';
-  const hasSessiosPath = state.getIn([ 'sessions', 'sessionPath' ]).includes('/sessions');
-  return {
-    sessionId,
-    loading: state.getIn([ 'sessions', 'loading' ]),
-    hasErrors: !!state.getIn([ 'sessions', 'errors' ]),
-    session: state.getIn([ 'sessions', 'current' ]),
-    hasSessionsPath: hasSessiosPath && !isAssist,
-  };
-}, {
-  fetchSession,
-  fetchSlackList,
+	return {
+		sessionId,
+		loading: state.getIn([ 'sessions', 'loading' ]),
+		hasErrors: !!state.getIn([ 'sessions', 'errors' ]),
+		session: state.getIn([ 'sessions', 'current' ]),
+	};
+	}, {
+	fetchSession,
+	fetchSlackList,
 })(Session));
