8328739c2770e2baba0615a072f99e80c4e00419
ShiKhu <alex.kaminsky.11@gmail.com>
2022-03-06 20:06:04 +0100

    fix(backend): project columns to select

--- backend/pkg/db/postgres/project.go
+++ backend/pkg/db/postgres/project.go
@@ -7,12 +7,12 @@ import (
 func (conn *Conn) GetProjectByKey(projectKey string) (*Project, error) {
 	p := &Project{ProjectKey: projectKey}
 	if err := conn.queryRow(`
-		SELECT max_session_duration, sample_rate, project_id, save_request_payloads
+		SELECT max_session_duration, sample_rate, project_id
 		FROM projects
 		WHERE project_key=$1 AND active = true
 	`,
 		projectKey,
-	).Scan(&p.MaxSessionDuration, &p.SampleRate, &p.ProjectID, &p.SaveRequestPayloads); err != nil {
+	).Scan(&p.MaxSessionDuration, &p.SampleRate, &p.ProjectID); err != nil {
 		return nil, err
 	}
 	return p, nil
@@ -22,14 +22,14 @@ func (conn *Conn) GetProjectByKey(projectKey string) (*Project, error) {
 func (conn *Conn) GetProject(projectID uint32) (*Project, error) {
 	p := &Project{ProjectID: projectID}
 	if err := conn.queryRow(`
-		SELECT project_key, max_session_duration,
+		SELECT project_key, max_session_duration, save_request_payloads,
 			metadata_1, metadata_2, metadata_3, metadata_4, metadata_5,
 			metadata_6, metadata_7, metadata_8, metadata_9, metadata_10
 		FROM projects
 		WHERE project_id=$1 AND active = true
 	`,
 		projectID,
-	).Scan(&p.ProjectKey, &p.MaxSessionDuration,
+	).Scan(&p.ProjectKey, &p.MaxSessionDuration, &p.SaveRequestPayloads,
 		&p.Metadata1, &p.Metadata2, &p.Metadata3, &p.Metadata4, &p.Metadata5,
 		&p.Metadata6, &p.Metadata7, &p.Metadata8, &p.Metadata9, &p.Metadata10); err != nil {
 		return nil, err
