3537b6c00f7a084c350426cc2332db31ed4d2478
Shekar Siri <sshekarsiri@gmail.com>
2022-03-09 15:58:20 +0100

    feat(ui) - pagination wip

--- frontend/app/components/ui/Pagination/Pagination.tsx
+++ frontend/app/components/ui/Pagination/Pagination.tsx
@@ -10,25 +10,41 @@ interface Props {
 }
 export default function Pagination(props: Props) {
     const { page, totalPages, onPageChange, limit = 5 } = props;
+    const [currentPage, setCurrentPage] = React.useState(page);
+    // const []
+
+    const changePage = (page: number) => {
+        if (page > 0 && page <= totalPages) {
+            onPageChange(page);
+            setCurrentPage(page);
+        }
+    }
     return (
         <div className="flex items-center">
             <button
-                className={cn("pagination-previous py-2 px-3", { "opacity-50": page === 1 })}
+                className={cn("py-2 px-3", { "opacity-50 cursor-default": page === 1 })}
                 disabled={page === 1}
-                onClick={() => onPageChange(page - 1)}
+                onClick={() => changePage(page - 1)}
             >
-                <Icon name="chevron-left" size="18" />
+                <Icon name="chevron-left" size="18" color={page === 1 ? 'gray-medium' : 'teal'} />
             </button>
             <span className="mr-2 color-gray-medium">Page</span>
-            <input type="number" className="p-1 bg-white border rounded w-16" value={page} />
+            <input
+                type="number"
+                className={cn("py-1 px-2 bg-white border rounded w-16", { "opacity-50 cursor-default": totalPages === 1 })}
+                value={currentPage}
+                min={1}
+                max={totalPages}
+                onChange={(e) => changePage(parseInt(e.target.value))}
+            />
             <span className="mx-3 color-gray-medium">of</span>
             <span >{totalPages}</span>
             <button
-                className="pagination-next py-2 px-3"
+                className={cn("py-2 px-3", { "opacity-50 cursor-default": page === totalPages })}
                 disabled={page === totalPages}
-                onClick={() => onPageChange(page + 1)}
+                onClick={() => changePage(page + 1)}
             >
-                <Icon name="chevron-right" size="18" />
+                <Icon name="chevron-right" size="18" color={page === totalPages ? 'gray-medium' : 'teal'} />
             </button>
         </div>
     )
