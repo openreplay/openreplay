235093cf539308fc55769ebe16a4629cdaad781a
Shekar Siri <sshekarsiri@gmail.com>
2022-03-09 12:49:45 +0100

    fix(ui) - save funnel from sessions

--- frontend/app/components/shared/SaveFunnelButton/SaveFunnelButton.tsx
+++ frontend/app/components/shared/SaveFunnelButton/SaveFunnelButton.tsx
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 import { IconButton } from 'UI';
 import FunnelSaveModal from 'App/components/Funnels/FunnelSaveModal';
-
-export default function SaveFunnelButton() {
+import { connect } from 'react-redux';
+import { init } from 'Duck/funnels';
+interface Props {
+  filter: any
+  init: (instance: any) => void
+}
+function SaveFunnelButton(props: Props) {
   const [showModal, setshowModal] = useState(false)
+
+  const handleClick = () => {
+    props.init({ filter: props.filter })
+    setshowModal(true)
+  }
   return (
     <div>
         <IconButton
             className="mr-2"
-            onClick={() => setshowModal(true)} primaryText label="SAVE FUNNEL" icon="funnel"
+            onClick={handleClick} primaryText label="SAVE FUNNEL" icon="funnel"
         />
 
         <FunnelSaveModal
@@ -18,3 +28,7 @@ export default function SaveFunnelButton() {
     </div>
   )
 }
+
+export default connect(state => ({
+  filter: state.getIn(['search', 'instance']),
+}), { init })(SaveFunnelButton);
