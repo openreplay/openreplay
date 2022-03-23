1e3bc978279ddc7eb1a1604370e3181774dcc574
Mehdi Osman <estradino@users.noreply.github.com>
2022-03-23 23:25:36 +0400

    Updated version to v1.5.4

--- frontend/env.js
+++ frontend/env.js
@@ -13,7 +13,7 @@ const oss = {
 	ORIGIN: () => 'window.location.origin',
 	API_EDP: () => 'window.location.origin + "/api"',
 	ASSETS_HOST: () => 'window.location.origin + "/assets"',
-	VERSION: '1.5.3',
+	VERSION: '1.5.4',
 	SOURCEMAP: true,
 	MINIO_ENDPOINT: process.env.MINIO_ENDPOINT,
 	MINIO_PORT: process.env.MINIO_PORT,
