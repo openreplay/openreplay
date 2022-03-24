811a921981f42cc8439091538872743d7adffb62
Shekar Siri <sshekarsiri@gmail.com>
2022-03-24 11:04:48 +0100

    feat(ui) - dashboards wip

--- frontend/app/components/Dashboard/components/MetricsView/MetricsView.tsx
+++ frontend/app/components/Dashboard/components/MetricsView/MetricsView.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Button, PageTitle, Link } from 'UI';
+import { Button, PageTitle, Icon, Link } from 'UI';
 import { withSiteId, dashboardMetricCreate } from 'App/routes';
+import MetricsList from '../MetricsList';
 
 function MetricsView(props) {
     return (
         <div>
-            <div className="flex items-center mb-4">
+            <div className="flex items-center mb-4 justify-between">
                 <PageTitle title="Metrics" className="mr-3" />
                 {/* <Link to={withSiteId(dashboardMetricCreate(dashboard.dashboardId), store.siteId)}><Button primary size="small">Add Metric</Button></Link> */}
+                <div className="ml-auto w-1/4">
+                    <input
+                        className="bg-white p-2 border rounded w-full"
+                        placeholder="Filter by title, type, dashboard and owner"
+                    />
+                </div>
             </div>
+            <MetricsList />
         </div>
     );
 }
