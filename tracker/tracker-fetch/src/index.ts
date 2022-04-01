0963edd7c4003e73c380ddb8b68597205b325da2
ShiKhu <alex.kaminsky.11@gmail.com>
2022-04-01 22:15:51 +0200

    feat(tracker-fetch):3.5.3: common improved sanitiser

--- tracker/tracker-fetch/src/index.ts
+++ tracker/tracker-fetch/src/index.ts
@@ -1,38 +1,48 @@
 import { App, Messages } from '@openreplay/tracker';
 
-interface Request {
-  url: string,
-  body: string | Object,
-  headers: Record<string, string>,
+interface RequestData {
+  body: BodyInit | null | undefined
+  headers: Record<string, string>
 }
 
-interface Response {
-  url: string,
-  status: number,
-  body: string,
-  headers: Record<string, string>,
+interface ResponseData {
+  body: string | Object | null
+  headers: Record<string, string>
 }
+
+interface RequestResponseData {
+  readonly status: number
+  readonly method: string
+  url: string
+  request: RequestData
+  response: ResponseData
+}
+
 
 export interface Options {
-  sessionTokenHeader?: string;
-  replaceDefault: boolean; // overrideDefault ?
-  failuresOnly: boolean;
-  ignoreHeaders: Array<string> | boolean;
-  requestSanitizer: ((Request) => Request | null) | null;
-  responseSanitizer: ((Response) => Response | null) | null;
+  sessionTokenHeader?: string
+  failuresOnly: boolean
+  overrideGlobal: boolean
+  ignoreHeaders: Array<string> | boolean
+  sanitiser?: (RequestResponseData) => RequestResponseData | null
+
+  requestSanitizer?: any
+  responseSanitizer?: any
 }
 
 export default function(opts: Partial<Options> = {}) {
   const options: Options = Object.assign(
     {
-      replaceDefault: false,
+      overrideGlobal: false,
       failuresOnly: false,
       ignoreHeaders: [ 'Cookie', 'Set-Cookie', 'Authorization' ],
-      requestSanitizer: null,
-      responseSanitizer: null,
     },
     opts,
   );
+  if (options.requestSanitizer && options.responseSanitizer) {
+    console.warn("OpenReplay fetch plugin: `requestSanitizer` and `responseSanitizer` options are depricated. Please, use `sanitiser` instead (check out documentation at https://docs.openreplay.com/plugins/fetch).")
+  }
+
   const origFetch = window.fetch
   return (app: App | null) => {
     if (app === null) {
@@ -90,56 +100,55 @@ export default function(opts: Partial<Options> = {}) {
           r.headers.forEach((v, n) => { if (!isHIgnoring(n)) resHs[n] = v })
         }
 
-        // Request forming
-        let reqBody = ''
-        if (typeof init.body === 'string') {
-          reqBody = init.body
-        } else if (typeof init.body === 'object') {
-          try {
-            reqBody = JSON.stringify(init.body)
-          } catch {}
-        }
-        let req: Request | null = {
-          url: input,
+        const req: RequestData = {
           headers: reqHs,
-          body: reqBody,
-        }
-        if (options.requestSanitizer !== null) {
-          req = options.requestSanitizer(req)
-          if (!req) {
-            return
-          }
+          body: init.body,
         }
 
         // Response forming
-        let res: Response | null = {
-          url: input,
-          status: r.status,
+        const res: ResponseData = {
           headers: resHs,
           body: text,
         }
-        if (options.responseSanitizer !== null) {
-          res = options.responseSanitizer(res)
-          if (!res) {
+
+        const method = typeof init.method === 'string' 
+          ? init.method.toUpperCase() 
+          : 'GET'
+        let reqResInfo: RequestResponseData | null = {
+          url: input,
+          method,
+          status: r.status,
+          request: req,
+          response: res,
+        }
+        if (options.sanitiser) {
+          try {
+            reqResInfo.response.body = JSON.parse(text) as Object // Why the returning type is "any"?
+          } catch {}
+          reqResInfo = options.sanitiser(reqResInfo)
+          if (!reqResInfo) {
             return
           }
         }
 
-        const reqStr = JSON.stringify({
-          headers: req.headers,
-          body: req.body,
-        })
-        const resStr = JSON.stringify({
-          headers: res.headers,
-          body: res.body,
-        })
+        const getStj = (r: RequestData | ResponseData): string => {
+          if (r && typeof r.body !== 'string') {
+            try {
+              r.body = JSON.stringify(r.body)
+            } catch {
+              r.body = "<unable to stringify>"
+              //app.log.warn("Openreplay fetch") // TODO: version check
+            }
+          }
+          return JSON.stringify(r)
+        }
 
         app.send(
           Messages.Fetch(
-            typeof init.method === 'string' ? init.method.toUpperCase() : 'GET',
-            input,
-            reqStr,
-            resStr,
+            method,
+            String(reqResInfo.url),
+            getStj(reqResInfo.request),
+            getStj(reqResInfo.response),
             r.status,
             startTime + performance.timing.navigationStart,
             duration,
@@ -147,8 +156,9 @@ export default function(opts: Partial<Options> = {}) {
         ) 
       });
       return response;
-    };
-    if (options.replaceDefault) {
+    }
+
+    if (options.overrideGlobal) {
       window.fetch = fetch
     }
     return fetch;
