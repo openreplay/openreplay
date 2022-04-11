875492e41842f7b742796af629dc060052842c9f
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-04-11 16:28:26 +0200

    feat(utilities): heapsnapshot multiple steps

--- utilities/utils/dump.js
+++ utilities/utils/dump.js
@@ -1,20 +1,35 @@
 const fs = require('fs');
 const v8 = require('v8');
+const express = require('express');
+const router = express.Router();
 
 const location = '/tmp/';
+let creationStatus = null;
+let fileName = null;
 
 async function createHeapSnapshot() {
-    const fileName = `${Date.now()}.heapsnapshot`;
+    if (creationStatus) {
+        return console.log(`In progress ${fileName}`);
+    }
+    if (fileName === null) {
+        fileName = `${Date.now()}.heapsnapshot`;
+    }
+    console.log(`Creating ${fileName}`);
     await fs.promises.writeFile(
         location + fileName,
         v8.getHeapSnapshot()
     );
-    return fileName;
+    console.log(`Created ${fileName}`);
+    creationStatus = true;
 }
 
 
-async function sendHeapSnapshot(req, res) {
-    const fileName = await createHeapSnapshot();
+async function downloadHeapSnapshot(req, res) {
+    if (creationStatus === null) {
+        return res.end("should call /new first");
+    } else if (!creationStatus) {
+        return res.end("should wait for done status");
+    }
     res.download(location + fileName, function (err) {
         try {
             fs.unlinkSync(location + fileName)
@@ -25,20 +40,24 @@ async function sendHeapSnapshot(req, res) {
     });
 }
 
-process.on('USR2', () => {
-    console.info('USR2 signal received.');
-});
-
-async function saveHeapSnapshot(req, res) {
-    const fileName = await createHeapSnapshot();
-    const path = location + fileName;
-    console.log(`Heapdump saved to ${path}`)
+function getHeapSnapshotStatus(req, res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({path}));
+    res.end(JSON.stringify({path: location + fileName, 'done': creationStatus}));
 }
 
-process.on('USR2', () => {
-    console.info('USR2 signal received.');
-});
-module.exports = {sendHeapSnapshot, saveHeapSnapshot}
+function createNewHeapSnapshot(req, res) {
+    creationStatus = false;
+    fileName = `${Date.now()}.heapsnapshot`;
+    setTimeout(() => {
+        createHeapSnapshot()
+    }, 0);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({path: location + fileName, 'done': creationStatus}));
+}
+
+router.get('/status', getHeapSnapshotStatus);
+router.get(`/new`, createNewHeapSnapshot);
+router.get(`/download`, downloadHeapSnapshot);
+module.exports = {router}
