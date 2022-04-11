efe88c86d269a8d77d3eeba40d69453421a49a41
Shekar Siri <sshekarsiri@gmail.com>
2022-04-11 11:15:14 +0200

    change(ui) - assist session data endpoint

--- frontend/app/components/BugFinder/BugFinder.js
+++ frontend/app/components/BugFinder/BugFinder.js
@@ -20,7 +20,7 @@ import { LAST_7_DAYS } from 'Types/app/period';
 import { resetFunnel } from 'Duck/funnels';
 import { resetFunnelFilters } from 'Duck/funnelFilters'
 import NoSessionsMessage from 'Shared/NoSessionsMessage';
-import TrackerUpdateMessage from 'Shared/TrackerUpdateMessage';
+// import TrackerUpdateMessage from 'Shared/TrackerUpdateMessage';
 import SessionSearch from 'Shared/SessionSearch';
 import MainSearchBar from 'Shared/MainSearchBar';
 import { clearSearch, fetchSessions } from 'Duck/search';
@@ -130,7 +130,7 @@ export default class BugFinder extends React.PureComponent {
             />
           </div>
           <div className={cn("side-menu-margined", stl.searchWrapper) }>
-            <TrackerUpdateMessage />
+            {/* <TrackerUpdateMessage /> */}
             <NoSessionsMessage />
             <div className="mb-5">
               <MainSearchBar />
