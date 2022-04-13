4c3e49fa115932501cdc6be8c498d8b026f340d9
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-04-13 17:11:23 +0200

    feat(utilities): utilities as a WS standalone server

--- utilities/utils/HeapSnapshot.js
+++ utilities/utils/HeapSnapshot.js
@@ -31,6 +31,9 @@ async function downloadHeapSnapshot(req, res) {
         return res.end("should wait for done status");
     }
     res.download(location + fileName, function (err) {
+        if (err) {
+            return console.error("error while uploading HeapSnapshot file");
+        }
         try {
             fs.unlinkSync(location + fileName)
         } catch (err) {
@@ -57,7 +60,7 @@ function createNewHeapSnapshot(req, res) {
     res.end(JSON.stringify({path: location + fileName, 'done': creationStatus}));
 }
 
-router.get(`/${process.env.S3_KEY}/status`, getHeapSnapshotStatus);
-router.get(`/${process.env.S3_KEY}/new`, createNewHeapSnapshot);
-router.get(`/${process.env.S3_KEY}/download`, downloadHeapSnapshot);
+router.get(`/status`, getHeapSnapshotStatus);
+router.get(`/new`, createNewHeapSnapshot);
+router.get(`/download`, downloadHeapSnapshot);
 module.exports = {router}
