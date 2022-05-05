b1bf0470865129063adf137c09cbc6e32d2f8294
Amit Abitbul <amit@rendit.tech>
2022-05-05 14:17:40 +0300

    Refactor mimetypes Add support on cache resources by its actual response mimetypes

--- backend/pkg/url/assets/url.go
+++ backend/pkg/url/assets/url.go
@@ -6,9 +6,23 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
+	"fmt"
+	"net/http"
+	"crypto/tls"
+	"os"
 	"openreplay/backend/pkg/flakeid"
 )
+
+var supportedMimeTypes = map[string][]string{
+	".css" : []string{"text/css"},
+	".woff" : []string{"font/woff", "application/font-woff"},
+	".woff2" : []string{"font/woff2", "application/font-woff2"},
+	".ttf" : []string{"font/ttf"},
+	".otf" : []string{"font/otf"},
+	".svg"  : []string{"image/svg+xml"},
+	".eot"  : []string{"application/vnd.ms-fontobject"},
+  }
+  
 
 func getSessionKey(sessionID uint64) string {
 	return strconv.FormatUint(
@@ -46,13 +60,77 @@ func isCachable(rawurl string) bool {
 	if u.Scheme != "http" && u.Scheme != "https" {
 		return false
 	}
+
+	var mimeTypesToIgnore []string
+
+	mimeTypesToIgnoreStr, mimeTypesToIgnoreExists := os.LookupEnv("IGNORE_MIMETYPES_LIST")
+	if mimeTypesToIgnoreExists {
+		mimeTypesToIgnore = strings.Split(mimeTypesToIgnoreStr, ",")
+	}
+	
 	ext := filepath.Ext(u.Path)
-	return ext == ".css" ||
-		ext == ".woff" ||
-		ext == ".woff2" ||
-		ext == ".ttf" ||
-		ext == ".otf" ||
-		ext == ".eot"
+
+	for k := range supportedMimeTypes {
+		if mimeTypesToIgnoreExists {
+			for i := range mimeTypesToIgnore {
+				if strings.HasSuffix(ext, mimeTypesToIgnore[i]) {
+					return false
+				}
+			}
+		}
+		if strings.EqualFold(k, ext) {
+			return true
+		}
+	}
+
+	checkResourceMimeType, ok := os.LookupEnv("CHECK_RESOURCE_MIMETYPE")
+
+	if ok && strings.EqualFold(checkResourceMimeType, "true") {
+		// Create a temporary httpClient
+		client := &http.Client{
+			Timeout: time.Duration(6) * time.Second,
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		}
+
+		req, _ := http.NewRequest("GET", rawurl, nil)
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+		// Set Custom Headers
+		for _, e := range os.Environ() {
+			pair := strings.SplitN(e, "=", 2)
+			pair[0] = strings.ToLower(pair[0])
+			if (strings.HasPrefix(pair[0], "x-")) {
+				req.Header.Set(pair[0], pair[1])
+			}
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			return false
+		}
+		defer res.Body.Close()
+		if res.StatusCode >= 400 {
+			fmt.Println("Status code is ", res.StatusCode)
+			return false
+		}
+
+		contentType := res.Header.Get("Content-Type")
+		if contentType == "" {
+			return false
+		}
+
+		for _, mimeTypes := range supportedMimeTypes {
+			for mimeTypeIndex := range mimeTypes {
+				if strings.EqualFold(contentType, mimeTypes[mimeTypeIndex]) {
+					return true
+				}
+			}
+		}
+	}
+	
+	return false
 }
 
 func GetFullCachableURL(baseURL string, relativeURL string) (string, bool) {
