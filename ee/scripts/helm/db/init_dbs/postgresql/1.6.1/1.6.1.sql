d8078c220dcdf0831ce0807f2b16940cb039596a
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-05-06 17:27:43 +0200

    feat(api): search user trails by username feat(db): index to search user trails by username

--- ee/scripts/helm/db/init_dbs/postgresql/1.6.1/1.6.1.sql
+++ ee/scripts/helm/db/init_dbs/postgresql/1.6.1/1.6.1.sql
@@ -12,4 +12,5 @@ ALTER TABLE IF EXISTS dashboards
 
 CREATE INDEX IF NOT EXISTS traces_created_at_idx ON traces (created_at);
 CREATE INDEX IF NOT EXISTS traces_action_idx ON traces (action);
+CREATE INDEX IF NOT EXISTS users_name_gin_idx ON users USING GIN (name gin_trgm_ops);
 COMMIT;
