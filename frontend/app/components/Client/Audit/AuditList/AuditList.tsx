f12931491a6941abf2cb7bc87b16f7563de4ae1e
Shekar Siri <sshekarsiri@gmail.com>
2022-05-06 12:06:55 +0200

    feat(ui) - audit - base views

---
+++ frontend/app/components/Client/Audit/AuditList/AuditList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+
+}
+function AuditList(props: Props) {
+    return (
+        <div>
+            List
+        </div>
+    );
+}
+
+export default AuditList;
