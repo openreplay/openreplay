f054b130bfe71eb351f64d4f60edf218ee9a6419
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-05-16 18:24:16 +0200

    feat(DB): changed metrics category from Overview to Monitoring Essentials

--- ee/scripts/helm/db/init_dbs/postgresql/init_schema.sql
+++ ee/scripts/helm/db/init_dbs/postgresql/init_schema.sql
@@ -1269,102 +1269,102 @@ LANGUAGE plpgsql;
 
 INSERT INTO metrics (name, category, default_config, is_predefined, is_template, is_public, predefined_key, metric_type,
                      view_type)
-VALUES ('Captured sessions', 'overview', '{
+VALUES ('Captured sessions', 'Monitoring Essentials', '{
   "col": 1,
   "row": 1,
   "position": 0
 }', true, true, true, 'count_sessions', 'predefined', 'overview'),
-       ('Request Load Time', 'overview', '{
+       ('Request Load Time', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_request_load_time', 'predefined', 'overview'),
-       ('Page Load Time', 'overview', '{
+       ('Page Load Time', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_page_load_time', 'predefined', 'overview'),
-       ('Image Load Time', 'overview', '{
+       ('Image Load Time', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_image_load_time', 'predefined', 'overview'),
-       ('DOM Content Load Start', 'overview', '{
+       ('DOM Content Load Start', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_dom_content_load_start', 'predefined', 'overview'),
-       ('First Meaningful paint', 'overview', '{
+       ('First Meaningful paint', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_first_contentful_pixel', 'predefined', 'overview'),
-       ('No. of Visited Pages', 'overview', '{
+       ('No. of Visited Pages', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_visited_pages', 'predefined', 'overview'),
-       ('Session Duration', 'overview', '{
+       ('Session Duration', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_session_duration', 'predefined', 'overview'),
-       ('DOM Build Time', 'overview', '{
+       ('DOM Build Time', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_pages_dom_buildtime', 'predefined', 'overview'),
-       ('Pages Response Time', 'overview', '{
+       ('Pages Response Time', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_pages_response_time', 'predefined', 'overview'),
-       ('Response Time', 'overview', '{
+       ('Response Time', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_response_time', 'predefined', 'overview'),
-       ('First Paint', 'overview', '{
+       ('First Paint', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_first_paint', 'predefined', 'overview'),
-       ('DOM Content Loaded', 'overview', '{
+       ('DOM Content Loaded', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_dom_content_loaded', 'predefined', 'overview'),
-       ('Time Till First byte', 'overview', '{
+       ('Time Till First byte', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_till_first_byte', 'predefined', 'overview'),
-       ('Time To Interactive', 'overview', '{
+       ('Time To Interactive', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_time_to_interactive', 'predefined', 'overview'),
-       ('Captured requests', 'overview', '{
+       ('Captured requests', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'count_requests', 'predefined', 'overview'),
-       ('Time To Render', 'overview', '{
+       ('Time To Render', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_time_to_render', 'predefined', 'overview'),
-       ('Memory Consumption', 'overview', '{
+       ('Memory Consumption', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_used_js_heap_size', 'predefined', 'overview'),
-       ('CPU Load', 'overview', '{
+       ('CPU Load', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
        }', true, true, true, 'avg_cpu', 'predefined', 'overview'),
-       ('Frame rate', 'overview', '{
+       ('Frame rate', 'Monitoring Essentials', '{
          "col": 1,
          "row": 1,
          "position": 0
