20f7c0fb70c7dc76235affa7b5c410a4d2653d5b
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-05-16 18:24:16 +0200

    feat(DB): changed metrics category from Overview to Monitoring Essentials

--- ee/scripts/helm/db/init_dbs/postgresql/1.6.1/1.6.1.sql
+++ ee/scripts/helm/db/init_dbs/postgresql/1.6.1/1.6.1.sql
@@ -13,4 +13,117 @@ ALTER TABLE IF EXISTS dashboards
 CREATE INDEX IF NOT EXISTS traces_created_at_idx ON traces (created_at);
 CREATE INDEX IF NOT EXISTS traces_action_idx ON traces (action);
 CREATE INDEX IF NOT EXISTS users_name_gin_idx ON users USING GIN (name gin_trgm_ops);
+
+INSERT INTO metrics (name, category, default_config, is_predefined, is_template, is_public, predefined_key, metric_type,
+                     view_type)
+VALUES ('Captured sessions', 'Monitoring Essentials', '{
+  "col": 1,
+  "row": 1,
+  "position": 0
+}', true, true, true, 'count_sessions', 'predefined', 'overview'),
+       ('Request Load Time', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_request_load_time', 'predefined', 'overview'),
+       ('Page Load Time', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_page_load_time', 'predefined', 'overview'),
+       ('Image Load Time', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_image_load_time', 'predefined', 'overview'),
+       ('DOM Content Load Start', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_dom_content_load_start', 'predefined', 'overview'),
+       ('First Meaningful paint', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_first_contentful_pixel', 'predefined', 'overview'),
+       ('No. of Visited Pages', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_visited_pages', 'predefined', 'overview'),
+       ('Session Duration', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_session_duration', 'predefined', 'overview'),
+       ('DOM Build Time', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_pages_dom_buildtime', 'predefined', 'overview'),
+       ('Pages Response Time', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_pages_response_time', 'predefined', 'overview'),
+       ('Response Time', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_response_time', 'predefined', 'overview'),
+       ('First Paint', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_first_paint', 'predefined', 'overview'),
+       ('DOM Content Loaded', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_dom_content_loaded', 'predefined', 'overview'),
+       ('Time Till First byte', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_till_first_byte', 'predefined', 'overview'),
+       ('Time To Interactive', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_time_to_interactive', 'predefined', 'overview'),
+       ('Captured requests', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'count_requests', 'predefined', 'overview'),
+       ('Time To Render', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_time_to_render', 'predefined', 'overview'),
+       ('Memory Consumption', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_used_js_heap_size', 'predefined', 'overview'),
+       ('CPU Load', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_cpu', 'predefined', 'overview'),
+       ('Frame rate', 'Monitoring Essentials', '{
+         "col": 1,
+         "row": 1,
+         "position": 0
+       }', true, true, true, 'avg_fps', 'predefined', 'overview')
+ON CONFLICT (predefined_key) DO UPDATE
+    SET name=excluded.name,
+        category=excluded.category,
+        default_config=excluded.default_config,
+        is_predefined=excluded.is_predefined,
+        is_template=excluded.is_template,
+        is_public=excluded.is_public,
+        metric_type=excluded.metric_type,
+        view_type=excluded.view_type;
+
 COMMIT;
