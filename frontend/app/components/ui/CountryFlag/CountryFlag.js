b63d560ba75e8aed49cd01f96cbdb846b1b096b1
sylenien <nikita@openreplay.com>
2022-05-16 09:35:31 +0200

    fix(ui): removed popup from country flag component; added bg to toggler head

--- frontend/app/components/ui/CountryFlag/CountryFlag.js
+++ frontend/app/components/ui/CountryFlag/CountryFlag.js
@@ -7,24 +7,17 @@ const CountryFlag = React.memo(({ country, className, style = {}, label = false 
 	const knownCountry = !!country && country !== 'UN';
   	const countryFlag = knownCountry ? country.toLowerCase() : '';
   	const countryName = knownCountry ? countries[ country ] : 'Unknown Country';
-	
+
 	return (
 		<div className="flex items-center" style={style}>
-			<Popup
-				trigger={ knownCountry 
+			{knownCountry
 				?  <div className={ cn(`flag flag-${ countryFlag }`, className, stl.default) } />
 				: (
 					<div className="flex items-center w-full">
 						<Icon name="flag-na" size="22" className="" />
 						<div className="ml-2 leading-none" style={{ whiteSpace: 'nowrap'}}>Unknown Country</div>
 					</div>
-				)
-				// : <div className={ cn('text-sm', className) }>{ "N/A" }</div>
-				}
-				content={ countryName }
-				inverted
-				size="tiny"
-			/>
+			)}
 			{ knownCountry && label && <div className={ stl.label }>{ countryName }</div> }
 		</div>
 	);
