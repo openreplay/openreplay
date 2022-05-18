3eb6db1a2534db789d8ef10de5c24332cd759d97
Shekar Siri <sshekarsiri@gmail.com>
2022-05-18 12:32:20 +0200

    Revert "Session Vault (#480)" (#481)
    This reverts commit 447ff3b3ab10d5f97148c844259e6e0d6056a21f.

--- frontend/app/components/shared/Bookmark/Bookmark.tsx
+++ frontend/app/components/shared/Bookmark/Bookmark.tsx
@@ -1,59 +1,26 @@
-//@ts-nocheck
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import stl from './bookmark.css'
 import { Icon } from 'UI'
 import { toggleFavorite } from 'Duck/sessions'
 import { connect } from 'react-redux'
-import { toast } from 'react-toastify';
-import { Tooltip } from 'react-tippy';
+// import Session from 'Types/session';
 
 interface Props {
-  toggleFavorite: (sessionId: string) => Promise<void>,
+  toggleFavorite: (session) => void,
   favorite: Boolean,
-  sessionId: any,
-  isEnterprise: Boolean
+  sessionId: any
 }
-function Bookmark(props : Props ) {  
-  const { sessionId, favorite, isEnterprise } = props;
-  const [isFavorite, setIsFavorite] = useState(favorite);
-  const ADDED_MESSAGE = isEnterprise ? 'Session added to vault' : 'Session added to your favorites';
-  const REMOVED_MESSAGE = isEnterprise ? 'Session removed from vault' : 'Session removed from your favorites';  
-  const TOOLTIP_TEXT_ADD = isEnterprise ? 'Add to vault' : 'Add to favorites';
-  const TOOLTIP_TEXT_REMOVE = isEnterprise ? 'Remove from vault' : 'Remove from favorites';
+function Bookmark({ toggleFavorite, sessionId, favorite } : Props ) {  
 
-  const ACTIVE_ICON = isEnterprise ? 'safe-fill' : 'star-solid';
-  const INACTIVE_ICON = isEnterprise ? 'safe' : 'star';
-
-
-  useEffect(() => {
-    setIsFavorite(favorite);
-  }, [favorite]);
-
-  const toggleFavorite = async () => {
-    props.toggleFavorite(sessionId).then(() => {
-      toast.success(isFavorite ? REMOVED_MESSAGE : ADDED_MESSAGE);
-      setIsFavorite(!isFavorite);
-    });
-  }
-
-  return (
-    <Tooltip 
-      delay={500}
-      arrow
-      title={isFavorite ? TOOLTIP_TEXT_REMOVE : TOOLTIP_TEXT_ADD}
-      hideOnClick={true}
+  return ( 
+    <div
+      className={ stl.favoriteWrapper }
+      onClick={ () => toggleFavorite(sessionId) }
+      data-favourite={ favorite }
     >
-      <div
-        className={ stl.favoriteWrapper }
-        onClick={ toggleFavorite }
-        data-favourite={ isFavorite }
-      >
-        <Icon name={ isFavorite ? ACTIVE_ICON : INACTIVE_ICON } color="teal" size="20" />
-      </div>
-    </Tooltip>
+      <Icon name={ favorite ? 'star-solid' : 'star' } size="20" />
+    </div>
   )
 }
 
-export default connect(state => ({
-  isEnterprise: state.getIn([ 'user', 'client', 'edition' ]) === 'ee',
-}), { toggleFavorite })(Bookmark)
+export default connect(null, { toggleFavorite })(Bookmark)
