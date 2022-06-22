f1f18c6f3f5476e3b43ea04a48ac918dac7c43a4
sylenien <nikita@openreplay.com>
2022-06-22 15:46:58 +0200

    fix(ui): small ui fixes and improvements

--- frontend/app/components/ui/CountryFlag/CountryFlag.js
+++ frontend/app/components/ui/CountryFlag/CountryFlag.js
@@ -12,14 +12,14 @@ const CountryFlag = React.memo(({ country, className, style = {}, label = false 
 	return (
 		<div className="flex items-center" style={style}>
 			{knownCountry
-				?  <div className={ cn(`mr-1 flag flag-${ countryFlag }`, className, stl.default) } />
+				?  <div className={ cn(`flag flag-${ countryFlag }`, className, stl.default) } />
 				: (
 					<div className="flex items-center w-full">
 						<Icon name="flag-na" size="22" className="" />
 						<div className="ml-2 leading-none" style={{ whiteSpace: 'nowrap'}}>Unknown Country</div>
 					</div>
 			)}
-			{ knownCountry && label && <div className={ stl.label }>{ countryName }</div> }
+			{ knownCountry && label && <div className={ cn(stl.label, 'ml-1') }>{ countryName }</div> }
 		</div>
 	);
 })
