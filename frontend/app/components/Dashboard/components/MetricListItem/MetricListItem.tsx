c548cd7cbe7b60fe9fd51a79b96f26edce0edb3c
Shekar Siri <sshekarsiri@gmail.com>
2022-06-23 16:29:19 +0200

    change(ui) - metric list item name casing

--- frontend/app/components/Dashboard/components/MetricListItem/MetricListItem.tsx
+++ frontend/app/components/Dashboard/components/MetricListItem/MetricListItem.tsx
@@ -44,7 +44,7 @@ function MetricListItem(props: Props) {
                     <div className="w-8 h-8 rounded-full bg-tealx-lightest flex items-center justify-center mr-2">
                         <Icon name={getIcon(metric.metricType)} size="14" color="tealx"  />
                     </div>
-                    <Link to={`/metrics/${metric.metricId}`} className="link">
+                    <Link to={`/metrics/${metric.metricId}`} className="link capitalize-first">
                         {metric.name}
                     </Link>
                 </div>
