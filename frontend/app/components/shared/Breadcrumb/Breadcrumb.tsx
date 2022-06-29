ea642cd92660b975c651b6f5c3b672e128170708
Shekar Siri <sshekarsiri@gmail.com>
2022-06-29 17:29:51 +0200

    change(ui) - capitalize first letter in breadcrumb

--- frontend/app/components/shared/Breadcrumb/Breadcrumb.tsx
+++ frontend/app/components/shared/Breadcrumb/Breadcrumb.tsx
@@ -17,9 +17,9 @@ function Breadcrumb(props: Props) {
                 }
                 return (
                     <div key={index} className="color-gray-darkest hover:color-teal group flex items-center">
-                        <Link to={item.to} className="capitalize-first flex items-center">
+                        <Link to={item.to} className="flex items-center">
                             {index === 0 && <Icon name="chevron-left" size={16} className="mr-1 group-hover:fill-teal" />}
-                            {item.label}
+                            <span className="capitalize-first">{item.label}</span>
                         </Link>
                         <span className="mx-2">/</span>
                     </div>
