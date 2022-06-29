8ad5d869f723f28329d93ece260e1ff5368bfd34
Alex Kaminskii <alex@openreplay.com>
2022-06-29 11:13:53 +0200

    fixup! fix(tracker): check node scrolls only on start

--- tracker/tracker/src/main/app/nodes.ts
+++ tracker/tracker/src/main/app/nodes.ts
@@ -57,8 +57,8 @@ export default class Nodes {
     }
     return id;
   }
-  callNodeCallbacks(node: Node, start: boolean): void {
-    this.nodeCallbacks.forEach((cb) => cb(node, start));
+  callNodeCallbacks(node: Node, isStart: boolean): void {
+    this.nodeCallbacks.forEach((cb) => cb(node, isStart));
   }
   getID(node: Node): number | undefined {
     return (node as any)[this.node_id];
