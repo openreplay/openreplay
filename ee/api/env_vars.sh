33652acb901b43dde9596473bd2431568e95baea
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-07-04 16:16:54 +0200

    feat(api): changed EE env handler

--- ee/api/env_vars.sh
+++ ee/api/env_vars.sh
@@ -7,21 +7,12 @@ else
   if [ -f "$override" ]; then
     cp $override .env.override
     override=.env.override
-    # to remove endOfLine form sed result
-    echo "" >> $override
-    sed 's/=.*//;/^$/d' $override > .replacements
 
     # to remove all defined os-env-vars
-    cat .replacements | while read line
+    cat $override | while read line
     do
-       unset $line
+       export $line
     done
-    rm .replacements
-
-    # to merge predefined .env with the override.env
-    cp .env .env.d
-    sort -u -t '=' -k 1,1 $override .env.d > .env
-    rm .env.d
   else
     echo "$override does not exist."
   fi
