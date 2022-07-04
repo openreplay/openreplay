6f75e7a0590e335f944d10516a04e1be8475d592
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-07-04 15:00:37 +0200

    feat(api): changed EE env handler

--- ee/api/env_vars.sh
+++ ee/api/env_vars.sh
@@ -5,6 +5,8 @@ if [[ -z "${ENV_CONFIG_OVERRIDE_PATH}" ]]; then
 else
   override=$ENV_CONFIG_OVERRIDE_PATH
   if [ -f "$override" ]; then
+    cp $override .env.override
+    $override=.env.override
     # to remove endOfLine form sed result
     echo "" >> $override
     sed 's/=.*//;/^$/d' $override > .replacements
