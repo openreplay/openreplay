a52cf1763e1a8a3e51f32eb432594c65c176e2cf
Daniel Lopes <daniel@dllsystem.com>
2022-07-05 11:03:12 -0300

    copy code to separate function

--- tracker/tracker-vuex/src/index.ts
+++ tracker/tracker-vuex/src/index.ts
@@ -1,10 +1,34 @@
-import { App, Messages } from '@openreplay/tracker';
+import { App, Messages } from "@openreplay/tracker";
 import { Encoder, sha1 } from "./syncod/index.js";
 
 export interface Options {
   filter: (mutation: any, state: any) => boolean;
   transformer: (state: any) => any;
   mutationTransformer: (mutation: any) => any;
+}
+
+function processMutationAndState(
+  app: App,
+  options: Options,
+  encoder: Encoder,
+  mutation: any,
+  state: any
+) {
+  if (options.filter(mutation, state)) {
+    try {
+      const { type } = mutation;
+      if (typeof type === "string" && type) {
+        app.send(Messages.StateAction(type));
+      }
+      const _mutation = encoder.encode(options.mutationTransformer(mutation));
+      const _state = encoder.encode(options.transformer(state));
+      const _table = encoder.commit();
+      for (let key in _table) app.send(Messages.OTable(key, _table[key]));
+      app.send(Messages.Vuex(_mutation, _state));
+    } catch {
+      encoder.clear();
+    }
+  }
 }
 
 export default function(opts: Partial<Options> = {}) {
@@ -22,51 +46,25 @@ export default function(opts: Partial<Options> = {}) {
     }
     const encoder = new Encoder(sha1, 50);
     return store => {
+      // Vuex
       if (store.subscribe) {
-        // vuex        
         store.subscribe((mutation, state) => {
-          if (options.filter(mutation, state)) {
-            try {
-              const { type } = mutation;
-              if (typeof type === 'string' && type) {
-                app.send(Messages.StateAction(type));
-              }
-              const _mutation = encoder.encode(
-                options.mutationTransformer(mutation)
-              );
-              const _state = encoder.encode(options.transformer(state));
-              const _table = encoder.commit();
-              for (let key in _table) app.send(Messages.OTable(key, _table[key]));
-              app.send(Messages.Vuex(_mutation, _state));
-            } catch {
-              encoder.clear();
-            }
-          }
+          processMutationAndState(app, options, encoder, mutation, state);
         });
-        // pinia
-        if (store.$onAction) {
-          store.$onAction(({ name, store, args, after, onError }) => {
-            try {
-              let state = {}
-              state[store.$id] = store.$state
-              const mutation = { type: name, payload: JSON.parse(JSON.stringify(args)) }
-              if (options.filter(mutation, state)) {
-                const { type } = mutation;
-                if (typeof type === 'string' && type) {
-                  app.send(Messages.StateAction(type));
-                }
-                const _mutation = encoder.encode(options.mutationTransformer(mutation));
-                const _state = encoder.encode(options.transformer(state));
-                const _table = encoder.commit();
-                for (let key in _table) app.send(Messages.OTable(key, _table[key]));
-                app.send(Messages.Vuex(_mutation, _state));
-              }
-            }
-            catch (_a) {
-              encoder.clear();
-            }
-          });
-        }
+      }
+      // Pinia
+      if (store.$onAction) {
+        store.$onAction(({ name, store, args }) => {
+          try {
+            let state = {};
+            state[store.$id] = store.$state;
+            const mutation = {
+              type: name,
+              payload: JSON.parse(JSON.stringify(args))
+            };
+            processMutationAndState(app, options, encoder, mutation, state);
+          } catch {}
+        });
       }
     };
   };
