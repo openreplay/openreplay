bb21df8b5fafc9402e257d4eeed5b5e54953ba68
Kraiem Taha Yassine <tahayk2@gmail.com>
2022-07-08 17:46:58 +0200

    v1.7.0 hot fixes
    feat(api): fixed resources read time (#585)
    feat(api): changed resources read time delta

--- api/chalicelib/core/resources.py
+++ api/chalicelib/core/resources.py
@@ -4,7 +4,7 @@ from decouple import config
 
 def get_by_session_id(session_id, project_id, start_ts, duration):
     with pg_client.PostgresClient() as cur:
-        delta = config("events_ts_delta", cast=int, default=5 * 60) * 1000
+        delta = config("events_ts_delta", cast=int, default=60 * 60) * 1000
         ch_query = """\
                 SELECT
                       timestamp AS datetime,
@@ -22,9 +22,10 @@ def get_by_session_id(session_id, project_id, start_ts, duration):
                     AND project_id= %(project_id)s
                     AND sessions.start_ts=%(start_ts)s
                     AND resources.timestamp>=%(res_start_ts)s
-                    AND resources.timestamp>=%(res_end_ts)s;"""
+                    AND resources.timestamp<=%(res_end_ts)s;"""
         params = {"session_id": session_id, "project_id": project_id, "start_ts": start_ts, "duration": duration,
                   "res_start_ts": start_ts - delta, "res_end_ts": start_ts + duration + delta, }
+        print(cur.mogrify(ch_query, params))
         cur.execute(cur.mogrify(ch_query, params))
         rows = cur.fetchall()
         return helper.list_to_camel_case(rows)
