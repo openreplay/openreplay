9ab3895aca82368a0eb38314a21b1666d5c028f9
sylenien <nikita@openreplay.com>
2022-07-13 11:57:08 +0200

    feat(ui): refactor multiview a bit

--- frontend/app/components/Session_/Player/Controls/AssistTabs/AssistTabs.tsx
+++ frontend/app/components/Session_/Player/Controls/AssistTabs/AssistTabs.tsx
@@ -6,30 +6,31 @@ import AssistSessionsList from '../AssistSessionsModal';
 
 interface ITab {
   onClick?: () => void;
+  onDoubleClick?: () => void;
   classNames?: string;
   children: React.ReactNode;
 }
 
 const Tab = (props: ITab) => (
   <div 
-    onClick={props.onClick} 
-    className={cn("p-1 rounded w-13 flex items-center justify-center cursor-pointer", props.classNames)}>
+   onDoubleClick={props.onDoubleClick} 
+    className={cn("p-1 rounded flex items-center justify-center cursor-pointer", props.classNames)}>
     {props.children}
   </div>
 )
 
-const InactiveTab = (props: Omit<ITab, 'children'>) => (
+const InactiveTab = (props: Omit<ITab, 'children' | 'onDoubleClick'>) => (
   <Tab onClick={props.onClick} classNames="hover:bg-gray-bg bg-gray-light">
     <Icon name="plus" size="22" color="white" />
   </Tab>
 )
 const ActiveTab = (props: Omit<ITab, 'children'>) => (
-  <Tab onClick={props.onClick} classNames="hover:bg-teal bg-borderColor-primary">
+  <Tab onDoubleClick={props.onDoubleClick} classNames="hover:bg-teal bg-borderColor-primary">
     <Icon name="play-fill-new" size="22" color="white" />
   </Tab>
 )
-const CurrentTab = (props: Omit<ITab, 'children'>) => (
-  <Tab onClick={props.onClick} classNames="bg-teal color-white">
+const CurrentTab = (props: Omit<ITab, 'children' | 'onClick' | 'onDoubleClick'>) => (
+  <Tab classNames="bg-teal color-white">
     <span style={{ fontSize: '0.65rem' }}>PLAYING</span>
   </Tab>
 )
@@ -39,7 +40,7 @@ function AssistTabs() {
 
   const showAssistList = () =>  showModal(<AssistSessionsList />, { right: true });
   return (
-    <div className="grid grid-cols-2 w-28" style={{ gap: '4px' }}>
+    <div className="grid grid-cols-2 w-28 h-full" style={{ gap: '4px' }}>
       <CurrentTab />
       <ActiveTab />
       <InactiveTab onClick={showAssistList} />
