1fc51b8cba219e2f9b6bb65f0060939fa5031ea2
Shekar Siri <sshekarsiri@gmail.com>
2022-07-25 15:26:35 +0200

    change(ui) - preferences - removed old

--- frontend/app/components/Client/Integrations/IntegrationItem.js
+++ frontend/app/components/Client/Integrations/IntegrationItem.js
@@ -4,24 +4,31 @@ import { Icon } from 'UI';
 import stl from './integrationItem.module.css';
 
 const onDocLinkClick = (e, link) => {
-  e.stopPropagation();
-  window.open(link, '_blank');
-}
+    e.stopPropagation();
+    window.open(link, '_blank');
+};
 
 const IntegrationItem = ({
-  deleteHandler = null, icon, url = null, title = '', description = '', onClick = null, dockLink = '', integrated = false
+    deleteHandler = null,
+    icon,
+    url = null,
+    title = '',
+    description = '',
+    onClick = null,
+    dockLink = '',
+    integrated = false,
 }) => {
-  return (
-    <div className={ cn(stl.wrapper, 'mb-4', { [stl.integrated] : integrated })} onClick={ e => onClick(e, url) }>
-      {integrated && (
-        <div className="m-2 absolute right-0 top-0 h-4 w-4 rounded-full bg-teal flex items-center justify-center">
-          <Icon name="check" size="14" color="white" />
+    return (
+        <div className={cn(stl.wrapper, 'mb-4', { [stl.integrated]: integrated })} onClick={(e) => onClick(e, url)}>
+            {integrated && (
+                <div className="m-2 absolute right-0 top-0 h-4 w-4 rounded-full bg-teal flex items-center justify-center">
+                    <Icon name="check" size="14" color="white" />
+                </div>
+            )}
+            <img className="h-12 w-12" src={'/assets/' + icon + '.svg'} alt="integration" />
+            <h4 className="my-2">{title}</h4>
         </div>
-      )}
-      <img className="h-12 w-12" src={'/assets/' + icon + '.svg'} alt="integration" />
-      <h4 className="my-2">{ title }</h4>
-    </div>
-  )
+    );
 };
 
 export default IntegrationItem;
