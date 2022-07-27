bfe12d65f81e0940f1ed1a0ef7097f98ca307fbe
Shekar Siri <sshekarsiri@gmail.com>
2022-07-27 15:13:09 +0200

    change(ui) - preferences - metadata - move the delete button inside the modal

--- frontend/app/components/Client/CustomFields/CustomFields.js
+++ frontend/app/components/Client/CustomFields/CustomFields.js
@@ -11,120 +11,128 @@ import ListItem from './ListItem';
 import { confirm } from 'UI';
 import AnimatedSVG, { ICONS } from 'Shared/AnimatedSVG/AnimatedSVG';
 
-@connect(state => ({
-  fields: state.getIn(['customFields', 'list']).sortBy(i => i.index),
-  field: state.getIn(['customFields', 'instance']),
-  loading: state.getIn(['customFields', 'fetchRequest', 'loading']),
-  sites: state.getIn([ 'site', 'list' ]),
-  errors: state.getIn([ 'customFields', 'saveRequest', 'errors' ]),
-}), {
-  init,
-  fetchList,
-  save,
-  remove,
-})
+@connect(
+    (state) => ({
+        fields: state.getIn(['customFields', 'list']).sortBy((i) => i.index),
+        field: state.getIn(['customFields', 'instance']),
+        loading: state.getIn(['customFields', 'fetchRequest', 'loading']),
+        sites: state.getIn(['site', 'list']),
+        errors: state.getIn(['customFields', 'saveRequest', 'errors']),
+    }),
+    {
+        init,
+        fetchList,
+        save,
+        remove,
+    }
+)
 @withPageTitle('Metadata - OpenReplay Preferences')
 class CustomFields extends React.Component {
-  state = { showModal: false, currentSite: this.props.sites.get(0), deletingItem: null };
+    state = { showModal: false, currentSite: this.props.sites.get(0), deletingItem: null };
 
-  componentWillMount() {
-    const activeSite = this.props.sites.get(0);
-    if (!activeSite) return;
-    
-    this.props.fetchList(activeSite.id);
-  }
+    componentWillMount() {
+        const activeSite = this.props.sites.get(0);
+        if (!activeSite) return;
 
-  save = (field) => {
-    const { currentSite } = this.state;
-    this.props.save(currentSite.id, field).then(() => {
-      const { errors } = this.props;
-      if (!errors || errors.size === 0) {
-        return this.closeModal();
-      }
-    });
-  };
-
-  closeModal = () => this.setState({ showModal: false });
-  init = (field) => {
-    this.props.init(field);
-    this.setState({ showModal: true });    
-  }
-
-  onChangeSelect = ({ value }) => {
-    const site = this.props.sites.find(s => s.id === value.value);
-    this.setState({ currentSite: site })
-    this.props.fetchList(site.id);
-  }
-
-  removeMetadata = async (field) => {
-    if (await confirm({
-      header: 'Metadata',
-      confirmation: `Are you sure you want to remove?`
-    })) {
-      const { currentSite } = this.state;
-      this.setState({ deletingItem: field.index });
-      this.props.remove(currentSite.id, field.index)
-        .then(() => this.setState({ deletingItem: null }));
+        this.props.fetchList(activeSite.id);
     }
-  }
 
-  render() {
-    const { fields, field, loading } = this.props;
-    const { showModal, currentSite, deletingItem } = this.state;
-    return (
-      <div>
-        <SlideModal
-          title={ `${ (field.exists() ? 'Update' : 'Add') + ' Metadata Field' }` }
-          size="small"
-          isDisplayed={ showModal }
-          content={ showModal && <CustomFieldForm onClose={ this.closeModal } onSave={ this.save } /> }
-          onClose={ this.closeModal }
-        />
-        <div className={ styles.tabHeader }>
-          <h3 className={ cn(styles.tabTitle, "text-2xl") }>{ 'Metadata' }</h3>
-          <div style={{ marginRight: '15px' }}>
-            <SiteDropdown
-              value={ currentSite && currentSite.id }
-              onChange={ this.onChangeSelect }
-            />
-          </div>
-          <IconButton circle icon="plus" outline onClick={ () => this.init() } />
-          <TextLink
-            icon="book"
-            className="ml-auto color-gray-medium"
-            href="https://docs.openreplay.com/installation/metadata"
-            label="Documentation"
-          />
-        </div>
-        
-        <Loader loading={ loading }>
-          <NoContent
-            title={
-              <div className="flex flex-col items-center justify-center">
-                <AnimatedSVG name={ICONS.EMPTY_STATE} size="170" />
-                <div className="mt-6 text-2xl">No data available.</div>
-              </div>
+    save = (field) => {
+        const { currentSite } = this.state;
+        this.props.save(currentSite.id, field).then(() => {
+            const { errors } = this.props;
+            if (!errors || errors.size === 0) {
+                return this.closeModal();
             }
-            size="small"
-            show={ fields.size === 0 }
-            // animatedIcon="empty-state"
-          >
-            <div className={ styles.list }>
-              { fields.filter(i => i.index).map(field => (
-                <ListItem
-                  disabled={deletingItem && deletingItem === field.index}
-                  key={ field._key }
-                  field={ field }
-                  onEdit={ this.init }
-                  onDelete={ () => this.removeMetadata(field) }
+        });
+    };
+
+    closeModal = () => this.setState({ showModal: false });
+    init = (field) => {
+        this.props.init(field);
+        this.setState({ showModal: true });
+    };
+
+    onChangeSelect = ({ value }) => {
+        const site = this.props.sites.find((s) => s.id === value.value);
+        this.setState({ currentSite: site });
+        this.props.fetchList(site.id);
+    };
+
+    removeMetadata = async (field) => {
+        if (
+            await confirm({
+                header: 'Metadata',
+                confirmation: `Are you sure you want to remove?`,
+            })
+        ) {
+            const { currentSite } = this.state;
+            this.setState({ deletingItem: field.index });
+            this.props.remove(currentSite.id, field.index).then(() => {
+                this.setState({ deletingItem: null });
+                this.closeModal();
+            });
+        }
+    };
+
+    render() {
+        const { fields, field, loading } = this.props;
+        const { showModal, currentSite, deletingItem } = this.state;
+        return (
+            <div>
+                <SlideModal
+                    title={`${(field.exists() ? 'Update' : 'Add') + ' Metadata Field'}`}
+                    size="small"
+                    isDisplayed={showModal}
+                    content={
+                        showModal && <CustomFieldForm onClose={this.closeModal} onSave={this.save} onDelete={() => this.removeMetadata(field)} />
+                    }
+                    onClose={this.closeModal}
                 />
-              ))}
+                <div className={styles.tabHeader}>
+                    <h3 className={cn(styles.tabTitle, 'text-2xl')}>{'Metadata'}</h3>
+                    <div style={{ marginRight: '15px' }}>
+                        <SiteDropdown value={currentSite && currentSite.id} onChange={this.onChangeSelect} />
+                    </div>
+                    <IconButton circle icon="plus" outline onClick={() => this.init()} />
+                    <TextLink
+                        icon="book"
+                        className="ml-auto color-gray-medium"
+                        href="https://docs.openreplay.com/installation/metadata"
+                        label="Documentation"
+                    />
+                </div>
+
+                <Loader loading={loading}>
+                    <NoContent
+                        title={
+                            <div className="flex flex-col items-center justify-center">
+                                <AnimatedSVG name={ICONS.EMPTY_STATE} size="170" />
+                                <div className="mt-6 text-2xl">No data available.</div>
+                            </div>
+                        }
+                        size="small"
+                        show={fields.size === 0}
+                        // animatedIcon="empty-state"
+                    >
+                        <div className={styles.list}>
+                            {fields
+                                .filter((i) => i.index)
+                                .map((field) => (
+                                    <ListItem
+                                        disabled={deletingItem && deletingItem === field.index}
+                                        key={field._key}
+                                        field={field}
+                                        onEdit={this.init}
+                                        // onDelete={ () => this.removeMetadata(field) }
+                                    />
+                                ))}
+                        </div>
+                    </NoContent>
+                </Loader>
             </div>
-          </NoContent>
-        </Loader>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default CustomFields;
--- frontend/app/components/Client/CustomFields/ListItem.js
+++ frontend/app/components/Client/CustomFields/ListItem.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import cn from 'classnames'
+import cn from 'classnames';
 import { Icon } from 'UI';
 import styles from './listItem.module.css';
 
-const ListItem = ({ field, onEdit, onDelete, disabled }) => {
-  return (
-    <div className={ cn(styles.wrapper, field.index === 0 ? styles.preDefined : '', { [styles.disabled] : disabled} ) } onClick={ () => field.index != 0 && onEdit(field) } >
-      <span>{ field.key }</span>      
-      <div className={ styles.actions } data-hidden={ field.index === 0}>
-        <div className={ styles.button } onClick={ (e) => { e.stopPropagation(); onDelete(field) } }>
+const ListItem = ({ field, onEdit, disabled }) => {
+    return (
+        <div
+            className={cn(styles.wrapper, field.index === 0 ? styles.preDefined : '', { [styles.disabled]: disabled })}
+            onClick={() => field.index != 0 && onEdit(field)}
+        >
+            <span>{field.key}</span>
+            <div className={styles.actions} data-hidden={field.index === 0}>
+                {/* <div className={ styles.button } onClick={ (e) => { e.stopPropagation(); onDelete(field) } }>
           <Icon name="trash" color="teal" size="16" />
+        </div> */}
+                <div className={styles.button}>
+                    <Icon name="edit" color="teal" size="18" />
+                </div>
+            </div>
         </div>
-        <div className={ styles.button }>
-          <Icon name="edit" color="teal" size="18"/>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ListItem;
