57435e43f92974c939c14cdf3362b2415b72095d
Shekar Siri <sshekarsiri@gmail.com>
2022-08-10 11:34:25 +0200

    change(ui) - tags casing

--- frontend/app/components/ui/TagBadge/TagBadge.js
+++ frontend/app/components/ui/TagBadge/TagBadge.js
@@ -4,32 +4,28 @@ import styles from './tagBadge.module.css';
 import { Icon } from 'UI';
 
 export default class TagBadge extends React.PureComponent {
-
-  onClick = () => {
-    if (this.props.onClick) {
-      this.props.onClick(this.props.text);
-    }
-  }
-
-  render() {
-    const {
-      className, text, onRemove, onClick, hashed = true, outline = false,
-    } = this.props;
-    return (
-      <div
-        className={ cn(styles.badge, { "cursor-pointer": !!onClick }, className) }
-        onClick={ this.onClick }
-        data-hashed={ hashed }
-        data-outline={ outline }
-      >
-        <span>{ text }</span>
-        { onRemove &&
-          <button type="button" onClick={ onRemove }>
-            <Icon name="close" size="12" />
-            {/* <i className={ styles.closeIcon } /> */}
-          </button>
+    onClick = () => {
+        if (this.props.onClick) {
+            this.props.onClick(this.props.text);
         }
-      </div>
-    );
-  }
+    };
+
+    render() {
+        const { className, text, onRemove, onClick, hashed = true, outline = false } = this.props;
+        return (
+            <div
+                className={cn(styles.badge, { 'cursor-pointer': !!onClick }, className)}
+                onClick={this.onClick}
+                data-hashed={hashed}
+                data-outline={outline}
+            >
+                <span>{text}</span>
+                {onRemove && (
+                    <button type="button" onClick={onRemove}>
+                        <Icon name="close" size="12" />
+                    </button>
+                )}
+            </div>
+        );
+    }
 }
