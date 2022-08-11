ccaf869ce9b6036044c01f2bf819e1dfbbfad11a
Shekar Siri <sshekarsiri@gmail.com>
2022-08-11 10:50:36 +0200

    change(ui) - session list icons

--- frontend/app/components/shared/SessionListContainer/components/SessionList/SessionList.tsx
+++ frontend/app/components/shared/SessionListContainer/components/SessionList/SessionList.tsx
@@ -20,17 +20,38 @@ interface Props {
     updateCurrentPage: (page: number) => void;
     setScrollPosition: (scrollPosition: number) => void;
     fetchSessions: () => void;
+    activeTab: any;
+    isEnterprise?: boolean;
 }
 function SessionList(props: Props) {
-    const { loading, list, currentPage, total, filters, lastPlayedSessionId, metaList } = props;
+    const { loading, list, currentPage, total, filters, lastPlayedSessionId, metaList, activeTab, isEnterprise = false } = props;
     const _filterKeys = filters.map((i: any) => i.key);
     const hasUserFilter = _filterKeys.includes(FilterKey.USERID) || _filterKeys.includes(FilterKey.USERANONYMOUSID);
+    const isBookmark = activeTab.type === 'bookmark';
+    const isVault = isBookmark && isEnterprise;
+    const NO_CONTENT = React.useMemo(() => {
+        if (isBookmark && !isEnterprise) {
+            return {
+                icon: ICONS.NO_BOOKMARKS,
+                message: 'You have no bookmarks',
+            };
+        } else if (isVault) {
+            return {
+                icon: ICONS.NO_SESSIONS_IN_VAULT,
+                message: 'You have no sessions in your vault',
+            };
+        }
+        return {
+            icon: ICONS.NO_SESSIONS,
+            message: activeTab.type === 'all' ? 'You have no sessions' : 'You have no sessions with ' + activeTab.name,
+        };
+    }, [isBookmark, isVault, activeTab]);
 
     useEffect(() => {
         const { scrollY } = props;
         window.scrollTo(0, scrollY);
         if (total === 0) {
-            props.fetchSessions()
+            props.fetchSessions();
         }
 
         return () => {
@@ -51,9 +72,10 @@ function SessionList(props: Props) {
             <NoContent
                 title={
                     <div className="flex items-center justify-center flex-col">
-                        <AnimatedSVG name={ICONS.NO_RESULTS} size={170} />
+                        <AnimatedSVG name={NO_CONTENT.icon} size={170} />
                         <div className="mt-2" />
-                        <NoContentMessage />
+                        <div className="text-center text-gray-600">{NO_CONTENT.message}</div>
+                        {/* <NoContentMessage /> */}
                     </div>
                 }
                 subtext={<div>Please try changing your search parameters.</div>}
@@ -97,6 +119,8 @@ export default connect(
         currentPage: state.getIn(['search', 'currentPage']) || 1,
         total: state.getIn(['sessions', 'total']) || 0,
         scrollY: state.getIn(['search', 'scrollY']),
+        activeTab: state.getIn(['search', 'activeTab']),
+        isEnterprise: state.getIn(['user', 'account', 'edition']) === 'ee',
     }),
     { updateCurrentPage, addFilterByKeyAndValue, setScrollPosition, fetchSessions }
 )(SessionList);
