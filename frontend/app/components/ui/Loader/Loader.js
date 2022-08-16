646c5e1f95736ed52237f500d24566108353d313
Shekar Siri <sshekarsiri@gmail.com>
2022-08-16 15:46:39 +0200

    change(ui) - loader size

--- frontend/app/components/ui/Loader/Loader.js
+++ frontend/app/components/ui/Loader/Loader.js
@@ -3,12 +3,23 @@ import cn from 'classnames';
 import styles from './loader.module.css';
 import AnimatedSVG, { ICONS } from 'Shared/AnimatedSVG/AnimatedSVG';
 
-const Loader = React.memo(({ className = '', loading = true, children = null, size = 30, style = { minHeight: '150px' } }) => (!loading ? children :
-	<div className={ cn(styles.wrapper, className) } style={style}>
-	  {/* <div className={ styles.loader } data-size={ size } /> */}
-	  <AnimatedSVG name={ICONS.LOADER} size={size} />
-	</div>
-));
+const Loader = React.memo(
+  ({
+    className = '',
+    loading = true,
+    children = null,
+    size = 50,
+    style = { minHeight: '150px' },
+  }) =>
+    !loading ? (
+      children
+    ) : (
+      <div className={cn(styles.wrapper, className)} style={style}>
+        {/* <div className={ styles.loader } data-size={ size } /> */}
+        <AnimatedSVG name={ICONS.LOADER} size={size} />
+      </div>
+    )
+);
 
 Loader.displayName = 'Loader';
 
