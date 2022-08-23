e11adb154ec6934810bf34513edc0d9d2a92af82
Shekar Siri <sshekarsiri@gmail.com>
2022-08-23 17:47:50 +0200

    change(ui) - support icon position to left

--- frontend/app/components/shared/SupportCallout/SupportCallout.tsx
+++ frontend/app/components/shared/SupportCallout/SupportCallout.tsx
@@ -4,7 +4,7 @@ import { Popup } from 'UI';
 
 function SupportCallout() {
   return (
-    <a href="https://slack.openreplay.com" target="_blank" className="fixed z-50 right-0 bottom-0">
+    <a href="https://slack.openreplay.com" target="_blank" className="fixed z-50 left-0 bottom-0">
       <div className="w-12 h-12 cursor-pointer m-4">
         <Popup content="OpenReplay community" delay={0}>
           <img src={SlackIcon} />
