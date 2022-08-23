eadb738792a7231aea97917dd5dbefe95838213e
Shekar Siri <sshekarsiri@gmail.com>
2022-08-23 14:38:15 +0200

    fix(ui) - icon position

--- frontend/app/components/shared/SupportCallout/SupportCallout.tsx
+++ frontend/app/components/shared/SupportCallout/SupportCallout.tsx
@@ -4,8 +4,8 @@ import { Popup } from 'UI';
 
 function SupportCallout() {
   return (
-    <a href="https://slack.openreplay.com" target="_blank">
-      <div className="w-12 h-12 absolute z-50 cursor-pointer right-0 bottom-0 m-4">
+    <a href="https://slack.openreplay.com" target="_blank" className="fixed z-50 right-0 bottom-0">
+      <div className="w-12 h-12 cursor-pointer m-4">
         <Popup content="OpenReplay community" delay={0}>
           <img src={SlackIcon} />
         </Popup>
