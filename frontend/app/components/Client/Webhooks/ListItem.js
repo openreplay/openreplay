239c1bef42ccc151844e72061991d30041cf96de
Shekar Siri <sshekarsiri@gmail.com>
2022-08-24 17:20:07 +0200

    change(ui) - webhooks font size and styles

--- frontend/app/components/Client/Webhooks/ListItem.js
+++ frontend/app/components/Client/Webhooks/ListItem.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Icon } from 'UI';
-import styles from './listItem.module.css';
 import { Button } from 'UI';
 
 const ListItem = ({ webhook, onEdit, onDelete }) => {
@@ -8,7 +6,7 @@ const ListItem = ({ webhook, onEdit, onDelete }) => {
         <div className="border-t group hover:bg-active-blue flex items-center justify-between py-3 px-5 cursor-pointer" onClick={onEdit}>
             <div>
                 <span>{webhook.name}</span>
-                <div className={styles.endpoint}>{webhook.endpoint}</div>
+                <div className="color-gray-medium">{webhook.endpoint}</div>
             </div>
             <div className="invisible group-hover:visible">
                 <Button variant="text-primary" icon="pencil" />
