a6739a1f593d3d7aee574726f833db798c7de5f2
sylenien <nikita@openreplay.com>
2022-08-25 17:16:33 +0200

    fix(ui): fix tracker localstorage

--- tracker/tracker/src/main/app/index.ts
+++ tracker/tracker/src/main/app/index.ts
@@ -115,8 +115,8 @@ export default class App {
         verbose: false,
         __is_snippet: false,
         __debug_report_edp: null,
-        localStorage: null,
-        sessionStorage: null,
+        localStorage: window?.localStorage,
+        sessionStorage: window?.sessionStorage,
       },
       options,
     )
