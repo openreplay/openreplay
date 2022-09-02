348a323a413ed7ba933d289968ed854d07fca10c
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2022-09-02 22:28:57 +0200

    feat(backend): added extra checks for urlMethod and resourceType in CH connector

--- backend/pkg/url/method.go
+++ backend/pkg/url/method.go
@@ -1,11 +1,21 @@
 package url
 
 var METHODS = []string{"GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"}
+var TYPES = []string{"other", "script", "stylesheet", "fetch", "img", "media"}
 
 func EnsureMethod(method string) string {
 	for _, m := range METHODS {
 		if m == method {
 			return method
+		}
+	}
+	return ""
+}
+
+func EnsureType(tp string) string {
+	for _, t := range TYPES {
+		if t == tp {
+			return tp
 		}
 	}
 	return ""
