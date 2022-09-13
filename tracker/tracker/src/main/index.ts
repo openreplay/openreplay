eed4233013cb7ab54629de902bee7172e1ed51f6
sylenien <nikita@openreplay.com>
2022-09-13 11:32:35 +0200

    change(tracker): change enum export, remoe attrs from hidden containers

--- tracker/tracker/src/main/index.ts
+++ tracker/tracker/src/main/index.ts
@@ -4,6 +4,8 @@ export { default as App } from './app/index.js'
 import { UserID, UserAnonymousID, RawCustomEvent, CustomIssue } from './app/messages.gen.js'
 import * as _Messages from './app/messages.gen.js'
 export const Messages = _Messages
+import { SanitizeLevel as SanitizeEnum } from './app/sanitizer.js'
+export const SanitizeLevel = SanitizeEnum
 
 import Connection from './modules/connection.js'
 import Console from './modules/console.js'
