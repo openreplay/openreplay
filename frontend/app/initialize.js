7f048731d2709cc2602e81a289c00c4519e6e702
Shekar Siri <sshekarsiri@gmail.com>
2022-09-14 19:01:17 +0530

    fix(ui) - date picker

--- frontend/app/initialize.js
+++ frontend/app/initialize.js
@@ -2,7 +2,6 @@ import './styles/index.scss';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './init';
-import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import Router from './Router';
