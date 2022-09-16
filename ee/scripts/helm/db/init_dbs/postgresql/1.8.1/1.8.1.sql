8017a9cfede9fe2abade2bf7f51c7045d3fbcb63
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-09-16 15:36:40 +0100

    feat(db): changed predefined metric layout for Fetch calls with Errors

--- ee/scripts/helm/db/init_dbs/postgresql/1.8.1/1.8.1.sql
+++ ee/scripts/helm/db/init_dbs/postgresql/1.8.1/1.8.1.sql
@@ -5,4 +5,23 @@ $$
 SELECT 'v1.8.1-ee'
 $$ LANGUAGE sql IMMUTABLE;
 
+
+INSERT INTO metrics (name, category, default_config, is_predefined, is_template, is_public, predefined_key, metric_type,
+                     view_type)
+VALUES ('Fetch Calls with Errors', 'errors', '{
+  "col": 4,
+  "row": 2,
+  "position": 0
+}', true, true, true, 'calls_errors', 'predefined', 'table')
+ON CONFLICT (predefined_key) DO UPDATE
+    SET name=excluded.name,
+        category=excluded.category,
+        default_config=excluded.default_config,
+        is_predefined=excluded.is_predefined,
+        is_template=excluded.is_template,
+        is_public=excluded.is_public,
+        metric_type=excluded.metric_type,
+        view_type=excluded.view_type;
+
+
 COMMIT;
