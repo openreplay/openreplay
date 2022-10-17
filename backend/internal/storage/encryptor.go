e72b6c440f555961ce4b0f15425f32050dcd076f
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2022-10-17 11:33:06 +0200

    fix(backend): fixed filling last block algorithm and saving to db

--- backend/internal/storage/encryptor.go
+++ backend/internal/storage/encryptor.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
-	"encoding/hex"
 	"errors"
 	"fmt"
+	"log"
 	"math/rand"
 )
 
@@ -25,13 +25,18 @@ func generateRandomBytes(size int) []byte {
 }
 
 func fillLastBlock(rawText []byte, blockSize int) []byte {
+	padding := blockSize - len(rawText)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(rawText, padtext...)
 	// Add {1,0} right after the end of text
-	rawText = append(rawText, byte(1))
-	// Calculate the number of bytes we need to add to fill the last block of data
-	paddingSize := blockSize - len(rawText)%blockSize
-	// Generating zeros for last block
-	paddingText := bytes.Repeat([]byte{byte(0)}, paddingSize)
-	return append(rawText, paddingText...)
+	//rawText = append(rawText, byte(1))
+	//log.Println(rawText)
+	//// Calculate the number of bytes we need to add to fill the last block of data
+	//paddingSize := blockSize - len(rawText)%blockSize
+	//// Generating zeros for last block
+	//paddingText := bytes.Repeat([]byte{byte(0)}, paddingSize)
+	//log.Println(paddingText)
+	//return append(rawText, paddingText...)
 }
 
 func encryptData(data, fullKey []byte) ([]byte, error) {
@@ -41,6 +46,7 @@ func encryptData(data, fullKey []byte) ([]byte, error) {
 	key, iv := fullKey[:16], fullKey[16:]
 	// Fill the last block of data by zeros
 	paddedData := fillLastBlock(data, aes.BlockSize)
+	log.Println(paddedData)
 	// Create new AES cipher with CBC encryptor
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -51,5 +57,20 @@ func encryptData(data, fullKey []byte) ([]byte, error) {
 	ciphertext := make([]byte, len(paddedData))
 	mode.CryptBlocks(ciphertext, paddedData)
 	// Return encrypted data
-	return []byte(hex.EncodeToString(ciphertext)), nil
+	return ciphertext, nil //[]byte(hex.EncodeToString(ciphertext)), nil
+}
+
+func decryptData(data, fullKey []byte) ([]byte, error) {
+	if len(fullKey) != 32 {
+		return nil, errors.New("wrong format of encryption key")
+	}
+	key, iv := fullKey[:16], fullKey[16:]
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("cbc encryptor failed: %s", err)
+	}
+	cbc := cipher.NewCBCDecrypter(block, iv)
+	res := make([]byte, len(data))
+	cbc.CryptBlocks(res, data)
+	return res, nil
 }
