d95597ce8638c2cffc58575e3abfd26c8b031d9e
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-10-28 17:11:25 +0200

    feat(chalice): debug sourcemaps.pye

--- api/chalicelib/core/sourcemaps.py
+++ api/chalicelib/core/sourcemaps.py
@@ -148,8 +148,13 @@ MAX_COLUMN_OFFSET = 60
 def fetch_missed_contexts(frames):
     source_cache = {}
     for i in range(len(frames)):
-        if len(frames[i]["context"]) > 0:
-            continue
+        try:
+            if len(frames[i]["context"]) > 0:
+                continue
+        except Exception:
+            print(">>>>>>>> fetch_missed_contexts exception:")
+            print(frames)
+            print("------------------")
         file_abs_path = frames[i]["frame"]["absPath"]
         if file_abs_path in source_cache:
             file = source_cache[file_abs_path]
