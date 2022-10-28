ac4fb637f8434e179d8193a2c8389910e4482408
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-10-28 17:51:55 +0200

    feat(chalice): changed create projects method

--- api/chalicelib/core/sourcemaps.py
+++ api/chalicelib/core/sourcemaps.py
@@ -148,14 +148,11 @@ MAX_COLUMN_OFFSET = 60
 def fetch_missed_contexts(frames):
     source_cache = {}
     for i in range(len(frames)):
-        try:
-            if len(frames[i]["context"]) > 0:
-                continue
-        except Exception:
-            print(">>>>>>>> fetch_missed_contexts exception:")
-            print(frames)
-            print("------------------")
-            raise
+        print(">>>>>>>> fetch_missed_contexts exception:")
+        print(frames[i])
+        print("------------------")
+        if len(frames[i]["context"]) > 0:
+            continue
         file_abs_path = frames[i]["frame"]["absPath"]
         if file_abs_path in source_cache:
             file = source_cache[file_abs_path]
