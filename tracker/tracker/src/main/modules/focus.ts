8a12f8ef39fa28b8b791a1a0a74c3d3e4959e334
Alex Kaminskii <alex@openreplay.com>
2022-10-31 14:36:58 +0100

    fix(tracker): maintain focus inside iframes

--- tracker/tracker/src/main/modules/focus.ts
+++ tracker/tracker/src/main/modules/focus.ts
@@ -1,43 +1,45 @@
 import type App from '../app/index.js'
-import { isNode } from '../app/guards.js'
-import { SetNodeFocus } from "../app/messages.gen";
+import { isNode, hasTag } from '../app/guards.js'
+import { SetNodeFocus } from '../app/messages.gen.js'
 
 export default function (app: App): void {
-    function findFocusedElement(): Element | null {
-        let focused_element = null
-        if (
-            document.hasFocus() &&
-            document.activeElement !== document.body &&
-            document.activeElement !== document.documentElement
-        ) {
-            focused_element = document.activeElement
-        }
-        return focused_element
+  function sendSetNodeFocus(n: Node) {
+    const id = app.nodes.getID(n)
+    if (id !== undefined) {
+      app.send(SetNodeFocus(id))
     }
+  }
 
-    function sendSetNodeFocus(n: Node) {
-        const id = app.nodes.getID(n)
-        if (id !== undefined) {
-            app.send(SetNodeFocus(id))
-        }
+  let blurred = false
+  app.nodes.attachNodeCallback((node) => {
+    if (!hasTag(node, 'BODY')) {
+      return
     }
-
-    app.attachEventListener(document, 'focus', (e: Event): void => {
-        if (isNode(e.target)) {
-            sendSetNodeFocus(e.target)
-        }
+    app.nodes.attachNodeListener(node, 'focus', (e: FocusEvent): void => {
+      if (!isNode(e.target)) {
+        return
+      }
+      sendSetNodeFocus(e.target)
+      blurred = false
     })
-
-    app.attachEventListener(document, 'blur', (): void => {
-        if (findFocusedElement() === null) {
+    app.nodes.attachNodeListener(node, 'blur', (e: FocusEvent): void => {
+      if (e.relatedTarget === null) {
+        blurred = true
+        setTimeout(() => {
+          if (blurred) {
             app.send(SetNodeFocus(-1))
-        }
+          }
+        }, 0)
+      }
     })
-
-    app.attachStartCallback(() => {
-        let elem = findFocusedElement()
-        if (elem !== null) {
-            sendSetNodeFocus(elem)
-        }
-    })
+  })
+  app.attachStartCallback(() => {
+    let elem = document.activeElement
+    while (elem && hasTag(elem, 'IFRAME') && elem.contentDocument) {
+      elem = elem.contentDocument.activeElement
+    }
+    if (elem && elem !== document.body) {
+      sendSetNodeFocus(elem)
+    }
+  }, true)
 }
