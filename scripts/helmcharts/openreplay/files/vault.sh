dd8ddbe5ecf9461229b78f168cd9cc3e844d9e6f
rjshrjndrn <rjshrjndrn@gmail.com>
2022-11-03 13:28:24 +0100

    fix(helm): Use vault local service token
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- scripts/helmcharts/openreplay/files/vault.sh
+++ scripts/helmcharts/openreplay/files/vault.sh
@@ -50,7 +50,6 @@ vault write database/config/postgres \
 vault auth enable kubernetes
 
 vault write auth/kubernetes/config \
-       token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
        kubernetes_host=https://${KUBERNETES_PORT_443_TCP_ADDR}:443 \
        kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 
