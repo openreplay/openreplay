06e61b95174cac889599eb6acd5e605de8fcb10f
Shekar Siri <sshekarsiri@gmail.com>
2022-11-08 10:36:52 +0100

    change(ui) - check boarding completion 75%

--- frontend/app/components/Header/Header.js
+++ frontend/app/components/Header/Header.js
@@ -60,7 +60,7 @@ const Header = (props) => {
       {!isPreferences && <DefaultMenuView siteId={siteId} />}
       {isPreferences && <PreferencesView />}
       <div className={styles.right}>
-        {boardingCompletion < 100 && !hideDiscover && (
+        {boardingCompletion < 75 && !hideDiscover && (
           <React.Fragment>
             <OnboardingExplore onComplete={() => setHideDiscover(true)} />
           </React.Fragment>
