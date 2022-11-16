40554266104ad7c08d6e998eaca4a093552974bc
Taha Yassine Kraiem <tahayk2@gmail.com>
2022-11-16 17:20:26 +0100

    feat(chalice): changed records file_key

--- ee/api/chalicelib/core/assist_records.py
+++ ee/api/chalicelib/core/assist_records.py
@@ -9,15 +9,13 @@ from chalicelib.utils.TimeUTC import TimeUTC
 def presign_records(project_id, data: schemas_ee.AssistRecordPayloadSchema, context: schemas_ee.CurrentContext):
     params = {"user_id": context.user_id, "project_id": project_id, **data.dict()}
 
-    key = f"{TimeUTC.now()}-{data.name}"
-    presigned_url = s3.get_presigned_url_for_upload(bucket=config('ASSIST_RECORDS_BUCKET'), expires_in=1800,
-                                                    key=s3.generate_file_key(project_id=project_id, key=key))
+    key = s3.generate_file_key(project_id=project_id, key=f"{TimeUTC.now()}-{data.name}")
+    presigned_url = s3.get_presigned_url_for_upload(bucket=config('ASSIST_RECORDS_BUCKET'), expires_in=1800, key=key)
     params["key"] = key
     with pg_client.PostgresClient() as cur:
         query = cur.mogrify(f"""INSERT INTO assist_records(project_id, user_id, name, file_key, duration, session_id)
                                 VALUES (%(project_id)s, %(user_id)s, %(name)s, %(key)s, 
-                                        %(duration)s, %(session_id)s);""",
-                            params)
+                                        %(duration)s, %(session_id)s);""", params)
         cur.execute(query)
     return presigned_url
 
