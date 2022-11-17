91709ac90981916cdcbc61a93852ffb34348417a
Alexander <zavorotynskiy@pm.me>
2022-11-17 16:15:25 +0100

    [Sink] splitting mob files without folders (#824)
    * feat(backend): split mob file into 2 without folders

--- backend/internal/storage/storage.go
+++ backend/internal/storage/storage.go
@@ -13,7 +13,6 @@ import (
 	"openreplay/backend/pkg/storage"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -71,42 +70,45 @@ func New(cfg *config.Config, s3 *storage.S3, metrics *monitoring.Metrics) (*Stor
 }
 
 func (s *Storage) UploadSessionFiles(msg *messages.SessionEnd) error {
-	sessionDir := strconv.FormatUint(msg.SessionID(), 10)
-	if err := s.uploadKey(msg.SessionID(), sessionDir+"/dom.mob", true, 5, msg.EncryptionKey); err != nil {
-		oldErr := s.uploadKey(msg.SessionID(), sessionDir, true, 5, msg.EncryptionKey)
-		if oldErr != nil {
-			return fmt.Errorf("upload file error: %s. failed checking mob file using old path: %s", err, oldErr)
-		}
-		// Exit method anyway because we don't have dev tools separation in prev version
-		return nil
-	}
-	if err := s.uploadKey(msg.SessionID(), sessionDir+"/devtools.mob", false, 4, msg.EncryptionKey); err != nil {
+	if err := s.uploadKey(msg.SessionID(), "/dom.mob", true, 5, msg.EncryptionKey); err != nil {
 		return err
+	}
+	if err := s.uploadKey(msg.SessionID(), "/devtools.mob", false, 4, msg.EncryptionKey); err != nil {
+		log.Printf("can't find devtools for session: %d, err: %s", msg.SessionID(), err)
 	}
 	return nil
 }
 
-// TODO: make a bit cleaner
-func (s *Storage) uploadKey(sessID uint64, key string, shouldSplit bool, retryCount int, encryptionKey string) error {
+// TODO: make a bit cleaner.
+// TODO: Of course, I'll do!
+func (s *Storage) uploadKey(sessID uint64, suffix string, shouldSplit bool, retryCount int, encryptionKey string) error {
 	if retryCount <= 0 {
 		return nil
 	}
-
 	start := time.Now()
-	file, err := os.Open(s.cfg.FSDir + "/" + key)
+	fileName := strconv.FormatUint(sessID, 10)
+	mobFileName := fileName
+	if suffix == "/devtools.mob" {
+		mobFileName += "devtools"
+	}
+	filePath := s.cfg.FSDir + "/" + mobFileName
+
+	// Check file size before download into memory
+	info, err := os.Stat(filePath)
+	if err != nil {
+		if info.Size() > s.cfg.MaxFileSize {
+			log.Printf("big file, size: %d, session: %d", info.Size(), sessID)
+			return nil
+		}
+	}
+	file, err := os.Open(filePath)
 	if err != nil {
 		return fmt.Errorf("File open error: %v; sessID: %s, part: %d, sessStart: %s\n",
-			err, key, sessID%16,
+			err, fileName, sessID%16,
 			time.UnixMilli(int64(flakeid.ExtractTimestamp(sessID))),
 		)
 	}
 	defer file.Close()
-
-	// Ignore "s" at the end of mob file name for "old" sessions
-	newVers := false
-	if strings.Contains(key, "/") {
-		newVers = true
-	}
 
 	var fileSize int64 = 0
 	fileInfo, err := file.Stat()
@@ -117,17 +119,18 @@ func (s *Storage) uploadKey(sessID uint64, key string, shouldSplit bool, retryCo
 	}
 
 	var encryptedData []byte
+	fileName += suffix
 	if shouldSplit {
 		nRead, err := file.Read(s.startBytes)
 		if err != nil {
 			log.Printf("File read error: %s; sessID: %s, part: %d, sessStart: %s",
 				err,
-				key,
+				fileName,
 				sessID%16,
 				time.UnixMilli(int64(flakeid.ExtractTimestamp(sessID))),
 			)
 			time.AfterFunc(s.cfg.RetryTimeout, func() {
-				s.uploadKey(sessID, key, shouldSplit, retryCount-1, encryptionKey)
+				s.uploadKey(sessID, suffix, shouldSplit, retryCount-1, encryptionKey)
 			})
 			return nil
 		}
@@ -146,11 +149,7 @@ func (s *Storage) uploadKey(sessID uint64, key string, shouldSplit bool, retryCo
 		}
 		// Compress and save to s3
 		startReader := bytes.NewBuffer(encryptedData)
-		startKey := key
-		if newVers {
-			startKey += "s"
-		}
-		if err := s.s3.Upload(s.gzipFile(startReader), startKey, "application/octet-stream", true); err != nil {
+		if err := s.s3.Upload(s.gzipFile(startReader), fileName+"s", "application/octet-stream", true); err != nil {
 			log.Fatalf("Storage: start upload failed.  %v\n", err)
 		}
 		// TODO: fix possible error (if we read less then FileSplitSize)
@@ -161,7 +160,7 @@ func (s *Storage) uploadKey(sessID uint64, key string, shouldSplit bool, retryCo
 			if err != nil {
 				log.Printf("File read error: %s; sessID: %s, part: %d, sessStart: %s",
 					err,
-					key,
+					fileName,
 					sessID%16,
 					time.UnixMilli(int64(flakeid.ExtractTimestamp(sessID))),
 				)
@@ -183,7 +182,7 @@ func (s *Storage) uploadKey(sessID uint64, key string, shouldSplit bool, retryCo
 			}
 			// Compress and save to s3
 			endReader := bytes.NewBuffer(encryptedData)
-			if err := s.s3.Upload(s.gzipFile(endReader), key+"e", "application/octet-stream", true); err != nil {
+			if err := s.s3.Upload(s.gzipFile(endReader), fileName+"e", "application/octet-stream", true); err != nil {
 				log.Fatalf("Storage: end upload failed. %v\n", err)
 			}
 		}
@@ -195,7 +194,7 @@ func (s *Storage) uploadKey(sessID uint64, key string, shouldSplit bool, retryCo
 		if err != nil {
 			log.Printf("File read error: %s; sessID: %s, part: %d, sessStart: %s",
 				err,
-				key,
+				fileName,
 				sessID%16,
 				time.UnixMilli(int64(flakeid.ExtractTimestamp(sessID))),
 			)
@@ -216,7 +215,7 @@ func (s *Storage) uploadKey(sessID uint64, key string, shouldSplit bool, retryCo
 			encryptedData = fileData
 		}
 		endReader := bytes.NewBuffer(encryptedData)
-		if err := s.s3.Upload(s.gzipFile(endReader), key+"s", "application/octet-stream", true); err != nil {
+		if err := s.s3.Upload(s.gzipFile(endReader), fileName, "application/octet-stream", true); err != nil {
 			log.Fatalf("Storage: end upload failed. %v\n", err)
 		}
 		s.archivingTime.Record(context.Background(), float64(time.Now().Sub(start).Milliseconds()))
