fe2c514a0cd4bd52c6c5985de80b27a90506cce0
MauricioGarciaS <47052044+MauricioGarciaS@users.noreply.github.com>
2022-11-21 18:45:39 +0100

    Solved SignalSchema not subscriptable

--- ee/api/chalicelib/core/signals.py
+++ ee/api/chalicelib/core/signals.py
@@ -9,8 +9,8 @@ def handle_frontend_signals(project_id: int, user_id: str, data: schemas_ee.Sign
     res = {'errors': 'query not executed'}
     insights_query = """INSERT INTO public.frontend_signals VALUES (%(project_id)s, %(user_id)s, %(timestamp)s, %(action)s, %(source)s, %(category)s, %(data)s)"""
     with pg_client.PostgresClient() as conn:
-        query = conn.mogrify(insights_query, {'project_id': project_id, 'user_id': user_id, 'timestamp': data['timestamp'], 'source': data['source'],
-                                      'category': data['category'], 'data': json.dumps(data['data'])})
+        query = conn.mogrify(insights_query, {'project_id': project_id, 'user_id': user_id, 'timestamp': data.timestamp, 'source': data.source,
+                                      'category': data.category, 'data': json.dumps(data.data)})
         conn.execute(query)
         res = helper.dict_to_camel_case(conn.fetchone())
     return res
