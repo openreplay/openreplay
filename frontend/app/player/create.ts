af6fd085e208f09a5183d3306421d6d45698adad
Alex Kaminskii <alex@openreplay.com>
2022-11-22 17:31:08 +0100

    fix(player): state type on creation

--- frontend/app/player/create.ts
+++ frontend/app/player/create.ts
@@ -1,8 +1,9 @@
 import SimpleStore from './_common/SimpleStore'
 import type { Store } from './player/types'
 
-import WebPlayer, { State as WebState} from './_web/WebPlayer'
+import WebPlayer from './_web/WebPlayer'
 
+type WebState = typeof WebPlayer.INITIAL_STATE //?
 type WebPlayerStore = Store<WebState>
 export type IWebPlayer = WebPlayer
 export type IWebPlayerStore = WebPlayerStore
