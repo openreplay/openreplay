93f65211a59b42fb061ac4a1ed3edb30d2651d86
sylenien <nikita@openreplay.com>
2022-11-22 14:27:25 +0100

    refactor(ui/player): fix store type for context

--- frontend/app/components/Session/playerContext.ts
+++ frontend/app/components/Session/playerContext.ts
@@ -1,12 +1,12 @@
 import { createContext } from 'react';
 import {
   IWebPlayer,
-  IStore
+  IWebPlayerStore
 } from 'Player'
 
 export interface IPlayerContext {
   player: IWebPlayer
-  store: IStore,
+  store: IWebPlayerStore,
 }
 export const defaultContextValue: IPlayerContext = { player: undefined, store: undefined}
 export const PlayerContext = createContext<IPlayerContext>(defaultContextValue);
