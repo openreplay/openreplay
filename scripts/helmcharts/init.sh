69f1d84ac3c4c3476b4173a6e3cb1cbb695c85bf
rjshrjndrn <rjshrjndrn@gmail.com>
2022-11-25 10:47:04 +0100

    chore(init): random secret for assist jwt secret
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- scripts/helmcharts/init.sh
+++ scripts/helmcharts/init.sh
@@ -108,6 +108,7 @@ sed_i_wrapper -i "s/accessKey: \"changeMeMinioAccessKey\"/accessKey: \"$(randomP
 sed_i_wrapper -i "s/secretKey: \"changeMeMinioPassword\"/secretKey: \"$(randomPass)\"/g" vars.yaml
 sed_i_wrapper -i "s/jwt_secret: \"SetARandomStringHere\"/jwt_secret: \"$(randomPass)\"/g" vars.yaml
 sed_i_wrapper -i "s/assistKey: \"SetARandomStringHere\"/assistKey: \"$(randomPass)\"/g" vars.yaml
+sed_i_wrapper -i "s/assistJWTSecret: \"SetARandomStringHere\"/assistJWTSecret: \"$(randomPass)\"/g" vars.yaml
 sed_i_wrapper -i "s/domainName: \"\"/domainName: \"${DOMAIN_NAME}\"/g" vars.yaml
 
 info "Setting proper permission for shared folder"
