ed6747d475b2ef94b195e0d601e0b081c7168a0e
Shekar Siri <sshekarsiri@gmail.com>
2022-12-12 18:55:43 +0530

    rebase dev

---
+++ frontend/app/components/Dashboard/components/MetricTypeItem/MetricTypeItem.tsx
@@ -0,0 +1,33 @@
+import { IconNames } from 'App/components/ui/SVG';
+import React from 'react';
+import { Icon } from 'UI';
+
+export interface MetricType {
+  title: string;
+  icon: IconNames;
+  description: string;
+  slug: string;
+}
+
+interface Props {
+  metric: MetricType;
+}
+
+function MetricTypeItem(props: Props) {
+  const {
+    metric: { title, icon, description, slug },
+  } = props;
+  return (
+    <div className="flex items-start p-4 hover:bg-active-blue cursor-pointer group hover-color-teal">
+      <div className="pr-4 pt-1">
+        <Icon name={icon} size="20" />
+      </div>
+      <div className="flex flex-col items-start text-left">
+        <div className="text-base group-hover:color-teal">{title}</div>
+        <div className="text-xs">{description}</div>
+      </div>
+    </div>
+  );
+}
+
+export default MetricTypeItem;
