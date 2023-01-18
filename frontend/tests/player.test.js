ed84fc90aeb90029d1c05b8562fbabb2fb05b54e
nick-delirium <nikita@openreplay.com>
2023-01-18 19:55:25 +0100

    change(ui): add CICD testing command

--- frontend/tests/player.test.js
+++
@@ -1,8 +0,0 @@
-import WebPlayer from 'App/player/web/WebPlayer';
-import SimpleStore from 'App/player/common/SimpleStore'
-
-let store = new SimpleStore({
-  ...WebPlayer.INITIAL_STATE,
-})
-
-const player = new WebPlayer(store, session, false)
