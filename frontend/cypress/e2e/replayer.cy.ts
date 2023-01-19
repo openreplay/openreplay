958797e8de0f620f7c9f8bec3dd739da63a8ba0f
nick-delirium <nikita@openreplay.com>
2023-01-19 16:27:06 +0100

    change(ui): fix env

--- frontend/cypress/e2e/replayer.cy.ts
+++ frontend/cypress/e2e/replayer.cy.ts
@@ -14,6 +14,7 @@ describe('Replayer visual match test', {
     cy.get(':nth-child(1) > .relative > .p-2').type(Cypress.env('account'))
     cy.get(':nth-child(2) > .relative > .p-2').type(Cypress.env('password'))
     cy.get('.h-10').click()
+    cy.wait(10000)
     cy.wait('@getAccount')
     cy.visit('3/session/7585361734083637?jumpto=5000&freeze=true')
     cy.wait(3000)
