0021819b9bc344cc7f41d022c6f455a59ff1b303
Alex Kaminskii <alex@openreplay.com>
2023-01-20 14:30:48 +0100

    feat(tracker): webworker unit tests

---
+++ tracker/tracker/src/webworker/BatchWriter.unit.test.ts
@@ -0,0 +1,6 @@
+import { describe, expect, test, jest, beforeEach, afterEach } from '@jest/globals'
+import BatchWriter from './BatchWriter.js'
+
+describe('BatchWriter', () => {
+  test('', () => {})
+})
