43272a0197ab614d886c0698e6d8da9c5d1cedff
rjshrjndrn <rjshrjndrn@gmail.com>
2023-01-23 15:30:05 +0100

    chore(init): More info on cli
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- scripts/helmcharts/init.sh
+++ scripts/helmcharts/init.sh
@@ -5,6 +5,7 @@ set -e
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
+BWHITE='\033[1;37m'
 NC='\033[0m' # No Color
 # --- helper functions for logs ---
 info()
@@ -180,3 +181,4 @@ function main() {
 main
 
 info "Configuration file is saved in /var/lib/openreplay/vars.yaml"
+info "Run ${BWHITE}openreplay -h${GREEN} to see the cli information to manage OpenReplay"
