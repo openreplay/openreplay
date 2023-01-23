8dda967a5f8740fda3d960fb7090d06b32f4cc0d
Alex Kaminskii <alex@openreplay.com>
2023-01-23 17:13:52 +0100

    fix(tracker): no empty tests

--- tracker/tracker/src/webworker/BatchWriter.unit.test.ts
+++
@@ -1,6 +0,0 @@
-import { describe, expect, test, jest, beforeEach, afterEach } from '@jest/globals'
-import BatchWriter from './BatchWriter.js'
-
-describe('BatchWriter', () => {
-  test('', () => {})
-})
