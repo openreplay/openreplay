b88a12abde856a9f27efcd04cda9b8a88698feda
Delirium <nikita@openreplay.com>
2023-02-03 16:58:08 +0100

    feat(ui/tracker): create integrated e2e testing for tracker and player
    * fix(ui): fix create note modal
    
    * change(ui): remove tests for a moment
    
    * change(ui): fix job name
    
    * change(ui): fix job action
    
    * change(ui): fix job action
    
    * change(ui): fix job action
    
    * change(ui): fix job action
    
    * change(ui): add testing public dir
    
    * change(ui): return silent server start
    
    * change(ui): fix session url
    
    * change(ui): fix session url
    
    * change(ui): fix session url
    
    * change(ui): fix mockup
    
    * change(ui): fix mockup
    
    * change(ui): fix snapshot images
    
    * change(ui): fix snapshot images
    
    * change(ui): fix testing app
    
    * change(ui): fix testing app
    
    * change(ui): fix testing flow and add loggs
    
    * change(ui): add network and events
    
    * change(ui): fix table view
    
    * change(ui): fix table view
    
    * change(ui): fix acc req intercept
    
    * change(ui): fix mob req intercept
    
    * change(ui): change jump method

--- frontend/cypress/e2e/replayer.cy.ts
+++ frontend/cypress/e2e/replayer.cy.ts
@@ -1,44 +1,150 @@
-describe('Replayer visual match test', {
-  viewportHeight: 900,
-  viewportWidth: 1400,
-}, () => {
-  it('Checking Replayer at breakpoints, user events and console', () => {
-    cy.intercept('**/api/account').as('getAccount')
-    cy.intercept('**/mobs/7585361734083637/dom.mobs?*').as('getFirstMob')
-    cy.intercept('**/mobs/7585361734083637/dom.mobe?*').as('getSecondMob')
+const SECOND = 1000;
 
-    cy.visit('/', {
-      onBeforeLoad: function (window) {
-        window.localStorage.setItem('notesFeatureViewed', 'true');
-      }
+describe(
+  'Replayer visual match test',
+  {
+    viewportHeight: 900,
+    viewportWidth: 1400,
+  },
+  () => {
+    it('Generating tracker session...', () => {
+      cy.intercept('**/api/account').as('getAccount');
+      cy.intercept('**/dom.mobs?*').as('getFirstMob')
+
+      cy.visit('http://0.0.0.0:3333', {
+        onBeforeLoad: function (window) {
+          window.localStorage.setItem('notesFeatureViewed', 'true');
+        },
+      });
+
+      cy.origin('http://localhost:3000/', { args: { SECOND } }, ({ SECOND }) => {
+        cy.visit('/');
+        cy.wait(SECOND * 3);
+        cy.get('#get-table').click()
+        cy.wait(SECOND * 3);
+        cy.get('#testrender').click();
+        cy.wait(SECOND * 3);
+        cy.get('#testrender').click();
+        cy.get('#get-main').click()
+
+        cy.get('#obscured-text').type('testing typing in obscured input');
+        cy.get('#visible-input').type('testing typing in visible input');
+        cy.wait(SECOND * 3);
+
+        cy.get('#redcounter').click().click().click();
+        cy.get('#test-api').click().click();
+        cy.get('#test-event').click().click();
+        cy.wait(SECOND * 3);
+
+        cy.log('finished generating a session')
+
+        cy.window().then((window) => {
+          cy.task('setValue', { key: 'url', value: window.__OPENREPLAY__.app.getSessionURL() });
+          cy.log(window.__OPENREPLAY__.app.getSessionURL());
+          cy.wait(SECOND * 3)
+        });
+      });
+
+
+      cy.task('getValue', 'url').as('firstAlias');
+
+      cy.visit('/');
+      cy.get('[data-test-id=login]').type(Cypress.env('account').replaceAll('"', ''));
+      cy.get('[data-test-id=password]').type(Cypress.env('password').replaceAll('"', ''));
+      cy.get('[data-test-id=log-button]').click();
+      cy.wait('@getAccount');
+      // checking real session
+      cy.get('@firstAlias').then((firstAlias) => {
+        cy.log(firstAlias);
+        cy.log('waiting for session to save')
+        cy.wait(SECOND * 180);
+        cy.visit(firstAlias.slice(27) + '?freeze=true');
+        cy.log('loading session')
+        cy.wait(SECOND * 25);
+
+        cy.window().then(win => {
+          const jumpMethod = win.playerJump ? win.playerJump : win.playerJumpToTime
+          jumpMethod(SECOND * 3)
+        })
+        cy.wait(SECOND * 3);
+        cy.matchImageSnapshot('Tracker-3');
+
+        cy.window().then(win => {
+          win.playerJump(SECOND * 6)
+        })
+        cy.wait(SECOND * 3);
+        cy.matchImageSnapshot('Tracker-5');
+
+        cy.window().then(win => {
+          const jumpMethod = win.playerJump ? win.playerJump : win.playerJumpToTime
+          jumpMethod(SECOND * 9)
+        })
+        cy.wait(SECOND * 3);
+        cy.matchImageSnapshot('Tracker-9');
+
+        cy.window().then(win => {
+          const jumpMethod = win.playerJump ? win.playerJump : win.playerJumpToTime
+          jumpMethod(SECOND * 20)
+        })
+        cy.wait(SECOND * 3);
+        cy.get('#control-button-redux > .controlButton-module__label--YznMl').click()
+        cy.wait(SECOND * 0.5)
+        cy.matchImageSnapshot('Tracker-19-redux');
+
+        cy.get('#control-button-network').click()
+        cy.wait(SECOND * 0.5)
+        cy.matchImageSnapshot('Tracker-19-network');
+
+        cy.get('#control-button-events').click()
+        cy.wait(SECOND * 0.5)
+        cy.matchImageSnapshot('Tracker-19-events');
+
+        cy.log('custom session test success')
+      });
+    });
+
+    it('Checking Replayer at breakpoints, user events and console', () => {
+      cy.intercept('**/api/account').as('getAccount')
+      cy.intercept('**/mobs/7585361734083637/dom.mobs?*').as('getFirstMob')
+      cy.intercept('**/mobs/7585361734083637/dom.mobe?*').as('getSecondMob')
+      cy.log('testing premade session')
+
+      cy.visit('http://0.0.0.0:3333', {
+        onBeforeLoad: function (window) {
+          window.localStorage.setItem('notesFeatureViewed', 'true');
+        }
+      })
+      cy.get('[data-test-id=login]').type(Cypress.env('account').replaceAll('"', ''));
+      cy.get('[data-test-id=password]').type(Cypress.env('password').replaceAll('"', ''));
+      cy.get('[data-test-id=log-button]').click();
+      cy.wait('@getAccount')
+      cy.wait(SECOND * 2)
+      cy.visit('3/session/7585361734083637?jumpto=5000&freeze=true')
+      cy.wait('@getFirstMob')
+      cy.wait('@getSecondMob')
+      cy.wait(SECOND * 2)
+
+      cy.window().then(win => {
+        const jumpMethod = win.playerJump ? win.playerJump : win.playerJumpToTime
+        jumpMethod(SECOND * 5)
+      })
+      cy.wait(SECOND * 4)
+      cy.matchImageSnapshot('1st-breakpoint');
+
+      cy.window().then(win => {
+        const jumpMethod = win.playerJump ? win.playerJump : win.playerJumpToTime
+        jumpMethod(SECOND * 21)
+      })
+      cy.wait(SECOND * 4)
+      cy.matchImageSnapshot('2nd-breakpoint');
+
+      cy.get('[data-openreplay-label="User Steps"]').click()
+      cy.wait(SECOND * 0.5)
+      cy.matchImageSnapshot('User-Events');
+
+      cy.get('#control-button-network > .controlButton-module__label--YznMl').click()
+      cy.wait(SECOND * 0.5)
+      cy.matchImageSnapshot('Network-Events');
     })
-    cy.get(':nth-child(1) > .relative > .p-2').type(Cypress.env('account').replaceAll("\"", ''))
-    cy.get(':nth-child(2) > .relative > .p-2').type(Cypress.env('password').replaceAll("\"", ''))
-    cy.get('.justify-center > .h-10').click()
-    cy.wait('@getAccount')
-    cy.wait(2000)
-    cy.visit('3/session/7585361734083637?jumpto=5000&freeze=true')
-    cy.wait('@getFirstMob')
-    cy.wait('@getSecondMob')
-    cy.wait(2000)
-    cy.window().then(win => {
-      win.playerJump(5000)
-    })
-    cy.wait(4000)
-
-    cy.matchImageSnapshot('1st-breakpoint');
-
-    cy.window().then(win => {
-      win.playerJump(21000)
-    })
-    cy.wait(4000)
-
-    cy.matchImageSnapshot('2nd-breakpoint');
-
-    cy.get('[data-openreplay-label="User Steps"]').click()
-    cy.matchImageSnapshot('User-Events');
-
-    cy.get('#control-button-network > .controlButton-module__label--YznMl').click()
-    cy.matchImageSnapshot('Network-Events');
-  })
-})
+  }
+);
---
+++ tracker/tracker-testing-playground/src/react-app-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly REACT_APP_OPEN_REPLAY_KEY: string;
+  }
+}
