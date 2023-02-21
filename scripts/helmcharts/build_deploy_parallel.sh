e8d41bbcd3630b00b0d1c6c4247c146771f0d2ad
rjshrjndrn <rjshrjndrn@gmail.com>
2023-02-21 10:30:54 +0000

    Updating parallel script

--- scripts/helmcharts/build_deploy_parallel.sh
+++ scripts/helmcharts/build_deploy_parallel.sh
@@ -8,6 +8,12 @@ set -e
 # Removing local alpine:latest image
 docker rmi alpine || true
 
+# Signing image
+# cosign sign --key awskms:///alias/openreplay-container-sign image_url:tag
+export SIGN_IMAGE=1
+export PUSH_IMAGE=1
+export AWS_DEFAULT_REGION="eu-central-1"
+export SIGN_KEY="awskms:///alias/openreplay-container-sign"
 echo $DOCKER_REPO
 [[ -z DOCKER_REPO ]] && {
     echo Set DOCKER_REPO="your docker registry"
@@ -22,9 +28,9 @@ echo $DOCKER_REPO
     tmux split-window "cd ../../frontend && IMAGE_TAG=$IMAGE_TAG DOCKER_REPO=$DOCKER_REPO PUSH_IMAGE=1 bash build.sh $@"
     tmux select-layout tiled
     tmux split-window "cd ../../sourcemap-reader && IMAGE_TAG=$IMAGE_TAG DOCKER_REPO=$DOCKER_REPO PUSH_IMAGE=1 bash build.sh $@"
-    tmux split-window "cd ../../api && IMAGE_TAG=$IMAGE_TAG DOCKER_REPO=$DOCKER_REPO PUSH_IMAGE=1 bash build.sh $@"
-    tmux split-window "cd ../../api && IMAGE_TAG=$IMAGE_TAG DOCKER_REPO=$DOCKER_REPO PUSH_IMAGE=1 bash build_alerts.sh $@"
-    tmux split-window "cd ../../api && IMAGE_TAG=$IMAGE_TAG DOCKER_REPO=$DOCKER_REPO PUSH_IMAGE=1 bash build_crons.sh $@"
+    tmux split-window "cd ../../api && IMAGE_TAG=$IMAGE_TAG DOCKER_REPO=$DOCKER_REPO PUSH_IMAGE=1 bash build.sh $@
+      && IMAGE_TAG=$IMAGE_TAG DOCKER_REPO=$DOCKER_REPO PUSH_IMAGE=1 bash build_alerts.sh $@
+      && IMAGE_TAG=$IMAGE_TAG DOCKER_REPO=$DOCKER_REPO PUSH_IMAGE=1 bash build_crons.sh $@"
     tmux select-layout tiled
 
 }
