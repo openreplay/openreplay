6657a59cb987566fffcdef888997db081987eb70
nick-delirium <nikita@openreplay.com>
2023-02-21 15:08:41 +0100

    change(ui): mock back response

---
+++ frontend/app/components/Header/HealthStatus/HealthModal/mock.ts
@@ -0,0 +1,187 @@
+export const response = {
+  databases: {
+    postgres: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    clickhouse: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+  },
+  ingestionPipeline: {
+    redis: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    kafka: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+  },
+  backendServices: {
+    alerts: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    assets: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    assist: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    chalice: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    db: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    ender: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    frontend: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    heuristics: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    http: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    'ingress-nginx': {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    integrations: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    peers: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    quickwit: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    sink: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    sourcemapreader: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    storage: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+    utilities: {
+      health: true,
+      details: {
+        version: 'v1.13',
+        schema: 'v1.10',
+        lastUpdatedOn: '12jan2023',
+      },
+    },
+  },
+  overall: {
+    health: true,
+    details: {
+      numberOfEventCaptured: 123000,
+      numberOfSessionsCaptured: 25678,
+    },
+    labels: {
+      parent: 'information',
+    },
+  },
+  ssl: true,
+};
