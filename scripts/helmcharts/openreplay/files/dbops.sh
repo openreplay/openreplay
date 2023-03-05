740fc4aa2e06972c6b2754811a9324e56e41f871
rjshrjndrn <rjshrjndrn@gmail.com>
2023-03-05 15:39:00 +0100

    fix(script): Race condition for migration version
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- scripts/helmcharts/openreplay/files/dbops.sh
+++ scripts/helmcharts/openreplay/files/dbops.sh
@@ -39,10 +39,10 @@ function migration() {
       echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'
     }
     all_versions=(`ls -l db/init_dbs/$db | grep -E ^d | grep -v create | awk '{print $NF}'`)
-    migration_versions=(`for ver in ${all_versions[*]}; do if [[ $(normalise_version $ver) > $(normalise_version "${PREVIOUS_APP_VERSION}") ]]; then echo $ver; fi; done`)
+    migration_versions=(`for ver in ${all_versions[*]}; do if [[ $(normalise_version $ver) > $(normalise_version "${PREVIOUS_APP_VERSION}") ]]; then echo $ver; fi; done | sort -V`)
     echo "Migration version: ${migration_versions[*]}"
     # Can't pass the space seperated array to ansible for migration. So joining them with ,
-    joined_migration_versions=$(IFS=, ; echo "${migration_versions[*]}")
+    joined_migration_versions=$(IFS=, ; printf '%s\n' "${migration_versions[@]}" | tac | tr '\n' ' '; echo)
     
     cd -
 
