0fe47eee487765f116054eea5b131e4d83e0c66e
Dayan Graham <d.graham50@hotmail.co.uk>
2023-03-13 16:58:39 +0000

    feat(assets): Add support for mutual TLS to allow the assets service to fetch files behind authentication walls (#1034)

--- backend/internal/config/assets/config.go
+++ backend/internal/config/assets/config.go
@@ -15,6 +15,9 @@ type Config struct {
 	AssetsSizeLimit      int               `env:"ASSETS_SIZE_LIMIT,required"`
 	AssetsRequestHeaders map[string]string `env:"ASSETS_REQUEST_HEADERS"`
 	UseProfiler          bool              `env:"PROFILER_ENABLED,default=false"`
+	ClientKeyFilePath    string            `env:"CLIENT_KEY_FILE_PATH"`
+	CaCertFilePath       string            `env:"CA_CERT_FILE_PATH"`
+	ClientCertFilePath   string            `env:"CLIENT_CERT_FILE_PATH"`
 }
 
 func New() *Config {
