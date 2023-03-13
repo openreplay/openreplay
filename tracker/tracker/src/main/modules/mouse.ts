c945d47a7c0cf46684cf9f81ca9180f9aa2d7c68
nick-delirium <nikita@openreplay.com>
2023-03-13 11:43:02 +0100

    change(tracker): tracker 5.0.1

--- tracker/tracker/src/main/modules/mouse.ts
+++ tracker/tracker/src/main/modules/mouse.ts
@@ -155,7 +155,7 @@ export default function (app: App): void {
             id,
             mouseTarget === target ? Math.round(performance.now() - mouseTargetTime) : 0,
             getTargetLabel(target),
-            getSelector(id, target),
+            isClickable(target) ? getSelector(id, target) : '',
           ),
           true,
         )
