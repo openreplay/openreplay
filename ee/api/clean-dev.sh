0654808f68f87d5d5f4cc9d3d69c5a2c31ef99de
Taha Yassine Kraiem <tahayk2@gmail.com>
2023-03-14 14:03:45 +0100

    feat(chalice): code cleaning

--- ee/api/clean-dev.sh
+++ ee/api/clean-dev.sh
@@ -35,6 +35,7 @@ rm -rf ./chalicelib/core/log_tool_stackdriver.py
 rm -rf ./chalicelib/core/log_tool_sumologic.py
 rm -rf ./chalicelib/core/metadata.py
 rm -rf ./chalicelib/core/mobile.py
+rm -rf ./chalicelib/core/sessions.py
 rm -rf ./chalicelib/core/sessions_assignments.py
 #exp rm -rf ./chalicelib/core/sessions_metas.py
 rm -rf ./chalicelib/core/sessions_mobs.py
