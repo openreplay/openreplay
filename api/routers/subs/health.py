11af70852008fa452e458fa23606db5a453481f1
Taha Yassine Kraiem <tahayk2@gmail.com>
2023-03-14 15:00:18 +0100

    feat(chalice): dynamic health-check endpoint

--- api/routers/subs/health.py
+++ api/routers/subs/health.py
@@ -1,15 +1,14 @@
-from typing import Union
-
-from fastapi import Body, Depends, Request
-
-import schemas
-from chalicelib.core import health
-from or_dependencies import OR_context
+from chalicelib.core import health, tenants
 from routers.base import get_routers
 
 public_app, app, app_apikey = get_routers()
 
+health_router = public_app
 
-@public_app.get('/health', tags=["dashboard"])
-def get_global_health():
+if tenants.tenants_exists(use_pool=False):
+    health_router = app
+
+
+@health_router.get('/health', tags=["health-check"])
+def get_global_health_status():
     return {"data": health.get_health()}
