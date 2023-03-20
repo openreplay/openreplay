23cf8ec01ef258f354631b3f93b582e073e64c1d
Taha Yassine Kraiem <tahayk2@gmail.com>
2023-03-20 13:31:35 +0100

    feat(DB): stop upgrade on version hopping

--- ee/scripts/schema/db/init_dbs/postgresql/1.5.4/1.5.4.sql
+++ ee/scripts/schema/db/init_dbs/postgresql/1.5.4/1.5.4.sql
@@ -1,4 +1,21 @@
 \set ON_ERROR_STOP true
+DO
+$$
+    DECLARE
+        previous_version CONSTANT text := 'v1.5.3-ee';
+        next_version     CONSTANT text := 'v1.5.4-ee';
+    BEGIN
+        IF (SELECT openreplay_version()) = previous_version THEN
+            raise notice 'valid previous DB version';
+        ELSEIF (SELECT openreplay_version()) = next_version THEN
+            raise notice 'new version detected, nothing to do';
+        ELSE
+            RAISE EXCEPTION 'upgrade to % failed, invalid previous version, expected %, got %', next_version,previous_version,(SELECT openreplay_version());
+        END IF;
+    END ;
+$$
+LANGUAGE plpgsql;
+
 SET client_min_messages TO NOTICE;
 BEGIN;
 CREATE OR REPLACE FUNCTION openreplay_version()
