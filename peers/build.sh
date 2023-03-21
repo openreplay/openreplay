d86b0939f7f808a3f053637cb64e6fe1cab75384
Taha Yassine Kraiem <tahayk2@gmail.com>
2023-03-21 16:40:45 +0100

    feat(peers): changed build script

--- peers/build.sh
+++ peers/build.sh
@@ -23,6 +23,7 @@ function build_api(){
     cp -R ../peers ../${destination}
     cd ../${destination}
     cp -R ../assist/utils .
+    cp ../sourcemap-reader/utils/health.js ./utils/.
     # Copy enterprise code
     [[ $1 == "ee" ]] && {
         cp -rf ../ee/peers/* ./
