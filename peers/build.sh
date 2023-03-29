0855466c47874f565bc769e4c4663cd2191d00d2
rjshrjndrn <rjshrjndrn@gmail.com>
2023-03-29 12:24:49 +0200

    fix(build): proper image tag
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- peers/build.sh
+++ peers/build.sh
@@ -16,6 +16,7 @@ check_prereq() {
 }
 
 [[ $1 == ee ]] && ee=true
+
 [[ $PATCH -eq 1 ]] && {
   image_tag="$(grep -ER ^.ppVersion ../scripts/helmcharts/openreplay/charts/$chart | xargs | awk '{print $2}'  | awk -F. -v OFS=. '{$NF += 1 ; print}')"
   [[ $ee == "true" ]] && {
