681dbf3fc128f41feea2ea12d5a11c551e9f7565
Taha Yassine Kraiem <tahayk2@gmail.com>
2023-04-07 12:05:18 +0100

    feat(chalice): fixed custom namespaces

--- api/chalicelib/core/health.py
+++ api/chalicelib/core/health.py
@@ -10,9 +10,7 @@ from chalicelib.utils import pg_client
 def app_connection_string(name, port, path):
     namespace = config("POD_NAMESPACE", default="app")
     conn_string = config("CLUSTER_URL", default="svc.cluster.local")
-    return (
-        "http://" + name + "." + namespace + "." + conn_string + ":" + port + "/" + path
-    )
+    return f"http://{name}.{namespace}.{conn_string}:{port}/{path}"
 
 
 HEALTH_ENDPOINTS = {
