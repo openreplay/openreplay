55616b9127e0a9d15a9c3faabed2a6b1d03b985a
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2023-04-11 12:41:52 +0200

    feat(backend): debug log for body content

--- backend/internal/http/router/handlers-web.go
+++ backend/internal/http/router/handlers-web.go
@@ -212,6 +212,9 @@ func (e *Router) pushMessagesHandlerWeb(w http.ResponseWriter, r *http.Request) 
 	}
 	bodySize = len(bodyBytes)
 
+	// DEBUG: print body
+	log.Printf("first bytes: %+v", bodyBytes[:10])
+
 	// Send processed messages to queue as array of bytes
 	// TODO: check bytes for nonsense crap
 	err = e.services.Producer.Produce(e.cfg.TopicRawWeb, sessionData.ID, bodyBytes)
