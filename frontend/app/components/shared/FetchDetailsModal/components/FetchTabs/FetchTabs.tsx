26e7f7f751201475bd6d8f12a9882557817d036b
Shekar Siri <sshekarsiri@gmail.com>
2023-05-04 13:03:05 +0200

    change(ui) - fetch details modal - no content text size

--- frontend/app/components/shared/FetchDetailsModal/components/FetchTabs/FetchTabs.tsx
+++ frontend/app/components/shared/FetchDetailsModal/components/FetchTabs/FetchTabs.tsx
@@ -86,7 +86,7 @@ function FetchTabs({ resource }: Props) {
             title={
               <div className="flex flex-col items-center justify-center">
                 <AnimatedSVG name={ICONS.NO_RESULTS} size={170} />
-                <div className="mt-6 text-2xl">
+                <div className="mt-6">
                   Body is Empty or not captured.
                   {' '}
                   <a href="https://docs.openreplay.com/installation/network-options" className="link" target="_blank">
@@ -118,7 +118,7 @@ function FetchTabs({ resource }: Props) {
             title={
               <div className="flex flex-col items-center justify-center">
                 <AnimatedSVG name={ICONS.NO_RESULTS} size={170} />
-                <div className="mt-6 text-2xl">
+                <div className="mt-6">
                   Body is Empty or not captured.
                   {' '}
                   <a href="https://docs.openreplay.com/installation/network-options" className="link" target="_blank">
