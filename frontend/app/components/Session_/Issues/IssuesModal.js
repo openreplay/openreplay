390f1f4786f05484d43019c7610ebbeaa9909008
Shekar Siri <sshekarsiri@gmail.com>
2023-05-04 13:39:51 +0200

    change(ui) - text chante on create issue and share popups

--- frontend/app/components/Session_/Issues/IssuesModal.js
+++ frontend/app/components/Session_/Issues/IssuesModal.js
@@ -13,7 +13,7 @@ const IssuesModal = ({
     <div className={ stl.wrapper }>
       <h3 className="text-xl font-semibold">
         {/* <Icon name={headerIcon} size="18" color="color-gray-darkest" />  */}
-        <span>{`Report an Issue on ${provider === 'jira' ? 'Jira' : 'Github'}`}</span>
+        <span>Create Issue</span>
       </h3>
       <Provider store={store}>
         <IssueForm sessionId={ sessionId } closeHandler={ closeHandler } />
