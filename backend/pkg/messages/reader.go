b121c42f5a6c5c5817bcbd1b770cedffc5b38cd8
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2023-05-10 13:00:33 +0200

    fix(backend): added support of deprecated batch meta message to pre-decoder

--- backend/pkg/messages/reader.go
+++ backend/pkg/messages/reader.go
@@ -89,12 +89,14 @@ func (m *messageReaderImpl) Parse() (err error) {
 			if err != nil {
 				return fmt.Errorf("read message err: %s", err)
 			}
-			if m.msgType == MsgBatchMeta || m.msgType == MsgBatchMetadata {
+			if m.msgType == MsgBatchMeta || m.msgType == MsgBatchMetadata || m.msgType == MsgBatchMetadataDeprecated {
 				if len(m.list) > 0 {
 					return fmt.Errorf("batch meta not at the start of batch")
 				}
 				switch message := msg.(type) {
 				case *BatchMetadata:
+					m.version = int(message.Version)
+				case *BatchMetadataDeprecated:
 					m.version = int(message.Version)
 				case *BatchMeta:
 					m.version = 0
