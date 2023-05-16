b71be3817d5a81b72466d0a90bee1c83d2eee5f7
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2023-05-16 07:55:03 +0000

    feat(backend): added azure blob storage support to chalice service

--- api/chalicelib/core/mobile.py
+++ api/chalicelib/core/mobile.py
@@ -1,5 +1,5 @@
 from chalicelib.core import projects
-from chalicelib.utils import s3
+from chalicelib.utils.objects.store import obj_store
 from decouple import config
 
 
@@ -7,7 +7,7 @@ def sign_keys(project_id, session_id, keys):
     result = []
     project_key = projects.get_project_key(project_id)
     for k in keys:
-        result.append(s3.get_presigned_url_for_sharing(bucket=config("iosBucket"),
-                                                       key=f"{project_key}/{session_id}/{k}",
-                                                       expires_in=60 * 60))
+        result.append(obj_store.get_presigned_url_for_sharing(bucket=config("iosBucket"),
+                                                              key=f"{project_key}/{session_id}/{k}",
+                                                              expires_in=60 * 60))
     return result
