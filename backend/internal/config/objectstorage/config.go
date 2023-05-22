24c827f49c876302ed40a2467d8f81fbaace379e
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2023-05-22 16:59:36 +0200

    feat(backend): temporary made s3 vars are not required

--- backend/internal/config/objectstorage/config.go
+++ backend/internal/config/objectstorage/config.go
@@ -6,13 +6,13 @@ type ObjectsConfig struct {
 	ServiceName string `env:"SERVICE_NAME,required"`
 	CloudName   string `env:"CLOUD,default=aws"`
 	// S3 storage configuration
-	S3Region string `env:"AWS_REGION_WEB,required"`
-	S3Bucket string `env:"S3_BUCKET_WEB,required"`
+	S3Region string `env:"AWS_REGION_WEB"`
+	S3Bucket string `env:"S3_BUCKET_WEB"`
 	// TEMP
-	AWSRegion      string `env:"AWS_REGION,required"`
-	S3BucketAssets string `env:"S3_BUCKET_ASSETS,required"`
+	AWSRegion      string `env:"AWS_REGION"`
+	S3BucketAssets string `env:"S3_BUCKET_ASSETS"`
 	//
-	S3BucketIOSImages string `env:"S3_BUCKET_IOS_IMAGES,required"`
+	S3BucketIOSImages string `env:"S3_BUCKET_IOS_IMAGES"`
 	// Azure Blob Storage configuration
 	AzureAccountName string `env:"AZURE_ACCOUNT_NAME,required"`
 	AzureAccountKey  string `env:"AZURE_ACCOUNT_KEY,required"`
