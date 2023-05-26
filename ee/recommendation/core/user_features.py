936429c14369f84cf995ea2e347a6bc4616abcf1
Taha Yassine Kraiem <tahayk2@gmail.com>
2023-05-26 16:22:05 +0200

    refactor(recommendation): optimized requests

--- ee/recommendation/core/user_features.py
+++ ee/recommendation/core/user_features.py
@@ -47,90 +47,39 @@ def signals_features(conn, **kwargs):
     assert 'projectId' in kwargs.keys(), 'projectId should be provided in kwargs'
     projectId = kwargs['projectId']
     events_threshold = config('events_threshold', default=10, cast=int)
-    query = conn.mogrify("""SELECT
-       COALESCE(Ta.project_id, Tb.project_id) as project_id,
-       COALESCE(Ta.session_id, Tb.session_id) as session_id,
-       COALESCE(Ta.user_id, Tb.user_id) as user_id,
-       COALESCE(Ta.viewer_id, Tb.viewer_id) as viewer_id,
-       COALESCE(Ta.events_count, Tb.events_count) as events_count,
-       COALESCE(Ta.errors_count, Tb.errors_count) as errors_count,
-       COALESCE(Ta.duration, Tb.duration) as duration,
-       COALESCE(Ta.country, Tb.country) as country,
-       COALESCE(Ta.issue_score, Tb.issue_score) as issue_score,
-       COALESCE(Ta.device_type, Tb.device_type) as device_type,
-       COALESCE(Ta.interesting or Tb.interesting, false) as train_label
-    FROM (
-               (SELECT T.project_id,
-                       T.session_id,
-                       T.user_id,
-                       T2.viewer_id,
-                       T.events_count,
-                       T.errors_count,
-                       T.duration,
-                       T.country,
-                       T.issue_score,
-                       T.device_type,
-                       T2.interesting
-                FROM (SELECT project_id, user_id as viewer_id, session_id, count(*) > 1 as interesting
-                      FROM (
-                               SELECT *, NULLIF(data ->> 'sessionId', '')::bigint as session_id
-                               FROM frontend_signals
-                               WHERE source = 'replay'
-                                 AND project_id = %(projectId)s) as Tin
-                      WHERE session_id is not null
-                      GROUP BY project_id, viewer_id, session_id) as T2
-                         INNER JOIN (SELECT project_id,
-                                            session_id,
-                                            user_id          as viewer_id,
-                                            user_id,
-                                            events_count,
-                                            errors_count,
-                                            duration,
-                                            user_country     as country,
-                                            issue_score,
-                                            user_device_type as device_type
-                                     FROM sessions
-                                     WHERE project_id = %(projectId)s AND duration IS NOT NULL) as T
-                                    ON T.session_id = T2.session_id)
-                  as Ta
-                  FULL OUTER JOIN (
-    SELECT T.project_id,
-           T.session_id,
-           T.user_id,
-           T.viewer_id,
-           T.events_count,
-           T.errors_count,
-           T.duration,
-           T.country,
-           T.issue_score,
-           T.device_type,
-           count(T.timestamp) > %(events_threshold)s as interesting
-    FROM (
-             SELECT T1.project_id,
-                    T1.session_id,
-                    T2.user_id          as viewer_id,
-                    T2.timestamp,
-                    T1.user_id,
-                    T1.events_count,
-                    T1.errors_count,
-                    T1.duration,
-                    T1.user_country     as country,
-                    T1.issue_score,
-                    T1.user_device_type as device_type
-             FROM (
-                      SELECT *
-                      FROM sessions
-                      WHERE project_id = %(projectId)s AND duration IS NOT NULL) as T1
-                      INNER JOIN (
-                 SELECT *, NULLIF(data ->> 'sessionId', '')::bigint as session_id
-                 FROM frontend_signals
-                 WHERE project_id = %(projectId)s) as T2
-                                 ON T1.session_id = T2.session_id) as T
-    GROUP BY T.project_id, T.session_id, T.user_id, T.viewer_id, T.events_count, T.errors_count, T.duration, T.country,
-             T.issue_score, T.device_type
-) as Tb
-                             ON Tb.session_id = Ta.session_id AND Tb.viewer_id=Ta.viewer_id
-    )""",
+    query = conn.mogrify("""SELECT T.project_id,
+                                   T.session_id,
+                                   T.user_id,
+                                   T2.viewer_id,
+                                   T.events_count,
+                                   T.errors_count,
+                                   T.duration,
+                                   T.country,
+                                   T.issue_score,
+                                   T.device_type,
+                                   T2.interesting
+                            FROM (SELECT project_id,
+                                         user_id                                                            as viewer_id,
+                                         session_id,
+                                         count(CASE WHEN source = 'replay' THEN 1 END) > 1 OR COUNT(1) > %(events_threshold)s as interesting
+                                  FROM frontend_signals
+                                  WHERE project_id = %(projectId)s
+                                    AND session_id is not null
+                                  GROUP BY project_id, viewer_id, session_id) as T2
+                                     INNER JOIN (SELECT project_id,
+                                                        session_id,
+                                                        user_id          as viewer_id,
+                                                        user_id,
+                                                        events_count,
+                                                        errors_count,
+                                                        duration,
+                                                        user_country     as country,
+                                                        issue_score,
+                                                        user_device_type as device_type
+                                                 FROM sessions
+                                                 WHERE project_id = %(projectId)s
+                                                   AND duration IS NOT NULL) as T
+                                                USING (session_id);""",
                          {"projectId": projectId, "events_threshold": events_threshold})
     conn.execute(query)
     res = conn.fetchall()
@@ -141,11 +90,20 @@ def user_favorite_sessions(projectId, conn):
     """
     Selects features from user_favorite_sessions table.
     """
-    query = """SELECT project_id, session_id, user_id, viewer_id, events_count, errors_count, duration, country, issue_score, device_type FROM (
-    (SELECT session_id, project_id, user_id, events_count, errors_count, duration, user_country as country, issue_score, user_device_type as device_type
-    FROM sessions WHERE project_id=%(projectId)s) AS T1
-    INNER JOIN (SELECT user_id as viewer_id, session_id as fav_session_id FROM user_favorite_sessions) as T2
-    ON T1.session_id=T2.fav_session_id) AS T"""
+    query = """SELECT project_id,
+                       session_id,
+                       T1.user_id,
+                       events_count,
+                       errors_count,
+                       duration,
+                       user_country     as country,
+                       issue_score,
+                       user_device_type as device_type,
+                       T2.user_id       AS viewer_id
+                FROM sessions AS T1
+                         INNER JOIN user_favorite_sessions as T2
+                                    USING (session_id)
+                WHERE project_id = %(projectId)s;"""
     conn.execute(
         conn.mogrify(query, {"projectId": projectId})
     )
@@ -157,11 +115,15 @@ def user_not_seen_sessions(projectId, limit, conn):
     """
     Selects features from user_viewed_sessions table.
     """
+    # TODO: fetch un-viewed sessions alone, and the users list alone, then cross join them in python
+    # and ignore deleted users (WHERE users.deleted_at ISNULL)
     query = """SELECT project_id, session_id, user_id, viewer_id, events_count, errors_count, duration, user_country as country, issue_score, user_device_type as device_type
 FROM (
-         (SELECT *
-         FROM sessions
-         WHERE project_id = %(projectId)s AND session_id NOT IN (SELECT session_id FROM user_viewed_sessions) AND duration IS NOT NULL
+         (SELECT sessions.*
+         FROM sessions LEFT JOIN user_viewed_sessions USING(session_id)
+         WHERE project_id = %(projectId)s  
+            AND duration IS NOT NULL
+            AND user_viewed_sessions.session_id ISNULL
          LIMIT %(limit)s) AS T1
              LEFT JOIN
          (SELECT user_id as viewer_id
