4e3332304ead41d4d9af11e0a1542784d872083d
nick-delirium <nikita@openreplay.com>
2023-05-31 12:52:51 +0200

    fix(tracker): fix breaking q sender test

--- tracker/tracker/src/webworker/QueueSender.unit.test.ts
+++ tracker/tracker/src/webworker/QueueSender.unit.test.ts
@@ -3,20 +3,38 @@ import QueueSender from './QueueSender.js'
 
 global.fetch = () => Promise.resolve(new Response()) // jsdom does not have it
 
-function mockFetch(status: number) {
+function mockFetch(status: number, headers?: Record<string, string>) {
   return jest
     .spyOn(global, 'fetch')
-    .mockImplementation(() => Promise.resolve({ status } as Response))
+    .mockImplementation((request) =>
+      Promise.resolve({ status, headers, request } as unknown as Response & {
+        request: RequestInfo
+      }),
+    )
 }
 const baseURL = 'MYBASEURL'
 const sampleArray = new Uint8Array(1)
 const randomToken = 'abc'
+
+const requestMock = {
+  body: sampleArray,
+  headers: { Authorization: 'Bearer abc' },
+  keepalive: true,
+  method: 'POST',
+}
+
+const gzipRequestMock = {
+  ...requestMock,
+  headers: { ...requestMock.headers, 'Content-Encoding': 'gzip' },
+}
+
 function defaultQueueSender({
   url = baseURL,
   onUnauthorised = () => {},
   onFailed = () => {},
-} = {}) {
-  return new QueueSender(baseURL, onUnauthorised, onFailed, 10, 1000)
+  onCompress = undefined,
+}: Record<string, any> = {}) {
+  return new QueueSender(baseURL, onUnauthorised, onFailed, 10, 1000, onCompress)
 }
 
 describe('QueueSender', () => {
@@ -42,6 +60,25 @@ describe('QueueSender', () => {
     expect(fetchMock).toBeCalledTimes(0)
     queueSender.push(sampleArray)
     expect(fetchMock).toBeCalledTimes(1)
+    expect(fetchMock.mock.calls[0][1]).toMatchObject(requestMock)
+  })
+  test('Sends compressed request if onCompress is provided and compressed batch is included', () => {
+    const queueSender = defaultQueueSender({ onCompress: () => true })
+    const fetchMock = mockFetch(200)
+
+    // @ts-ignore
+    const spyOnCompress = jest.spyOn(queueSender, 'onCompress')
+    // @ts-ignore
+    const spyOnSendNext = jest.spyOn(queueSender, 'sendNext')
+
+    queueSender.authorise(randomToken)
+    queueSender.push(sampleArray)
+    expect(spyOnCompress).toBeCalledTimes(1)
+    queueSender.sendCompressed(sampleArray)
+    expect(fetchMock).toBeCalledTimes(1)
+    expect(spyOnSendNext).toBeCalledTimes(1)
+    expect(spyOnCompress).toBeCalledTimes(1)
+    expect(fetchMock.mock.calls[0][1]).toMatchObject(gzipRequestMock)
   })
   test('Calls fetch on authorisation if there was a push() call before', () => {
     const queueSender = defaultQueueSender()
