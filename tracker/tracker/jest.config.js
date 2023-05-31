5ecd7b86f89287b4185937427a899fc8dba4218e
nick-delirium <nikita@openreplay.com>
2023-05-31 12:04:11 +0200

    fix(tracker): fix test conf

--- tracker/tracker/jest.config.js
+++ tracker/tracker/jest.config.js
@@ -3,7 +3,7 @@ const config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts}', '!src/**/*.d.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
   // .js file extension fix
   moduleNameMapper: {
     '(.+)\\.js': '$1',
