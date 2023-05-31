d958daa61ed4fb7a6d3abb877132ddf2b723bdcf
nick-delirium <nikita@openreplay.com>
2023-05-31 11:41:40 +0200

    feat(tracker): export test coverage for tracker

--- tracker/tracker/jest.config.js
+++ tracker/tracker/jest.config.js
@@ -2,6 +2,8 @@
 const config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts}', '!src/**/*.d.ts'],
   // .js file extension fix
   moduleNameMapper: {
     '(.+)\\.js': '$1',
