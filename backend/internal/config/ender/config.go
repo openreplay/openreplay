6952f8d56fc2897f893b535622c6f8fc64a7a12d
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2023-06-01 13:13:44 +0200

    fix(backend): fixed issue with wrong expiration timeout for projects in cache layer

--- backend/internal/config/ender/config.go
+++ backend/internal/config/ender/config.go
@@ -3,19 +3,20 @@ package ender
 import (
 	"openreplay/backend/internal/config/common"
 	"openreplay/backend/internal/config/configurator"
+	"time"
 )
 
 type Config struct {
 	common.Config
 	common.Postgres
-	ProjectExpirationTimeoutMs int64  `env:"PROJECT_EXPIRATION_TIMEOUT_MS,default=1200000"`
-	GroupEnder                 string `env:"GROUP_ENDER,required"`
-	LoggerTimeout              int    `env:"LOG_QUEUE_STATS_INTERVAL_SEC,required"`
-	TopicRawWeb                string `env:"TOPIC_RAW_WEB,required"`
-	ProducerTimeout            int    `env:"PRODUCER_TIMEOUT,default=2000"`
-	PartitionsNumber           int    `env:"PARTITIONS_NUMBER,required"`
-	UseEncryption              bool   `env:"USE_ENCRYPTION,default=false"`
-	UseProfiler                bool   `env:"PROFILER_ENABLED,default=false"`
+	ProjectExpiration time.Duration `env:"PROJECT_EXPIRATION,default=20m"`
+	GroupEnder        string        `env:"GROUP_ENDER,required"`
+	LoggerTimeout     int           `env:"LOG_QUEUE_STATS_INTERVAL_SEC,required"`
+	TopicRawWeb       string        `env:"TOPIC_RAW_WEB,required"`
+	ProducerTimeout   int           `env:"PRODUCER_TIMEOUT,default=2000"`
+	PartitionsNumber  int           `env:"PARTITIONS_NUMBER,required"`
+	UseEncryption     bool          `env:"USE_ENCRYPTION,default=false"`
+	UseProfiler       bool          `env:"PROFILER_ENABLED,default=false"`
 }
 
 func New() *Config {
