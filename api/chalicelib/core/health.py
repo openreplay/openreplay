d3c866b443b75e72b47837b223a502cc2b7fc627
Kraiem Taha Yassine <tahayk2@gmail.com>
2023-06-08 18:12:47 +0200

    Api v1.13.0 (#1320)
    * feat(chalice): fixed health check

--- api/chalicelib/core/health.py
+++ api/chalicelib/core/health.py
@@ -206,8 +206,7 @@ def get_health():
     return __process_health(health_map=health_map)
 
 
-
-def __process_health(tenant_id, health_map):
+def __process_health(health_map):
     for parent_key in health_map.keys():
         if config(f"SKIP_H_{parent_key.upper()}", cast=bool, default=False):
             health_map.pop(parent_key)
