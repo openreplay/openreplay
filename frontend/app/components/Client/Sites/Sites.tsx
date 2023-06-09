30e84ff9f552040943be2318175d186da385f988
Shekar Siri <sshekarsiri@gmail.com>
2023-06-09 10:46:39 +0200

    change(ui): prefs project settings

---
+++ frontend/app/components/Client/Sites/Sites.tsx
@@ -0,0 +1,176 @@
+import React, { useState } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import withPageTitle from 'HOCs/withPageTitle';
+import { Loader, Button, TextLink, NoContent } from 'UI';
+import { init, remove, fetchGDPR, setSiteId } from 'Duck/site';
+import stl from './sites.module.css';
+import NewSiteForm from './NewSiteForm';
+import { confirm, PageTitle } from 'UI';
+import SiteSearch from './SiteSearch';
+import AddProjectButton from './AddProjectButton';
+import InstallButton from './InstallButton';
+import ProjectKey from './ProjectKey';
+import { getInitials } from 'App/utils';
+import AnimatedSVG, { ICONS } from 'Shared/AnimatedSVG/AnimatedSVG';
+import cn from 'classnames';
+import { useModal } from 'App/components/Modal';
+
+const NEW_SITE_FORM = 'NEW_SITE_FORM';
+
+type Site = {
+  id: string;
+  name: string;
+  host: string;
+  projectKey: string;
+};
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+const Sites = ({
+  loading,
+  sites,
+  user,
+  init,
+  remove,
+  setSiteId,
+}: PropsFromRedux) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [modalContent, setModalContent] = useState<string | null>(null);
+
+  const isAdmin = user.admin || user.superAdmin;
+  const filteredSites = sites.filter((site) =>
+    site.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const edit = (site: Site) => {
+    init(site);
+    setModalContent(NEW_SITE_FORM);
+  };
+
+  const removeSite = async (site: Site) => {
+    if (
+      await confirm({
+        header: 'Projects',
+        confirmation: `Are you sure you want to delete this Project? We won't be able to record anymore sessions.`,
+      })
+    ) {
+      remove(site.id);
+      setSiteId(null);
+    }
+  };
+
+  const { showModal, hideModal } = useModal();
+
+  const EditButton = ({ isAdmin, onClick }: { isAdmin: boolean; onClick: () => void }) => {
+    const _onClick = () => {
+      onClick();
+      showModal(<NewSiteForm onClose={hideModal} />, { right: true });
+    };
+
+    return (
+      <Button
+        icon="edit"
+        variant="text-primary"
+        disabled={!isAdmin}
+        onClick={_onClick}
+      />
+    );
+  };
+
+  return (
+    <Loader loading={loading}>
+      <div className={stl.wrapper}>
+        <div className={cn(stl.tabHeader, 'px-5 pt-5')}>
+          <PageTitle
+            title={<div className="mr-4">Projects</div>}
+            actionButton={
+              <TextLink
+                icon="book"
+                href="https://docs.openreplay.com/installation"
+                label="Installation Docs"
+              />
+            }
+          />
+
+          <div className="flex ml-auto items-center">
+            <AddProjectButton isAdmin={isAdmin} />
+            <div className="mx-2" />
+            <SiteSearch onChange={(value) => setSearchQuery(value)} />
+          </div>
+        </div>
+
+        <div className={stl.list}>
+          <NoContent
+            title={
+              <div className="flex flex-col items-center justify-center">
+                <AnimatedSVG name={ICONS.NO_PROJECTS} size={170} />
+                <div className="text-center text-gray-600 my-4">
+                  No matching results.
+                </div>
+              </div>
+            }
+            size="small"
+            show={!loading && filteredSites.length === 0}
+          >
+            <div className="grid grid-cols-12 gap-2 w-full items-center px-5 py-3 font-medium">
+              <div className="col-span-4">Project Name</div>
+              <div className="col-span-3">Key</div>
+              <div className="col-span-2">Capture Rate</div>
+              <div className="col-span-3"></div>
+            </div>
+            {filteredSites.map((_site) => (
+              <div
+                key={_site.id}
+                className="grid grid-cols-12 gap-2 w-full group hover:bg-active-blue items-center border-t px-5 py-3"
+              >
+                <div className="col-span-4">
+                  <div className="flex items-center">
+                    <div className="relative flex items-center justify-center w-10 h-10">
+                      <div className="absolute left-0 right-0 top-0 bottom-0 mx-auto w-10 h-10 rounded-full opacity-30 bg-tealx" />
+                      <div className="text-lg uppercase color-tealx">
+                        {getInitials(_site.name)}
+                      </div>
+                    </div>
+                    <span className="ml-2">{_site.host}</span>
+                  </div>
+                </div>
+                <div className="col-span-3">
+                  <ProjectKey
+                    value={_site.projectKey}
+                    tooltip="Project key copied to clipboard"
+                  />
+                </div>
+                <div className="col-span-2">100%</div>
+                <div className="col-span-3 justify-self-end flex items-center">
+                  <div className="mr-4">
+                    <InstallButton site={_site} />
+                  </div>
+                  <div className="invisible group-hover:visible">
+                    <EditButton isAdmin={isAdmin} onClick={() => init(_site)} />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </NoContent>
+        </div>
+      </div>
+    </Loader>
+  );
+};
+
+const mapStateToProps = (state: any) => ({
+  site: state.getIn(['site', 'instance']),
+  sites: state.getIn(['site', 'list']),
+  loading: state.getIn(['site', 'loading']),
+  user: state.getIn(['user', 'account']),
+  account: state.getIn(['user', 'account']),
+});
+
+const connector = connect(mapStateToProps, {
+  init,
+  remove,
+  fetchGDPR,
+  setSiteId,
+});
+
+export default connector(withPageTitle('Projects - OpenReplay Preferences')(Sites));
