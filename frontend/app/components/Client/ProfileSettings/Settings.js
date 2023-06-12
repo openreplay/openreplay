01d7c3676d91381c6a0224b5a78993cd30a71f67
Shekar Siri <sshekarsiri@gmail.com>
2023-06-12 16:54:10 +0200

    fix(ui): account details updates

--- frontend/app/components/Client/ProfileSettings/Settings.js
+++ frontend/app/components/Client/ProfileSettings/Settings.js
@@ -4,70 +4,70 @@ import { Button, Input, Form } from 'UI';
 import { updateAccount, updateClient } from 'Duck/user';
 import styles from './profileSettings.module.css';
 
-@connect(state => ({
-  accountName: state.getIn([ 'user', 'account', 'name' ]),
-  organizationName: state.getIn([ 'user', 'account', 'tenantName' ]),
-  loading: state.getIn([ 'user', 'updateAccountRequest', 'loading' ]) ||
-    state.getIn([ 'user', 'putClientRequest', 'loading' ]),
-}), {
-  updateAccount,
-  updateClient,
-})
+@connect(
+  (state) => ({
+    accountName: state.getIn(['user', 'account', 'name']),
+    organizationName: state.getIn(['user', 'account', 'tenantName']),
+    loading:
+      state.getIn(['user', 'updateAccountRequest', 'loading']) ||
+      state.getIn(['user', 'putClientRequest', 'loading']),
+  }),
+  {
+    updateAccount,
+    updateClient,
+  }
+)
 export default class Settings extends React.PureComponent {
   state = {
     accountName: this.props.accountName,
     organizationName: this.props.organizationName,
-  }
+  };
 
   onChange = ({ target: { value, name } }) => {
-    this.setState({ changed: true, [ name ]: value });
-  }
+    this.setState({ changed: true, [name]: value });
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
     const { accountName, organizationName } = this.state;
-    const promises = [];
-    if (accountName !== this.props.accountName) {
-      promises.push(this.props.updateAccount({ name: accountName }));
-    }
-    if (organizationName !== this.props.organizationName) {
-      promises.push(this.props.updateClient({ name: organizationName }));
-    }
-    Promise.all(promises)
+    this.props
+      .updateClient({ name: accountName, tenantName: organizationName })
       .then(() => this.setState({ changed: false }));
-  }
+  };
 
   render() {
     const { loading } = this.props;
     const { accountName, organizationName, changed, copied } = this.state;
 
     return (
-      <Form onSubmit={ this.handleSubmit } className={ styles.form }>
+      <Form onSubmit={this.handleSubmit} className={styles.form}>
         <Form.Field>
-          <label htmlFor="accountName">{ 'Name' }</label>
+          <label htmlFor="accountName">{'Name'}</label>
           <Input
             name="accountName"
             id="accountName"
             type="text"
-            onChange={ this.onChange }
-            value={ accountName }
+            onChange={this.onChange}
+            value={accountName}
             maxLength={50}
           />
         </Form.Field>
 
         <Form.Field>
-          <label htmlFor="organizationName">{ 'Organization' }</label>
+          <label htmlFor="organizationName">{'Organization'}</label>
           <Input
             name="organizationName"
             id="organizationName"
             type="text"
-            onChange={ this.onChange }
-            value={ organizationName }
+            onChange={this.onChange}
+            value={organizationName}
             maxLength={50}
           />
         </Form.Field>
 
-        <Button variant="outline" loading={ loading } disabled={ !changed } type="submit">{ 'Update' }</Button>
+        <Button variant="outline" loading={loading} disabled={!changed} type="submit">
+          {'Update'}
+        </Button>
       </Form>
     );
   }
