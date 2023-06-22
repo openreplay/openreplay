d2826bf79390a1b1447a054014eee5a942282ef5
Taha Yassine Kraiem <tahayk2@gmail.com>
2023-06-22 11:53:32 +0100

    refactored(chalice): removed feature-flags refactored(DB): removed feature-flags

--- api/routers/core_dynamic.py
+++ api/routers/core_dynamic.py
@@ -7,7 +7,7 @@ from starlette.responses import RedirectResponse, FileResponse
 
 import schemas
 from chalicelib.core import sessions, errors, errors_viewed, errors_favorite, sessions_assignments, heatmaps, \
-    sessions_favorite, assist, sessions_notes, click_maps, sessions_replay, signup, feature_flags
+    sessions_favorite, assist, sessions_notes, click_maps, sessions_replay, signup
 from chalicelib.core import sessions_viewed
 from chalicelib.core import tenants, users, projects, license
 from chalicelib.core import webhook
@@ -488,34 +488,3 @@ async def get_all_notes(projectId: int, data: schemas.SearchNoteSchema = Body(..
 async def click_map_search(projectId: int, data: schemas.FlatClickMapSessionsSearch = Body(...),
                            context: schemas.CurrentContext = Depends(OR_context)):
     return {"data": click_maps.search_short_session(user_id=context.user_id, data=data, project_id=projectId)}
-
-
-@app.post('/{project_id}/feature-flags/search', tags=["feature flags"])
-async def search_feature_flags(project_id: int,
-                               data: schemas.SearchFlagsSchema = Body(...),
-                               context: schemas.CurrentContext = Depends(OR_context)):
-    return feature_flags.search_feature_flags(project_id=project_id, user_id=context.user_id, data=data)
-
-
-@app.get('/{project_id}/feature-flags/{feature_flag_id}', tags=["feature flags"])
-async def get_feature_flag(project_id: int, feature_flag_id: int):
-    return feature_flags.get_feature_flag(project_id=project_id, feature_flag_id=feature_flag_id)
-
-
-@app.post('/{project_id}/feature-flags', tags=["feature flags"])
-async def add_feature_flag(project_id: int, data: schemas.FeatureFlagSchema = Body(...),
-                           context: schemas.CurrentContext = Depends(OR_context)):
-    return feature_flags.create_feature_flag(project_id=project_id, user_id=context.user_id, feature_flag_data=data)
-
-
-@app.put('/{project_id}/feature-flags/{feature_flag_id}', tags=["feature flags"])
-async def update_feature_flag(project_id: int, feature_flag_id: int, data: schemas.FeatureFlagSchema = Body(...),
-                              context: schemas.CurrentContext = Depends(OR_context)):
-    return feature_flags.update_feature_flag(project_id=project_id, feature_flag_id=feature_flag_id,
-                                             user_id=context.user_id,
-                                             feature_flag=data)
-
-
-@app.delete('/{project_id}/feature-flags/{feature_flag_id}', tags=["feature flags"])
-async def delete_feature_flag(project_id: int, feature_flag_id: int, _=Body(None)):
-    return {"data": feature_flags.delete_feature_flag(project_id=project_id, feature_flag_id=feature_flag_id)}
