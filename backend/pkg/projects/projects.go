e4ca169a57ca6ce0a09ce62c981871df0d50bde6
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2023-06-27 11:35:39 +0200

    feat(backend): implemented new cache in projects

--- backend/pkg/projects/projects.go
+++ backend/pkg/projects/projects.go
@@ -1,8 +1,8 @@
 package projects
 
 import (
+	"openreplay/backend/pkg/cache"
 	"openreplay/backend/pkg/db/postgres"
-	"sync"
 	"time"
 )
 
@@ -17,82 +17,38 @@ type ProjectMeta struct {
 }
 
 type projectsImpl struct {
-	db                       *postgres.Conn
-	projects                 map[uint32]*ProjectMeta
-	projectsByKeys           sync.Map
-	projectExpirationTimeout time.Duration
+	db             *postgres.Conn
+	projectsByID   cache.Cache
+	projectsByKeys cache.Cache
 }
 
 func New(db *postgres.Conn) Projects {
-	return &projectsImpl{db: db}
-}
-
-//---------------------------------------------
-
-func (c *projectsImpl) GetProjectByKey(projectKey string) (*Project, error) {
-	pmInterface, found := c.projectsByKeys.Load(projectKey)
-	if found {
-		if pm, ok := pmInterface.(*ProjectMeta); ok {
-			if time.Now().Before(pm.expirationTime) {
-				return pm.Project, nil
-			}
-		}
+	return &projectsImpl{
+		db:           db,
+		projectsByID: cache.New(time.Minute*5, time.Minute*10),
 	}
-
-	p, err := c.GetProjectByKey(projectKey)
-	if err != nil {
-		return nil, err
-	}
-	//c.projects[ p.ProjectID ] = &ProjectMeta{ p, time.Now().Add(c.projectExpirationTimeout) }
-	c.projectsByKeys.Store(projectKey, p)
-	return p, nil
 }
 
 func (c *projectsImpl) GetProject(projectID uint32) (*Project, error) {
-	if c.projects[projectID] != nil &&
-		time.Now().Before(c.projects[projectID].expirationTime) {
-		return c.projects[projectID].Project, nil
+	if proj, ok := c.projectsByID.Get(projectID); ok {
+		return proj.(*Project), nil
 	}
-	p, err := c.GetProject(projectID)
+	p, err := c.getProject(projectID)
 	if err != nil {
 		return nil, err
 	}
-	c.projects[projectID] = &ProjectMeta{p, time.Now().Add(c.projectExpirationTimeout)}
-	//c.projectsByKeys.Store(p.ProjectKey, c.projects[ projectID ])
+	c.projectsByID.Set(projectID, p)
 	return p, nil
 }
 
-//---------------------------------------------
-
-func (c *projectsImpl) getProjectByKey(projectKey string) (*Project, error) {
-	p := &Project{ProjectKey: projectKey}
-	if err := c.db.Pool.QueryRow(`
-		SELECT max_session_duration, sample_rate, project_id, beacon_size
-		FROM projects
-		WHERE project_key=$1 AND active = true
-	`,
-		projectKey,
-	).Scan(&p.MaxSessionDuration, &p.SampleRate, &p.ProjectID, &p.BeaconSize); err != nil {
+func (c *projectsImpl) GetProjectByKey(projectKey string) (*Project, error) {
+	if proj, ok := c.projectsByKeys.Get(projectKey); ok {
+		return proj.(*Project), nil
+	}
+	p, err := c.getProjectByKey(projectKey)
+	if err != nil {
 		return nil, err
 	}
-	return p, nil
-}
-
-// TODO: logical separation of metadata
-func (c *projectsImpl) getProject(projectID uint32) (*Project, error) {
-	p := &Project{ProjectID: projectID}
-	if err := c.db.Pool.QueryRow(`
-		SELECT project_key, max_session_duration, save_request_payloads,
-			metadata_1, metadata_2, metadata_3, metadata_4, metadata_5,
-			metadata_6, metadata_7, metadata_8, metadata_9, metadata_10
-		FROM projects
-		WHERE project_id=$1 AND active = true
-	`,
-		projectID,
-	).Scan(&p.ProjectKey, &p.MaxSessionDuration, &p.SaveRequestPayloads,
-		&p.Metadata1, &p.Metadata2, &p.Metadata3, &p.Metadata4, &p.Metadata5,
-		&p.Metadata6, &p.Metadata7, &p.Metadata8, &p.Metadata9, &p.Metadata10); err != nil {
-		return nil, err
-	}
+	c.projectsByKeys.Set(projectKey, p)
 	return p, nil
 }
