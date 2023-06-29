98e94465045f7c0c35e0040edda0186972949571
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2023-06-29 15:39:54 +0200

    feat(backend): adapt EE version of message handler

--- ee/backend/internal/db/datasaver/methods.go
+++ ee/backend/internal/db/datasaver/methods.go
@@ -1,15 +1,14 @@
 package datasaver
 
 import (
-	"errors"
 	"log"
-	"openreplay/backend/pkg/sessions"
 
-	"openreplay/backend/pkg/db/cache"
 	"openreplay/backend/pkg/db/clickhouse"
 	"openreplay/backend/pkg/db/types"
 	"openreplay/backend/pkg/env"
+	"openreplay/backend/pkg/messages"
 	"openreplay/backend/pkg/queue"
+	"openreplay/backend/pkg/sessions"
 )
 
 func (s *saverImpl) init() {
@@ -17,70 +16,64 @@ func (s *saverImpl) init() {
 	if err := s.ch.Prepare(); err != nil {
 		log.Fatalf("can't prepare clickhouse: %s", err)
 	}
-	s.pg.Conn.SetClickHouse(s.ch)
+	s.pg.SetClickHouse(s.ch)
 	if s.cfg.UseQuickwit {
 		s.producer = queue.NewProducer(s.cfg.MessageSizeLimit, true)
 	}
 }
 
-func (s *saverImpl) handleExtraMessage(msg Message) error {
-	// Send data to quickwit
-	s.sendToFTS(msg)
-
+func (s *saverImpl) handleExtraMessage(msg messages.Message) error {
 	// Get session data
 	var (
 		session *sessions.Session
 		err     error
 	)
-	if msg.TypeID() == MsgSessionEnd {
-		session, err = s.pg.GetSession(msg.SessionID())
+
+	if msg.TypeID() == messages.MsgSessionEnd {
+		session, err = s.sessions.GetUpdated(msg.SessionID())
 	} else {
-		session, err = s.pg.Cache.GetSession(msg.SessionID())
+		session, err = s.sessions.Get(msg.SessionID())
 	}
-	if session == nil {
-		if err != nil && !errors.Is(err, cache.NilSessionInCacheError) {
-			log.Printf("Error on session retrieving from cache: %v, SessionID: %v, Message: %v", err, msg.SessionID(), msg)
-		}
+	if err != nil || session == nil {
+		log.Printf("Error on session retrieving from cache: %v, SessionID: %v, Message: %v", err, msg.SessionID(), msg)
 		return err
 	}
 
+	// Send data to quickwit
+	s.sendToFTS(msg, session.ProjectID)
+
 	// Handle message
 	switch m := msg.(type) {
-	case *SessionEnd:
+	case *messages.SessionEnd:
 		return s.ch.InsertWebSession(session)
-	case *PerformanceTrackAggr:
+	case *messages.PerformanceTrackAggr:
 		return s.ch.InsertWebPerformanceTrackAggr(session, m)
-	case *MouseClick:
+	case *messages.MouseClick:
 		return s.ch.InsertWebClickEvent(session, m)
-	case *InputEvent:
+	case *messages.InputEvent:
 		return s.ch.InsertWebInputEvent(session, m)
 	// Unique for Web
-	case *PageEvent:
+	case *messages.PageEvent:
 		return s.ch.InsertWebPageEvent(session, m)
-	case *ResourceTiming:
+	case *messages.ResourceTiming:
 		return s.ch.InsertWebResourceEvent(session, m)
-	case *JSException:
+	case *messages.JSException:
 		return s.ch.InsertWebErrorEvent(session, types.WrapJSException(m))
-	case *IntegrationEvent:
+	case *messages.IntegrationEvent:
 		return s.ch.InsertWebErrorEvent(session, types.WrapIntegrationEvent(m))
-	case *IssueEvent:
+	case *messages.IssueEvent:
 		return s.ch.InsertIssue(session, m)
-	case *CustomEvent:
+	case *messages.CustomEvent:
 		return s.ch.InsertCustom(session, m)
-	case *NetworkRequest:
-		project, err := s.pg.Cache.GetProject(session.ProjectID)
-		if err != nil {
-			log.Printf("can't get project: %s", err)
-		} else {
-			if err := s.ch.InsertRequest(session, m, project.SaveRequestPayloads); err != nil {
-				log.Printf("can't insert request event into clickhouse: %s", err)
-			}
+	case *messages.NetworkRequest:
+		if err := s.ch.InsertRequest(session, m, session.SaveRequestPayload); err != nil {
+			log.Printf("can't insert request event into clickhouse: %s", err)
 		}
-	case *GraphQL:
+	case *messages.GraphQL:
 		return s.ch.InsertGraphQL(session, m)
-	case *InputChange:
+	case *messages.InputChange:
 		return s.ch.InsertWebInputDuration(session, m)
-	case *MouseThrashing:
+	case *messages.MouseThrashing:
 		return s.ch.InsertMouseThrashing(session, m)
 	}
 	return nil
