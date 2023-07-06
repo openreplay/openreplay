ae0e9a11673c1617738fdb5ab221b96e8087aaeb
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2023-07-06 14:45:53 +0200

    feat(backend): added comment about TLS for redis

--- backend/internal/config/redis/config.go
+++ backend/internal/config/redis/config.go
@@ -3,7 +3,7 @@ package redis
 import "time"
 
 type Redis struct {
-	ConnectionURL     string        `env:"REDIS_STRING"`
+	ConnectionURL     string        `env:"REDIS_STRING"` // use "rediss" instead of "redis" schema for TLS connection
 	MaxLength         int64         `env:"REDIS_STREAMS_MAX_LEN,default=100000"`
 	ReadCount         int64         `env:"REDIS_STREAMS_READ_COUNT,default=1"`
 	ReadBlockDuration time.Duration `env:"REDIS_STREAMS_READ_BLOCK_DURATION,default=200ms"`
