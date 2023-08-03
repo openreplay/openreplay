81f70ddd4d81fce51db4437c6238bfadd60dbef7
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2023-08-03 18:05:36 +0400

    feat(backend): logic improvements in sessionEnder

--- backend/cmd/ender/main.go
+++ backend/cmd/ender/main.go
@@ -77,28 +77,69 @@ func main() {
 		case <-tick:
 			failedSessionEnds := make(map[uint64]uint64)
 			duplicatedSessionEnds := make(map[uint64]uint64)
+			negativeDuration := make(map[uint64]uint64)
+			shorterDuration := make(map[uint64]int64)
+			diffDuration := make(map[uint64]int64)
+			noSessionInDB := make(map[uint64]uint64)
+			updatedDurations := 0
+			newSessionEnds := 0
+
+			type SessionEndType int
+			const (
+				FailedSessionEnd SessionEndType = iota + 1
+				DuplicatedSessionEnd
+				NegativeDuration
+				ShorterDuration
+				DiffDuration
+				NewSessionEnd
+				NoSessionInDB
+			)
 
 			// Find ended sessions and send notification to other services
-			sessions.HandleEndedSessions(func(sessionID uint64, timestamp uint64) bool {
+			sessions.HandleEndedSessions(func(sessionID uint64, timestamp uint64) (bool, int) {
 				msg := &messages.SessionEnd{Timestamp: timestamp}
 				currDuration, err := pg.GetSessionDuration(sessionID)
 				if err != nil {
 					log.Printf("getSessionDuration failed, sessID: %d, err: %s", sessionID, err)
+				}
+				sess, err := pg.Cache.GetSession(sessionID)
+				if err != nil {
+					log.Printf("can't get session from database to compare durations, sessID: %d, err: %s", sessionID, err)
+				} else {
+					newDur := timestamp - sess.Timestamp
+					// Skip if session was ended before with same duration
+					if currDuration == newDur {
+						duplicatedSessionEnds[sessionID] = currDuration
+						return true, int(DuplicatedSessionEnd)
+					}
+					// Skip if session was ended before with longer duration
+					if currDuration > newDur {
+						shorterDuration[sessionID] = int64(currDuration) - int64(newDur)
+						return true, int(ShorterDuration)
+					}
 				}
 				newDuration, err := pg.InsertSessionEnd(sessionID, msg.Timestamp)
 				if err != nil {
 					if strings.Contains(err.Error(), "integer out of range") {
 						// Skip session with broken duration
 						failedSessionEnds[sessionID] = timestamp
-						return true
+						return true, int(FailedSessionEnd)
+					}
+					if strings.Contains(err.Error(), "is less than zero for uint64") {
+						negativeDuration[sessionID] = timestamp
+						return true, int(NegativeDuration)
+					}
+					if strings.Contains(err.Error(), "no rows in result set") {
+						noSessionInDB[sessionID] = timestamp
+						return true, int(NoSessionInDB)
 					}
 					log.Printf("can't save sessionEnd to database, sessID: %d, err: %s", sessionID, err)
-					return false
+					return false, 0
 				}
+				// Check one more time just in case
 				if currDuration == newDuration {
-					// Skip session end duplicate
 					duplicatedSessionEnds[sessionID] = currDuration
-					return true
+					return true, int(DuplicatedSessionEnd)
 				}
 				if cfg.UseEncryption {
 					if key := storage.GenerateEncryptionKey(); key != nil {
@@ -111,22 +152,34 @@ func main() {
 				}
 				if err := producer.Produce(cfg.TopicRawWeb, sessionID, msg.Encode()); err != nil {
 					log.Printf("can't send sessionEnd to topic: %s; sessID: %d", err, sessionID)
-					return false
+					return false, 0
 				}
-				return true
+				if currDuration != 0 {
+					diffDuration[sessionID] = int64(newDuration) - int64(currDuration)
+					updatedDurations++
+				} else {
+					newSessionEnds++
+				}
+				return true, int(NewSessionEnd)
 			})
-			if len(failedSessionEnds) > 0 {
-				log.Println("sessions with wrong duration:", failedSessionEnds)
+			if n := len(failedSessionEnds); n > 0 {
+				log.Println("sessions with wrong duration:", n, failedSessionEnds)
 			}
-			if len(duplicatedSessionEnds) > 0 {
-				log.Println("session end duplicates:", duplicatedSessionEnds)
+			if n := len(negativeDuration); n > 0 {
+				log.Println("sessions with negative duration:", n, negativeDuration)
 			}
+			if n := len(noSessionInDB); n > 0 {
+				log.Printf("sessions without info in DB: %d, %v", n, noSessionInDB)
+			}
+			log.Printf("[INFO] failed: %d, negative: %d, shorter: %d, same: %d, updated: %d, new: %d, not found: %d",
+				len(failedSessionEnds), len(negativeDuration), len(shorterDuration), len(duplicatedSessionEnds),
+				updatedDurations, newSessionEnds, len(noSessionInDB))
 			producer.Flush(cfg.ProducerTimeout)
 			if err := consumer.CommitBack(intervals.EVENTS_BACK_COMMIT_GAP); err != nil {
 				log.Printf("can't commit messages with offset: %s", err)
 			}
 		case msg := <-consumer.Rebalanced():
-			log.Println(msg)
+			log.Printf("Rebalanced: %+v", msg)
 		default:
 			if !memoryManager.HasFreeMemory() {
 				continue
