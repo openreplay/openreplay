2dc2a9d0a12a598fc5aaa1c1fb0dbaa1418cae43
nick-delirium <nikita@openreplay.com>
2023-08-04 17:14:06 +0200

    fix(tracker): remove beacons from tracker

--- tracker/tracker/src/main/modules/tabs.ts
+++ tracker/tracker/src/main/modules/tabs.ts
@@ -9,20 +9,5 @@ export default function (app: App): void {
     }
   }
 
-  function sendBeacon() {
-    const data = {
-      tabId: app.session.getTabId(),
-      token: app.session.getSessionToken(),
-    }
-    const headers = {
-      'Content-Type': 'application/json',
-    }
-    // @ts-ignore
-    const blob = new Blob([JSON.stringify(data)], headers)
-
-    navigator.sendBeacon(app.options.ingestPoint + '/v1/web/beacon', blob)
-  }
-  app.attachEventListener(window, 'beforeunload', sendBeacon as EventListener, false, false)
-
   app.attachEventListener(window, 'focus', changeTab as EventListener, false, false)
 }
