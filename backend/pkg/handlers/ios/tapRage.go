7dbeb04b4b5fe25c902bbc5959ab9e4427db9523
Alexander Zavorotynskiy <zavorotynskiy@pm.me>
2023-09-08 10:36:10 +0200

    feat(backend): added payload for tapRage events

--- backend/pkg/handlers/ios/tapRage.go
+++ backend/pkg/handlers/ios/tapRage.go
@@ -1,6 +1,8 @@
 package ios
 
 import (
+	"encoding/json"
+	"log"
 	"openreplay/backend/pkg/handlers"
 	. "openreplay/backend/pkg/messages"
 )
@@ -15,6 +17,34 @@ type TapRageDetector struct {
 	firstInARawTimestamp uint64
 	firstInARawSeqIndex  uint64
 	countsInARow         int
+}
+
+func (h *TapRageDetector) createPayload() string {
+	p, err := json.Marshal(struct{ Count int }{h.countsInARow})
+	if err != nil {
+		log.Printf("can't marshal TapRage payload to json: %s", err)
+		return ""
+	}
+	return string(p)
+}
+
+func (h *TapRageDetector) Build() Message {
+	if h.countsInARow >= MinTapsInARow {
+		event := &IOSIssueEvent{
+			Type:          "tap_rage",
+			ContextString: h.lastLabel,
+			Timestamp:     h.firstInARawTimestamp,
+			Payload:       h.createPayload(),
+		}
+		event.Index = h.firstInARawSeqIndex // Associated Index/ MessageID ?
+		return event
+	}
+	h.lastTimestamp = 0
+	h.lastLabel = ""
+	h.firstInARawTimestamp = 0
+	h.firstInARawSeqIndex = 0
+	h.countsInARow = 0
+	return nil
 }
 
 func (h *TapRageDetector) Handle(message Message, timestamp uint64) Message {
@@ -38,22 +68,4 @@ func (h *TapRageDetector) Handle(message Message, timestamp uint64) Message {
 		event = h.Build()
 	}
 	return event
-}
-
-func (h *TapRageDetector) Build() Message {
-	if h.countsInARow >= MinTapsInARow {
-		event := &IOSIssueEvent{
-			Type:          "tap_rage",
-			ContextString: h.lastLabel,
-			Timestamp:     h.firstInARawTimestamp,
-		}
-		event.Index = h.firstInARawSeqIndex // Associated Index/ MessageID ?
-		return event
-	}
-	h.lastTimestamp = 0
-	h.lastLabel = ""
-	h.firstInARawTimestamp = 0
-	h.firstInARawSeqIndex = 0
-	h.countsInARow = 0
-	return nil
 }
