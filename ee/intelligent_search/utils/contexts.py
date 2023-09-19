0da10c24f7000a09285497e84f0918351bd8ee16
MauricioGarciaS <47052044+MauricioGarciaS@users.noreply.github.com>
2023-09-19 17:03:20 +0200

    feature(intelligent-search): Changed llama.cpp for llama in GPU now contained in API

--- ee/intelligent_search/utils/contexts.py
+++ ee/intelligent_search/utils/contexts.py
@@ -302,3 +302,51 @@ search_context_v2 = """[[AI_BOT]]: We have a SQL table called sessions that cont
 [[USER]]: Can you translate the following text into SQL query: {user_question}
 [[AI_BOT]]:
 """
+
+search_context_v3 = """We have a SQL table called sessions that contains the columns: Click, Text_Input, Visited_URL, Custom_Events, Network_Request->url, Network_Request->status_code, Network_Request->method, Network_Request->duration, GraphQL->name, GraphQL->method, GraphQL->request_body, GraphQL->response_body, State_Action, Error_Message, Issue, User_OS, User_Browser, User_Device, Platform, Version_ID, Referrer, Duration, User_Country, User_City, User_State, User_Id, User_Anonymous_Id, DOM_Complete->time_to_render, DOM_Complete->url, Larges_Contentful_Paint->time_to_load, Larges_Contentful_Paint->url, Time_to_First_Byte->time_to_load, Time_to_First_Byte->url, Avg_CPU_Load->percentage, Avg_CPU_Load->url, Avg_Memory_Usage->percentage, Avg_Memory_Usage->url, Failed_Request->name and Plan.
+[[USER]]: What is the attribute of the Click column?
+[[AI_BOT]]: Click is a string whose value X means that during the session the user clicked in the X
+[[USER]]: What's the attribute of Text_Input?
+[[AI_BOT]]: Text_Input is a string whose value X means that during the sessions the user typed X
+[[USER]]: What's the attribute of Visited_URL?
+[[AI_BOT]]: Visited_URL is a string whose value X means that the user X visited the url path X
+[[USER]]: What's the attribute of Custom_Events?
+[[AI_BOT]]: Custom_Events is a string whose value X means that this event happened during the session
+[[USER]]: What's the attribute of Network_Request?
+[[AI_BOT]]: Network_Request->url is the requested url,  Network_Request->status_code is the status of the request, Network_Request->method is the request method and Network_Request->duration is the duration of the request in miliseconds.
+[[USER]]: What's the attribute of GraphQL
+[[AI_BOT]]: GraphQL->name is the name of the GraphQL event, GraphQL->method is the GraphQL method, GraphQL->request_body is the request payload and GraphQL->response_body is the response
+[[USER]]: What's the attribute of State_Action?
+[[AI_BOT]]: State_Action is a integer
+[[USER]]: What's the attribute of Error_Message?
+[[AI_BOT]]: Error_Message is a string representing the error that arised in the session
+[[USER]]: What's the attribute of Referrer?
+[[AI_BOT]]: Referrer is a string representing the url that refered to the current site
+[[USER]]: What's the attribute of Duration?
+[[AI_BOT]]: Duration is an integer representing the lenght of the session in minutes
+[[USER]]: What's the attribute of User_Country?
+[[AI_BOT]]: User_Country is a string representing the Country of the session
+[[USER]]: What's the attribute of User_City?
+[[AI_BOT]]: User_City is a string representing the City of the session
+[[USER]]: What's the attribute of User_State?
+[[AI_BOT]]: User_State is a string representing the State of City where the session was recorded
+[[USER]]: What's the attribute of User_Id?
+[[AI_BOT]]: User_Id is a string representing the id of the user
+[[USER]]: What's the attribute of User_AnonymousId?
+[[AI_BOT]]: User_AnonymousId is a string representing the anonymous id of the user
+[[USER]]: What's the attribute of DOM_Complete?
+[[AI_BOT]]: DOM_Complete->time_to_render is the time to render the url in miliseconds and DOM_Complete->url is the rendered url
+[[USER]]: What's the attribute of Largest_Contentful_Paint?
+[[AI_BOT]]: Largest_Contentful_Paint->time_to_load is the time to load the heaviest content in miliseconds and Largest_Contentful_Paint is the contents url
+[[USER]]: What's the attribute of Time_to_First_Byte?
+[[AI_BOT]]: Time_to_First_Byte->time_to_load is the time to get the first response byte from url in miliseconds and Time_to_First_Byte->url is the url
+[[USER]]: What's the attribute of Avg_CPU_Load?
+[[AI_BOT]]: Avg_CPU_Load->percentage is an integer representing the porcentage of average cpu load in the url and Avg_CPU_Load->url is the url
+[[USER]]: What's the attribute of Avg_Memory_Usage?
+[[AI_BOT]]: Avg_Memory_Usage->percentage is the porcentage of average memory usage in the url and the Avg_Memory_Usage->url is the url
+[[USER]]: What's the attribute of Failed_Request?
+[[AI_BOT]]: Failed_Request->name is a string representing an url that had a Failed Request event
+[[USER]]: What's the attribute of Plan?
+[[AI_BOT]]: Plan is a string that could be 'pay_as_you_go', 'trial', 'free', 'enterprise'
+[[USER]]: Can you translate the following text into SQL query: {user_question}
+[[AI_BOT]]:"""
--- ee/intelligent_search/utils/llm_request.py
+++ ee/intelligent_search/utils/llm_request.py
@@ -32,7 +32,7 @@ class LLMResponseProcessing:
     def send_question_to_llm(self):
         # {"content":"\n1. Define your website's purpose and goals: Determine why you want to build the website, what information you want to share with users, and how you want to engage with them.\n2. Choose a domain name and web hosting provider: Pick a unique and memorable domain name and select a reliable web hosting provider that meets your needs in terms of space, bandwidth, and technical support.\n3. Plan your website's design and layout: Sketch out a rough wireframe of your website to determine the overall structure, layout, and functionality.\n4. Create content for your website:","generation_settings":{"frequency_penalty":0.0,"grammar":"","ignore_eos":false,"logit_bias":[],"mirostat":0,"mirostat_eta":0.10000000149011612,"mirostat_tau":5.0,"model":"models/7B/llama-2-7b-chat.ggmlv3.q8_0.bin","n_ctx":2048,"n_keep":0,"n_predict":128,"n_probs":0,"penalize_nl":true,"presence_penalty":0.0,"repeat_last_n":64,"repeat_penalty":1.100000023841858,"seed":4294967295,"stop":[],"stream":false,"temp":0.800000011920929,"tfs_z":1.0,"top_k":40,"top_p":0.949999988079071,"typical_p":1.0},"model":"models/7B/llama-2-7b-chat.ggmlv3.q8_0.bin","prompt":" Building a website can be done in 10 simple steps:","stop":true,"stopped_eos":false,"stopped_limit":true,"stopped_word":false,"stopping_word":"","timings":{"predicted_ms":26957.898,"predicted_n":127,"predicted_per_second":4.711049800692917,"predicted_per_token_ms":212.26691338582677,"prompt_ms":1684.0330000000001,"prompt_n":14,"prompt_per_second":8.313376281818705,"prompt_per_token_ms":120.28807142857144},"tokens_cached":141,"tokens_evaluated":14,"tokens_predicted":128,"truncated":false}
         # TODO: Save all metrics and save them into DB (included question and answer) (two tables maybe?)
-        # TODO: Send request only if llm available to get loads, save for later otherwise 
+        # TODO: Send request only if llm available to get loads, save for later otherwise
         if not self.queue.empty():
             question = self.queue.get()
             return filter_sql_where_statement(call_llm_endpoint(self.host, self.port, 'completion', question, **self.extra_params)['content'])
@@ -43,3 +43,4 @@ class LLMResponseProcessing:
         while not self.queue.empty():
             responses.append(self.send_question_to_llm())
         return responses
+
