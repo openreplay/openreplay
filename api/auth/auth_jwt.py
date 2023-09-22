a0f44d974c815f82b4600d0fbc901b5d466b4599
Taha Yassine Kraiem <tahayk2@gmail.com>
2023-09-23 00:20:12 +0100

    feat(chalice): refresh token debug

--- api/auth/auth_jwt.py
+++ api/auth/auth_jwt.py
@@ -27,7 +27,7 @@ class JWTAuth(HTTPBearer):
         super(JWTAuth, self).__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request) -> Optional[schemas.CurrentContext]:
-        if request.url.path == "/refresh":
+        if request.url.path in ["/refresh", "/api/refresh"]:
             refresh_token = request.cookies.get("refreshToken")
             jwt_payload = authorizers.jwt_refresh_authorizer(scheme="Bearer", token=refresh_token)
             if jwt_payload is None or jwt_payload.get("jti") is None:
