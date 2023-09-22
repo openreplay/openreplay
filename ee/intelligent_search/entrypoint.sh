3a090ec595718e3c674ea7a6ef241f1057a453ce
MauricioGarciaS <47052044+MauricioGarciaS@users.noreply.github.com>
2023-09-22 15:42:59 +0200

    Updated Dockerfile to use GPU and download LLM from S3

--- ee/intelligent_search/entrypoint.sh
+++ ee/intelligent_search/entrypoint.sh
@@ -1,1 +1,6 @@
+find /api/ -type f -name "download_llm.sh" -exec sed -i "s#{{S3_LLM_DIR}}#${S3_LLM_DIR}#g" {} \;
+find /api/ -type f -name "download_llm.sh" -exec sed -i "s#{{CHECKPOINT_DIR}}#${CHECKPOINT_DIR}#g" {} \;
+find /api/ -type f -name "download_llm.sh" -exec sed -i "s#{{S3_TOKENIZER_PATH}}#${S3_TOKENIZER_PATH}#g" {} \;
+find /api/ -type f -name "download_llm.sh" -exec sed -i "s#{{TOKENIZER_PATH}}#${TOKENIZER_PATH}#g" {} \;
+./download_llm.sh
 uvicorn main:app --host 0.0.0.0 --port 8082
