3a34ad50264725fa2478d57a0faf9994787ef234
nick-delirium <nikita@openreplay.com>
2023-10-12 12:33:57 +0200

    fix(tracker): add "thank you" section, refactor file to make it readable

--- tracker/tracker/src/main/modules/userTesting.ts
+++ tracker/tracker/src/main/modules/userTesting.ts
@@ -1,42 +1,40 @@
+function createElement(tag: string, className: string, styles: any, textContent?: string) {
+  const element = document.createElement(tag)
+  element.className = className
+  Object.assign(element.style, styles)
+  if (textContent) {
+    element.textContent = textContent
+  }
+  return element
+}
+
 class UserTestManager {
-  private readonly bg = document.createElement('div')
-  private readonly container = document.createElement('div')
+  private readonly bg = createElement('div', 'bg', bgStyle)
+  private readonly container = createElement('div', 'container', containerStyle)
   private widgetGuidelinesVisible = true
   private widgetTasksVisible = false
+  private widgetVisible = true
+  private descriptionSection: HTMLElement | null = null
+  private taskSection: HTMLElement | null = null
+  private endSection: HTMLElement | null = null
+  private stopButton: HTMLElement | null = null
 
   createGreeting(title: string, micRequired: boolean, cameraRequired: boolean) {
-    // Create background
-    this.bg.className = 'bg_or'
-    Object.assign(this.bg.style, bgStyle)
+    const titleElement = createElement('div', 'title', titleStyle, title)
+    const descriptionElement = createElement(
+      'div',
+      'description',
+      descriptionStyle,
+      'Welcome, this session will be recorded. You have complete control, and can stop the session at any time.',
+    )
+    const noticeElement = createElement(
+      'div',
+      'notice',
+      noticeStyle,
+      'Please note that your audio, video, and screen will be recorded for research purposes during this test.',
+    )
+    const buttonElement = createElement('div', 'button', buttonStyle, 'Read guidelines to begin')
 
-    // Create container
-    this.container.className = 'container_or'
-    Object.assign(this.container.style, containerStyle)
-
-    // Create title
-    const titleElement = document.createElement('div')
-    titleElement.className = 'title_or'
-    titleElement.textContent = title
-    Object.assign(titleElement.style, titleStyle)
-
-    // Create description
-    const descriptionElement = document.createElement('div')
-    descriptionElement.className = 'description_or'
-    descriptionElement.textContent =
-      'Welcome, this session will be recorded. You have complete control, and can stop the session at any time.'
-    Object.assign(descriptionElement.style, descriptionStyle)
-
-    // Create notice
-    const noticeElement = document.createElement('div')
-    noticeElement.className = 'notice_or'
-    noticeElement.textContent =
-      'Please note that your audio, video, and screen will be recorded for research purposes during this test.'
-    Object.assign(noticeElement.style, noticeStyle)
-
-    // Create button
-    const buttonElement = document.createElement('div')
-    buttonElement.className = 'button_or'
-    buttonElement.textContent = 'Read guidelines to begin'
     buttonElement.onclick = () => {
       this.container.innerHTML = ''
       this.showWidget(
@@ -57,18 +55,9 @@ class UserTestManager {
         ],
       )
     }
-    Object.assign(buttonElement.style, buttonStyle)
 
-    // Append elements to container
-    this.container.appendChild(titleElement)
-    this.container.appendChild(descriptionElement)
-    this.container.appendChild(noticeElement)
-    this.container.appendChild(buttonElement)
-
-    // Append container to background
+    this.container.append(titleElement, descriptionElement, noticeElement, buttonElement)
     this.bg.appendChild(this.container)
-
-    // Append background to body
     document.body.appendChild(this.bg)
   }
 
@@ -93,128 +82,124 @@ class UserTestManager {
     })
     // Create title section
     const titleSection = this.createTitleSection()
-    this.container.appendChild(titleSection)
     Object.assign(this.container.style, containerWidgetStyle)
-
-    // Create description section
     const descriptionSection = this.createDescriptionSection(description)
-    this.container.appendChild(descriptionSection)
-
-    // Create tasks section
     const tasksSection = this.createTasksSection(tasks)
-    this.hideTaskSection()
-    this.container.appendChild(tasksSection)
+    const stopButton = createElement('div', 'stop_bn_or', stopWidgetStyle, 'Abort Session')
 
-    // Create stop button
-    const stopButton = document.createElement('div')
-    stopButton.className = 'stop'
-    stopButton.textContent = 'Abort Session'
-    Object.assign(stopButton.style, stopWidgetStyle)
-    this.container.appendChild(stopButton)
+    this.container.append(titleSection, descriptionSection, tasksSection, stopButton)
+    this.taskSection = tasksSection
+    this.descriptionSection = descriptionSection
+    this.stopButton = stopButton
+    this.hideTaskSection()
   }
 
   createTitleSection() {
-    const title = document.createElement('div')
-    title.className = 'title'
-    Object.assign(title.style, titleWidgetStyle)
+    const title = createElement('div', 'title', titleWidgetStyle)
+    const leftIcon = createElement('div', 'left_icon', {}, '(icn)')
+    const titleText = createElement('div', 'title_text', {}, 'Test name goes here')
+    const rightIcon = createElement('div', 'right_icon', { marginLeft: 'auto' }, '(icn)')
 
-    const leftIcon = document.createElement('div')
-    leftIcon.textContent = '(icn)'
-    const titleText = document.createElement('div')
-    titleText.textContent = 'Test name goes here'
-    const rightIcon = document.createElement('div')
-    rightIcon.textContent = '(icn)'
-    rightIcon.style.marginLeft = 'auto'
+    title.append(leftIcon, titleText, rightIcon)
 
-    title.appendChild(leftIcon)
-    title.appendChild(titleText)
-    title.appendChild(rightIcon)
-
+    const toggleWidget = (isVisible: boolean) => {
+      this.widgetVisible = isVisible
+      Object.assign(
+        this.container.style,
+        this.widgetVisible
+          ? containerWidgetStyle
+          : { border: 'none', background: 'none', padding: 0 },
+      )
+      if (this.taskSection) {
+        Object.assign(
+          this.taskSection.style,
+          this.widgetVisible ? descriptionWidgetStyle : { display: 'none' },
+        )
+      }
+      if (this.descriptionSection) {
+        Object.assign(
+          this.descriptionSection.style,
+          this.widgetVisible ? descriptionWidgetStyle : { display: 'none' },
+        )
+      }
+      if (this.endSection) {
+        Object.assign(
+          this.endSection.style,
+          this.widgetVisible ? descriptionWidgetStyle : { display: 'none' },
+        )
+      }
+      if (this.stopButton) {
+        Object.assign(
+          this.stopButton.style,
+          this.widgetVisible ? stopWidgetStyle : { display: 'none' },
+        )
+      }
+      return isVisible
+    }
+    title.onclick = () => toggleWidget(!this.widgetVisible)
+    this.collapseWidget = () => toggleWidget(false)
     return title
   }
 
   createDescriptionSection(description: string[]) {
-    const section = document.createElement('div')
-    section.className = 'description_or'
-    Object.assign(section.style, descriptionWidgetStyle)
-
-    const titleContainer = document.createElement('div')
-    const title = document.createElement('div')
-    const icon = document.createElement('div')
-
-    titleContainer.className = 'descrtitle_or'
-    title.textContent = 'Introduction & Guidelines'
-    icon.textContent = '(icn)'
-
-    titleContainer.appendChild(title)
-    titleContainer.appendChild(icon)
-    Object.assign(titleContainer.style, descrtitleStyle)
-
-    const content = document.createElement('div')
-    Object.assign(content.style, contentStyle)
-
+    const section = createElement('div', 'description_section_or', descriptionWidgetStyle)
+    const titleContainer = createElement('div', 'descrtitle', descrtitleStyle)
+    const title = createElement('div', 'title', {}, 'Introduction & Guidelines')
+    const icon = createElement('div', 'icon', symbolIcon, '-')
+    const content = createElement('div', 'content', contentStyle)
     const ul = document.createElement('ul')
     ul.innerHTML = description.map((item) => `<li>${item}</li>`).join('')
-    content.appendChild(ul)
+    const button = createElement('div', 'button_begin_or', buttonWidgetStyle, 'Begin Test')
 
-    const button = document.createElement('div')
-    button.className = 'button_or'
-    button.textContent = 'Begin Test'
-    Object.assign(button.style, buttonWidgetStyle)
-    content.appendChild(button)
+    titleContainer.append(title, icon)
+    content.append(ul, button)
+    section.append(titleContainer, content)
 
-    section.appendChild(titleContainer)
-    section.appendChild(content)
-
-    const hideDescription = () => {
-      Object.assign(content.style, {
-        display: 'none',
-      })
-      this.widgetGuidelinesVisible = false
-    }
-    const showDescription = () => {
-      Object.assign(content.style, contentStyle)
-      this.widgetGuidelinesVisible = true
+    const toggleDescriptionVisibility = () => {
+      this.widgetGuidelinesVisible = !this.widgetGuidelinesVisible
+      icon.textContent = this.widgetGuidelinesVisible ? '-' : '+'
+      Object.assign(
+        content.style,
+        this.widgetGuidelinesVisible ? contentStyle : { display: 'none' },
+      )
     }
 
-    titleContainer.onclick = () => {
-      if (this.widgetGuidelinesVisible) {
-        hideDescription()
-      } else {
-        showDescription()
-      }
-    }
+    titleContainer.onclick = toggleDescriptionVisibility
     button.onclick = () => {
-      hideDescription()
+      toggleDescriptionVisibility()
       this.showTaskSection()
     }
+
     return section
   }
 
-  createTasksSection(
-    tasks: {
-      title: string
-      description: string
-    }[],
-  ) {
+  createTasksSection(tasks: { title: string; description: string }[]) {
     let currentTaskIndex = 0
-    const section = document.createElement('div')
-    section.className = 'description_or'
-    Object.assign(section.style, descriptionWidgetStyle)
+    const section = createElement('div', 'task_section_or', descriptionWidgetStyle)
+    const titleContainer = createElement('div', 'descrtitle_or', descrtitleStyle)
+    const title = createElement('div', 'title', {}, 'Tasks')
+    const icon = createElement('div', 'icon', symbolIcon, '-')
+    const content = createElement('div', 'content', contentStyle)
+    const pagination = createElement('div', 'pagination', paginationStyle)
+    const leftArrow = createElement('span', 'leftArrow', {}, '<')
+    const rightArrow = createElement('span', 'rightArrow', {}, '>')
+    const taskCard = createElement('div', 'taskCard', taskDescriptionCard)
+    const taskText = createElement('div', 'taskText', taskTextStyle)
+    const taskDescription = createElement('div', 'taskDescription', taskDescriptionStyle)
+    const taskButtons = createElement('div', 'taskButtons', taskButtonsRow)
+    const closePanelButton = createElement(
+      'div',
+      'closePanelButton',
+      taskButtonStyle,
+      'Collapse panel',
+    )
+    const nextButton = createElement('div', 'nextButton', taskButtonBorderedStyle, 'Done, next')
 
-    const titleContainer = document.createElement('div')
-    const title = document.createElement('div')
-    const icon = document.createElement('div')
-
-    titleContainer.className = 'descrtitle_or'
-    title.textContent = 'Tasks'
-    icon.textContent = '(icn)'
-    titleContainer.appendChild(title)
-    titleContainer.appendChild(icon)
-    Object.assign(titleContainer.style, descrtitleStyle)
-
-    const content = document.createElement('div')
-    Object.assign(content.style, contentStyle)
+    titleContainer.append(title, icon)
+    taskCard.append(taskText, taskDescription)
+    taskButtons.append(closePanelButton, nextButton)
+    content.append(pagination, taskCard, taskButtons)
+    section.append(titleContainer, content)
 
     const updateTaskContent = () => {
       const task = tasks[currentTaskIndex]
@@ -222,41 +207,37 @@ class UserTestManager {
       taskDescription.textContent = task.description
     }
 
-    // Pagination
-    const pagination = document.createElement('div')
-    Object.assign(pagination.style, paginationStyle)
-
-    const leftArrow = document.createElement('span')
-    leftArrow.textContent = '<'
-    const rightArrow = document.createElement('span')
-    rightArrow.textContent = '>'
-
     pagination.appendChild(leftArrow)
-
     tasks.forEach((_, index) => {
-      const pageNumber = document.createElement('span')
+      const pageNumber = createElement('span', `or_task_${index}`, {}, (index + 1).toString())
       pageNumber.id = `or_task_${index}`
-      pageNumber.textContent = (index + 1).toString()
-      pagination.appendChild(pageNumber)
+      pagination.append(pageNumber)
     })
-
     pagination.appendChild(rightArrow)
 
-    // Task content
-    const taskText = document.createElement('div')
-    Object.assign(taskText.style, taskTextStyle)
+    const toggleTasksVisibility = () => {
+      this.widgetTasksVisible = !this.widgetTasksVisible
+      icon.textContent = this.widgetTasksVisible ? '-' : '+'
+      Object.assign(content.style, this.widgetTasksVisible ? contentStyle : { display: 'none' })
+    }
+    this.hideTaskSection = () => {
+      icon.textContent = '+'
+      Object.assign(content.style, {
+        display: 'none',
+      })
+      this.widgetTasksVisible = false
+      return false
+    }
+    this.showTaskSection = () => {
+      icon.textContent = '-'
+      Object.assign(content.style, contentStyle)
+      this.widgetTasksVisible = true
+      return true
+    }
 
-    const taskDescription = document.createElement('div')
-    Object.assign(taskDescription.style, taskDescriptionStyle)
+    titleContainer.onclick = toggleTasksVisibility
+    closePanelButton.onclick = this.collapseWidget
 
-    // Task buttons
-    const taskButtons = document.createElement('div')
-    const closePanelButton = document.createElement('button')
-    closePanelButton.textContent = 'Collapse panel'
-    Object.assign(closePanelButton.style, taskButtonStyle)
-
-    const nextButton = document.createElement('button')
-    nextButton.textContent = 'Done, next'
     nextButton.onclick = () => {
       if (currentTaskIndex < tasks.length - 1) {
         currentTaskIndex++
@@ -271,50 +252,15 @@ class UserTestManager {
             Object.assign(taskEl.style, taskNumberDone)
           }
         }
-      }
-    }
-    Object.assign(nextButton.style, taskButtonStyle)
-
-    taskButtons.appendChild(closePanelButton)
-    taskButtons.appendChild(nextButton)
-
-    content.appendChild(pagination)
-    content.appendChild(taskText)
-    content.appendChild(taskDescription)
-    content.appendChild(taskButtons)
-
-    section.appendChild(titleContainer)
-    section.appendChild(content)
-
-    // Toggle logic
-    const hideTasks = () => {
-      Object.assign(content.style, {
-        display: 'none',
-      })
-      this.widgetTasksVisible = false
-      return false
-    }
-    this.hideTaskSection = hideTasks
-
-    const showTasks = () => {
-      Object.assign(content.style, contentStyle)
-      this.widgetTasksVisible = true
-      return true
-    }
-    this.showTaskSection = showTasks
-
-    closePanelButton.onclick = () => hideTasks()
-    titleContainer.onclick = () => {
-      if (this.widgetTasksVisible) {
-        hideTasks()
       } else {
-        showTasks()
+        this.showEndSection()
       }
     }
 
     updateTaskContent()
     setTimeout(() => {
       const firstTaskEl = document.getElementById('or_task_0')
+      console.log(firstTaskEl, taskNumberActive)
       if (firstTaskEl) {
         Object.assign(firstTaskEl.style, taskNumberActive)
       }
@@ -322,8 +268,45 @@ class UserTestManager {
     return section
   }
 
+  showEndSection() {
+    const section = createElement('div', 'end_section_or', endSectionStyle)
+    const title = createElement(
+      'div',
+      'end_title_or',
+      {
+        fontSize: '1.25rem',
+        fontWeight: '500',
+      },
+      'Thank you! 👍',
+    )
+    const description = createElement(
+      'div',
+      'end_description_or',
+      {},
+      'Thank you for participating in our user test. Your feedback has been captured and will be used to enhance our website. \n' +
+        '\n' +
+        'We appreciate your time and valuable input.',
+    )
+    const button = createElement('div', 'end_button_or', buttonWidgetStyle, 'End Session')
+
+    if (this.taskSection) {
+      this.container.removeChild(this.taskSection)
+    }
+    if (this.descriptionSection) {
+      this.container.removeChild(this.descriptionSection)
+    }
+    if (this.stopButton) {
+      this.container.removeChild(this.stopButton)
+    }
+
+    section.append(title, description, button)
+    this.endSection = section
+    this.container.append(section)
+  }
+
   hideTaskSection = () => false
   showTaskSection = () => true
+  collapseWidget = () => false
 }
 
 // Styles
@@ -446,6 +429,7 @@ const titleWidgetStyle = {
 
 const descriptionWidgetStyle = {
   boxSizing: 'border-box',
+  display: 'block',
   width: '100%',
   borderRadius: '0.375rem',
   border: '1px solid #D9D9D9',
@@ -458,6 +442,21 @@ const descriptionWidgetStyle = {
   fontStyle: 'normal',
   fontWeight: '400',
   lineHeight: '1.375rem',
+}
+
+const endSectionStyle = {
+  ...descriptionWidgetStyle,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '0.625rem',
+}
+
+const symbolIcon = {
+  fontSize: '1.25rem',
+  fontWeight: '500',
+  cursor: 'pointer',
+  color: '#394EFF',
 }
 
 const buttonWidgetStyle = {
@@ -485,6 +484,7 @@ const buttonWidgetStyle = {
 const stopWidgetStyle = {
   marginTop: '2rem',
   cursor: 'pointer',
+  display: 'block',
 }
 
 const paginationStyle = {
@@ -522,10 +522,9 @@ const taskDescriptionCard = {
   background: '#F5F7FF',
   boxShadow: '0px 2px 0px 0px rgba(0, 0, 0, 0.04)',
   display: 'flex',
+  flexDirection: 'column',
   padding: '0.625rem 0.9375rem',
-  justifyContent: 'center',
-  alignItems: 'flex-start',
-  gap: '0.625rem',
+  gap: '0.5rem',
   alignSelf: 'stretch',
 }
 
@@ -534,12 +533,37 @@ const taskTextStyle = {
 }
 
 const taskDescriptionStyle = {
-  color: 'rgba(0, 0, 0, 0.85)',
+  color: '#8C8C8C',
 }
 
 const taskButtonStyle = {
   marginRight: '0.5rem',
   cursor: 'pointer',
+  color: '#394EFF',
+  textAlign: 'center',
+  fontFamily: 'Roboto',
+  fontSize: '0.875rem',
+  fontStyle: 'normal',
+  fontWeight: '500',
+  lineHeight: '1.375rem',
+}
+const taskButtonBorderedStyle = {
+  ...taskButtonStyle,
+  display: 'flex',
+  padding: '0.25rem 0.9375rem',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '0.5rem',
+  borderRadius: '0.25rem',
+  border: '1px solid #394EFF',
+}
+
+const taskButtonsRow = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '100%',
+  boxSizing: 'border-box',
 }
 
 // Usage
