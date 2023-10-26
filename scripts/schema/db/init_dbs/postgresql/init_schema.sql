ac50dfbebb677942b40ef46e62090d372bdced8e
Shekar Siri <sshekarsiri@gmail.com>
2023-10-26 17:47:03 +0200

    fix(ui): assist stats sql

--- scripts/schema/db/init_dbs/postgresql/init_schema.sql
+++ scripts/schema/db/init_dbs/postgresql/init_schema.sql
@@ -1124,8 +1124,8 @@ $$
                 event_type  varchar NOT NULL,
                 event_state varchar NOT NULL,
                 timestamp   integer NOT NULL,
-                user_id     varchar,
-                agent_id    varchar
+                duration    integer,
+                agent_id    integer
             );
 
             CREATE TABLE IF NOT EXISTS public.assist_events_aggregates
