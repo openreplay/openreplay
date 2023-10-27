405a13efe5825a6d3a6ca7d6b13561c632ccf54b
Taha Yassine Kraiem <tahayk2@gmail.com>
2023-10-27 15:07:37 +0200

    fix(chalice): changed server level logging

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-uvicorn app:app --host 0.0.0.0 --port $LISTEN_PORT --proxy-headers --log-level ${LOGLEVEL:-warning}
+uvicorn app:app --host 0.0.0.0 --port $LISTEN_PORT --proxy-headers --log-level ${S_LOGLEVEL:-warning}
