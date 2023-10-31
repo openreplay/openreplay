31e97bae0e8fb6b9c911838a5f390b71e1f502da
rjshrjndrn <rjshrjndrn@gmail.com>
2023-10-31 19:40:29 +0100

    frotend: adding asist creds
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- frontend/app/player/web/assist/AssistManager.ts
+++ frontend/app/player/web/assist/AssistManager.ts
@@ -152,6 +152,7 @@ export default class AssistManager {
       const urlObject = new URL(window.env.API_EDP || window.location.origin) // does it handle ssl automatically?
 
       const socket: Socket = this.socket = io(urlObject.origin, {
+        withCredentials: true,
         multiplex: true,
         transports: ['websocket'],
         path: '/ws-assist/socket',
