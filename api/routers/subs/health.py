019729e13909d6bcdab05a6affa9788a6f359c3a
Amirouche BOUBEKKI <amirouche@openreplay.com>
2023-11-03 11:55:29 +0100

    api: make all routes async functions.

--- api/routers/subs/health.py
+++ api/routers/subs/health.py
@@ -8,7 +8,7 @@ public_app, app, app_apikey = get_routers()
 
 
 @app.get('/healthz', tags=["health-check"])
-def get_global_health_status():
+async def get_global_health_status():
     if config("LOCAL_DEV", cast=bool, default=False):
         return {"data": ""}
     return {"data": health.get_health()}
@@ -16,7 +16,7 @@ def get_global_health_status():
 
 if not tenants.tenants_exists(use_pool=False):
     @public_app.get('/health', tags=["health-check"])
-    def get_public_health_status():
+    async def get_public_health_status():
         if tenants.tenants_exists():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not Found")
 
