7d63adcc0aa38280257d10d8cb57d4f4595453d7
Alexander <zavorotynskiy@pm.me>
2023-11-06 14:35:11 +0100

    feat(assist): removed unnecessary _ symbol from app name

--- assist/utils/metrics.js
+++ assist/utils/metrics.js
@@ -3,7 +3,7 @@ const client = require('prom-client')
 // Create a Registry which registers the metrics
 const register = new client.Registry()
 register.setDefaultLabels({
-    app: 'assist_'
+    app: 'assist'
 })
 
 // Enable the collection of default metrics
