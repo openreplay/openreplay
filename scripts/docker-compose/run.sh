32ba042847c7c6babf1c6a3a74df6b7ed0709005
rjshrjndrn <rjshrjndrn@gmail.com>
2023-11-07 17:24:53 +0100

    docker-compose: init scripts

--- scripts/docker-compose/run.sh
+++ scripts/docker-compose/run.sh
@@ -1,9 +1,9 @@
-# Load variables from root_var.env into the current shell's environment
+# Load variables from common.env into the current shell's environment
 set -a  # automatically export all variables
 source common.env
 set +a
 
 # Use the `envsubst` command to substitute the shell environment variables into reference_var.env and output to a combined .env
-find ./ -type f -iname "*.env" ! -path './common.env' -exec /bin/bash -c "git checkout -- {}; cp {} {}.bak; envsubst < {}.bak > {}; rm {}.bak" \;
+find ./ -type f -iname "*.env" -iname docker-compose.yml ! -path './common.env' -exec /bin/bash -c "git checkout -- {}; cp {} {}.bak; envsubst < {}.bak > {}; rm {}.bak" \;
 
-docker-compose up -d
+sudo -E docker-compose up -d
