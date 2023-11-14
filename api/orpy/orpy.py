4ea05f269edb7c2360e07e26679dea4f39572887
ⵄⵎⵉⵔⵓⵛ ⴰⵜ ⴰⵎⴰⵣⵉⵖ <amirouche@hyper.dev>
2023-11-14 16:37:42 +0100

    wip

--- api/orpy/orpy.py
+++ api/orpy/orpy.py
@@ -25,7 +25,7 @@ def make_timestamper():
     loop = asyncio.get_event_loop()
 
     def timestamp():
-        # Wanna be faster than datetime.now().timestamp()
+        # Faster than datetime.now().timestamp()
         # approximation of current epoch time in float seconds
         out = start + loop.time() - start_monotonic
         return out
@@ -33,16 +33,31 @@ def make_timestamper():
     return timestamp
 
 
-Application = namedtuple("orpy", ("database", "http", "make_timestamp"))
+Application = namedtuple(
+    "Application",
+    (
+        "database",
+        "http",
+        "make_timestamp",
+        # Background loop, and its tasks
+        "loop",
+        "tasks",
+    ),
+)
+
+
+def spawn(coroutine):
+    context.get().tasks.add(coroutine)
 
 
 # TODO: use uvicorn lifespan
 async def make_application():
-    # TODO: integrate with python stdlib logging
+    # https://loguru.readthedocs.io/en/stable/resources/migration.html
     log.remove()
     log.add(sys.stderr, enqueue=True, backtrace=True, diagnose=ORPY_DEBUG)
 
     # TODO: replace with uvicorn lifespan
+    # TODO: pick configuration from .env with decouple
     database = psycopg_pool.AsyncConnectionPool(
         "dbname=amirouche user=amirouche password=amirouche"
     )
@@ -55,6 +70,7 @@ async def make_application():
         database,
         http,
         make_timestamp,
+        set(),
     )
 
     return app
@@ -71,17 +87,102 @@ def route(method, *components):
     return wrapper
 
 
-application: Application = ContextVar("orpy", default=None)
-request: dict = ContextVar("scope")
+Context = namedtuple("Context", ["application", "scope", "receive"])
+application: Application = ContextVar("application", default=None)
+context: Context = ContextVar("context", default=None)
 
 
 @route("GET")
-async def helloworld():
-    return 200, [(b"content-type", b"text/plain")], b"hello world from uvicorn"
+async def index():
+    return 200, [(b"content-type", b"text/plain")], b"hello from orpy"
 
 
-async def http(scope, receive, send):
-    path = scope["path"]
+def jsonify(obj):
+    return json.dumps(obj).encode("utf8")
+
+
+async def txn():
+    # TODO: rename s/database/postgresql/g
+    async with context.get().database.connection() as cnx:
+        async with cnx.transaction():
+            yield cnx
+
+
+async def _query_basic_authentication_new_invitation(txn, user_id, invitation_token):
+    # XXX: Investigate whether this will break user password? What does
+    # the table basic authentication do?
+    sql = """
+    UPDATE public.basic_authentication
+    SET invitation_token = %(invitation_token)s,
+        invited_at = timezone('utc'::text, now()),
+        change_pwd_expire_at = NULL,
+        change_pwd_token = NULL
+    WHERE user_id=%(user_id)s
+    """
+    await txn.execute(query, user_id, invitation_token)
+
+
+def _format_invitation_link(token):
+    return "{}{}{}".format(config("SITE_URL"), config("invitation_link"), token)
+
+
+import secrets
+
+
+async def _query_user_by_email(txn, email):
+    sql = """
+    SELECT  users.user_id,
+            -1 AS tenant_id,
+            users.email,
+            users.role,
+            users.name,
+            -1 AS tenant_id,
+            (CASE WHEN users.role = 'owner' THEN TRUE ELSE FALSE END) AS super_admin,
+            (CASE WHEN users.role = 'admin' THEN TRUE ELSE FALSE END) AS admin,
+            (CASE WHEN users.role = 'member' THEN TRUE ELSE FALSE END) AS member,
+            TRUE AS has_password
+    FROM public.users
+    WHERE users.email = %(email)s AND users.deleted_at IS NULL
+    """
+
+    await txn.execute(sql, email)
+    row = await txn.fetchrow()
+    return camelCase(row)
+
+
+def _task_reset_password_link(email):
+    async with txn() as txn:
+        user = await user_by_email(txn, data.email)
+        if user is None:
+            return
+        # TODO: document magic number 64
+        token = secrets.token_urlsafe(64)
+        invitation_link = await user_new_invitation(txn, user["userId"])
+        email = template_forgot_password.format(**user, invitation_link=invitation_link)
+        await email_send(email)
+
+
+@route("GET", "password", "reset-link")
+def public_reset_password_link():
+    data = json.loads(await orpy.get().receive())
+    if not captcha.is_valid(data.captcha):
+        out = jsonify({"errors": ["Invalid capatcha"]})
+        return 400, [(b"content-type", "application/javascript")], out
+    if not context.features.smtp():
+        out = jsonify(
+            {
+                "errors": [
+                    "No SMTP configuration. Please, ask your admin to reset your password manually."
+                ]
+            }
+        )
+        return 400, [(b"content-type", "application/javascript")], out
+    spawn(_task_reset_password_link(email))
+    return 200, [(b"content-type", "application/javascript")], out
+
+
+async def http(send):
+    path = context.get().scope["path"]
 
     if path.startswith("/static/"):
         # XXX: Secure the /static/* route, and avoid people poking at
@@ -161,7 +262,7 @@ async def http(scope, receive, send):
             }
         )
     else:
-        method = scope["method"]
+        method = context.get().scope["method"]
         route = tuple([method] + path.split("/")[1:-1])
 
         log.debug("matching route: {}", route)
@@ -213,8 +314,10 @@ async def http(scope, receive, send):
 async def handle(scope, receive, send):
     log.debug("ASGI scope: {}", scope)
 
+    context.set(Context(application, scope, receive))
+
     if scope["type"] == "http":
-        await http(scope, receive, send)
+        await http(send)
 
 
 def orpy():
