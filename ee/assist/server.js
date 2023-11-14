35e8558fd0afb99e3bc14caad06cab04539a5da7
Alexander <zavorotynskiy@pm.me>
2023-11-14 11:37:43 +0100

    fix(assist): fixed metrics import in assist-ee

--- ee/assist/server.js
+++ ee/assist/server.js
@@ -3,6 +3,16 @@ const {request_logger} = require('./utils/helper');
 const express = require('express');
 const health = require("./utils/health");
 const assert = require('assert').strict;
+const register = require('./utils/metrics').register;
+
+health.healthApp.get('/metrics', async (req, res) => {
+    try {
+        res.set('Content-Type', register.contentType);
+        res.end(await register.metrics());
+    } catch (ex) {
+        res.status(500).end(ex);
+    }
+});
 
 let socket;
 if (process.env.redis === "true") {
