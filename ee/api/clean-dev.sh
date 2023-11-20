f7eca7b45c8a3463a17d81850e72646e46a2acd5
Taha Yassine Kraiem <tahayk2@gmail.com>
2023-11-20 14:49:00 +0100

    feat(chalice): canvas replay feature

--- ee/api/clean-dev.sh
+++ ee/api/clean-dev.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+rm -rf ./app_alerts.py
+rm -rf ./auth/__init__.py
+rm -rf ./auth/auth_apikey.py
+rm -rf ./build.sh
+rm -rf ./build_alerts.sh
+rm -rf ./build_crons.sh
 rm -rf ./chalicelib/core/alerts.py
-#exp rm -rf ./chalicelib/core/alerts_processor.py
 rm -rf ./chalicelib/core/announcements.py
-rm -rf ./chalicelib/core/autocomplete.py
+rm -rf ./chalicelib/core/assist.py
 rm -rf ./chalicelib/core/authorizers.py
+rm -rf ./chalicelib/core/autocomplete.py
 rm -rf ./chalicelib/core/click_maps.py
 rm -rf ./chalicelib/core/collaboration_base.py
 rm -rf ./chalicelib/core/collaboration_msteams.py
 rm -rf ./chalicelib/core/collaboration_slack.py
 rm -rf ./chalicelib/core/countries.py
 rm -rf ./chalicelib/core/custom_metrics_predefined.py
-rm -rf ./chalicelib/core/feature_flags.py
-#exp rm -rf ./chalicelib/core/errors.py
-rm -rf ./chalicelib/core/errors_favorite.py
-#exp rm -rf ./chalicelib/core/events.py
-rm -rf ./chalicelib/core/events_ios.py
 rm -rf ./chalicelib/core/dashboards.py
+rm -rf ./chalicelib/core/errors_favorite.py
+rm -rf ./chalicelib/core/events_ios.py
+rm -rf ./chalicelib/core/feature_flags.py
 rm -rf ./chalicelib/core/funnels.py
+rm -rf ./chalicelib/core/heatmaps.py
 rm -rf ./chalicelib/core/integration_base.py
 rm -rf ./chalicelib/core/integration_base_issue.py
 rm -rf ./chalicelib/core/integration_github.py
@@ -38,18 +43,18 @@ rm -rf ./chalicelib/core/log_tool_stackdriver.py
 rm -rf ./chalicelib/core/log_tool_sumologic.py
 rm -rf ./chalicelib/core/metadata.py
 rm -rf ./chalicelib/core/mobile.py
+rm -rf ./chalicelib/core/performance_event.py
+rm -rf ./chalicelib/core/saved_search.py
 rm -rf ./chalicelib/core/sessions.py
 rm -rf ./chalicelib/core/sessions_assignments.py
-#exp rm -rf ./chalicelib/core/sessions_metas.py
 rm -rf ./chalicelib/core/sessions_mobs.py
-#exp rm -rf ./chalicelib/core/significance.py
 rm -rf ./chalicelib/core/socket_ios.py
 rm -rf ./chalicelib/core/sourcemaps.py
 rm -rf ./chalicelib/core/sourcemaps_parser.py
 rm -rf ./chalicelib/saml
-rm -rf ./chalicelib/utils/html/
 rm -rf ./chalicelib/utils/__init__.py
 rm -rf ./chalicelib/utils/args_transformer.py
+rm -rf ./chalicelib/utils/canvas.py
 rm -rf ./chalicelib/utils/captcha.py
 rm -rf ./chalicelib/utils/dev.py
 rm -rf ./chalicelib/utils/email_handler.py
@@ -58,40 +63,30 @@ rm -rf ./chalicelib/utils/errors_helper.py
 rm -rf ./chalicelib/utils/event_filter_definition.py
 rm -rf ./chalicelib/utils/github_client_v3.py
 rm -rf ./chalicelib/utils/helper.py
+rm -rf ./chalicelib/utils/html/
 rm -rf ./chalicelib/utils/jira_client.py
 rm -rf ./chalicelib/utils/metrics_helper.py
 rm -rf ./chalicelib/utils/pg_client.py
 rm -rf ./chalicelib/utils/smtp.py
 rm -rf ./chalicelib/utils/sql_helper.py
-rm -rf ./chalicelib/utils/strings.py
-rm -rf ./chalicelib/utils/TimeUTC.py
 rm -rf ./chalicelib/utils/storage/generators.py
 rm -rf ./chalicelib/utils/storage/interface.py
 rm -rf ./chalicelib/utils/storage/s3.py
-rm -rf ./routers/app/__init__.py
+rm -rf ./chalicelib/utils/strings.py
+rm -rf ./chalicelib/utils/TimeUTC.py
 rm -rf ./crons/__init__.py
-rm -rf ./routers/subs/__init__.py
-rm -rf ./routers/__init__.py
-rm -rf ./chalicelib/core/assist.py
-rm -rf ./auth/__init__.py
-rm -rf ./auth/auth_apikey.py
-rm -rf ./build.sh
-rm -rf ./build_crons.sh
-rm -rf ./routers/base.py
-rm -rf ./routers/core.py
 rm -rf ./crons/core_crons.py
 rm -rf ./db_changes.sql
 rm -rf ./Dockerfile_bundle
 rm -rf ./entrypoint.bundle.sh
-rm -rf ./chalicelib/core/heatmaps.py
+rm -rf ./routers/__init__.py
+rm -rf ./routers/app/__init__.py
+rm -rf ./routers/base.py
+rm -rf ./routers/core.py
+rm -rf ./routers/subs/__init__.py
 rm -rf ./routers/subs/v1_api.py
-#exp rm -rf ./chalicelib/core/custom_metrics.py
-rm -rf ./chalicelib/core/performance_event.py
-rm -rf ./chalicelib/core/saved_search.py
-rm -rf ./app_alerts.py
-rm -rf ./build_alerts.sh
-rm -rf ./run-dev.sh
 rm -rf ./run-alerts-dev.sh
+rm -rf ./run-dev.sh
 rm -rf ./schemas/overrides.py
 rm -rf ./schemas/schemas.py
 rm -rf ./schemas/transformers_validators.py
