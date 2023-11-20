a9447d35064b6f24b9cf7379df24953449640f90
Rajesh Rajendran <rjshrjndrn@users.noreply.github.com>
2023-11-20 10:00:15 +0100

    refactor(sourcemapreader): renaming references #1676 (#1678)

--- api/chalicelib/core/health.py
+++ api/chalicelib/core/health.py
@@ -27,7 +27,7 @@ HEALTH_ENDPOINTS = {
     "integrations": app_connection_string("integrations-openreplay", 8888, "metrics"),
     "peers": app_connection_string("peers-openreplay", 8888, "health"),
     "sink": app_connection_string("sink-openreplay", 8888, "metrics"),
-    "sourcemaps-reader": app_connection_string(
+    "sourcemapreader": app_connection_string(
         "sourcemapreader-openreplay", 8888, "health"
     ),
     "storage": app_connection_string("storage-openreplay", 8888, "metrics"),
@@ -196,7 +196,7 @@ def get_health():
             "integrations": __check_be_service("integrations"),
             "peers": __check_be_service("peers"),
             "sink": __check_be_service("sink"),
-            "sourcemaps-reader": __check_be_service("sourcemaps-reader"),
+            "sourcemapreader": __check_be_service("sourcemapreader"),
             "storage": __check_be_service("storage")
         },
         "details": __get_sessions_stats,
