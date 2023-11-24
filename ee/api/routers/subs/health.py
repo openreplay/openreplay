3bc51206a13bcecb4f1cd3d52df659ce30bea5a2
Amirouche BOUBEKKI <amirouche@openreplay.com>
2023-11-24 10:18:18 +0100

    Async chalice.core.tenants:tenants_exists, and propagate

--- ee/api/routers/subs/health.py
+++ ee/api/routers/subs/health.py
@@ -20,4 +20,4 @@ if not tenants.tenants_exists_sync(use_pool=False):
         if await tenants.tenants_exists():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not Found")
 
-        return {"data": health.get_health()}
+    return {"data": health.get_health()}
