f393bb48f336f5ebbab56cdf7f3b4a1dde035fbd
nick-delirium <nikita@openreplay.com>
2023-12-01 15:07:01 +0100

    fix(ui): uxt fixes

--- frontend/app/mstore/uxtestingStore.ts
+++ frontend/app/mstore/uxtestingStore.ts
@@ -27,25 +27,24 @@ interface Response {
   duration: number;
 }
 
-const defaultGuidelines = `
-Introduction:localStorage
-Thank you for participating in this important stage of our [Website/App] development. Your insights will help us enhance the [Desktop/Mobile] experience for all users.localStoragelocalStorage
+const defaultGuidelines = `Introduction:
+Thank you for participating in this important stage of our [Website/App] development. Your insights will help us enhance the [Desktop/Mobile] experience for all users.
 
-Before You Begin:localStorage
-• Device: Ensure you're using a [Desktop/Mobile] for this test.localStoragelocalStorage
+Before You Begin:
+• Device: Ensure you're using a [Desktop/Mobile] for this test.
 
-• Environment: Choose a quiet location where you can focus without interruptions.localStoragelocalStorage
+• Environment: Choose a quiet location where you can focus without interruptions.
 
-Test Guidelines:localStorage
-1. Task Flow: You will perform a series of tasks as you normally would when using a [Website/App].localStoragelocalStorage
+Test Guidelines:
+1. Task Flow: You will perform a series of tasks as you normally would when using a [Website/App].
 
-2. Think Aloud: Please verbalize your thoughts. If something is confusing, interesting, or pleasing, let us know.localStoragelocalStorage
+2. Think Aloud: Please verbalize your thoughts. If something is confusing, interesting, or pleasing, let us know.
 
-3. No Right or Wrong: There are no correct answers here, just your honest experience.localStoragelocalStorage
+3. No Right or Wrong: There are no correct answers here, just your honest experience.
 
-4. Pace Yourself: Take your time, there's no rush to complete the tasks quickly.localStoragelocalStorage
+4. Pace Yourself: Take your time, there's no rush to complete the tasks quickly.
 
-5. Technical Issues: If you encounter any issues, please describe what you were attempting to do when the issue occurred.localStorage
+5. Technical Issues: If you encounter any issues, please describe what you were attempting to do when the issue occurred.
 `
 
 export default class UxtestingStore {
