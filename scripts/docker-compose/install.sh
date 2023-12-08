36c373d6efeb6a48a36b84ffb3f8a40fbcabbb23
rjshrjndrn <rjshrjndrn@gmail.com>
2023-12-08 11:23:41 +0100

    docker-compose: fix profiles
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- scripts/docker-compose/install.sh
+++ scripts/docker-compose/install.sh
@@ -88,7 +88,7 @@ set +a
 # Use the `envsubst` command to substitute the shell environment variables into reference_var.env and output to a combined .env
 find ./ -type f \( -iname "*.env" -o -iname "docker-compose.yaml" \) ! -name "common.env" -exec /bin/bash -c 'file="{}"; git checkout -- "$file"; cp "$file" "$file.bak"; envsubst < "$file.bak" > "$file"; rm "$file.bak"' \;
 sudo -E docker-compose pull --no-parallel
-sudo -E docker-compose up -d
+sudo -E docker compose --profile migration up -d
 cp common.env common.env.bak
 echo "🎉🎉🎉  Done! 🎉🎉🎉"
 
