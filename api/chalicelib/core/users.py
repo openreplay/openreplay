0f716a2efbd18ef838f122b9e51651ed09aa346a
amirouche <amirouche@openreplay.com>
2023-12-11 14:23:45 +0100

    fix(api): avoid circular import.

--- api/chalicelib/core/users.py
+++ api/chalicelib/core/users.py
@@ -5,7 +5,6 @@ from decouple import config
 from fastapi import BackgroundTasks
 
 import schemas
-from app import app
 from chalicelib.core import authorizers, metadata, projects
 from chalicelib.core import tenants, assist
 from chalicelib.utils import email_helper, smtp
@@ -573,6 +572,8 @@ def change_jwt_iat_jti(user_id):
 
 
 async def refresh_jwt_iat_jti(user_id):
+    from app import app
+
     async with app.state.postgresql.connection() as cnx:
         query = f"""UPDATE public.users
                                 SET jwt_iat = timezone('utc'::text, now()-INTERVAL '10s'),
