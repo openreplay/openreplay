dcf6d24abdbb524c1573cf5ca68088e5d83c7913
Kraiem Taha Yassine <tahayk2@gmail.com>
2023-12-14 17:55:14 +0100

    fix(chalice): fix experimental sessions search with negative events and performance filters at the same time (#1777)

--- ee/api/chalicelib/core/sessions_exp.py
+++ ee/api/chalicelib/core/sessions_exp.py
@@ -434,7 +434,6 @@ def search_table_of_individual_issues(data: schemas.SessionsSearchPayloadSchema,
         full_args["issues_limit"] = data.limit
         full_args["issues_limit_s"] = (data.page - 1) * data.limit
         full_args["issues_limit_e"] = data.page * data.limit
-        print(full_args)
         main_query = cur.format(f"""SELECT issues.type                             AS name,
                                                  issues.context_string                   AS value,
                                                  COUNT(DISTINCT raw_sessions.session_id) AS session_count,
@@ -1391,15 +1390,15 @@ def search_query_parts_ch(data: schemas.SessionsSearchPayloadSchema, error_statu
                         _value_conditions_not.append(_p)
                         value_conditions_not.append(p)
                 del _value_conditions_not
-                sequence_conditions += value_conditions_not
+                # sequence_conditions += value_conditions_not
                 events_extra_join += f"""LEFT ANTI JOIN ( SELECT DISTINCT session_id
                                         FROM {MAIN_EVENTS_TABLE} AS main
                                         WHERE {' AND '.join(__events_where_basic)}
                                             AND ({' OR '.join(value_conditions_not)})) AS sub USING(session_id)"""
 
-            # if has_values:
-            #     events_conditions = [c for c in list(set(sequence_conditions))]
-            #     events_conditions_where.append(f"({' OR '.join(events_conditions)})")
+            if has_values and len(sequence_conditions) > 0:
+                events_conditions = [c for c in list(set(sequence_conditions))]
+                events_conditions_where.append(f"({' OR '.join(events_conditions)})")
 
             events_query_part = f"""SELECT main.session_id,
                                         MIN(main.datetime) AS first_event_ts,
