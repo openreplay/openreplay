8f8268d0de0a53f24505ee9642b47deb0cbbbe84
nick-delirium <nikita@openreplay.com>
2024-01-02 17:23:39 +0100

    fix(tracker): fix assist tarcker v

--- tracker/tracker-assist/src/index.ts
+++ tracker/tracker-assist/src/index.ts
@@ -8,17 +8,16 @@ import Assist from './Assist.js'
 export default function(opts?: Partial<Options>) {
   return function(app: App | null, appOptions: { __DISABLE_SECURE_MODE?: boolean } = {}) {
     // @ts-ignore
-    if (app === null || !navigator?.mediaDevices?.getUserMedia) { // 93.04% browsers
+    if (app === null || !navigator?.mediaDevices?.getUserMedia) {
       return
     }
     if (!app.checkRequiredVersion || !app.checkRequiredVersion('REQUIRED_TRACKER_VERSION')) {
       console.warn('OpenReplay Assist: couldn\'t load. The minimum required version of @openreplay/tracker@REQUIRED_TRACKER_VERSION is not met')
       return
     }
-    app.notify.log('OpenReplay Assist initializing.')
+
     const assist = new Assist(app, opts, appOptions.__DISABLE_SECURE_MODE)
     app.debug.log(assist)
     return assist
-
   }
 }
