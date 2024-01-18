0c2dd6f9f1ebbc7e4abe58095099415ec86d0ea4
Mehdi Osman <estradino@users.noreply.github.com>
2024-01-18 09:10:38 -0500

    Updated year

--- frontend/app/components/shared/Copyright.tsx
+++ frontend/app/components/shared/Copyright.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Copyright() {
     return (
         <div className="fixed bottom-0 m-auto text-center mb-6 color-gray-medium">
-          © 2023 OpenReplay. All rights reserved. <a className="underline" href="https://openreplay.com/privacy.html" target="_blank">Privacy</a> and <a className="underline" href="https://openreplay.com/terms.html" target="_blank">Terms</a>.
+          © 2024 OpenReplay. All rights reserved. <a className="underline" href="https://openreplay.com/privacy.html" target="_blank">Privacy</a> and <a className="underline" href="https://openreplay.com/terms.html" target="_blank">Terms</a>.
         </div>
     );
 }
