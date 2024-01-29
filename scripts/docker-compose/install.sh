621f63d90ea36c99cdd471dfa147b4d7f5f6054d
Mohammad H Alijany <137192507+alijany@users.noreply.github.com>
2024-01-29 12:08:18 +0330

    fix(scrips) Improve Docker Installation Scripts (#1866)
    * fix(scrips) fix docker installation commands
    
    * fix(scripts) check docker compose installation
    
    * fix(script) revert scripts styles

--- scripts/docker-compose/install.sh
+++ scripts/docker-compose/install.sh
@@ -55,11 +55,24 @@ info "Grabbing latest apt caches"
 sudo apt update
 
 # setup docker
-info "Setting up Docker"
-sudo apt install docker.io docker-compose -y
+# Check if Docker is already installed
+if ! command -v docker &> /dev/null; then
+	info "Setting up Docker"
+	sudo apt install docker.io -y
 
-# enable docker without sudo
-sudo usermod -aG docker "${USER}" || true
+	# enable docker without sudo
+	sudo usermod -aG docker "${USER}" || true
+else
+	echo "Docker is already installed. Skipping Docker installation."
+fi
+
+# Check if Docker Compose is already installed
+if ! command -v docker-compose &>/dev/null && ! command -v docker compose &>/dev/null; then
+	info "Setting up Docker Compose"
+	sudo apt install docker-compose -y
+else
+	echo "Docker Compose is already installed. Skipping Docker Compose installation."
+fi
 
 # Prompt for DOMAIN_NAME input
 echo -e "${GREEN}Please provide your domain name.${NC}"
@@ -115,8 +128,16 @@ case $yn in
 		exit 1;;
 esac
 
-sudo -E docker-compose --parallel 1 pull
-sudo -E docker-compose --profile migration up --force-recreate --build -d
+if command -v docker-compose >/dev/null 2>&1; then
+	# Docker Compose V1 is installed.
+	sudo -E docker-compose --parallel 1 pull
+	sudo -E docker-compose --profile migration up --force-recreate --build -d
+else
+	# Docker Compose V2 or higher is installed.
+	sudo -E docker compose --parallel 1 pull
+	sudo -E docker compose --profile migration up --force-recreate --build -d
+fi
+
 cp common.env common.env.bak
 echo "🎉🎉🎉  Done! 🎉🎉🎉"
 
