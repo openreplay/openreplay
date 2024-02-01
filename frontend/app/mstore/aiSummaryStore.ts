190a70d172cf24fc25b8917ef94ff04f6bd824a4
nick-delirium <nikita@openreplay.com>
2024-02-01 13:36:32 +0100

    feat(ui): rm console log

--- frontend/app/mstore/aiSummaryStore.ts
+++ frontend/app/mstore/aiSummaryStore.ts
@@ -40,35 +40,17 @@ export default class AiSummaryStore {
       while (true) {
         const { done, value } = await reader.read();
         if (done) {
-          // Process any remaining incomplete word at the end of the stream
+          // Processing any remaining incomplete word at the end of the stream
           if (lastIncompleteWord) {
             this.appendText(lastIncompleteWord + ' ');
           }
           break;
         }
         const textChunk = new TextDecoder().decode(value, { stream: true });
-        console.log(textChunk, done, value)
         processTextChunk(textChunk);
       }
     } catch (error) {
       console.log(error);
     }
-
-    // try {
-    //   await new Response(stream).text();
-    // } catch (error) {
-    //   console.log(error);
-    // }
-
-    // while (true) {
-    //   const { value, done } = await reader.read();
-    //   if (done) break;
-    //
-    //   const textChunk = decoder.decode(value, { stream: true });
-    //   console.log(textChunk)
-    //   textChunk.split(" ").forEach(word => {
-    //     this.appendText(word + " ");
-    //   });
-    // }
   };
 }
