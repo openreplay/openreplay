9879bd65bb747600ac875fb9901a2d5f44ed5571
Kraiem Taha Yassine <tahayk2@gmail.com>
2024-02-01 17:56:41 +0100

    feat(DB): DB changes for v1.18.0 (#1871)

---
+++ scripts/schema/db/rollback_dbs/postgresql/1.18.0/1.18.0.sql
@@ -0,0 +1,29 @@
+\set previous_version 'v1.18.0'
+\set next_version 'v1.17.0'
+SELECT openreplay_version()                       AS current_version,
+       openreplay_version() = :'previous_version' AS valid_previous,
+       openreplay_version() = :'next_version'     AS is_next
+\gset
+
+\if :valid_previous
+\echo valid previous DB version :'previous_version', starting DB downgrade to :'next_version'
+BEGIN;
+SELECT format($fn_def$
+CREATE OR REPLACE FUNCTION openreplay_version()
+    RETURNS text AS
+$$
+SELECT '%1$s'
+$$ LANGUAGE sql IMMUTABLE;
+$fn_def$, :'next_version')
+\gexec
+
+--
+
+
+COMMIT;
+
+\elif :is_next
+\echo new version detected :'next_version', nothing to do
+\else
+\warn skipping DB downgrade of :'next_version', expected previous version :'previous_version', found :'current_version'
+\endif
