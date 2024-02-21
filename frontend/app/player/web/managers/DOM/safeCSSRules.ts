2152d1c3db61cdcdde31bd299513cef5e73d5577
Delirium <nikita@openreplay.com>
2024-02-21 16:00:25 +0100

    fix(ui): skip 0 index for orphan sheets, skip -moz- checks for css rules (#1900)

--- frontend/app/player/web/managers/DOM/safeCSSRules.ts
+++ frontend/app/player/web/managers/DOM/safeCSSRules.ts
@@ -1,16 +1,15 @@
 import logger from 'App/logger';
 
 export function insertRule(
-  sheet: { insertRule: (rule: string, index: number) => void },
+  sheet: { insertRule: (rule: string, index?: number) => void },
   msg: { rule: string, index: number }
 ) {
+
   try {
     sheet.insertRule(msg.rule, msg.index)
   } catch (e) {
-    logger.warn(e, msg)
     try {
-      sheet.insertRule(msg.rule, 0) // TODO: index renumeration in case of subsequent rule deletion
-      logger.warn("Inserting rule into 0-index", e, msg)
+      sheet.insertRule(msg.rule)
     } catch (e) {
       logger.warn("Cannot insert rule.", e, msg)
     }
