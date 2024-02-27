e07b7b10bbec49430f70ab75411169f0b4c83df7
rjshrjndrn <rjshrjndrn@gmail.com>
2024-02-27 10:36:06 +0100

    fix(helm): minio init
    For new minio, the command changed for setting access.
    
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- scripts/helmcharts/openreplay/files/minio.sh
+++ scripts/helmcharts/openreplay/files/minio.sh
@@ -2,18 +2,16 @@
 
 set -e
 
-
 cd /tmp
 
 buckets=("mobs" "sessions-assets" "static" "sourcemaps" "sessions-mobile-assets" "quickwit" "vault-data" "records")
 
-
 mc alias set minio $MINIO_HOST $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 
 function init() {
-echo "Initializing minio"
+    echo "Initializing minio"
 
-cat <<EOF > /tmp/lifecycle.json
+    cat <<EOF >/tmp/lifecycle.json
 {
   "Rules": [
     {
@@ -40,31 +38,30 @@ cat <<EOF > /tmp/lifecycle.json
 }
 EOF
 
-for bucket in ${buckets[*]}; do
-mc mb minio/${bucket} || true
-done
-mc ilm import minio/mobs < /tmp/lifecycle.json || true
+    for bucket in ${buckets[*]}; do
+        mc mb minio/${bucket} || true
+    done
+    mc ilm import minio/mobs </tmp/lifecycle.json || true
 
-#####################################################
-# Creating frontend bucket; Do not change this block!
-# !! PUBLIC BUCKETS !!
-#####################################################
-mc mb minio/frontend || true
-mc policy set download minio/frontend || true
-mc policy set download minio/sessions-assets || true
+    #####################################################
+    # Creating public bucket; Do not change this block!
+    # !! PUBLIC BUCKETS !!
+    #####################################################
+    mc policy set download minio/sessions-assets || true
+    mc anonymous set download minio/sessions-assets || true
 
 }
 
 # /bin/bash kafka.sh migrate $migration_versions
 case "$1" in
-    migrate)
-        init
-        ;;
-    init)
-        init
-        ;;
-    *)
-        echo "Unknown operation for minio migration; exiting."
-        exit 1
-        ;;
+migrate)
+    init
+    ;;
+init)
+    init
+    ;;
+*)
+    echo "Unknown operation for minio migration; exiting."
+    exit 1
+    ;;
 esac
