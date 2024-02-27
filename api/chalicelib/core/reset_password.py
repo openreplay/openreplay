4922adf5c939d0490317e765f1934f451180b249
Taha Yassine Kraiem <tahayk2@gmail.com>
2024-02-27 13:25:55 +0100

    refactor(chalice): optimised forget password

--- api/chalicelib/core/reset_password.py
+++ api/chalicelib/core/reset_password.py
@@ -1,5 +1,7 @@
 import logging
 
+from fastapi import BackgroundTasks
+
 import schemas
 from chalicelib.core import users
 from chalicelib.utils import email_helper, captcha, helper, smtp
@@ -7,7 +9,7 @@ from chalicelib.utils import email_helper, captcha, helper, smtp
 logger = logging.getLogger(__name__)
 
 
-def reset(data: schemas.ForgetPasswordPayloadSchema):
+def reset(data: schemas.ForgetPasswordPayloadSchema, background_tasks: BackgroundTasks):
     logger.info(f"forget password request for: {data.email}")
     if helper.allow_captcha() and not captcha.is_valid(data.g_recaptcha_response):
         return {"errors": ["Invalid captcha."]}
@@ -16,7 +18,9 @@ def reset(data: schemas.ForgetPasswordPayloadSchema):
     a_users = users.get_by_email_only(data.email)
     if a_users:
         invitation_link = users.generate_new_invitation(user_id=a_users["userId"])
-        email_helper.send_forgot_password(recipient=data.email, invitation_link=invitation_link)
+        background_tasks.add_task(email_helper.send_forgot_password,
+                                  recipient=data.email,
+                                  invitation_link=invitation_link)
     else:
         logger.warning(f"!!!invalid email address [{data.email}]")
     return {"data": {"state": "A reset link will be sent if this email exists in our system."}}
