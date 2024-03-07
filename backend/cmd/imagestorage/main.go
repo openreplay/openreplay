c2e25fa84b81e17d30ad5575578aa7e1e9ccc031
Alexander <zavorotynskiy@pm.me>
2024-03-07 11:01:35 +0100

    feat(backend): finished image-storage

--- backend/cmd/imagestorage/main.go
+++ backend/cmd/imagestorage/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"openreplay/backend/pkg/objectstorage/store"
 	"os"
 	"os/signal"
 	"strconv"
@@ -12,29 +11,30 @@ import (
 
 	config "openreplay/backend/internal/config/imagestorage"
 	"openreplay/backend/internal/screenshot-handler"
+	"openreplay/backend/pkg/logger"
 	"openreplay/backend/pkg/messages"
 	"openreplay/backend/pkg/metrics"
 	storageMetrics "openreplay/backend/pkg/metrics/imagestorage"
+	"openreplay/backend/pkg/objectstorage/store"
 	"openreplay/backend/pkg/queue"
 )
 
 func main() {
+	ctx := context.Background()
+	log := logger.New()
+	cfg := config.New()
+
 	m := metrics.New()
 	m.Register(storageMetrics.List())
 
-	log.SetFlags(log.LstdFlags | log.LUTC | log.Llongfile)
-
-	cfg := config.New()
-
 	objStore, err := store.NewStore(&cfg.ObjectsConfig)
 	if err != nil {
-		log.Fatalf("can't init object storage: %s", err)
+		log.Fatal(ctx, "can't init object storage: %s", err)
 	}
 
-	srv, err := screenshot_handler.New(cfg, objStore)
+	srv, err := screenshot_handler.New(cfg, log, objStore)
 	if err != nil {
-		log.Printf("can't init storage service: %s", err)
-		return
+		log.Fatal(ctx, "can't init storage service: %s", err)
 	}
 
 	workDir := cfg.FSDir
@@ -45,7 +45,7 @@ func main() {
 			cfg.TopicRawImages,
 		},
 		messages.NewImagesMessageIterator(func(data []byte, sessID uint64) {
-			log.Printf("Received message for session %d\n", sessID)
+			sessCtx := context.WithValue(context.Background(), "sessionID", sessID)
 			checkSessionEnd := func(data []byte) (messages.Message, error) {
 				reader := messages.NewBytesReader(data)
 				msgType, err := reader.ReadUint()
@@ -64,13 +64,13 @@ func main() {
 
 			if _, err := checkSessionEnd(data); err == nil {
 				// Pack all screenshots from mobile session, compress and upload to object storage
-				if err := srv.PackScreenshots(sessID, workDir+"/screenshots/"+strconv.FormatUint(sessID, 10)+"/"); err != nil {
-					log.Printf("upload session err: %s, sessID: %d", err, sessID)
+				if err := srv.PackScreenshots(sessCtx, sessID, workDir+"/screenshots/"+strconv.FormatUint(sessID, 10)+"/"); err != nil {
+					log.Error(sessCtx, "can't pack screenshots: %s", err)
 				}
 			} else {
 				// Unpack new screenshots package from mobile session
-				if err := srv.Process(sessID, data); err != nil {
-					log.Printf("can't process mobile screenshots: %s", err)
+				if err := srv.Process(sessCtx, sessID, data); err != nil {
+					log.Error(sessCtx, "can't process screenshots: %s", err)
 				}
 			}
 		}, nil, true),
@@ -78,7 +78,7 @@ func main() {
 		cfg.MessageSizeLimit,
 	)
 
-	log.Printf("Image storage service started\n")
+	log.Info(ctx, "Image storage service started")
 
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
@@ -87,21 +87,21 @@ func main() {
 	for {
 		select {
 		case sig := <-sigchan:
-			log.Printf("Caught signal %v: terminating\n", sig)
+			log.Info(ctx, "Caught signal %v: terminating", sig)
 			srv.Wait()
 			consumer.Close()
 			os.Exit(0)
 		case <-counterTick:
 			srv.Wait()
 			if err := consumer.Commit(); err != nil {
-				log.Printf("can't commit messages: %s", err)
+				log.Error(ctx, "can't commit messages: %s", err)
 			}
 		case msg := <-consumer.Rebalanced():
-			log.Println(msg)
+			log.Info(ctx, "Rebalanced: %v", msg)
 		default:
 			err := consumer.ConsumeNext()
 			if err != nil {
-				log.Fatalf("Error on images consumption: %v", err)
+				log.Fatal(ctx, "Error on images consumption: %v", err)
 			}
 		}
 	}
