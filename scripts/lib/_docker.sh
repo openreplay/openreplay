0f9eca733a0833c49871542c7e41458e1f7b7d33
rjshrjndrn <rjshrjndrn@gmail.com>
2024-03-14 17:59:49 +0100

    fix(build): source script
    Shouldn't have "" else the empty string will cause issue.
    
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- scripts/lib/_docker.sh
+++ scripts/lib/_docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker() {
-    local docker_cmd=${DOCKER_RUNTIME}
+    local docker_cmd=${DOCKER_RUNTIME:-"docker"}
     [[ $docker_cmd == "docker" ]] && docker_cmd=$(which docker)
     if [[ "$1" == "build" ]]; then
         shift
         # Reconstruct command with DOCKER_ARGS before the '.'
-        $docker_cmd build "${DOCKER_BUILD_ARGS}" "$@"
+        $docker_cmd build ${DOCKER_BUILD_ARGS} "$@"
     else
         $docker_cmd "$@"
     fi
