f9cfb5447090135eccfe8c419a76ec93af5ad22b
rjshrjndrn <rjshrjndrn@gmail.com>
2024-04-05 18:05:06 +0200

    chore(release): Updated version to v1.18.0

--- scripts/helmcharts/init.sh
+++ scripts/helmcharts/init.sh
@@ -43,8 +43,8 @@ function exists() {
 ## Prepping the infra
 
 # Mac os doesn't have gnu sed, which will cause compatibility issues.
-# This wrapper will help to check the sed, and use the correct version="v1.16.0"
-# Ref: https://stackoverflow.com/questions/37639496/how-can-i-check-the-version="v1.16.0"
+# This wrapper will help to check the sed, and use the correct version="v1.18.0"
+# Ref: https://stackoverflow.com/questions/37639496/how-can-i-check-the-version="v1.18.0"
 function is_gnu_sed() {
     sed --version >/dev/null 2>&1
 }
