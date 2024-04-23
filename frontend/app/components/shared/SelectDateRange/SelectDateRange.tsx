bce6d67253461eddf12e062275e4337bce70dbc3
nick-delirium <nikita@openreplay.com>
2024-04-23 15:55:34 +0200

    fix ui: fixes for session list header

--- frontend/app/components/shared/SelectDateRange/SelectDateRange.tsx
+++ frontend/app/components/shared/SelectDateRange/SelectDateRange.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Select as AntSelect } from 'antd';
-import { DATE_RANGE_OPTIONS, CUSTOM_RANGE } from 'App/dateRange';
-import Select from 'Shared/Select';
+import { DownOutlined } from '@ant-design/icons';
 import Period from 'Types/app/period';
-import { components } from 'react-select';
-import DateRangePopup from 'Shared/DateRangeDropdown/DateRangePopup';
-import OutsideClickDetectingDiv from 'Shared/OutsideClickDetectingDiv';
+import { Dropdown, DatePicker } from 'antd';
 import cn from 'classnames';
 import { observer } from 'mobx-react-lite';
+import React from 'react';
+import { components } from 'react-select';
+
+import { CUSTOM_RANGE, DATE_RANGE_OPTIONS } from 'App/dateRange';
+
+import DateRangePopup from 'Shared/DateRangeDropdown/DateRangePopup';
+import OutsideClickDetectingDiv from 'Shared/OutsideClickDetectingDiv';
+import Select from 'Shared/Select';
 
 interface Props {
   period: any;
@@ -23,8 +26,11 @@ interface Props {
 
 function SelectDateRange(props: Props) {
   const [isCustom, setIsCustom] = React.useState(false);
+  const [isCustomOpen, setIsCustomOpen] = React.useState(false);
   const { right = false, period, disableCustom = false, timezone } = props;
-  let selectedValue = DATE_RANGE_OPTIONS.find((obj: any) => obj.value === period.rangeName);
+  let selectedValue = DATE_RANGE_OPTIONS.find(
+    (obj: any) => obj.value === period.rangeName
+  );
   const options = DATE_RANGE_OPTIONS.filter((obj: any) =>
     disableCustom ? obj.value !== CUSTOM_RANGE : true
   );
@@ -33,7 +39,7 @@ function SelectDateRange(props: Props) {
     if (value === CUSTOM_RANGE) {
       setTimeout(() => {
         setIsCustom(true);
-      }, 1)
+      }, 1);
     } else {
       // @ts-ignore
       props.onChange(new Period({ rangeName: value }));
@@ -42,27 +48,48 @@ function SelectDateRange(props: Props) {
 
   const onApplyDateRange = (value: any) => {
     // @ts-ignore
-    const range = new Period({ rangeName: CUSTOM_RANGE, start: value.start, end: value.end });
+    const range = new Period({
+      rangeName: CUSTOM_RANGE,
+      start: value.start,
+      end: value.end,
+    });
     props.onChange(range);
     setIsCustom(false);
   };
 
   const isCustomRange = period.rangeName === CUSTOM_RANGE;
   const customRange = isCustomRange ? period.rangeFormatted() : '';
-  
+
   if (props.isAnt) {
     const onAntUpdate = (val: any) => {
       onChange(val);
     };
     return (
       <div className={'relative'}>
-        <AntSelect
-          size={props.small ? 'small' : undefined}
-          options={options}
+        <Dropdown
+          menu={{
+            items: options.map((opt) => ({
+              label: opt.label,
+              key: opt.value,
+            })),
+            defaultSelectedKeys: selectedValue?.value
+              ? [selectedValue.value]
+              : undefined,
+            onClick: (e: any) => {
+              onChange(e.key);
+            },
+          }}
           onChange={onAntUpdate}
           style={{ width: 170 }}
           defaultValue={selectedValue?.value ?? undefined}
-        />
+        >
+          <div
+            className={'cursor-pointer font-semibold flex items-center gap-2'}
+          >
+            <div>{isCustomRange ? customRange : selectedValue?.label}</div>
+            <DownOutlined />
+          </div>
+        </Dropdown>
         {isCustom && (
           <OutsideClickDetectingDiv
             onClickOutside={(e: any) => {
@@ -70,7 +97,12 @@ function SelectDateRange(props: Props) {
                 e.target.parentElement.parentElement.classList.contains(
                   'rc-time-picker-panel-select'
                 ) ||
-                e.target.parentElement.parentElement.classList[0]?.includes('-menu')
+                e.target.parentElement.parentElement.classList[0]?.includes(
+                  '-menu'
+                )
+                || e.target.className.includes(
+                  'ant-picker'
+                )
               ) {
                 return false;
               }
@@ -78,7 +110,10 @@ function SelectDateRange(props: Props) {
             }}
           >
             <div
-              className={cn('absolute top-0 mt-10 z-40', { 'right-0': right })}
+              className={cn(
+                'absolute top-0 mt-10 z-40',
+                { 'right-0': right }
+              )}
               style={{
                 width: '770px',
                 fontSize: '14px',
@@ -124,11 +159,13 @@ function SelectDateRange(props: Props) {
               e.target.parentElement.parentElement.classList.contains(
                 'rc-time-picker-panel-select'
               ) ||
-              e.target.parentElement.parentElement.classList[0]?.includes('-menu')
+              e.target.parentElement.parentElement.classList[0]?.includes(
+                '-menu'
+              )
             ) {
               return false;
             }
-            console.log('outside')
+            console.log('outside');
             setIsCustom(false);
           }}
         >
--- frontend/app/components/shared/SessionsTabOverview/components/SessionSort/SessionSort.tsx
+++ frontend/app/components/shared/SessionsTabOverview/components/SessionSort/SessionSort.tsx
@@ -1,4 +1,5 @@
-import { Select } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { Dropdown } from 'antd';
 import React from 'react';
 import { connect } from 'react-redux';
 
@@ -13,8 +14,9 @@ const sortOptionsMap = {
 };
 
 const sortOptions = Object.entries(sortOptionsMap).map(([value, label]) => ({
-  value,
+  // value,
   label,
+  key: value,
 }));
 
 interface Props {
@@ -26,21 +28,32 @@ interface Props {
 
 function SessionSort(props: Props) {
   const { sort, order } = props.filter;
-  const onSort = (value: string) => {
-    const [sort, order] = value.split('-');
+  const onSort = ({ key }: { key: string }) => {
+    const [sort, order] = key.split('-');
     const sign = order === 'desc' ? -1 : 1;
     props.applyFilter({ order, sort });
     props.sort(sort, sign);
   };
 
   const defaultOption = `${sort}-${order}`;
+
   return (
-    <Select
-      style={{ width: 160 }}
-      options={sortOptions}
-      onChange={onSort}
-      defaultValue={defaultOption}
-    />
+    <Dropdown
+      menu={{
+        items: sortOptions,
+        defaultSelectedKeys: [defaultOption],
+        onClick: onSort,
+      }}
+    >
+      <div
+        className={
+          'cursor-pointer flex items-center justify-end gap-2 font-semibold'
+        }
+      >
+        <div>{sortOptionsMap[defaultOption]}</div>
+        <DownOutlined />
+      </div>
+    </Dropdown>
   );
 }
 
