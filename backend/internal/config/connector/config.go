04e7eeef604a9991dc20265ada89374484c35c86
Alexander <zavorotynskiy@pm.me>
2024-04-24 16:38:26 +0200

    feat(backend): added projects filter to connector logic

--- backend/internal/config/connector/config.go
+++ backend/internal/config/connector/config.go
@@ -5,6 +5,8 @@ import (
 	"openreplay/backend/internal/config/configurator"
 	"openreplay/backend/internal/config/objectstorage"
 	"openreplay/backend/internal/config/redis"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -24,10 +26,30 @@ type Config struct {
 	TopicAnalytics     string        `env:"TOPIC_ANALYTICS,required"`
 	CommitBatchTimeout time.Duration `env:"COMMIT_BATCH_TIMEOUT,default=5s"`
 	UseProfiler        bool          `env:"PROFILER_ENABLED,default=false"`
+	ProjectIDs         string        `env:"PROJECT_IDS"`
 }
 
 func New() *Config {
 	cfg := &Config{}
 	configurator.Process(cfg)
 	return cfg
+}
+
+func (c *Config) GetAllowedProjectIDs() []int {
+	stringIDs := strings.Split(c.ProjectIDs, ",")
+	if len(stringIDs) == 0 {
+		return nil
+	}
+	ids := make([]int, 0, len(stringIDs))
+	for _, id := range stringIDs {
+		intID, err := strconv.Atoi(id)
+		if err != nil {
+			continue
+		}
+		ids = append(ids, intID)
+	}
+	if len(ids) == 0 {
+		return nil
+	}
+	return ids
 }
