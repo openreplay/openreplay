7032a3159b24f51fd5dfbae7b2a260d1c7eb8d3d
Taha Yassine Kraiem <tahayk2@gmail.com>
2024-04-26 13:42:27 +0200

    fix(DB): added missing user_device_type

--- ee/scripts/schema/db/init_dbs/clickhouse/1.18.0/1.18.0.sql
+++ ee/scripts/schema/db/init_dbs/clickhouse/1.18.0/1.18.0.sql
@@ -3,3 +3,6 @@ CREATE OR REPLACE FUNCTION openreplay_version AS() -> 'v1.18.0-ee';
 ALTER TABLE experimental.sessions
     ADD COLUMN IF NOT EXISTS screen_width Nullable(Int16),
     ADD COLUMN IF NOT EXISTS screen_height Nullable(Int16);
+
+ALTER TABLE experimental.sessions
+    MODIFY COLUMN IF EXISTS user_device_type Enum8('other'=0, 'desktop'=1, 'mobile'=2,'tablet'=3);
