0f872e3b9e9c5a0fa4cedc70eab25913ac249843
nick-delirium <nikita@openreplay.com>
2024-05-02 16:43:12 +0200

    fix ui: change searchbar margin to 1280 trigger

--- frontend/app/components/shared/MainSearchBar/MainSearchBar.tsx
+++ frontend/app/components/shared/MainSearchBar/MainSearchBar.tsx
@@ -37,7 +37,7 @@ const MainSearchBar = (props: Props) => {
       <div style={{ flex: 3, marginRight: '10px' }}>
         {isSaas ? <AiSessionSearchField /> : <SessionSearchField />}
       </div>
-      <div className="flex items-center gap-2 my-2 lg:my-0" style={{ flex: 2 }}>
+      <div className="flex items-center gap-2 my-2 xl:my-0" style={{ flex: 2 }}>
         <TagList />
         <SavedSearch />
         <Button
