c44f6be552bd0cb4bb44932bad17679e4adefc59
Shekar Siri <sshekarsiri@gmail.com>
2024-05-27 14:30:28 +0200

    change(ui): path analysis clear filter

--- frontend/app/components/shared/Insights/SankeyChart/SankeyChart.tsx
+++ frontend/app/components/shared/Insights/SankeyChart/SankeyChart.tsx
@@ -1,140 +1,140 @@
-import React, { useState } from 'react';
-import { Sankey, ResponsiveContainer } from 'recharts';
+import React, {useState} from 'react';
+import {Sankey, ResponsiveContainer} from 'recharts';
 import CustomLink from './CustomLink';
 import CustomNode from './CustomNode';
-import { NoContent } from 'UI';
+import {NoContent} from 'UI';
 
 interface Node {
-  idd: number;
-  name: string;
-  eventType: string;
-  avgTimeFromPrevious: number | null;
+    idd: string;
+    name: string;
+    eventType: string;
+    avgTimeFromPrevious: number | null;
 }
 
 interface Link {
-  id: string;
-  eventType: string;
-  value: number;
-  source: number;
-  target: number;
+    id: string;
+    eventType: string;
+    value: number;
+    source: string;
+    target: string;
 }
 
 interface Data {
-  nodes: Node[];
-  links: Link[];
+    nodes: Node[];
+    links: Link[];
 }
 
 interface Props {
-  data: Data;
-  nodeWidth?: number;
-  height?: number;
-  onChartClick?: (filters: any[]) => void;
+    data: Data;
+    nodeWidth?: number;
+    height?: number;
+    onChartClick?: (filters: any[]) => void;
 }
 
-const SankeyChart: React.FC<Props> = ({
-                                        data,
-                                        height = 240,
-                                        onChartClick
-                                      }: Props) => {
-  const [highlightedLinks, setHighlightedLinks] = useState<string[]>([]);
-  const [hoveredLinks, setHoveredLinks] = useState<string[]>([]);
+const SankeyChart: React.FC<Props> = ({data, height = 240, onChartClick}: Props) => {
+    const [highlightedLinks, setHighlightedLinks] = useState<string[]>([]);
+    const [hoveredLinks, setHoveredLinks] = useState<string[]>([]);
 
-  function findPreviousLinks(targetNodeIndex: number): Link[] {
-    const previousLinks: Link[] = [];
-    const visitedNodes: Set<number> = new Set();
+    function findPreviousLinks(targetNodeIndex: number): Link[] {
+        const previousLinks: Link[] = [];
+        const visitedNodes: Set<number> = new Set();
 
-    const findPreviousLinksRecursive = (nodeIndex: number) => {
-      visitedNodes.add(nodeIndex);
+        const findPreviousLinksRecursive = (nodeIndex: number) => {
+            visitedNodes.add(nodeIndex);
 
-      for (const link of data.links) {
-        if (link.target === nodeIndex && !visitedNodes.has(link.source)) {
-          previousLinks.push(link);
-          findPreviousLinksRecursive(link.source);
-        }
-      }
+            for (const link of data.links) {
+                if (link.target === nodeIndex && !visitedNodes.has(link.source)) {
+                    previousLinks.push(link);
+                    findPreviousLinksRecursive(link.source);
+                }
+            }
+        };
+
+        findPreviousLinksRecursive(targetNodeIndex);
+
+        return previousLinks;
+    }
+
+    const handleLinkMouseEnter = (linkData: any) => {
+        const {payload} = linkData;
+        const link: any = data.links.find(link => link.id === payload.id);
+        const previousLinks: any = findPreviousLinks(link.source).reverse();
+        previousLinks.push({id: payload.id});
+        setHoveredLinks(previousLinks.map((link: any) => link.id));
     };
 
-    findPreviousLinksRecursive(targetNodeIndex);
+    const clickHandler = () => {
+        setHighlightedLinks(hoveredLinks);
 
-    return previousLinks;
-  }
+        const firstLink = data.links.find(link => link.id === hoveredLinks[0]) || null;
+        const lastLink = data.links.find(link => link.id === hoveredLinks[hoveredLinks.length - 1]) || null;
 
-  const handleLinkMouseEnter = (linkData: any) => {
-    const { payload } = linkData;
-    const link: any = data.links.find(link => link.id === payload.id);
-    const previousLinks: any = findPreviousLinks(link.source).reverse();
-    previousLinks.push({ id: payload.id });
-    setHoveredLinks(previousLinks.map((link: any) => link.id));
-  };
+        const firstNode = data.nodes[firstLink?.source];
+        const lastNode = data.nodes[lastLink?.target];
 
-  const clickHandler = () => {
-    setHighlightedLinks(hoveredLinks);
+        const filters = [];
 
-    const firstLink = data.links.find(link => link.id === hoveredLinks[0]) || null;
-    const lastLink = data.links.find(link => link.id === hoveredLinks[hoveredLinks.length - 1]) || null;
+        if (firstNode) {
+            filters.push({
+                operator: 'is',
+                type: firstNode.eventType,
+                value: [firstNode.name],
+                isEvent: true
+            });
+        }
 
-    const firstNode = data.nodes[firstLink?.source];
-    const lastNode = data.nodes[lastLink.target];
+        if (lastNode) {
+            filters.push({
+                operator: 'is',
+                type: lastNode.eventType,
+                value: [lastNode.name],
+                isEvent: true
+            });
+        }
 
-    const filters = [];
+        onChartClick?.(filters);
+    };
 
-    if (firstNode) {
-      filters.push({
-        operator: 'is',
-        type: firstNode.eventType,
-        value: [firstNode.name],
-        isEvent: true
-      });
-    }
+    // const processedData = processData(data);
 
-    if (lastNode) {
-      filters.push({
-        operator: 'is',
-        type: lastNode.eventType,
-        value: [lastNode.name],
-        isEvent: true
-      });
-    }
-
-    onChartClick?.(filters);
-  };
-
-
-  return (
-    <NoContent
-      style={{ paddingTop: '80px' }}
-      show={!data.nodes.length || !data.links.length}
-      title={'No data for the selected time period.'}
-    >
-      <ResponsiveContainer height={height} width='100%'>
-        <Sankey
-          data={data}
-          iterations={128}
-          node={<CustomNode />}
-          sort={true}
-          onClick={clickHandler}
-          link={({ source, target, id, ...linkProps }, index) => (
-            <CustomLink
-              {...linkProps}
-              hoveredLinks={hoveredLinks}
-              activeLinks={highlightedLinks}
-              strokeOpacity={highlightedLinks.includes(id) ? 0.8 : 0.2}
-              onMouseEnter={() => handleLinkMouseEnter(linkProps)}
-              onMouseLeave={() => setHoveredLinks([])}
-            />
-          )}
-          margin={{ right: 200, bottom: 50 }}
+    return (
+        <NoContent
+            style={{paddingTop: '80px'}}
+            show={!data.nodes.length || !data.links.length}
+            title={'No data for the selected time period.'}
         >
-          <defs>
-            <linearGradient id={'linkGradient'}>
-              <stop offset='0%' stopColor='rgba(57, 78, 255, 0.2)' />
-              <stop offset='100%' stopColor='rgba(57, 78, 255, 0.2)' />
-            </linearGradient>
-          </defs>
-        </Sankey>
-      </ResponsiveContainer>
-    </NoContent>
-  );
+            <ResponsiveContainer height={height} width='100%'>
+                <Sankey
+                    data={data}
+                    node={<CustomNode/>}
+                    nodePadding={20}
+                    sort={true}
+                    nodeWidth={4}
+                    iterations={128}
+                    // linkCurvature={0.9}
+                    onClick={clickHandler}
+                    link={({source, target, id, ...linkProps}, index) => (
+                        <CustomLink
+                            {...linkProps}
+                            hoveredLinks={hoveredLinks}
+                            activeLinks={highlightedLinks}
+                            strokeOpacity={highlightedLinks.includes(id) ? 0.8 : 0.2}
+                            onMouseEnter={() => handleLinkMouseEnter(linkProps)}
+                            onMouseLeave={() => setHoveredLinks([])}
+                        />
+                    )}
+                    margin={{right: 130, bottom: 50}}
+                >
+                    <defs>
+                        <linearGradient id={'linkGradient'}>
+                            <stop offset='0%' stopColor='rgba(57, 78, 255, 0.2)'/>
+                            <stop offset='100%' stopColor='rgba(57, 78, 255, 0.2)'/>
+                        </linearGradient>
+                    </defs>
+                </Sankey>
+            </ResponsiveContainer>
+        </NoContent>
+    );
 };
 
 export default SankeyChart;
