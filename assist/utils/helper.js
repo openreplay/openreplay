80af24f91ae1f3dbc8ee20ab39dd5329778e3a88
Alexander <zavorotynskiy@pm.me>
2024-05-27 16:00:46 +0200

    feat(assist): count a search filters for full match results only (#2209)

--- assist/utils/helper.js
+++ assist/utils/helper.js
@@ -63,13 +63,11 @@ const extractSessionIdFromRequest = function (req) {
     return undefined;
 }
 
-const isValidSession = function (sessionInfo, filters, counter) {
+const isValidSession = function (sessionInfo, filters) {
+    const result = {matched: false, filters: {}};
     for (const [filterName, body] of Object.entries(filters)) { // range by filter names (key)
         if (body.values === undefined || body.values === null) {
-            return false;
-        }
-        if (!counter[filterName]) {
-            counter[filterName] = {};
+            return result;
         }
         let found = false;
         for (const [sessKey, sessValue] of Object.entries(sessionInfo)) {
@@ -77,8 +75,10 @@ const isValidSession = function (sessionInfo, filters, counter) {
                 continue;
             }
             if (typeof (sessValue) === "object") {
-                if (isValidSession(sessValue, {[filterName]: body}, counter)) {
+                const partRes = isValidSession(sessValue, {[filterName]: body})
+                if (partRes.matched) {
                     found = true;
+                    Object.assign(result.filters, partRes.filters);
                     break;
                 }
             } else if (sessKey.toLowerCase() === filterName.toLowerCase()) {
@@ -86,8 +86,7 @@ const isValidSession = function (sessionInfo, filters, counter) {
                     if (body.operator === "is" && v && String(sessValue).toLowerCase() === String(v).toLowerCase()
                         || body.operator !== "is" && String(sessValue).toLowerCase().indexOf(String(v).toLowerCase()) >= 0) {
                         found = true;
-                        if (counter[filterName][v]) counter[filterName][v]++;
-                        else counter[filterName][v] = 1;
+                        result.filters[filterName] = v;
                         break;
                     }
                 }
@@ -97,10 +96,11 @@ const isValidSession = function (sessionInfo, filters, counter) {
             }
         }
         if (!found) {
-            return false;
+            return result;
         }
     }
-    return true;
+    result.matched = true;
+    return result;
 }
 
 const getValidAttributes = function (sessionInfo, query) {
