15bcb904172d9ea0210f6af2aa65aee95b1c5d30
rjshrjndrn <rjshrjndrn@gmail.com>
2024-05-28 12:03:39 +0200

    chore(install): Improved array splitting for kafka
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- scripts/helmcharts/openreplay/files/kafka.sh
+++ scripts/helmcharts/openreplay/files/kafka.sh
@@ -6,40 +6,40 @@ set -e
 RETENTION_TIME=${RETENTION_TIME:-345600000}
 
 topics=(
-  "raw"
-  "raw-ios"
-  "trigger"
-  "cache"
-  "analytics"
-  "storage-failover"
-  "quickwit"
+    "raw"
+    "raw-ios"
+    "trigger"
+    "cache"
+    "analytics"
+    "storage-failover"
+    "quickwit"
 )
 
 touch /tmp/config.txt
 
 if [[ $KAFKA_SSL == "true" ]]; then
-  echo 'security.protocol=SSL' > /tmp/config.txt
+    echo 'security.protocol=SSL' >/tmp/config.txt
 fi
 
 function init() {
-  echo "Initializing kafka"
-  for topic in ${topics[*]}; do
-    echo "Creating topic: $topic"
-    # TODO: Have to check an idempotent way of creating topics.
-    kafka-topics.sh --create --bootstrap-server ${KAFKA_HOST}:${KAFKA_PORT} --replication-factor 2 --partitions 16 --topic ${topic} --command-config /tmp/config.txt || true
-    kafka-configs.sh --bootstrap-server ${KAFKA_HOST}:${KAFKA_PORT} --entity-type topics --alter --add-config retention.ms=${RETENTION_TIME} --entity-name=${topic} --command-config /tmp/config.txt || true
-  done
+    echo "Initializing kafka"
+    for topic in "${topics[@]}"; do
+        echo "Creating topic: $topic"
+        # TODO: Have to check an idempotent way of creating topics.
+        kafka-topics.sh --create --bootstrap-server ${KAFKA_HOST}:${KAFKA_PORT} --replication-factor 2 --partitions 16 --topic ${topic} --command-config /tmp/config.txt || true
+        kafka-configs.sh --bootstrap-server ${KAFKA_HOST}:${KAFKA_PORT} --entity-type topics --alter --add-config retention.ms=${RETENTION_TIME} --entity-name=${topic} --command-config /tmp/config.txt || true
+    done
 }
 
 # /bin/bash kafka.sh migrate $migration_versions
 case "$1" in
-  migrate)
+migrate)
     init
     ;;
-  init)
+init)
     init
     ;;
-  *)
+*)
     echo "Unknown operation for kafka migration; exiting."
     exit 1
     ;;
