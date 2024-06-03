bfca5354ab92eb09daaa66abbd286f49f8ce76fc
Delirium <nikita@openreplay.com>
2024-06-03 11:14:59 +0200

    feat: titles for tabs
    * feat tracker: send initial title, parse titles better
    
    * feat ui: tab name styles

--- frontend/app/components/Session/Player/SharedComponents/Tab.tsx
+++ frontend/app/components/Session/Player/SharedComponents/Tab.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { Tooltip } from 'antd';
 import cn from 'classnames';
+import React from 'react';
 
 interface Props {
   i: number;
@@ -15,7 +16,9 @@ function Tab({ i, tab, currentTab, changeTab, isLive, isClosed, name }: Props) {
   return (
     <div
       key={tab}
-      style={{ marginBottom: '-2px' }}
+      style={{
+        marginBottom: '-2px',
+      }}
       onClick={() => changeTab?.(tab)}
       className={cn(
         'self-end py-1 px-4 text-sm',
@@ -23,10 +26,27 @@ function Tab({ i, tab, currentTab, changeTab, isLive, isClosed, name }: Props) {
         currentTab === tab
           ? 'border-gray-lighter border-t border-l border-r !border-b-white bg-white rounded-tl rounded-tr font-semibold'
           : 'cursor-pointer border-gray-lighter !border-b !border-t-transparent !border-l-transparent !border-r-transparent',
-        isClosed ? 'line-through': ''
       )}
     >
-      {name ? name : `Tab ${i + 1}`}
+      <Tooltip title={name && name.length > 20 ? name : ''}>
+        <div className={'flex items-center gap-2'}>
+          <div
+            className={
+              'bg-gray-light rounded-full min-w-5 min-h-5 w-5 h-5 flex items-center justify-center text-xs'
+            }
+          >
+            <div>{i + 1}</div>
+          </div>
+          <div
+            className={cn('whitespace-nowrap', isClosed ? 'line-through' : '')}
+            style={{
+              maxWidth: 114,
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+            }}
+          >{name ? name : `Tab ${i + 1}`}</div>
+        </div>
+      </Tooltip>
     </div>
   );
 }
