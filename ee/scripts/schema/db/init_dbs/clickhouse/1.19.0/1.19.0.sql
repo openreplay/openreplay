412d804a8dede27fead4749590e1dc0b1b1c26cc
Taha Yassine Kraiem <tahayk2@gmail.com>
2024-07-03 13:00:31 +0200

    fix(DB): fixed exp-sessions_mv

--- ee/scripts/schema/db/init_dbs/clickhouse/1.19.0/1.19.0.sql
+++ ee/scripts/schema/db/init_dbs/clickhouse/1.19.0/1.19.0.sql
@@ -82,3 +82,61 @@ WHERE datetime >= now() - INTERVAL 7 DAY;
 
 ALTER TABLE experimental.ios_events
     DROP COLUMN IF EXISTS coordinate;
+
+DROP TABLE IF EXISTS experimental.sessions_l7d_mv;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS experimental.sessions_l7d_mv
+            ENGINE = ReplacingMergeTree(_timestamp)
+                PARTITION BY toYYYYMMDD(datetime)
+                ORDER BY (project_id, datetime, session_id)
+                TTL datetime + INTERVAL 7 DAY
+                SETTINGS index_granularity = 512
+            POPULATE
+AS
+SELECT session_id,
+       project_id,
+       tracker_version,
+       rev_id,
+       user_uuid,
+       user_os,
+       user_os_version,
+       user_browser,
+       user_browser_version,
+       user_device,
+       user_device_type,
+       user_country,
+       user_city,
+       user_state,
+       platform,
+       datetime,
+       timezone,
+       duration,
+       pages_count,
+       events_count,
+       errors_count,
+       utm_source,
+       utm_medium,
+       utm_campaign,
+       user_id,
+       user_anonymous_id,
+       issue_types,
+       referrer,
+       base_referrer,
+       issue_score,
+       screen_width,
+       screen_height,
+       metadata_1,
+       metadata_2,
+       metadata_3,
+       metadata_4,
+       metadata_5,
+       metadata_6,
+       metadata_7,
+       metadata_8,
+       metadata_9,
+       metadata_10,
+       _timestamp
+FROM experimental.sessions
+WHERE datetime >= now() - INTERVAL 7 DAY
+  AND isNotNull(duration)
+  AND duration > 0;
