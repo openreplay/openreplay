3cb4833699bcecc090553e2e07353d3b10884321
nick-delirium <nikita@openreplay.com>
2024-07-03 11:11:09 +0200

    fix ui: fix warnings

--- frontend/app/components/Dashboard/components/MetricsView/MetricsView.tsx
+++ frontend/app/components/Dashboard/components/MetricsView/MetricsView.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import withPageTitle from 'HOCs/withPageTitle';
 import MetricsList from '../MetricsList';
-import { useObserver } from 'mobx-react-lite';
+import { observer } from 'mobx-react-lite';
 import MetricViewHeader from '../MetricViewHeader';
 
 interface Props {
   siteId: string;
 }
 function MetricsView({ siteId }: Props) {
-  return useObserver(() => (
+  return (
     <div style={{ maxWidth: '1360px', margin: 'auto' }} className="bg-white rounded-lg shadow-sm pt-4 border">
       <MetricViewHeader siteId={siteId} />
       <MetricsList siteId={siteId} />
     </div>
-  ));
+  );
 }
 
-export default withPageTitle('Cards - OpenReplay')(MetricsView);
+export default withPageTitle('Cards - OpenReplay')(observer(MetricsView));
