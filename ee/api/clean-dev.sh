0ddd196df47d74076211e558b7096b404cc80497
Taha Yassine Kraiem <tahayk2@gmail.com>
2024-07-05 14:59:41 +0200

    refactor(chalice): refactored code

--- ee/api/clean-dev.sh
+++ ee/api/clean-dev.sh
@@ -93,3 +93,4 @@ rm -rf ./schemas/schemas.py
 rm -rf ./schemas/transformers_validators.py
 rm -rf ./orpy.py
 rm -rf ./chalicelib/core/usability_testing/
+rm -rf ./chalicelib/core/db_request_handler.py
