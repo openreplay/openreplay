27043a40ecfab61d9334e2effabe16fba440f914
nick-delirium <nikita@openreplay.com>
2024-07-10 13:11:03 +0200

    header for player, comments, icons, etc

--- frontend/app/components/Spots/SpotPlayer/CommentsSection.tsx
+++ frontend/app/components/Spots/SpotPlayer/CommentsSection.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { SpotComment } from "App/services/spotService";
+import { X } from 'lucide-react'
+import { resentOrDate } from "App/date";
 
-function CommentsSection({ comments }: { comments: SpotComment[] }) {
+function CommentsSection({ comments, onClose }: { comments: SpotComment[], onClose?: () => void }) {
   return (
     <div className={'h-full p-4 bg-white border border-gray-light'} style={{ width: 220 }}>
-      <div className={'flex items-center justify-between'}>
+    <div className={'flex items-center justify-between'}>
         <div className={'font-semibold'}>Comments</div>
-        <div>x</div>
+        <div onClick={onClose} className={'p-1 cursor-pointer'}>
+          <X size={16} />
+        </div>
       </div>
       {comments.map((comment) => (
-        <div key={comment.createdAt} className={'my-4'}>
+        <div key={comment.createdAt} className={'my-4 flex flex-col gap-2'}>
           <div className={'flex items-center gap-2'}>
-            <div>S</div>
-            <div>{comment.user}</div>
+            <div
+              className={'w-8 h-8 bg-tealx rounded-full flex items-center justify-center color-white uppercase'}
+            >
+              {comment.user[0]}
+            </div>
+            <div className={'font-semibold'}>{comment.user}</div>
           </div>
           <div>{comment.text}</div>
-          <div>{comment.createdAt}</div>
+          <div className={'text-disabled-text'}>{resentOrDate(new Date(comment.createdAt).getTime())}</div>
         </div>
       ))}
     </div>
