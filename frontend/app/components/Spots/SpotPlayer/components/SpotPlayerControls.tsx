b9c8dd2bdf249849bda66314e4f5ae666b17577e
nick-delirium <nikita@openreplay.com>
2024-07-15 10:38:13 +0200

    interval buttons etc

--- frontend/app/components/Spots/SpotPlayer/components/SpotPlayerControls.tsx
+++ frontend/app/components/Spots/SpotPlayer/components/SpotPlayerControls.tsx
@@ -2,7 +2,12 @@ import { SPEED_OPTIONS } from 'Player/player/Player';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 
-import { SpeedOptions } from 'App/components/Session_/Player/Controls/components/ControlsComponents';
+import {
+  IntervalSelector,
+  JumpBack,
+  JumpForward,
+  SpeedOptions,
+} from 'App/components/Session_/Player/Controls/components/ControlsComponents';
 import {
   FullScreenButton,
   PlayButton,
@@ -10,8 +15,9 @@ import {
   PlayingState,
 } from 'App/player-ui';
 import ControlButton from 'Components/Session_/Player/Controls/ControlButton';
+import { SKIP_INTERVALS } from 'Components/Session_/Player/Controls/Controls';
 
-import spotPlayerStore, { PANELS, PanelType } from "../spotPlayerStore";
+import spotPlayerStore, { PANELS, PanelType } from '../spotPlayerStore';
 
 function SpotPlayerControls() {
   const toggleFullScreen = () => {
@@ -29,8 +35,18 @@ function SpotPlayerControls() {
     : PlayingState.Paused;
 
   const togglePanel = (panel: PanelType) => {
-    spotPlayerStore.setActivePanel(panel === spotPlayerStore.activePanel ? null : panel);
-  }
+    spotPlayerStore.setActivePanel(
+      panel === spotPlayerStore.activePanel ? null : panel
+    );
+  };
+
+  const back = () => {
+    spotPlayerStore.setTime(spotPlayerStore.time - spotPlayerStore.skipInterval);
+  };
+  const forth = () => {
+    spotPlayerStore.setTime(spotPlayerStore.time + spotPlayerStore.skipInterval);
+  };
+
   return (
     <div className={'w-full p-4 flex items-center gap-4 bg-white'}>
       <PlayButton togglePlay={togglePlay} state={playState} iconSize={36} />
@@ -47,6 +63,25 @@ function SpotPlayerControls() {
         />
         <span>/</span>
         <div>{spotPlayerStore.durationString}</div>
+      </div>
+
+      <div
+        className="rounded ml-1 bg-white border-gray-lighter flex items-center"
+        style={{ gap: 1 }}
+      >
+        <JumpBack
+          backTenSeconds={back}
+          currentInterval={spotPlayerStore.skipInterval}
+        />
+        <IntervalSelector
+          skipIntervals={SKIP_INTERVALS}
+          setSkipInterval={spotPlayerStore.setSkipInterval}
+          currentInterval={spotPlayerStore.skipInterval}
+        />
+        <JumpForward
+          forthTenSeconds={forth}
+          currentInterval={spotPlayerStore.skipInterval}
+        />
       </div>
 
       <SpeedOptions
