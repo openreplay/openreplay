49672ecef07bec68b7bde500fc35f620c3fbe035
nick-delirium <nikita@openreplay.com>
2024-07-16 15:05:57 +0200

    hls lib instead of videojs

--- frontend/app/components/Spots/SpotPlayer/components/SpotVideoContainer.tsx
+++ frontend/app/components/Spots/SpotPlayer/components/SpotVideoContainer.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import spotPlayerStore from "../spotPlayerStore";
 import { observer } from 'mobx-react-lite';
+import Hls from 'hls.js'
 
 function SpotVideoContainer({ videoURL }: { videoURL: string }) {
   const videoRef = React.useRef<HTMLVideoElement>(null);
   const playbackTime = React.useRef(0)
+
+  React.useEffect(() => {
+    if (Hls.isSupported() && videoRef.current) {
+      if (videoURL.includes('.m3u8')) {
+        const hls = new Hls({ workerPath: '/hls-worker.js' })
+        hls.loadSource(videoURL)
+        hls.attachMedia(videoRef.current)
+      } else {
+        videoRef.current.src = videoURL
+      }
+    }
+  }, [])
 
   React.useEffect(() => {
     if (spotPlayerStore.isPlaying) {
@@ -36,12 +49,25 @@ function SpotVideoContainer({ videoURL }: { videoURL: string }) {
       videoRef.current.playbackRate = spotPlayerStore.playbackRate
     }
   }, [spotPlayerStore.playbackRate])
+
+  const getTypeFromUrl = (url: string) => {
+    const ext = url.split('.').pop()
+    if (ext === 'mp4') {
+      return 'video/mp4'
+    }
+    if (ext === 'webm') {
+      return 'video/webm'
+    }
+    if (ext === 'ogg') {
+      return 'video/ogg'
+    }
+    return 'video/mp4'
+  }
   return (
     <video
       ref={videoRef}
       className={'object-contain absolute top-0 left-0 w-full h-full bg-gray-lightest cursor-pointer'}
       onClick={() => spotPlayerStore.setIsPlaying(!spotPlayerStore.isPlaying)}
-      src={videoURL}
     />
   )
 }
