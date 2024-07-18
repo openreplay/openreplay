a0aea20d36a665e814182b512d9efc7d85656816
Taha Yassine Kraiem <tahayk2@gmail.com>
2024-07-18 17:56:28 +0200

    refactor(chalice): refactored schemas refactor(chalice): cleaned scripts feat(chalice): search sessions by CSS selector (PG)

--- api/chalicelib/utils/sql_helper.py
+++ api/chalicelib/utils/sql_helper.py
@@ -6,7 +6,6 @@ import schemas
 def get_sql_operator(op: Union[schemas.SearchEventOperator, schemas.ClickEventExtraOperator]):
     return {
         schemas.SearchEventOperator.IS: "=",
-        schemas.SearchEventOperator.IS_ANY: "IN",
         schemas.SearchEventOperator.ON: "=",
         schemas.SearchEventOperator.ON_ANY: "IN",
         schemas.SearchEventOperator.IS_NOT: "!=",
@@ -15,13 +14,22 @@ def get_sql_operator(op: Union[schemas.SearchEventOperator, schemas.ClickEventEx
         schemas.SearchEventOperator.NOT_CONTAINS: "NOT ILIKE",
         schemas.SearchEventOperator.STARTS_WITH: "ILIKE",
         schemas.SearchEventOperator.ENDS_WITH: "ILIKE",
+        # Selector operators:
+        schemas.ClickEventExtraOperator.IS: "=",
+        schemas.ClickEventExtraOperator.IS_NOT: "!=",
+        schemas.ClickEventExtraOperator.CONTAINS: "ILIKE",
+        schemas.ClickEventExtraOperator.NOT_CONTAINS: "NOT ILIKE",
+        schemas.ClickEventExtraOperator.STARTS_WITH: "ILIKE",
+        schemas.ClickEventExtraOperator.ENDS_WITH: "ILIKE",
     }.get(op, "=")
 
 
 def is_negation_operator(op: schemas.SearchEventOperator):
     return op in [schemas.SearchEventOperator.IS_NOT,
                   schemas.SearchEventOperator.NOT_ON,
-                  schemas.SearchEventOperator.NOT_CONTAINS]
+                  schemas.SearchEventOperator.NOT_CONTAINS,
+                  schemas.ClickEventExtraOperator.IS_NOT,
+                  schemas.ClickEventExtraOperator.NOT_CONTAINS]
 
 
 def reverse_sql_operator(op):
