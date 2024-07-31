f454450b4accedf7d54d97e4785c18c0bd63534f
Alexander <zavorotynskiy@pm.me>
2024-07-31 17:30:21 +0200

    feat(spot): manual method's name mapping

--- ee/backend/pkg/spot/api/tracer.go
+++ ee/backend/pkg/spot/api/tracer.go
@@ -6,30 +6,27 @@ import (
 	"openreplay/backend/pkg/spot/service"
 )
 
+var routeMatch = map[string]string{
+	"POST" + "/v1/spots":                  "createSpot",
+	"GET" + "/v1/spots/{id}":              "getSpot",
+	"PATCH" + "/v1/spots/{id}":            "updateSpot",
+	"GET" + "/v1/spots":                   "getSpots",
+	"DELETE" + "/v1/spots":                "deleteSpots",
+	"POST" + "/v1/spots/{id}/comment":     "addComment",
+	"POST" + "/v1/spots/{id}/uploaded":    "uploadedSpot",
+	"GET" + "/v1/spots/{id}/video":        "getSpotVideo",
+	"GET" + "/v1/spots/{id}/public-key":   "getPublicKey",
+	"PATCH" + "/v1/spots/{id}/public-key": "updatePublicKey",
+}
+
 func (e *Router) requestParser(r *http.Request) *service.RequestData {
 	pathTemplate, err := mux.CurrentRoute(r).GetPathTemplate()
 	if err != nil {
 		e.log.Error(r.Context(), "failed to get path template: %s", err)
 	}
-	currRoute := mux.CurrentRoute(r)
-	if currRoute == nil {
-		e.log.Error(r.Context(), "failed to get current route")
-	}
-	assignedMethods, err := currRoute.GetMethods()
-	if err != nil {
-		e.log.Error(r.Context(), "failed to get methods: %s", err)
-	}
-	e.log.Info(r.Context(), "assigned methods: %v", assignedMethods)
-	assignedMethod := ""
-	if len(assignedMethods) == 0 {
-		e.log.Error(r.Context(), "no methods assigned to the route")
-	} else {
-		assignedMethod = assignedMethods[0]
-	}
-	e.log.Info(r.Context(), "assigned method: %s", assignedMethod)
 	e.log.Info(r.Context(), "path template: %s", pathTemplate)
 	return &service.RequestData{
-		Action:     assignedMethod,
+		Action:     routeMatch[r.Method+pathTemplate],
 		Method:     r.Method,
 		PathFormat: pathTemplate,
 		Endpoint:   r.URL.Path,
