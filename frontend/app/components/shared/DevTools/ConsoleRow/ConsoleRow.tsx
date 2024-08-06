c494efc85844fd2fc2bf346fd08e79cd9c655e9f
nick-delirium <nikita@openreplay.com>
2024-08-06 14:09:57 +0200

    parse links in console rows, fix injected method parse?

--- frontend/app/components/shared/DevTools/ConsoleRow/ConsoleRow.tsx
+++ frontend/app/components/shared/DevTools/ConsoleRow/ConsoleRow.tsx
@@ -22,6 +22,22 @@ function ConsoleRow(props: Props) {
     setExpanded(!expanded);
   };
 
+  const urlRegex = /(https?:\/\/[^\s)]+)/g;
+  const renderLine = (l: string) => {
+    const parts = l.split(urlRegex);
+    const formattedLine = parts.map((part, index) => {
+      if (urlRegex.test(part)) {
+        return (
+          <a key={`link-${index}`}  className={'link text-main'} href={part} target="_blank" rel="noopener noreferrer">
+            {part}
+          </a>
+        );
+      }
+      return part;
+    });
+
+    return formattedLine
+  }
   return (
     <div
       style={style}
@@ -58,7 +74,7 @@ function ConsoleRow(props: Props) {
           expanded &&
           lines.map((l: string, i: number) => (
             <div key={l.slice(0, 4) + i} className="ml-4 mb-1" style={{ fontFamily: 'Menlo, Monaco, Consolas' }}>
-              {l}
+              {renderLine(l)}
             </div>
           ))}
       </div>
