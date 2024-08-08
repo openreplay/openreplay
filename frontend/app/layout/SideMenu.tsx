be02bb9dcefdfd070dde66383be2461d50c79b2a
nick-delirium <nikita@openreplay.com>
2024-08-08 10:11:51 +0200

    init or card

--- frontend/app/layout/SideMenu.tsx
+++ frontend/app/layout/SideMenu.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Menu, Typography, Tag } from 'antd';
-import SVG from 'UI/SVG';
-import * as routes from 'App/routes';
-import { bookmarks, client, CLIENT_DEFAULT_TAB, CLIENT_TABS, fflags, notes, sessions, withSiteId } from 'App/routes';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
-import { categories as main_menu, MENU, preferences, PREFERENCES_MENU } from './data';
-import { connect } from 'react-redux';
-import { MODULES } from 'Components/Client/Modules';
+import { Menu, Tag, Typography, Button } from 'antd';
+import { ArrowRightOutlined } from '@ant-design/icons';
 import cn from 'classnames';
-import { Icon, Divider } from 'UI';
+import React from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+
 import SupportModal from 'App/layout/SupportModal';
+import * as routes from 'App/routes';
+import {
+  CLIENT_DEFAULT_TAB,
+  CLIENT_TABS,
+  bookmarks,
+  client,
+  fflags,
+  notes,
+  sessions,
+  withSiteId,
+} from 'App/routes';
+import { MODULES } from 'Components/Client/Modules';
 import { setActiveTab } from 'Duck/search';
+import { Divider, Icon } from 'UI';
+import SVG from 'UI/SVG';
+
+import {
+  MENU,
+  PREFERENCES_MENU,
+  categories as main_menu,
+  preferences,
+} from './data';
 
 const { Text } = Typography;
 
@@ -18,7 +35,7 @@ const TabToUrlMap = {
   all: sessions() as '/sessions',
   bookmark: bookmarks() as '/bookmarks',
   notes: notes() as '/notes',
-  flags: fflags() as '/feature-flags'
+  flags: fflags() as '/feature-flags',
 };
 
 interface Props extends RouteComponentProps {
@@ -31,7 +48,15 @@ interface Props extends RouteComponentProps {
 }
 
 function SideMenu(props: Props) {
-  const { activeTab, siteId, modules, location, account, isEnterprise, isCollapsed } = props;
+  const {
+    activeTab,
+    siteId,
+    modules,
+    location,
+    account,
+    isEnterprise,
+    isCollapsed,
+  } = props;
   const isPreferencesActive = location.pathname.includes('/client/');
   const [supportOpen, setSupportOpen] = React.useState(false);
   const isAdmin = account.admin || account.superAdmin;
@@ -39,8 +64,8 @@ function SideMenu(props: Props) {
   let menu: any[] = React.useMemo(() => {
     const sourceMenu = isPreferencesActive ? preferences : main_menu;
 
-    return sourceMenu.map(category => {
-      const updatedItems = category.items.map(item => {
+    return sourceMenu.map((category) => {
+      const updatedItems = category.items.map((item) => {
         if (isEnterprise) {
           if (item.key === MENU.BOOKMARKS) {
             return { ...item, hidden: true };
@@ -61,39 +86,45 @@ function SideMenu(props: Props) {
         if (item.hidden) return item;
 
         const isHidden = [
-          (item.key === MENU.RECOMMENDATIONS && modules.includes(MODULES.RECOMMENDATIONS)),
-          (item.key === MENU.FEATURE_FLAGS && modules.includes(MODULES.FEATURE_FLAGS)),
-          (item.key === MENU.NOTES && modules.includes(MODULES.NOTES)),
-          (item.key === MENU.LIVE_SESSIONS && modules.includes(MODULES.ASSIST)),
-          (item.key === MENU.SESSIONS && modules.includes(MODULES.OFFLINE_RECORDINGS)),
-          (item.key === MENU.ALERTS && modules.includes(MODULES.ALERTS)),
-          (item.isAdmin && !isAdmin),
-          (item.isEnterprise && !isEnterprise),
-        ].some(cond => cond);
+          item.key === MENU.RECOMMENDATIONS &&
+            modules.includes(MODULES.RECOMMENDATIONS),
+          item.key === MENU.FEATURE_FLAGS &&
+            modules.includes(MODULES.FEATURE_FLAGS),
+          item.key === MENU.NOTES && modules.includes(MODULES.NOTES),
+          item.key === MENU.LIVE_SESSIONS && modules.includes(MODULES.ASSIST),
+          item.key === MENU.SESSIONS &&
+            modules.includes(MODULES.OFFLINE_RECORDINGS),
+          item.key === MENU.ALERTS && modules.includes(MODULES.ALERTS),
+          item.isAdmin && !isAdmin,
+          item.isEnterprise && !isEnterprise,
+        ].some((cond) => cond);
 
         return { ...item, hidden: isHidden };
       });
 
-      const allItemsHidden = updatedItems.every(item => item.hidden);
+      const allItemsHidden = updatedItems.every((item) => item.hidden);
 
       return {
         ...category,
         items: updatedItems,
-        hidden: allItemsHidden
+        hidden: allItemsHidden,
       };
     });
   }, [isAdmin, isEnterprise, isPreferencesActive, modules]);
 
   React.useEffect(() => {
     const currentLocation = location.pathname;
-    const tab = Object.keys(TabToUrlMap).find((tab: keyof typeof TabToUrlMap) => currentLocation.includes(TabToUrlMap[tab]));
+    const tab = Object.keys(TabToUrlMap).find((tab: keyof typeof TabToUrlMap) =>
+      currentLocation.includes(TabToUrlMap[tab])
+    );
     if (tab && tab !== activeTab) {
       props.setActiveTab({ type: tab });
     }
   }, [location.pathname]);
 
   const menuRoutes: any = {
-    [MENU.EXIT]: () => props.history.push(withSiteId(routes.sessions(), siteId)),
+    [MENU.EXIT]: () =>
+      props.history.push(withSiteId(routes.sessions(), siteId)),
     [MENU.SESSIONS]: () => withSiteId(routes.sessions(), siteId),
     [MENU.BOOKMARKS]: () => withSiteId(routes.bookmarks(), siteId),
     [MENU.VAULT]: () => withSiteId(routes.bookmarks(), siteId),
@@ -107,7 +138,8 @@ function SideMenu(props: Props) {
     [MENU.USABILITY_TESTS]: () => withSiteId(routes.usabilityTesting(), siteId),
     [MENU.SPOTS]: () => withSiteId(routes.spotsList(), siteId),
     [PREFERENCES_MENU.ACCOUNT]: () => client(CLIENT_TABS.PROFILE),
-    [PREFERENCES_MENU.SESSION_LISTING]: () => client(CLIENT_TABS.SESSIONS_LISTING),
+    [PREFERENCES_MENU.SESSION_LISTING]: () =>
+      client(CLIENT_TABS.SESSIONS_LISTING),
     [PREFERENCES_MENU.INTEGRATIONS]: () => client(CLIENT_TABS.INTEGRATIONS),
     [PREFERENCES_MENU.METADATA]: () => client(CLIENT_TABS.CUSTOM_FIELDS),
     [PREFERENCES_MENU.WEBHOOKS]: () => client(CLIENT_TABS.WEBHOOKS),
@@ -117,7 +149,7 @@ function SideMenu(props: Props) {
     [PREFERENCES_MENU.TEAM]: () => client(CLIENT_TABS.MANAGE_USERS),
     [PREFERENCES_MENU.NOTIFICATIONS]: () => client(CLIENT_TABS.NOTIFICATIONS),
     [PREFERENCES_MENU.BILLING]: () => client(CLIENT_TABS.BILLING),
-    [PREFERENCES_MENU.MODULES]: () => client(CLIENT_TABS.MODULES)
+    [PREFERENCES_MENU.MODULES]: () => client(CLIENT_TABS.MODULES),
   };
 
   const handleClick = (item: any) => {
@@ -150,9 +182,14 @@ function SideMenu(props: Props) {
   return (
     <>
       <Menu
-        mode='inline' onClick={handleClick}
+        mode="inline"
+        onClick={handleClick}
         style={{ marginTop: '8px', border: 'none' }}
-        selectedKeys={menu.flatMap(category => category.items.filter((item: any) => isMenuItemActive(item.key)).map(item => item.key))}
+        selectedKeys={menu.flatMap((category) =>
+          category.items
+            .filter((item: any) => isMenuItemActive(item.key))
+            .map((item) => item.key)
+        )}
       >
         {menu.map((category, index) => (
           <React.Fragment key={category.key}>
@@ -160,83 +197,168 @@ function SideMenu(props: Props) {
               <>
                 {index > 0 && <Divider style={{ margin: '6px 0' }} />}
 
-                {category.items.filter((item: any) => !item.hidden).map((item: any) => {
-                  const isActive = isMenuItemActive(item.key);
+                {category.items
+                  .filter((item: any) => !item.hidden)
+                  .map((item: any) => {
+                    const isActive = isMenuItemActive(item.key);
 
-                  if (item.key === MENU.EXIT) {
-                    return (
+                    if (item.key === MENU.EXIT) {
+                      return (
+                        <Menu.Item
+                          key={item.key}
+                          style={{ paddingLeft: '20px' }}
+                          icon={
+                            <Icon
+                              name={item.icon}
+                              size={16}
+                              color={isActive ? 'teal' : ''}
+                            />
+                          }
+                          className={cn('!rounded-lg hover-fill-teal')}
+                        >
+                          {item.label}
+                        </Menu.Item>
+                      );
+                    }
+
+                    if (item.key === MENU.SPOTS) {
+                      return (
+                        <Menu.Item
+                          key={item.key}
+                          icon={
+                            <Icon
+                              name={item.icon}
+                              size={16}
+                              color={isActive ? 'teal' : ''}
+                            />
+                          }
+                          style={{ paddingLeft: '20px' }}
+                          className={cn('!rounded-lg hover-fill-teal !pe-0')}
+                          itemIcon={
+                            item.leading ? (
+                              <Icon
+                                name={item.leading}
+                                size={16}
+                                color={isActive ? 'teal' : ''}
+                              />
+                            ) : null
+                          }
+                        >
+                          <div
+                            style={{
+                              display: 'flex',
+                              justifyContent: 'space-between',
+                              alignItems: 'center',
+                            }}
+                          >
+                            {item.label}
+                            <Tag
+                              color="cyan"
+                              bordered={false}
+                              className="text-xs"
+                            >
+                              {' '}
+                              Beta{' '}
+                            </Tag>
+                          </div>
+                        </Menu.Item>
+                      );
+                    }
+
+                    return item.children ? (
+                      <Menu.SubMenu
+                        key={item.key}
+                        title={
+                          <Text className={cn('ml-5 !rounded')}>
+                            {item.label}
+                          </Text>
+                        }
+                        icon={<SVG name={item.icon} size={16} />}
+                      >
+                        {item.children.map((child: any) => (
+                          <Menu.Item
+                            className={cn('ml-8', {
+                              'ant-menu-item-selected !bg-active-dark-blue':
+                                isMenuItemActive(child.key),
+                            })}
+                            key={child.key}
+                          >
+                            {child.label}
+                          </Menu.Item>
+                        ))}
+                      </Menu.SubMenu>
+                    ) : (
                       <Menu.Item
                         key={item.key}
+                        icon={
+                          <Icon
+                            name={item.icon}
+                            size={16}
+                            color={isActive ? 'teal' : ''}
+                            className={'hover-fill-teal'}
+                          />
+                        }
                         style={{ paddingLeft: '20px' }}
-                        icon={<Icon name={item.icon} size={16} color={isActive ? 'teal' : ''} />}
                         className={cn('!rounded-lg hover-fill-teal')}
+                        itemIcon={
+                          item.leading ? (
+                            <Icon
+                              name={item.leading}
+                              size={16}
+                              color={isActive ? 'teal' : ''}
+                            />
+                          ) : null
+                        }
                       >
                         {item.label}
                       </Menu.Item>
                     );
-                  }
-
-                  if (item.key === MENU.SPOTS) {
-                    return (
-                      <Menu.Item
-                        key={item.key}
-                        icon={<Icon name={item.icon} size={16} color={isActive ? 'teal' : ''} />}
-                        style={{ paddingLeft: '20px' }}
-                        className={cn('!rounded-lg hover-fill-teal !pe-0')}
-                        itemIcon={item.leading ? <Icon name={item.leading} size={16} color={isActive ? 'teal' : ''} /> : null}
-                      >
-                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                          {item.label}
-                          <Tag color="cyan" bordered={false} className='text-xs'> Beta </Tag>
-                        </div>
-                      </Menu.Item>
-                    );
-                  }
-
-                  return item.children ? (
-                    <Menu.SubMenu
-                      key={item.key}
-                      title={<Text className={cn('ml-5 !rounded')}>{item.label}</Text>}
-                      icon={<SVG name={item.icon} size={16} />}
-                    >
-                      {item.children.map((child: any) => (
-                        <Menu.Item
-                          className={cn('ml-8', { 'ant-menu-item-selected !bg-active-dark-blue': isMenuItemActive(child.key) })}
-                          key={child.key}
-                        >
-                          {child.label}
-                        </Menu.Item>
-                      ))}
-                    </Menu.SubMenu>
-                  ) : (
-                    <Menu.Item
-                      key={item.key}
-                      icon={<Icon name={item.icon} size={16} color={isActive ? 'teal' : ''} className={'hover-fill-teal'} />}
-                      style={{ paddingLeft: '20px' }}
-                      className={cn('!rounded-lg hover-fill-teal')}
-                      itemIcon={item.leading ? <Icon name={item.leading} size={16} color={isActive ? 'teal' : ''} /> : null}
-                    >
-                      {item.label}
-                    </Menu.Item>
-                  );
-                })}
+                  })}
               </>
             )}
           </React.Fragment>
         ))}
       </Menu>
+      <InitORCard />
       <SupportModal onClose={() => setSupportOpen(false)} open={supportOpen} />
     </>
   );
 }
 
+function InitORCard() {
+  return (
+    <div
+      className={'shadow-sm flex flex-col gap-4 bg-white items-center p-4 mx-auto rounded'}
+      style={{ width: 236 }}
+    >
+      <img src={'assets/img/init-or.png'} width={200} height={120} />
+      <div className={'font-semibold'}>
+        Discover the full potential of OpenReplay!
+      </div>
+      <div>
+        With features like Session Replay, Product Analytics, Co-Browsing, and
+        Usability Testing, to enable a stellar product experience.
+      </div>
+      <Button
+        type="primary"
+        ghost
+        icon={<ArrowRightOutlined />}
+        iconPosition={'end'}
+      >
+        Setup your first project
+      </Button>
+    </div>
+  );
+}
+
 export default withRouter(
-  connect((state: any) => ({
-    modules: state.getIn(['user', 'account', 'settings', 'modules']) || [],
-    activeTab: state.getIn(['search', 'activeTab', 'type']),
-    isEnterprise: state.getIn(['user', 'account', 'edition']) === 'ee',
-    account: state.getIn(['user', 'account'])
-  }),
+  connect(
+    (state: any) => ({
+      modules: state.getIn(['user', 'account', 'settings', 'modules']) || [],
+      activeTab: state.getIn(['search', 'activeTab', 'type']),
+      isEnterprise: state.getIn(['user', 'account', 'edition']) === 'ee',
+      account: state.getIn(['user', 'account']),
+    }),
     { setActiveTab }
   )(SideMenu)
 );
