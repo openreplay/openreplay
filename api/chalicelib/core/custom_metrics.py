d75735696f6adf54eda52ca3a57fafcb510f1764
Taha Yassine Kraiem <tahayk2@gmail.com>
2024-08-09 17:26:59 +0100

    feat(chalice): table of referrers

--- api/chalicelib/core/custom_metrics.py
+++ api/chalicelib/core/custom_metrics.py
@@ -137,6 +137,10 @@ def __get_table_of_urls(project_id: int, data: schemas.CardTable, user_id: int =
     return __get_table_of_series(project_id=project_id, data=data)
 
 
+def __get_table_of_referrers(project_id: int, data: schemas.CardTable, user_id: int = None):
+    return __get_table_of_series(project_id=project_id, data=data)
+
+
 def __get_table_chart(project_id: int, data: schemas.CardTable, user_id: int):
     supported = {
         schemas.MetricOfTable.SESSIONS: __get_table_of_sessions,
@@ -147,6 +151,7 @@ def __get_table_chart(project_id: int, data: schemas.CardTable, user_id: int):
         schemas.MetricOfTable.USER_DEVICE: __get_table_of_devises,
         schemas.MetricOfTable.USER_COUNTRY: __get_table_of_countries,
         schemas.MetricOfTable.VISITED_URL: __get_table_of_urls,
+        schemas.MetricOfTable.REFERRER: __get_table_of_referrers,
     }
     return supported.get(data.metric_of, not_supported)(project_id=project_id, data=data, user_id=user_id)
 
