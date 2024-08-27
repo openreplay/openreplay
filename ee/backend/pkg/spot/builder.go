3e98ac083093668d96e8659a880e047716b49ec9
Alexander <zavorotynskiy@pm.me>
2024-08-27 12:06:09 +0200

    feat(spot): fixed ee service builder

--- ee/backend/pkg/spot/builder.go
+++ ee/backend/pkg/spot/builder.go
@@ -1,4 +1,4 @@
-package service
+package spot
 
 import (
 	"openreplay/backend/internal/config/spot"
@@ -8,16 +8,18 @@ import (
 	"openreplay/backend/pkg/objectstorage"
 	"openreplay/backend/pkg/objectstorage/store"
 	"openreplay/backend/pkg/spot/auth"
+	"openreplay/backend/pkg/spot/service"
+	"openreplay/backend/pkg/spot/transcoder"
 )
 
 type ServicesBuilder struct {
 	Flaker     *flakeid.Flaker
 	ObjStorage objectstorage.ObjectStorage
 	Auth       auth.Auth
-	Spots      Spots
-	Keys       Keys
-	Transcoder Transcoder
-	Tracer     Tracer
+	Spots      service.Spots
+	Keys       service.Keys
+	Transcoder transcoder.Transcoder
+	Tracer     service.Tracer
 }
 
 func NewServiceBuilder(log logger.Logger, cfg *spot.Config, pgconn pool.Pool) (*ServicesBuilder, error) {
@@ -30,13 +32,14 @@ func NewServiceBuilder(log logger.Logger, cfg *spot.Config, pgconn pool.Pool) (*
 	if err != nil {
 		return nil, err
 	}
+	spots := service.NewSpots(log, pgconn, flaker)
 	return &ServicesBuilder{
 		Flaker:     flaker,
 		ObjStorage: objStore,
-		Auth:       auth.NewAuth(log, cfg.JWTSecret, pgconn),
-		Spots:      NewSpots(log, pgconn, flaker),
-		Keys:       NewKeys(log, pgconn),
-		Transcoder: NewTranscoder(cfg, log, objStore, pgconn),
+		Auth:       auth.NewAuth(log, cfg.JWTSecret, cfg.JWTSpotSecret, pgconn),
+		Spots:      spots,
+		Keys:       service.NewKeys(log, pgconn),
+		Transcoder: transcoder.NewTranscoder(cfg, log, objStore, pgconn, spots),
 		Tracer:     tracer,
 	}, nil
 }
