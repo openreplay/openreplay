deff01a89402bb1ea7ef853813826cc8aac46131
nick-delirium <nikita@openreplay.com>
2024-08-28 15:53:18 +0200

    refactor svgs

--- spot/entrypoints/popup/App.tsx
+++ spot/entrypoints/popup/App.tsx
@@ -6,6 +6,14 @@ import Settings from "@/entrypoints/popup/Settings";
 import { createSignal, createEffect, onMount } from "solid-js";
 import Dropdown from "@/entrypoints/popup/Dropdown";
 import Button from "@/entrypoints/popup/Button";
+import {
+  ChevronSvg,
+  RecordDesktopSvg,
+  RecordTabSvg,
+  HomePageSvg,
+  SlackSvg,
+  SettingsSvg,
+} from "./Icons";
 
 async function getAudioDevices() {
   try {
@@ -80,33 +88,7 @@ function Header({ openSettings }: { openSettings: () => void }) {
           onClick={openSettings}
         >
           <div class={"cursor-pointer p-2 hover:bg-indigo-50 rounded-full"}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-cog"
-            >
-              <path d="M12 20a8 8 0 1 0 0-16 8 8 0 0 0 0 16Z" />
-              <path d="M12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
-              <path d="M12 2v2" />
-              <path d="M12 22v-2" />
-              <path d="m17 20.66-1-1.73" />
-              <path d="M11 10.27 7 3.34" />
-              <path d="m20.66 17-1.73-1" />
-              <path d="m3.34 7 1.73 1" />
-              <path d="M14 12h8" />
-              <path d="M2 12h2" />
-              <path d="m20.66 7-1.73 1" />
-              <path d="m3.34 17 1.73-1" />
-              <path d="m17 3.34-1 1.73" />
-              <path d="m11 13.73-4 6.93" />
-            </svg>
+            <SettingsSvg />
           </div>
         </div>
       </div>
@@ -128,7 +110,7 @@ function App() {
   const [mic, setMic] = createSignal(false);
   const [selectedAudioDevice, setSelectedAudioDevice] = createSignal("");
   const [hasPermissions, setHasPermissions] = createSignal(false);
-  
+
   onMount(() => {
     browser.runtime.onMessage.addListener((message) => {
       if (message.type === "popup:no-login") {
@@ -261,7 +243,7 @@ function AudioPicker(props: IAudioPicker) {
 
   createEffect(() => {
     chrome.storage.local.get("audioPerm", (data) => {
-      console.log('audioPerm', data.audioPerm);
+      console.log("audioPerm", data.audioPerm);
       if (data.audioPerm && audioDevices().length === 0) {
         props.setHasPermissions(true);
         void checkAudioDevices();
@@ -286,7 +268,7 @@ function AudioPicker(props: IAudioPicker) {
       props.setSelectedAudioDevice(audioDevices[0]?.id || "");
     }
   };
-  
+
   const checkAudio = async () => {
     if (checkedAudioDevices() > 0) {
       return;
@@ -337,7 +319,9 @@ function AudioPicker(props: IAudioPicker) {
       >
         {audioDevices().length === 0 ? (
           <div class="max-w-64 block leading-tight cursor-pointer whitespace-nowrap overflow-hidden font-normal">
-            {checkedAudioDevices() === 1 ? 'Loading audio devices' : 'Grant microphone access'}
+            {checkedAudioDevices() === 1
+              ? "Loading audio devices"
+              : "Grant microphone access"}
           </div>
         ) : (
           <Dropdown
@@ -349,110 +333,6 @@ function AudioPicker(props: IAudioPicker) {
         <ChevronSvg />
       </div>
     </div>
-  );
-}
-
-function ChevronSvg() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      className="lucide lucide-chevron-down"
-    >
-      <path d="m6 9 6 6 6-6" />
-    </svg>
-  );
-}
-
-function RecordDesktopSvg() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      className="lucide lucide-monitor-dot"
-    >
-      <circle cx="19" cy="6" r="3" />
-      <path d="M22 12v3a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h9" />
-      <path d="M12 17v4" />
-      <path d="M8 21h8" />
-    </svg>
-  );
-}
-
-function RecordTabSvg() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      className="lucide lucide-app-window"
-    >
-      <rect x="2" y="4" width="20" height="16" rx="2" />
-      <path d="M10 4v4" />
-      <path d="M2 8h20" />
-      <path d="M6 4v4" />
-    </svg>
-  )
-}
-
-function HomePageSvg() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="lucide lucide-house"
-    >
-      <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-      <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-    </svg>
-  );
-}
-
-function SlackSvg() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="lucide lucide-circle-help"
-    >
-      <circle cx="12" cy="12" r="10" />
-      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-      <path d="M12 17h.01" />
-    </svg>
   );
 }
 
