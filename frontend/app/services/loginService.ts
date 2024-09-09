98e18990d9d638d9572ab23f6521494c7df42841
nick-delirium <nikita@openreplay.com>
2024-09-09 09:48:26 +0200

    ui remove spot login query

--- frontend/app/services/loginService.ts
+++ frontend/app/services/loginService.ts
@@ -2,7 +2,7 @@ import BaseService from "./BaseService";
 
 export default class LoginService extends BaseService {
   public async login({ email, password, captchaResponse }: { email: string, password: string, captchaResponse?: string }) {
-    return this.client.post('/login?spot=true', {
+    return this.client.post('/login', {
       email: email.trim(),
       password,
       'g-recaptcha-response': captchaResponse,
