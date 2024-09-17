4b3563cacd633408d5d3606f8648dbc7b42cbe8b
Kraiem Taha Yassine <tahayk2@gmail.com>
2024-09-17 12:42:34 +0200

    fix(chalice): fixed issues-tracking error handler (#2593)
    (cherry picked from commit 5d786bde562d209c048ceeb11440da0f25711412)

--- api/routers/core.py
+++ api/routers/core.py
@@ -395,10 +395,10 @@ def get_all_issue_tracking_projects(context: schemas.CurrentContext = Depends(OR
     if error is not None:
         return error
     data = integration.issue_handler
-    if "errors" in data:
+    if isinstance(data, dict) and "errors" in data:
         return data
     data = data.get_projects()
-    if "errors" in data:
+    if isinstance(data, dict) and "errors" in data:
         return data
     return {"data": data}
 
@@ -410,10 +410,10 @@ def get_integration_metadata(integrationProjectId: int, context: schemas.Current
     if error is not None:
         return error
     data = integration
-    if "errors" in data:
+    if isinstance(data, dict) and "errors" in data:
         return data
     data = data.issue_handler.get_metas(integrationProjectId)
-    if "errors" in data:
+    if isinstance(data, dict) and "errors" in data:
         return data
     return {"data": data}
 
