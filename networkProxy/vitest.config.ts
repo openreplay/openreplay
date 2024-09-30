cac0f3edc0ea2c628e0c1b2330995ce47b4b5194
nick-delirium <nikita@openreplay.com>
2024-09-30 12:52:11 +0200

    network proxy: add vitest coverage

--- networkProxy/vitest.config.ts
+++ networkProxy/vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig(({ mode }) => ({
   define: { global: 'window' },
   test: {
-    environment: "jsdom"
+    environment: "jsdom",
+    coverage: {
+      provider: 'istanbul' // or 'v8'
+    },
   }
 }));
