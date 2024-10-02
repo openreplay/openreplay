97d669ebf165b3e4822926a5f22a08b9ffac7b3a
nick-delirium <nikita@openreplay.com>
2024-10-02 09:47:36 +0200

    fixing iframe issues...

--- tracker/tracker/src/main/app/index.ts
+++ tracker/tracker/src/main/app/index.ts
@@ -354,7 +354,6 @@ export default class App {
 
     const thisTab = this.session.getTabId()
     const catchParentMessage = (event: MessageEvent) => {
-      if (!this.active()) return
       const { data } = event
       if (!data) return
       if (data.line === proto.parentAlive) {
@@ -371,6 +370,9 @@ export default class App {
     }
 
     window.addEventListener('message', catchParentMessage)
+    this.attachStopCallback(() => {
+      window.removeEventListener('message', catchParentMessage)
+    })
 
     if (this.bc !== null) {
       this.bc.postMessage({
@@ -427,7 +429,6 @@ export default class App {
        * */
       let crossdomainFrameCount = 0
       const catchIframeMessage = (event: MessageEvent) => {
-        if (!this.active()) return;
         const { data } = event
         if (!data) return
         if (data.line === proto.iframeSignal) {
@@ -513,6 +514,9 @@ export default class App {
         }
       }
       window.addEventListener('message', catchIframeMessage)
+      this.attachStopCallback(() => {
+        window.removeEventListener('message', catchIframeMessage)
+      })
     }
   }
 
