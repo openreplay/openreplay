eb7e7de463b10489867bd4365d20f752fe25f573
Alexander <zavorotynskiy@pm.me>
2024-11-15 10:56:58 +0100

    feat(backend): fix to save the latest message tracker just after the token has been expired

--- backend/pkg/token/http.go
+++ backend/pkg/token/http.go
@@ -11,7 +11,7 @@ const BEARER_SCHEMA = "Bearer "
 func (tokenizer *Tokenizer) ParseFromHTTPRequest(r *http.Request) (*TokenData, error) {
 	header := r.Header.Get("Authorization")
 	if !strings.HasPrefix(header, BEARER_SCHEMA) {
-		return nil, errors.New("Missing token")
+		return nil, errors.New("missing token")
 	}
 	token := header[len(BEARER_SCHEMA):]
 	return tokenizer.Parse(token)
