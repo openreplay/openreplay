99085a95a11a643975ac3d7510399fed1d927c7c
Shekar Siri <sshekarsiri@gmail.com>
2024-11-27 16:13:26 +0100

    feat(analytics): dashboards

---
+++ backend/internal/config/analytics/config.go
@@ -0,0 +1,29 @@
+package analytics
+
+import (
+	"time"
+
+	"openreplay/backend/internal/config/common"
+	"openreplay/backend/internal/config/configurator"
+	"openreplay/backend/internal/config/objectstorage"
+	"openreplay/backend/internal/config/redis"
+	"openreplay/backend/pkg/env"
+	"openreplay/backend/pkg/logger"
+)
+
+type Config struct {
+	common.Config
+	common.Postgres
+	redis.Redis
+	objectstorage.ObjectsConfig
+	common.HTTP
+	FSDir             string        `env:"FS_DIR,required"`
+	ProjectExpiration time.Duration `env:"PROJECT_EXPIRATION,default=10m"`
+	WorkerID          uint16
+}
+
+func New(log logger.Logger) *Config {
+	cfg := &Config{WorkerID: env.WorkerID()}
+	configurator.Process(log, cfg)
+	return cfg
+}
