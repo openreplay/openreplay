eb3db54dcb6dfc97998ae53a02a8ac2ad23d0669
Alexander <zavorotynskiy@pm.me>
2025-01-22 15:52:39 +0100

    feat(analytics): session/user trends

--- backend/pkg/analytics/charts/charts.go
+++ backend/pkg/analytics/charts/charts.go
@@ -3,6 +3,8 @@ package charts
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"openreplay/backend/pkg/analytics/cards"
 
 	"openreplay/backend/pkg/db/postgres/pool"
 	"openreplay/backend/pkg/logger"
@@ -15,6 +17,7 @@ type Charts interface {
 type chartsImpl struct {
 	log    logger.Logger
 	pgconn pool.Pool
+	chConn driver.Conn
 }
 
 func New(log logger.Logger, conn pool.Pool) (Charts, error) {
@@ -24,7 +27,39 @@ func New(log logger.Logger, conn pool.Pool) (Charts, error) {
 	}, nil
 }
 
+// def get_chart()
 func (s *chartsImpl) GetData(projectId int, userID uint64, req *GetCardChartDataRequest) ([]DataPoint, error) {
+	if req == nil {
+		return nil, fmt.Errorf("request is empty")
+	}
+	switch {
+	case req.MetricType == "funnel":
+		return nil, fmt.Errorf("funnel metric type is not supported yet")
+	case req.MetricType == "heatMap":
+		return nil, fmt.Errorf("heatMap metric type is not supported yet")
+	case req.MetricType == "pathAnalysis":
+		return nil, fmt.Errorf("pathAnalysis metric type is not supported yet")
+
+	case req.MetricType == "timeseries":
+		return s.getTimeseriesCharts(projectId, userID, req)
+	case req.MetricType == "table":
+		return nil, fmt.Errorf("table metric type is not supported yet")
+
+	case req.MetricType == "errors":
+		fallthrough
+	case req.MetricType == "performance":
+		fallthrough
+	case req.MetricType == "resources":
+		fallthrough
+	case req.MetricType == "webVitals":
+		return s.getMetric(projectId, userID, req)
+
+	case req.MetricType == "retention":
+		return nil, fmt.Errorf("retention metric type is not supported yet")
+	case req.MetricType == "stickiness":
+		return nil, fmt.Errorf("stickiness metric type is not supported yet")
+
+	}
 	jsonInput := `
     {
         "data": [
@@ -47,4 +82,41 @@ func (s *chartsImpl) GetData(projectId int, userID uint64, req *GetCardChartData
 	}
 
 	return resp.Data, nil
+}
+
+func (s *chartsImpl) getMetric(projectID int, userID uint64, req *GetCardChartDataRequest) ([]DataPoint, error) {
+	switch req.MetricOf {
+	case "countSessions": // metrics.get_processed_sessions
+		return nil, fmt.Errorf("countSessions metric type is not supported yet")
+	case "avgVisitedPages": // metrics.get_user_activity_avg_visited_pages
+		return nil, fmt.Errorf("avgVisitedPages metric type is not supported yet")
+	case "countRequests": // metrics.get_top_metrics_count_requests
+		return nil, fmt.Errorf("countRequests metric type is not supported yet")
+	case "impactedSessionsByJsErrors": // metrics.get_impacted_sessions_by_js_errors
+		return nil, fmt.Errorf("impactedSessionsByJsErrors metric type is not supported yet")
+	case "domainsErrors4xx": // metrics.get_domains_errors_4xx
+		return nil, fmt.Errorf("domainsErrors4xx metric type is not supported yet")
+	case "domainsErrors5xx": // metrics.get_domains_errors_5xx
+		return nil, fmt.Errorf("domainsErrors5xx metric type is not supported yet")
+	case "errorsPerDomains": // metrics.get_errors_per_domains
+		return nil, fmt.Errorf("errorsPerDomains metric type is not supported yet")
+	case "errorsPerType": // metrics.get_errors_per_type
+		return nil, fmt.Errorf("errorsPerType metric type is not supported yet")
+
+	}
+	return nil, fmt.Errorf("metric type is not supported yet")
+
+}
+
+func (s *chartsImpl) getTimeseriesCharts(projectID int, userID uint64, req *GetCardChartDataRequest) ([]DataPoint, error) {
+	charts := []interface{}{}
+	for _, series := range req.Series {
+		res, err := s.searchSeries(projectID, series)
+		if err != nil {
+			return nil, err
+		}
+		charts = append(charts, res)
+	}
+	results := []interface{}{}
+	return results, nil
 }
