8320e23258bc09f1857b50b0b5774268596a7bd0
nick-delirium <nikita@openreplay.com>
2025-01-24 14:06:07 +0100

    tracker: rm sanitizer thing

--- tracker/tracker/src/main/modules/network.ts
+++ tracker/tracker/src/main/modules/network.ts
@@ -81,8 +81,8 @@ export default function (app: App, opts: Partial<Options> = {}) {
 
   const ignoreHeaders = options.ignoreHeaders
   const isHIgnored = Array.isArray(ignoreHeaders)
-    ? (name: string) => ignoreHeaders.includes(name)
-    : () => ignoreHeaders
+                     ? (name: string) => ignoreHeaders.includes(name)
+                     : () => ignoreHeaders
 
   const stHeader =
     options.sessionTokenHeader === true ? 'X-OpenReplay-SessionToken' : options.sessionTokenHeader
@@ -97,32 +97,22 @@ export default function (app: App, opts: Partial<Options> = {}) {
   }
 
   function sanitize(reqResInfo: RequestResponseData) {
-    let finalRequestData = reqResInfo
     if (!options.capturePayload) {
       // @ts-ignore
-      delete finalRequestData.request.body
-      delete finalRequestData.response.body
+      delete reqResInfo.request.body
+      delete reqResInfo.response.body
     }
     if (options.sanitizer) {
-      const resBody = finalRequestData.response.body
+      const resBody = reqResInfo.response.body
       if (typeof resBody === 'string') {
         // Parse response in order to have handy view in sanitization function
         try {
-          finalRequestData.response.body = JSON.parse(resBody)
+          reqResInfo.response.body = JSON.parse(resBody)
         } catch {}
       }
-      finalRequestData = options.sanitizer(finalRequestData) as RequestResponseData
+      return options.sanitizer(reqResInfo)
     }
-    if (finalRequestData.request.body) {
-      finalRequestData.request.body = filterBody(finalRequestData.request.body)
-    }
-    if (finalRequestData.response.body) {
-      finalRequestData.response.body = filterBody(finalRequestData.response.body)
-    }
-    finalRequestData.request.headers = filterHeaders(finalRequestData.request.headers)
-    finalRequestData.response.headers = filterHeaders(finalRequestData.response.headers)
-
-    return finalRequestData
+    return reqResInfo
   }
 
   function stringify(r: RequestData | ResponseData): string {
@@ -153,7 +143,7 @@ export default function (app: App, opts: Partial<Options> = {}) {
             NetworkRequest(
               message.requestType,
               message.method,
-              tryFilterUrl(message.url),
+              message.url,
               message.request,
               message.response,
               message.status,
@@ -370,130 +360,5 @@ export default function (app: App, opts: Partial<Options> = {}) {
 
   if (options.captureInIframes) {
     app.observer.attachContextCallback(app.safe(patchWindow))
-  }
-}
-
-export const sensitiveParams = new Set([
-  'password',
-  'pass',
-  'pwd',
-  'mdp',
-  'token',
-  'bearer',
-  'jwt',
-  'api_key',
-  'api-key',
-  'apiKey',
-  'key',
-  'secret',
-  'id',
-  'user',
-  'userId',
-  'email',
-  'ssn',
-  'name',
-  'firstname',
-  'lastname',
-  'birthdate',
-  'dob',
-  'address',
-  'zip',
-  'zipcode',
-  'x-api-key',
-  'www-authenticate',
-  'x-csrf-token',
-  'x-requested-with',
-  'x-forwarded-for',
-  'x-real-ip',
-  'cookie',
-  'authorization',
-  'auth',
-  'proxy-authorization',
-  'set-cookie',
-  'account_key',
-])
-
-export function filterHeaders(headers: Record<string, string> | { name: string; value: string }[]) {
-  const filteredHeaders: Record<string, string> = {}
-  if (Array.isArray(headers)) {
-    headers.forEach(({ name, value }) => {
-      if (sensitiveParams.has(name.toLowerCase())) {
-        filteredHeaders[name] = '******'
-      } else {
-        filteredHeaders[name] = value
-      }
-    })
-  } else {
-    for (const [key, value] of Object.entries(headers)) {
-      if (sensitiveParams.has(key.toLowerCase())) {
-        filteredHeaders[key] = '******'
-      } else {
-        filteredHeaders[key] = value
-      }
-    }
-  }
-  return filteredHeaders
-}
-
-export function filterBody(body: any): string {
-  if (!body) {
-    return body
-  }
-
-  let parsedBody
-  let isJSON = false
-
-  try {
-    parsedBody = JSON.parse(body)
-    isJSON = true
-  } catch (e) {
-    // not json
-  }
-
-  if (isJSON) {
-    obscureSensitiveData(parsedBody)
-    return JSON.stringify(parsedBody)
-  } else {
-    const params = new URLSearchParams(body)
-    for (const key of params.keys()) {
-      if (sensitiveParams.has(key.toLowerCase())) {
-        params.set(key, '******')
-      }
-    }
-
-    return params.toString()
-  }
-}
-
-export function obscureSensitiveData(obj: Record<string, any> | any[]) {
-  if (Array.isArray(obj)) {
-    obj.forEach(obscureSensitiveData)
-  } else if (obj && typeof obj === 'object') {
-    for (const key in obj) {
-      if (Object.hasOwn(obj, key)) {
-        if (sensitiveParams.has(key.toLowerCase())) {
-          obj[key] = '******'
-        } else if (obj[key] !== null && typeof obj[key] === 'object') {
-          obscureSensitiveData(obj[key])
-        }
-      }
-    }
-  }
-}
-
-export function tryFilterUrl(url: string) {
-  if (!url) return ''
-  try {
-    const urlObj = new URL(url)
-    if (urlObj.searchParams) {
-      for (const key of urlObj.searchParams.keys()) {
-        if (sensitiveParams.has(key.toLowerCase())) {
-          urlObj.searchParams.set(key, '******')
-        }
-      }
-    }
-    return urlObj.toString()
-  } catch (e) {
-    return url
   }
 }
