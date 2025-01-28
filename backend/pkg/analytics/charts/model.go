b10ed1e58a5100949986ee4d90b65e42a2f90a9e
Shekar Siri <sshekarsiri@gmail.com>
2025-01-28 10:16:51 +0100

    feat(analytics): filters

--- backend/pkg/analytics/charts/model.go
+++ backend/pkg/analytics/charts/model.go
@@ -8,14 +8,50 @@ type DataPoint struct {
 }
 
 type GetCardChartDataRequest struct {
-	MetricType   string             `json:"metricType" validate:"required,oneof=timeseries table funnel errors performance resources webVitals pathAnalysis retention stickiness heatMap"`
-	MetricOf     string             `json:"metricOf" validate:"required,oneof=sessionCount userCount"`
-	ViewType     string             `json:"viewType" validate:"required,oneof=lineChart areaChart barChart pieChart progressChart table metric"`
-	MetricFormat string             `json:"metricFormat" validate:"required,oneof=default percentage"`
-	SessionID    int64              `json:"sessionId"`
-	Series       []cards.CardSeries `json:"series" validate:"required,dive"`
+	StartTimestamp int64              `json:"startTimestamp" validate:"required"`
+	EndTimestamp   int64              `json:"endTimestamp" validate:"required"`
+	Density        int                `json:"density" validate:"required"`
+	MetricType     string             `json:"metricType" validate:"required,oneof=timeseries table funnel errors performance resources webVitals pathAnalysis retention stickiness heatMap"`
+	MetricOf       string             `json:"metricOf" validate:"required,oneof=sessionCount userCount"`
+	ViewType       string             `json:"viewType" validate:"required,oneof=lineChart areaChart barChart pieChart progressChart table metric"`
+	MetricFormat   string             `json:"metricFormat" validate:"required,oneof=default percentage"`
+	SessionID      int64              `json:"sessionId"`
+	Series         []cards.CardSeries `json:"series" validate:"required,dive"`
 }
 
 type GetCardChartDataResponse struct {
 	Data []DataPoint `json:"data"`
 }
+
+type MetricType string
+type MetricOfTimeseries string
+type MetricOfTable string
+
+const (
+	MetricTypeTimeseries MetricType = "TIMESERIES"
+	MetricTypeTable      MetricType = "TABLE"
+
+	MetricOfTimeseriesSessionCount MetricOfTimeseries = "SESSION_COUNT"
+	MetricOfTimeseriesUserCount    MetricOfTimeseries = "USER_COUNT"
+
+	MetricOfTableVisitedURL  MetricOfTable = "VISITED_URL"
+	MetricOfTableIssues      MetricOfTable = "ISSUES"
+	MetricOfTableUserCountry MetricOfTable = "USER_COUNTRY"
+	MetricOfTableUserDevice  MetricOfTable = "USER_DEVICE"
+	MetricOfTableUserBrowser MetricOfTable = "USER_BROWSER"
+)
+
+type SessionsSearchPayload struct {
+	StartTimestamp int64
+	EndTimestamp   int64
+	Filters        []SessionSearchFilter
+}
+
+type SessionSearchFilter struct {
+	Type     FilterType
+	Value    interface{}
+	Operator SearchEventOperator
+}
+
+type SearchEventOperator string // Define constants as needed
+type FilterType string          // Define constants as needed
