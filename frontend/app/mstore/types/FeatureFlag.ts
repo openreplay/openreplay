8d5860a7751bcd74583e1ebcf808dcc6149922a4
Shekar Siri <sshekarsiri@gmail.com>
2025-01-29 15:43:53 +0100

    change(ui): removed log

--- frontend/app/mstore/types/FeatureFlag.ts
+++ frontend/app/mstore/types/FeatureFlag.ts
@@ -9,7 +9,6 @@ export class Conditions {
 
   constructor(data?: Record<string, any>, isConditional?: boolean, isMobile?: boolean) {
     makeAutoObservable(this);
-    console.log('data', data)
     this.name = data?.name;
     if (data && (data.rolloutPercentage || data.captureRate)) {
       this.rolloutPercentage = data.rolloutPercentage ?? data.captureRate;
