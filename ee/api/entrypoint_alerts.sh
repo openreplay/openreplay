b771d2803123327179ccf33a137ccdd0f16a7aa4
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-02-10 10:45:03 +0100

    fix(alerts): fixed alerts

--- ee/api/entrypoint_alerts.sh
+++ ee/api/entrypoint_alerts.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export TZ=UTC
 export ASSIST_KEY=ignore
+export CH_POOL=false
 sh env_vars.sh
 source /tmp/.env.override
 uvicorn app:app --host 0.0.0.0 --port 8888  --log-level ${S_LOGLEVEL:-warning}
