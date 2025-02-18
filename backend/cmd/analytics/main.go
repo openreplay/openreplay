b9f9675ba25655e2687cec68f364c024eabc3b23
Shekar Siri <sshekarsiri@gmail.com>
2025-02-18 11:42:39 +0100

    feat(analytics): base structure

--- backend/cmd/analytics/main.go
+++ backend/cmd/analytics/main.go
@@ -2,71 +2,49 @@ package main
 
 import (
 	"context"
-	"os"
-	"os/signal"
-	"syscall"
-
+	analyticsConfig "openreplay/backend/internal/config/analytics"
+	"openreplay/backend/pkg/analytics"
+	"openreplay/backend/pkg/analytics/db"
+	"openreplay/backend/pkg/db/postgres/pool"
 	"openreplay/backend/pkg/logger"
+	"openreplay/backend/pkg/metrics"
+	analyticsMetrics "openreplay/backend/pkg/metrics/analytics"
+	databaseMetrics "openreplay/backend/pkg/metrics/database"
+	"openreplay/backend/pkg/metrics/web"
+	"openreplay/backend/pkg/server"
+	"openreplay/backend/pkg/server/api"
 )
 
 func main() {
 	ctx := context.Background()
 	log := logger.New()
-	log.Info(ctx, "Cacher service started")
+	cfg := analyticsConfig.New(log)
+	webMetrics := web.New("analytics")
+	metrics.New(log, append(webMetrics.List(), append(analyticsMetrics.List(), databaseMetrics.List()...)...))
 
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
-
-	for {
-		select {
-		case sig := <-sigchan:
-			log.Error(ctx, "Caught signal %v: terminating", sig)
-			os.Exit(0)
-		}
+	pgConn, err := pool.New(cfg.Postgres.String())
+	if err != nil {
+		log.Fatal(ctx, "can't init postgres connection: %s", err)
 	}
-}
+	defer pgConn.Close()
 
-//
-//import (
-//	"context"
-//
-//	analyticsConfig "openreplay/backend/internal/config/analytics"
-//	"openreplay/backend/pkg/analytics"
-//	"openreplay/backend/pkg/db/postgres/pool"
-//	"openreplay/backend/pkg/logger"
-//	"openreplay/backend/pkg/metrics"
-//	"openreplay/backend/pkg/metrics/database"
-//	"openreplay/backend/pkg/metrics/web"
-//	"openreplay/backend/pkg/server"
-//	"openreplay/backend/pkg/server/api"
-//)
-//
-//func main() {
-//	ctx := context.Background()
-//	log := logger.New()
-//	cfg := analyticsConfig.New(log)
-//	// Observability
-//	webMetrics := web.New("analytics")
-//	dbMetrics := database.New("analytics")
-//	metrics.New(log, append(webMetrics.List(), dbMetrics.List()...))
-//
-//	pgConn, err := pool.New(dbMetrics, cfg.Postgres.String())
-//	if err != nil {
-//		log.Fatal(ctx, "can't init postgres connection: %s", err)
-//	}
-//	defer pgConn.Close()
-//
-//	builder, err := analytics.NewServiceBuilder(log, cfg, webMetrics, dbMetrics, pgConn)
-//	if err != nil {
-//		log.Fatal(ctx, "can't init services: %s", err)
-//	}
-//
-//	router, err := api.NewRouter(&cfg.HTTP, log)
-//	if err != nil {
-//		log.Fatal(ctx, "failed while creating router: %s", err)
-//	}
-//	router.AddHandlers(api.NoPrefix, builder.CardsAPI, builder.DashboardsAPI, builder.ChartsAPI)
-//	router.AddMiddlewares(builder.Auth.Middleware, builder.RateLimiter.Middleware, builder.AuditTrail.Middleware)
-//
-//	server.Run(ctx, log, &cfg.HTTP, router)
-//}
+	chConn, err := db.NewConnector(cfg.Clickhouse)
+	if err != nil {
+		log.Fatal(ctx, "can't init clickhouse connection: %s", err)
+	}
+	defer chConn.Stop()
+
+	builder, err := analytics.NewServiceBuilder(log, cfg, webMetrics, pgConn, chConn)
+	if err != nil {
+		log.Fatal(ctx, "can't init services: %s", err)
+	}
+
+	router, err := api.NewRouter(&cfg.HTTP, log)
+	if err != nil {
+		log.Fatal(ctx, "failed while creating router: %s", err)
+	}
+	router.AddHandlers(api.NoPrefix, builder.CardsAPI, builder.DashboardsAPI, builder.ChartsAPI)
+	router.AddMiddlewares(builder.Auth.Middleware, builder.RateLimiter.Middleware, builder.AuditTrail.Middleware)
+
+	server.Run(ctx, log, &cfg.HTTP, router)
+}
