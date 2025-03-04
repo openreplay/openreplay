78ddbb92339a6810eb969faee924d24c8c6d2179
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-03-04 17:03:46 +0100

    fix(chalice): fixed error-exp

--- ee/api/chalicelib/core/errors/__init__.py
+++ ee/api/chalicelib/core/errors/__init__.py
@@ -4,9 +4,10 @@ from decouple import config
 
 logger = logging.getLogger(__name__)
 
+from . import errors as errors_legacy
+
 if config("EXP_ERRORS_SEARCH", cast=bool, default=False):
     logger.info(">>> Using experimental error search")
-    from . import errors as errors_legacy
     from . import errors_ch as errors
     from . import errors_details_exp as errors_details
 else:
