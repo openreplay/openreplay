e174a11466b09885aaea5d7a79c4620d3d4ac6e2
Shekar Siri <sshekarsiri@gmail.com>
2025-03-05 11:22:26 +0100

    fix(ui): update spots list header title for consistency
    Change header title from "Spot List" to "Spots" to improve UI consistency
    and make the heading more concise.
    
    Signed-off-by: Shekar Siri <sshekarsiri@gmail.com>

--- frontend/app/components/Spots/SpotsList/SpotsListHeader.tsx
+++ frontend/app/components/Spots/SpotsList/SpotsListHeader.tsx
@@ -49,7 +49,7 @@ const SpotsListHeader = observer(
     return (
       <div className={'flex items-center justify-between w-full'}>
         <div className="flex gap-1 items-center">
-          <h1 className={'text-2xl capitalize mr-2'}>Spot List</h1>
+          <h1 className={'text-2xl capitalize mr-2'}>Spots</h1>
           <ReloadButton buttonSize={'small'} onClick={onRefresh} iconSize={14} />
         </div>
 
