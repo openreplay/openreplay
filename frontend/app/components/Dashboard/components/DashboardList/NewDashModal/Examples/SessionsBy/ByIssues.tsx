ea36664257b6f1c1a7f297c0e80d7853364505f5
Андрей Бабушкин <andreybabushkin2000@gmail.com>
2025-03-10 23:18:59 +0100

    updated locales

--- frontend/app/components/Dashboard/components/DashboardList/NewDashModal/Examples/SessionsBy/ByIssues.tsx
+++ frontend/app/components/Dashboard/components/DashboardList/NewDashModal/Examples/SessionsBy/ByIssues.tsx
@@ -4,29 +4,31 @@ import { Icon } from 'UI';
 
 import ExCard from '../ExCard';
 import ByComponent from './Component';
+import { useTranslation } from 'react-i18next';
 
 function ByIssues(props: any) {
+  const { t } = useTranslation();
   const rows = [
     {
-      label: 'Dead Click',
+      label: t('Dead Click'),
       progress: 85,
       value: '2.5K',
       icon: <Icon name="color/issues/dead_click" size={26} />,
     },
     {
-      label: 'Click Rage',
+      label: t('Click Rage'),
       progress: 25,
       value: '405',
       icon: <Icon name="color/issues/click_rage" size={26} />,
     },
     {
-      label: 'Bad Request',
+      label: t('Bad Request'),
       progress: 5,
       value: '302',
       icon: <Icon name="color/issues/bad_request" size={26} />,
     },
     {
-      label: 'Mouse Thrashing',
+      label: t('Mouse Thrashing'),
       progress: 3,
       value: '194',
       icon: <Icon name="color/issues/mouse_thrashing" size={26} />,
