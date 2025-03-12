bd09160a4a76c5b8570b0391eb5880dbf10c2d77
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-03-12 13:42:30 +0100

    fix(chalice): fixed search usability-test's sessions

--- ee/api/chalicelib/core/sessions/__init__.py
+++ ee/api/chalicelib/core/sessions/__init__.py
@@ -6,13 +6,12 @@ logger = logging.getLogger(__name__)
 from . import sessions_pg
 from . import sessions_pg as sessions_legacy
 from . import sessions_ch
+from . import sessions_search as sessions_search_legacy
 
 if config("EXP_SESSIONS_SEARCH", cast=bool, default=False):
     logger.info(">>> Using experimental sessions search")
     from . import sessions_ch as sessions
-    from . import sessions_search as sessions_search_legacy
     from . import sessions_search_exp as sessions_search
 else:
     from . import sessions as sessions
     from . import sessions_search as sessions_search
-    from . import sessions_search as sessions_search_legacy
