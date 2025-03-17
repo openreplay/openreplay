b00daab890bd4c241bb49481c2e122639c6b201d
nick-delirium <nikita@openreplay.com>
2025-03-17 14:33:10 +0100

    tracker: keep spaces, remove data from page location msg

--- tracker/tracker/src/main/app/sanitizer.ts
+++ tracker/tracker/src/main/app/sanitizer.ts
@@ -41,7 +41,7 @@ export interface Options {
 export const stringWiper = (input: string) =>
   input
     .trim()
-    .replace(/[^\f\n\r\t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]/g, '*')
+    .replace(/[^\f\n\r\t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff\s]/g, '*')
 
 export default class Sanitizer {
   private readonly obscured: Set<number> = new Set()
