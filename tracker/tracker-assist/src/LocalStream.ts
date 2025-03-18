7365d8639c3195c117999019f02d7212395a51c8
Andrey Babushkin <55714097+reyand43@users.noreply.github.com>
2025-03-18 13:07:09 +0300

    updated widget link (#3158)
    * updated widget link
    
    * fix calls
    
    * updated widget url

--- tracker/tracker-assist/src/LocalStream.ts
+++ tracker/tracker-assist/src/LocalStream.ts
@@ -1,88 +1,86 @@
-declare global {
-   interface HTMLCanvasElement {
-     captureStream(frameRate?: number): MediaStream;
-  }
-}
-
-function dummyTrack(): MediaStreamTrack { 
-  const canvas = document.createElement('canvas')//, { width: 0, height: 0})
-  canvas.setAttribute('data-openreplay-hidden', '1')
-  canvas.width=canvas.height=2 // Doesn't work when 1 (?!)
-  const ctx = canvas.getContext('2d')
-  ctx?.fillRect(0, 0, canvas.width, canvas.height)
-  requestAnimationFrame(function draw(){
-    ctx?.fillRect(0,0, canvas.width, canvas.height)
-    requestAnimationFrame(draw)
-  })
-  // Also works. Probably it should be done once connected.
-  //setTimeout(() => { ctx?.fillRect(0,0, canvas.width, canvas.height) }, 4000)
-  return canvas.captureStream(60).getTracks()[0]
-}
-
-export default function RequestLocalStream(): Promise<LocalStream> {
-  return navigator.mediaDevices.getUserMedia({ audio:true, })
-    .then(aStream => {
-      const aTrack = aStream.getAudioTracks()[0]
-
-      if (!aTrack) { throw new Error('No audio tracks provided') }
-      return new _LocalStream(aTrack)
-    })
+export default function RequestLocalStream(
+  pc: RTCPeerConnection,
+  toggleVideoCb?: () => void
+): Promise<LocalStream> {
+  return navigator.mediaDevices
+    .getUserMedia({ audio: true, video: false })
+    .then((stream) => {
+      const aTrack = stream.getAudioTracks()[0];
+      if (!aTrack) {
+        throw new Error("No audio tracks provided");
+      }
+      stream.getTracks().forEach((track) => {
+        pc.addTrack(track, stream);
+      });
+      return new _LocalStream(stream, pc, toggleVideoCb);
+    });
 }
 
 class _LocalStream {
-  private mediaRequested = false
-  readonly stream: MediaStream
-  private readonly vdTrack: MediaStreamTrack
-  constructor(aTrack: MediaStreamTrack) {
-    this.vdTrack = dummyTrack()
-    this.stream = new MediaStream([ aTrack, this.vdTrack, ])
+  private mediaRequested = false;
+  readonly stream: MediaStream;
+  readonly vTrack: MediaStreamTrack;
+  readonly pc: RTCPeerConnection;
+  readonly toggleVideoCb?: () => void;
+  constructor(stream: MediaStream, pc: RTCPeerConnection, toggleVideoCb?: () => void) {
+    this.stream = stream;
+    this.pc = pc;
+    this.toggleVideoCb = toggleVideoCb;
   }
 
   toggleVideo(): Promise<boolean> {
+    const videoTracks = this.stream.getVideoTracks();
     if (!this.mediaRequested) {
-      return navigator.mediaDevices.getUserMedia({video:true,})
-      .then(vStream => {
-        const vTrack = vStream.getVideoTracks()[0]
-        if (!vTrack) {
-          throw new Error('No video track provided')
-        }
-        this.stream.addTrack(vTrack)
-        this.stream.removeTrack(this.vdTrack)
-        this.mediaRequested = true
-        if (this.onVideoTrackCb) {
-          this.onVideoTrackCb(vTrack)
-        }
-        return true
-      })
-      .catch(e => {
-        // TODO: log
-        console.error(e)
-        return false
-      })
+      return navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((vStream) => {
+          const vTrack = vStream.getVideoTracks()[0];
+          if (!vTrack) {
+            throw new Error("No video track provided");
+          }
+
+          this.pc.addTrack(vTrack, this.stream);
+          this.stream.addTrack(vTrack);
+
+          if (this.toggleVideoCb) {
+            this.toggleVideoCb();
+          }
+
+          this.mediaRequested = true;
+
+          if (this.onVideoTrackCb) {
+            this.onVideoTrackCb(vTrack);
+          }
+          return true;
+        })
+        .catch((e) => {
+          // TODO: log
+          return false;
+        });
+    } else {
+      videoTracks.forEach((track) => {
+        track.enabled = !track.enabled;
+      });
     }
-    let enabled = true
-    this.stream.getVideoTracks().forEach(track => {
-      track.enabled = enabled = enabled && !track.enabled
-    })
-    return Promise.resolve(enabled)
+    return Promise.resolve(videoTracks[0].enabled);
   }
 
   toggleAudio(): boolean {
-    let enabled = true
-    this.stream.getAudioTracks().forEach(track => {
-      track.enabled = enabled = enabled && !track.enabled
-    })
-    return enabled
+    let enabled = true;
+    this.stream.getAudioTracks().forEach((track) => {
+      track.enabled = enabled = enabled && !track.enabled;
+    });
+    return enabled;
   }
 
-  private onVideoTrackCb: ((t: MediaStreamTrack) => void) | null = null
+  private onVideoTrackCb: ((t: MediaStreamTrack) => void) | null = null;
   onVideoTrack(cb: (t: MediaStreamTrack) => void) {
-    this.onVideoTrackCb = cb
+    this.onVideoTrackCb = cb;
   }
 
   stop() {
-    this.stream.getTracks().forEach(t => t.stop())
+    this.stream.getTracks().forEach((t) => t.stop());
   }
 }
 
-export type LocalStream = InstanceType<typeof _LocalStream>
+export type LocalStream = InstanceType<typeof _LocalStream>;
