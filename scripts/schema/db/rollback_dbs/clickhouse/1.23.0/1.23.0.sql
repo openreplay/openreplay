3d448320bf11b93fcf875357ef80af7f3bc5b7b8
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-03-20 16:57:59 +0100

    refactor(DB): changed DB structure for product analytics

--- scripts/schema/db/rollback_dbs/clickhouse/1.23.0/1.23.0.sql
+++ scripts/schema/db/rollback_dbs/clickhouse/1.23.0/1.23.0.sql
@@ -2,3 +2,5 @@ CREATE OR REPLACE FUNCTION openreplay_version AS() -> 'v1.22.0';
 
 
 DROP TABLE IF EXISTS experimental.user_viewed_sessions;
+
+DROP TABLE IF EXISTS product_analytics.event_properties;
