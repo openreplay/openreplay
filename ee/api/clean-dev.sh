b3c69795a08284f6dd044baa2d30103419cd1d76
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-03-21 16:56:44 +0100

    fix(chalice): fixed wrong import

--- ee/api/clean-dev.sh
+++ ee/api/clean-dev.sh
@@ -114,3 +114,4 @@ rm -rf ./chalicelib/core/errors/errors_ch.py
 rm -rf ./chalicelib/core/errors/errors_details.py
 rm -rf ./chalicelib/utils/contextual_validators.py
 rm -rf ./routers/subs/product_analytics.py
+rm -rf ./schemas/product_analytics.py
