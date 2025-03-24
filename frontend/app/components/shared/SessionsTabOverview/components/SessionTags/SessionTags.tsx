eba22e0efa452f64595f4cbba5f45892408add16
nick-delirium <nikita@openreplay.com>
2025-03-24 17:03:02 +0100

    ui: always show sessiontags

--- frontend/app/components/shared/SessionsTabOverview/components/SessionTags/SessionTags.tsx
+++ frontend/app/components/shared/SessionsTabOverview/components/SessionTags/SessionTags.tsx
@@ -20,73 +20,13 @@ const tagIcons = {
 function SessionTags() {
   const { t } = useTranslation();
   const screens = useBreakpoint();
-  const { projectsStore, sessionStore, searchStore } = useStore();
-  const total = sessionStore.total;
+  const { projectsStore, searchStore } = useStore();
   const platform = projectsStore.active?.platform || '';
   const activeTab = searchStore.activeTags;
-  const [isMobile, setIsMobile] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement | null>(null);
 
-  const filteredOptions = issues_types
-    .filter(
-      (tag) =>
-        tag.type !== 'mouse_thrashing' &&
-        (platform === 'web'
-          ? tag.type !== types.TAP_RAGE
-          : tag.type !== types.CLICK_RAGE),
-    )
-    .map((tag) => ({
-      value: tag.type,
-      icon: tagIcons[tag.type],
-      label: t(tag.name),
-    }));
-
-  // Find the currently active option
-  const activeOption =
-    filteredOptions.find((option) => option.value === activeTab[0]) ||
-    filteredOptions[0];
-
-  // Check if on mobile
-  useEffect(() => {
-    const checkIfMobile = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    checkIfMobile();
-    window.addEventListener('resize', checkIfMobile);
-
-    return () => {
-      window.removeEventListener('resize', checkIfMobile);
-    };
-  }, []);
-
-  // Close dropdown when clicking outside
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        dropdownRef.current &&
-        !(dropdownRef.current as HTMLElement).contains(event.target as Node)
-      ) {
-        setIsDropdownOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
-  // Handler for dropdown item selection
-  const handleSelectOption = (value: string) => {
-    searchStore.toggleTag(value as any);
-    setIsDropdownOpen(false);
-  };
-
-  if (total === 0 && (activeTab.length === 0 || activeTab[0] === 'all')) {
-    return null;
-  }
+  React.useEffect(() => {
+    searchStore.toggleTag(types.ALL);
+  }, [projectsStore.activeSiteId])
 
   return (
     <div className="flex items-center">
