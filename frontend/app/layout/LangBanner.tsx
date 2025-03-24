b7a71bb296ddb96e93db0977029b193ddb064e32
nick-delirium <nikita@openreplay.com>
2025-03-24 11:08:35 +0100

    ui: change language selection ui

---
+++ frontend/app/layout/LangBanner.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  Languages, X
+} from 'lucide-react'
+import { Button } from 'antd';
+import { useHistory } from "react-router-dom";
+import { client } from 'App/routes'
+
+function LangBanner({ onClose }: { onClose: () => void }) {
+  const history = useHistory()
+
+  const onClick = () => {
+    history.push(client('account'))
+  }
+  return (
+    <div className={'px-4 py-2 bg-yellow flex items-center w-screen gap-2'}>
+      <div>
+        OpenReplay now supports French, Russian, Chinese, and Spanish 🎉. Update your language in settings.
+      </div>
+      <div className={'ml-auto'} />
+      <Button icon={<Languages size={14} />} size={'small'} onClick={onClick}>
+        Change Language
+      </Button>
+      <Button icon={<X size={16} />} type={'text'} shape={'circle'} onClick={onClose} size={'small'} />
+    </div>
+  )
+}
+
+export default LangBanner
