ec2c42c688ca48bf084a5b0dfacc0f57f9dc018a
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-03-25 13:11:30 +0100

    refactor(DB): changed product analytics DB structure

--- ee/scripts/schema/db/init_dbs/clickhouse/create/init_schema.sql
+++ ee/scripts/schema/db/init_dbs/clickhouse/create/init_schema.sql
@@ -642,6 +642,7 @@ CREATE TABLE IF NOT EXISTS product_analytics.group_properties
 CREATE TABLE IF NOT EXISTS product_analytics.all_events
 (
     project_id          UInt16,
+    auto_captured       BOOL     DEFAULT FALSE,
     event_name          String,
     display_name        String   DEFAULT '',
     description         String   DEFAULT '',
@@ -651,7 +652,7 @@ CREATE TABLE IF NOT EXISTS product_analytics.all_events
     created_at          DateTime64,
     _timestamp          DateTime DEFAULT now()
 ) ENGINE = ReplacingMergeTree(_timestamp)
-      ORDER BY (project_id, event_name);
+      ORDER BY (project_id, auto_captured, event_name);
 
 
 -- The full list of event-properties (used to tell which property belongs to which event)
--- scripts/schema/db/init_dbs/clickhouse/1.23.0/1.23.0.sql
+++ scripts/schema/db/init_dbs/clickhouse/1.23.0/1.23.0.sql
@@ -22,3 +22,19 @@ CREATE TABLE IF NOT EXISTS product_analytics.event_properties
     _timestamp    DateTime DEFAULT now()
 ) ENGINE = ReplacingMergeTree(_timestamp)
       ORDER BY (project_id, event_name, property_name);
+
+DROP TABLE IF EXISTS product_analytics.all_events;
+CREATE TABLE IF NOT EXISTS product_analytics.all_events
+(
+    project_id          UInt16,
+    auto_captured       BOOL     DEFAULT FALSE,
+    event_name          String,
+    display_name        String   DEFAULT '',
+    description         String   DEFAULT '',
+    event_count_l30days UInt32   DEFAULT 0,
+    query_count_l30days UInt32   DEFAULT 0,
+
+    created_at          DateTime64,
+    _timestamp          DateTime DEFAULT now()
+) ENGINE = ReplacingMergeTree(_timestamp)
+      ORDER BY (project_id, auto_captured, event_name);
