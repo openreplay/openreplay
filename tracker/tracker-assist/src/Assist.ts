af7b46516fc5d15e0264211086f1af3d8605a93e
Andrey Babushkin <55714097+reyand43@users.noreply.github.com>
2025-04-03 16:14:46 +0200

    Assist fix canvas stream (#3261)
    * add agent info to assist and tracker
    
    * removed AGENTS_CONNECTED event

--- tracker/tracker-assist/src/Assist.ts
+++ tracker/tracker-assist/src/Assist.ts
@@ -26,6 +26,7 @@ interface AgentInfo {
   name: string;
   peerId: string;
   query: string;
+  socketId?: string;
 }
 
 export interface Options {
@@ -439,11 +440,11 @@ export default class Assist {
       }
     });
 
-    socket.on("AGENTS_CONNECTED", (ids: string[]) => {
+    socket.on("AGENTS_INFO_CONNECTED", (agentsInfo: AgentInfo[]) => {
       this.cleanCanvasConnections();
-      ids.forEach((id) => {
-        const agentInfo = this.agents[id]?.agentInfo;
-        this.agents[id] = {
+      agentsInfo.forEach((agentInfo) => {
+        if (!agentInfo.socketId) return;
+        this.agents[agentInfo.socketId] = {
           agentInfo,
           onDisconnect: this.options.onAgentConnect?.(agentInfo),
         };
@@ -451,6 +452,7 @@ export default class Assist {
       if (this.app.active()) {
         this.assistDemandedRestart = true;
         this.app.stop();
+        this.app.clearBuffers();
         this.app.waitStatus(0).then(() => {
           this.app.allowAppStart();
           setTimeout(() => {
@@ -460,7 +462,7 @@ export default class Assist {
                 this.assistDemandedRestart = false;
               })
               .then(() => {
-                this.remoteControl?.reconnect(ids);
+                this.remoteControl?.reconnect(Object.keys(this.agents));
               })
               .catch((e) => app.debug.error(e));
           }, 100);
@@ -526,6 +528,13 @@ export default class Assist {
           } catch (e) {
             app.debug.error("Error adding ICE candidate", e);
           }
+        } else {
+          this.iceCandidatesBuffer.set(
+            data.id,
+            this.iceCandidatesBuffer
+              .get(data.id)
+              ?.concat([data.candidate]) || [data.candidate]
+          );
         }
       }
     );
@@ -862,6 +871,7 @@ export default class Assist {
             iceServers: this.config,
           });
           this.setupPeerListeners(uniqueId);
+          this.applyBufferedIceCandidates(uniqueId);
 
           stream.getTracks().forEach((track) => {
             this.canvasPeers[uniqueId]?.addTrack(track, stream);
