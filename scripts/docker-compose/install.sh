87176695ec40cdff9f661d30ec7b9c3100a2d1fe
rjshrjndrn <rjshrjndrn@gmail.com>
2025-04-18 17:23:59 +0200

    chore(docker): don't pull parallel
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- scripts/docker-compose/install.sh
+++ scripts/docker-compose/install.sh
@@ -12,42 +12,42 @@ NC='\033[0m' # No Color
 
 # --- Helper functions for logs ---
 info() {
-	echo -e "${GREEN}[INFO] $1 ${NC} 👍"
+    echo -e "${GREEN}[INFO] $1 ${NC} 👍"
 }
 
 warn() {
-	echo -e "${YELLOW}[WARN] $1 ${NC} ⚠️"
+    echo -e "${YELLOW}[WARN] $1 ${NC} ⚠️"
 }
 
 fatal() {
-	echo -e "${RED}[FATAL] $1 ${NC} 🔥"
-	exit 1
+    echo -e "${RED}[FATAL] $1 ${NC} 🔥"
+    exit 1
 }
 
 # Function to check if a command exists
 function exists() {
-	type "$1" &>/dev/null
+    type "$1" &>/dev/null
 }
 
 # Generate a random password using openssl
 randomPass() {
-	exists openssl || {
-		info "Installing openssl... 🔐"
-		sudo apt update &>/dev/null
-		sudo apt install openssl -y &>/dev/null
-	}
-	openssl rand -hex 10
+    exists openssl || {
+        info "Installing openssl... 🔐"
+        sudo apt update &>/dev/null
+        sudo apt install openssl -y &>/dev/null
+    }
+    openssl rand -hex 10
 }
 
 # Create dynamic passwords and update the environment file
 function create_passwords() {
-	info "Creating dynamic passwords..."
-	sed -i "s/change_me_domain/${DOMAIN_NAME}/g" common.env
-	sed -i "s/change_me_jwt/$(randomPass)/g" common.env
-	sed -i "s/change_me_s3_key/$(randomPass)/g" common.env
-	sed -i "s/change_me_s3_secret/$(randomPass)/g" common.env
-	sed -i "s/change_me_pg_password/$(randomPass)/g" common.env
-	info "Passwords created and updated in common.env file."
+    info "Creating dynamic passwords..."
+    sed -i "s/change_me_domain/${DOMAIN_NAME}/g" common.env
+    sed -i "s/change_me_jwt/$(randomPass)/g" common.env
+    sed -i "s/change_me_s3_key/$(randomPass)/g" common.env
+    sed -i "s/change_me_s3_secret/$(randomPass)/g" common.env
+    sed -i "s/change_me_pg_password/$(randomPass)/g" common.env
+    info "Passwords created and updated in common.env file."
 }
 
 # update apt cache
@@ -72,23 +72,27 @@ echo -e "${GREEN}"
 read -rp "Enter DOMAIN_NAME: " DOMAIN_NAME
 echo -e "${NC}"
 if [[ -z $DOMAIN_NAME ]]; then
-	fatal "DOMAIN_NAME variable is empty. Please provide a valid domain name to proceed."
+    fatal "DOMAIN_NAME variable is empty. Please provide a valid domain name to proceed."
 fi
 info "Using domain name: $DOMAIN_NAME 🌐"
-echo "CADDY_DOMAIN=\"$DOMAIN_NAME\"" >> common.env 
+echo "CADDY_DOMAIN=\"$DOMAIN_NAME\"" >>common.env
 
 read -p "Is the domain on a public DNS? (y/n) " yn
-case $yn in 
-	y ) echo "$DOMAIN_NAME is on a public DNS";
-        ;;
-	n ) echo "$DOMAIN_NAME is on a private DNS";
-		#add TLS internal to caddyfile
-		#In local network Caddy can't reach Let's Encrypt servers to get a certificate 
-		mv Caddyfile Caddyfile.public
-		mv Caddyfile.private Caddyfile
-		;;
-	* ) echo invalid response;
-		exit 1;;
+case $yn in
+y)
+    echo "$DOMAIN_NAME is on a public DNS"
+    ;;
+n)
+    echo "$DOMAIN_NAME is on a private DNS"
+    #add TLS internal to caddyfile
+    #In local network Caddy can't reach Let's Encrypt servers to get a certificate
+    mv Caddyfile Caddyfile.public
+    mv Caddyfile.private Caddyfile
+    ;;
+*)
+    echo invalid response
+    exit 1
+    ;;
 esac
 
 # Create passwords if they don't exist
@@ -103,23 +107,27 @@ set +a
 # Use the `envsubst` command to substitute the shell environment variables into reference_var.env and output to a combined .env
 find ./ -type f \( -iname "*.env" -o -iname "docker-compose.yaml" \) ! -name "common.env" -exec /bin/bash -c 'file="{}"; git checkout -- "$file"; cp "$file" "$file.bak"; envsubst < "$file.bak" > "$file"; rm "$file.bak"' \;
 
-case $yn in 
-	y ) echo "$DOMAIN_NAME is on a public DNS";
-		##No changes needed
-        ;;
-	n ) echo "$DOMAIN_NAME is on a private DNS";
-		##Add a variable to chalice.env file
-		echo "SKIP_H_SSL=True" >> chalice.env
-		;;
-	* ) echo invalid response;
-		exit 1;;
+case $yn in
+y)
+    echo "$DOMAIN_NAME is on a public DNS"
+    ##No changes needed
+    ;;
+n)
+    echo "$DOMAIN_NAME is on a private DNS"
+    ##Add a variable to chalice.env file
+    echo "SKIP_H_SSL=True" >>chalice.env
+    ;;
+*)
+    echo invalid response
+    exit 1
+    ;;
 esac
 
 services=$(sudo -E docker-compose config --services)
 for service in $services; do
-  echo "Pulling image for $service..."
-  sudo -E docker-compose pull $service
-  sleep 5
+    echo "Pulling image for $service..."
+    sudo -E docker-compose pull $service --no-parallel
+    sleep 5
 done
 
 sudo -E docker-compose --profile migration up --force-recreate --build -d
