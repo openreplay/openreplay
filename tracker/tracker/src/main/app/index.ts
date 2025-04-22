3fd506a8127cdea6988ef3ac106447bfe8a57162
Andrey Babushkin <55714097+reyand43@users.noreply.github.com>
2025-04-22 17:59:25 +0200

    Css batching (#3326)
    * tracker: initial css inlining functionality
    
    * tracker: add tests, adjust sheet id, stagger rule sending
    
    * ui: rereoute custom html component fragments
    
    * removed sorting
    
    ---------
    
    Co-authored-by: nick-delirium <nikita@openreplay.com>

--- tracker/tracker/src/main/app/index.ts
+++ tracker/tracker/src/main/app/index.ts
@@ -318,13 +318,14 @@ export default class App {
       __save_canvas_locally: false,
       localStorage: null,
       sessionStorage: null,
-      disableStringDict: false,
+      disableStringDict: true,
       forceSingleTab: false,
       assistSocketHost: '',
       fixedCanvasScaling: false,
       disableCanvas: false,
       captureIFrames: true,
       disableSprites: false,
+      inlineRemoteCss: true,
       obscureTextEmails: true,
       obscureTextNumbers: false,
       crossdomain: {
@@ -818,7 +819,7 @@ export default class App {
     this.debug.error('OpenReplay error: ', context, e)
   }
 
-  send(message: Message, urgent = false): void {
+  send = (message: Message, urgent = false): void => {
     if (this.activityState === ActivityState.NotActive) {
       return
     }
