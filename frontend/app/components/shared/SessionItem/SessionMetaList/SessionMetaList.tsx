18f8ee9d15f12d2a6b089706ef1c715d397025ed
nick-delirium <nikita@openreplay.com>
2025-05-06 16:21:02 +0200

    ui: fix max meta length, add horizontal layout for player

--- frontend/app/components/shared/SessionItem/SessionMetaList/SessionMetaList.tsx
+++ frontend/app/components/shared/SessionItem/SessionMetaList/SessionMetaList.tsx
@@ -8,13 +8,14 @@ interface Props {
   metaList: any[];
   maxLength?: number;
   onMetaClick?: (meta: { name: string, value: string }) => void;
+  horizontal?: boolean;
 }
 
 export default function SessionMetaList(props: Props) {
-  const { className = '', metaList, maxLength = 14 } = props;
+  const { className = '', metaList, maxLength = 14, horizontal = false } = props;
 
   return (
-    <div className={cn('flex items-center flex-wrap gap-1', className)}>
+    <div className={cn('flex items-center gap-1', horizontal ? '' : 'flex-wrap', className)}>
       {metaList.slice(0, maxLength).map(({ label, value }, index) => (
           <div key={index} className='cursor-pointer' onClick={() => props.onMetaClick?.({ name: `_${label}`, value })}>
             <MetaItem label={label} value={`${value}`} />
