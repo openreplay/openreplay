ed88d32c32d904b9e11195758a493772e109f22f
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-05-27 14:38:27 +0200

    refactor(DB): support incident events

--- ee/scripts/schema/db/init_dbs/postgresql/init_schema.sql
+++ ee/scripts/schema/db/init_dbs/postgresql/init_schema.sql
@@ -352,7 +352,8 @@ CREATE TYPE issue_type AS ENUM (
     'custom',
     'js_exception',
     'mouse_thrashing',
-    'app_crash'
+    'app_crash',
+    'incident'
     );
 
 CREATE TABLE public.issues
