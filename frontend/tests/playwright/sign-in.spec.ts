373238429e02992c5563367bf00fe05ffb406b00
Andrey Babushkin <55714097+reyand43@users.noreply.github.com>
2025-06-03 14:32:28 +0200

    E2e tests frontend (#3473)
    * add playwright
    
    * add e2e test
    
    * add test
    
    * add e2e test
    
    * add e2e tests
    
    * fixed config

--- frontend/tests/playwright/sign-in.spec.ts
+++ frontend/tests/playwright/sign-in.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('Sign in flow', async ({ page }) => {
+  const LOGIN = process.env.TEST_FOSS_LOGIN || '';
+  const PASSWORD = process.env.TEST_FOSS_PASSWORD || '';
   await page.goto('/');
-  await page.locator('[data-test-id="login"]').fill('andrei@openreplay.com');
-  await page.locator('[data-test-id="password"]').fill('Andrey123!');
+  await page
+        .locator('[data-test-id="login"]')
+        .fill(LOGIN);
+  await page.locator('[data-test-id="password"]').fill(PASSWORD);
   await page.locator('[data-test-id="log-button"]').click();
   await expect(page.getByRole('heading', { name: 'Sessions' })).toBeVisible();
 });
