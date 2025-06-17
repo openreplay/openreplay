edff6ea2b80fd259fa54a22fa1f06583ff60aee5
nick-delirium <nikita@openreplay.com>
2025-06-17 10:54:28 +0200

    ui: fixing widget data

--- frontend/app/components/Dashboard/components/WidgetChart/WidgetChart.tsx
+++ frontend/app/components/Dashboard/components/WidgetChart/WidgetChart.tsx
@@ -2,7 +2,6 @@ import React, { useState, useRef, useEffect } from 'react';
 import LineChart from 'App/components/Charts/LineChart';
 import BarChart from 'App/components/Charts/BarChart';
 import PieChart from 'App/components/Charts/PieChart';
-import ColumnChart from 'App/components/Charts/ColumnChart';
 import SankeyChart from 'Components/Charts/SankeyChart';
 
 import CustomMetricPercentage from 'App/components/Dashboard/Widgets/CustomMetricsWidgets/CustomMetricPercentage';
@@ -56,7 +55,7 @@ function WidgetChart(props: Props) {
   const { isSaved = false, metric, isTemplate, height } = props;
   const { dashboardStore, metricStore } = useStore();
   const _metric: any = props.metric;
-  const data = _metric.data;
+  const [data, setData] = useState(_metric.data ?? { chart: [] });
   const { period } = dashboardStore;
   const { drillDownPeriod } = dashboardStore;
   const { drillDownFilter } = dashboardStore;
@@ -160,7 +159,11 @@ function WidgetChart(props: Props) {
     dashboardStore
       .fetchMetricChartData(metric, payload, isSaved, period, isComparison)
       .then((res) => {
-        if (isComparison) setCompData(res);
+        if (isComparison) {
+          setCompData(res);
+        } else {
+          setData(res);
+        }
         clearTimeout(tm);
         setStale(false);
       })
