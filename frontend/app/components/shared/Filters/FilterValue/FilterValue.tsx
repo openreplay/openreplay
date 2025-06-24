a1132b3b8695e7b85a9e3ba7330e65f34c4889d9
Shekar Siri <sshekarsiri@gmail.com>
2025-06-24 14:45:29 +0200

    Comment out unused filter type cases Remove unused filter type cases and components

--- frontend/app/components/shared/Filters/FilterValue/FilterValue.tsx
+++ frontend/app/components/shared/Filters/FilterValue/FilterValue.tsx
@@ -1,12 +1,8 @@
 import React, { useState, useCallback, useMemo, useEffect } from 'react';
 import { FilterKey, FilterCategory, FilterType } from 'Types/filter/filterType';
-import { debounce } from 'App/utils';
 import { assist as assistRoute, isRoute } from 'App/routes';
 import { observer } from 'mobx-react-lite';
 import FilterDuration from '../FilterDuration';
-import FilterValueDropdown from '../FilterValueDropdown';
-import FilterAutoCompleteLocal from '../FilterAutoCompleteLocal';
-import FilterAutoComplete from '../FilterAutoComplete';
 import ValueAutoComplete from 'Shared/Filters/FilterValue/ValueAutoComplete';
 import { Input, Select } from 'antd';
 import { useStore } from '@/mstore';
@@ -20,46 +16,9 @@ interface Props {
   eventName?: string;
 }
 
-function BaseFilterLocalAutoComplete(props: any) {
-  return (
-    <FilterAutoCompleteLocal
-      value={props.value}
-      showCloseButton={props.showCloseButton}
-      onApplyValues={props.onApplyValues}
-      onRemoveValue={props.onRemoveValue}
-      onSelect={props.onSelect}
-      icon={props.icon}
-      placeholder={props.placeholder}
-      isAutoOpen={props.isAutoOpen}
-      modalProps={props.modalProps}
-      type={props.name}
-      allowDecimals={props.allowDecimals}
-      isMultiple={props.isMultiple}
-    />
-  );
-}
-
-function BaseDropDown(props: any) {
-  return (
-    <FilterValueDropdown
-      value={props.value}
-      isAutoOpen={props.isAutoOpen}
-      placeholder={props.placeholder}
-      options={props.options}
-      onApplyValues={props.onApplyValues}
-      search={props.search}
-      onAddValue={props.onAddValue}
-      onRemoveValue={props.onRemoveValue}
-      showCloseButton={props.showCloseButton}
-      showOrButton={props.showOrButton}
-      isMultiple={props.isMultiple}
-    />
-  );
-}
-
 function FilterValue(props: Props) {
-  const { filter, onUpdate, isConditional, eventName } = props; // Destructure props early
-  const isAutoOpen = filter.autoOpen; // Assume parent now controls this correctly
+  const { filter, onUpdate, isConditional, eventName = '' } = props;
+  const isAutoOpen = filter.autoOpen;
   const { searchStore } = useStore();
 
   const [durationValues, setDurationValues] = useState(() => ({
@@ -67,11 +26,13 @@ function FilterValue(props: Props) {
     maxDuration: filter.value?.length > 1 ? filter.value[1] : filter.value?.[0],
   }));
 
+  // Update duration values when filter changes
   useEffect(() => {
     if (filter.name === FilterType.DURATION) {
       const incomingMin = filter.value?.[0];
       const incomingMax =
         filter.value?.length > 1 ? filter.value[1] : filter.value?.[0];
+
       if (
         durationValues.minDuration !== incomingMin ||
         durationValues.maxDuration !== incomingMax
@@ -82,54 +43,22 @@ function FilterValue(props: Props) {
         });
       }
     }
-  }, [filter.value, filter.name]);
-
-  const showCloseButton = filter.value.length > 1;
-  const showOrButton = filter.value.length > 1;
-
-  const onAddValue = useCallback(() => {
-    const newValue = filter.value.concat('');
-    onUpdate({ ...filter, value: newValue });
-  }, [filter, onUpdate]);
+  }, [
+    filter.value,
+    filter.name,
+    durationValues.minDuration,
+    durationValues.maxDuration,
+  ]);
 
   const onApplyValues = useCallback(
     (values: string[]) => {
       onUpdate({ ...filter, value: values });
-
       void searchStore.fetchSessions();
     },
-    [filter, onUpdate],
+    [filter, onUpdate, searchStore],
   );
-
-  const onRemoveValue = useCallback(
-    (valueIndex: any) => {
-      const newValue = filter.value.filter(
-        (_: any, index: any) => index !== valueIndex,
-      );
-      onUpdate({ ...filter, value: newValue });
-    },
-    [filter, onUpdate],
-  );
-
-  const stableOnChange = useCallback(
-    (e: any, item: any, valueIndex: any) => {
-      const newValues = filter.value.map((val: any, _index: any) => {
-        if (_index === valueIndex) {
-          return item;
-        }
-        return val;
-      });
-      onUpdate({ ...filter, value: newValues });
-    },
-    [filter, onUpdate],
-  );
-
-  const debounceOnSelect = useCallback(debounce(stableOnChange, 500), [
-    stableOnChange,
-  ]);
 
   const onDurationChange = useCallback((newValues: any) => {
-    console.log('newValues', newValues);
     setDurationValues((current) => ({ ...current, ...newValues }));
   }, []);
 
@@ -148,19 +77,13 @@ function FilterValue(props: Props) {
           value: [durationValues.minDuration, durationValues.maxDuration],
         });
       }
-    } else {
-      onUpdate({
-        ...filter,
-        value: filter.value[0],
-      });
     }
   }, [
     filter,
     onUpdate,
-    filter.value,
     durationValues.minDuration,
     durationValues.maxDuration,
-  ]); // Add durationValues dependency
+  ]);
 
   const params = useMemo(() => {
     let baseParams: any = {
@@ -168,15 +91,14 @@ function FilterValue(props: Props) {
       name: filter.name,
       isEvent: filter.isEvent,
       id: filter.id,
-      // propertyName: filter.propertyName,
-      // eventName: filter.name,
     };
-    if (filter.isEvent || eventName != '') {
+
+    if (filter.isEvent || eventName) {
       baseParams.eventName = eventName;
     }
+
     if (!filter.isEvent) {
       baseParams.propertyName = filter.name;
-
       if (filter.eventName) {
         baseParams.eventName = filter.eventName;
       }
@@ -185,25 +107,50 @@ function FilterValue(props: Props) {
     if (filter.category === FilterCategory.METADATA) {
       baseParams = { type: FilterKey.METADATA, key: filter.key };
     }
+
     if (isRoute(ASSIST_ROUTE, window.location.pathname)) {
       baseParams = { ...baseParams, live: true };
     }
+
     return baseParams;
-  }, [filter.key, filter.name, filter.isEvent, filter.id, filter.category]);
+  }, [
+    filter.key,
+    filter.name,
+    filter.isEvent,
+    filter.id,
+    filter.category,
+    filter.eventName,
+    eventName,
+  ]);
 
-  const value = filter.value;
+  const handleNumberInputBlur = useCallback(
+    (e: React.FocusEvent<HTMLInputElement>) => {
+      const newValue = e.target.value;
+      onUpdate({ ...filter, value: [newValue] });
+    },
+    [filter, onUpdate],
+  );
 
+  const handleBooleanChange = useCallback(
+    (value: boolean) => {
+      onUpdate({ ...filter, value });
+    },
+    [filter, onUpdate],
+  );
+
+  // Render different input types based on filter data type
   switch (filter.dataType) {
     case FilterType.STRING:
       return (
         <ValueAutoComplete
-          initialValues={value}
+          initialValues={filter.value}
           isAutoOpen={isAutoOpen}
           onApplyValues={onApplyValues}
           params={params}
           commaQuery={true}
         />
       );
+
     case FilterType.DURATION:
       return (
         <FilterDuration
@@ -214,73 +161,29 @@ function FilterValue(props: Props) {
           isConditional={isConditional}
         />
       );
+
     case FilterType.NUMBER:
     case FilterType.INTEGER:
-      return (
-        <Input
-          type="number"
-          defaultValue={value}
-          size="small"
-          className="rounded-lg"
-          style={{ width: '80px' }}
-          // onChange={(e) => {
-          //   const newValue = e.target.value;
-          //   onUpdate({ ...filter, value: [newValue] });
-          // }}
-          placeholder={filter.placeholder}
-          onBlur={(e) => {
-            const newValue = e.target.value;
-            onUpdate({ ...filter, value: [newValue] });
-          }}
-        />
-        // <BaseFilterLocalAutoComplete
-        //   value={value}
-        //   showCloseButton={showCloseButton}
-        //   onApplyValues={onApplyValues}
-        //   onRemoveValue={onRemoveValue}
-        //   onSelect={debounceOnSelect}
-        //   icon={filter.icon}
-        //   placeholder={filter.placeholder}
-        //   isAutoOpen={isAutoOpen}
-        //   modalProps={{ placeholder: '' }}
-        //   type="number"
-        //   allowDecimals={false}
-        //   isMultiple={false}
-        // />
-      );
-    case FilterType.DROPDOWN:
-      return (
-        <BaseDropDown
-          value={value}
-          isAutoOpen={isAutoOpen}
-          placeholder={filter.placeholder}
-          options={filter.options}
-          onApplyValues={onApplyValues}
-        />
-      );
     case FilterType.DOUBLE:
       return (
         <Input
           type="number"
-          value={value}
+          defaultValue={filter.value}
           size="small"
           className="rounded-lg"
           style={{ width: '80px' }}
-          onChange={(e) => {
-            const newValue = e.target.value;
-            onUpdate({ ...filter, value: newValue });
-          }}
           placeholder={filter.placeholder}
-          onBlur={handleBlur}
+          onBlur={handleNumberInputBlur}
         />
       );
+
     case FilterType.BOOLEAN:
       return (
         <Select
-          value={value}
+          value={filter.value}
           size="small"
           style={{ width: '80px' }}
-          onChange={(value: any) => onUpdate({ ...filter, value })}
+          onChange={handleBooleanChange}
           placeholder={filter.placeholder}
           options={[
             { label: 'True', value: true },
@@ -288,59 +191,9 @@ function FilterValue(props: Props) {
           ]}
         />
       );
-    case FilterType.NUMBER_MULTIPLE:
-      return (
-        <BaseFilterLocalAutoComplete
-          value={value}
-          showCloseButton={showCloseButton}
-          onApplyValues={onApplyValues}
-          onRemoveValue={onRemoveValue}
-          onSelect={debounceOnSelect}
-          icon={filter.icon}
-          placeholder={filter.placeholder}
-          isAutoOpen={isAutoOpen}
-          modalProps={{ placeholder: '' }}
-          type="number"
-          isMultiple={true}
-        />
-      );
-    case FilterType.ISSUE:
-    case FilterType.MULTIPLE_DROPDOWN:
-      return (
-        <BaseDropDown
-          value={value}
-          isAutoOpen={isAutoOpen}
-          placeholder={filter.placeholder}
-          options={filter.options}
-          onApplyValues={onApplyValues}
-          search
-          onAddValue={onAddValue}
-          onRemoveValue={onRemoveValue}
-          showCloseButton={showCloseButton}
-          showOrButton={showOrButton}
-        />
-      );
-    case FilterType.MULTIPLE:
-      return (
-        <FilterAutoComplete
-          value={value}
-          isAutoOpen={isAutoOpen}
-          showCloseButton={showCloseButton}
-          showOrButton={showOrButton}
-          onApplyValues={onApplyValues}
-          onRemoveValue={onRemoveValue}
-          method="GET"
-          endpoint="/PROJECT_ID/events/search" // TODO: Replace PROJECT_ID dynamically if needed
-          params={params}
-          headerText=""
-          placeholder={filter.placeholder}
-          onSelect={stableOnChange}
-          icon={filter.icon}
-          modalProps={{ placeholder: 'Search' }}
-        />
-      );
+
     default:
-      console.warn('Unsupported filter type in FilterValue:', filter.name);
+      console.warn('Unsupported filter type in FilterValue:', filter.dataType);
       return null;
   }
 }
