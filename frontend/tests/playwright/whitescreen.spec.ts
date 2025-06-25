86bf3e4f31ab02dacdf7786fc914274a0590825e
nick-delirium <nikita@openreplay.com>
2025-06-25 17:31:42 +0200

    ui: change session pick method

--- frontend/tests/playwright/whitescreen.spec.ts
+++ frontend/tests/playwright/whitescreen.spec.ts
@@ -10,15 +10,7 @@ test('The freshest session from openreplay website doesnt have white screen', as
   await page.locator('[data-test-id="password"]').fill(PASSWORD);
   await page.locator('[data-test-id="log-button"]').click();
   await page.waitForTimeout(1000);
-  await page
-    .locator('[data-test-id="session-list-header"]')
-    .locator('[data-test-id="widget-select-date-range"]')
-    .click();
-  await page.getByText('Past 30 Days').click();
-  await page.locator('[data-test-id="project-dropdown"]').click();
-  await page.getByRole('button', { name: 'Android caret-down' }).click();
-  await page.getByText('OpenReplay Documentation Site').click();
-
+  await page.goto('http://localhost:3333/65/sessions?f[0].k=location&f[0].op=isAny&f[0].v[0]=&rv=LAST_30_DAYS&s=startTs&o=desc&eo=then')
   await page.waitForTimeout(1000);
   const borderBlocks = await page.locator('.border-b').elementHandles();
   if (borderBlocks.length >= 2) {
