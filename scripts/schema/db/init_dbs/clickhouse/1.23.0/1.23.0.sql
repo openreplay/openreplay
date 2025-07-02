d9d3cd685382b751f4b5a05109db0ce74c13ce96
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-07-02 15:12:47 +0200

    refactor(DB): changed DB structure

--- scripts/schema/db/init_dbs/clickhouse/1.23.0/1.23.0.sql
+++ scripts/schema/db/init_dbs/clickhouse/1.23.0/1.23.0.sql
@@ -13,6 +13,16 @@ ALTER TABLE experimental.sessions
 
 CREATE OR REPLACE FUNCTION openreplay_version AS() -> 'v1.23.0';
 
+CREATE TABLE IF NOT EXISTS experimental.user_favorite_sessions
+(
+    project_id UInt16,
+    user_id    UInt32,
+    session_id UInt64,
+    _timestamp DateTime DEFAULT now(),
+    sign       Int8
+) ENGINE = CollapsingMergeTree(sign)
+      PARTITION BY toYYYYMM(_timestamp)
+      ORDER BY (project_id, user_id, session_id);
 
 CREATE TABLE IF NOT EXISTS experimental.user_viewed_sessions
 (
