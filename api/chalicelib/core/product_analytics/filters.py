020c2cffea54ec1620c4ba18e1355caf98205a50
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-07-07 18:22:29 +0200

    refactor(chalice): unified response for filters

--- api/chalicelib/core/product_analytics/filters.py
+++ api/chalicelib/core/product_analytics/filters.py
@@ -10,117 +10,104 @@ def get_sessions_filters(project_id: int):
                 "id": "sf_1",
                 "name": schemas.FilterType.REFERRER,
                 "displayName": "Referrer",
-                "possibleTypes": [
-                    "String"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_2",
                 "name": schemas.FilterType.DURATION,
                 "displayName": "Duration",
-                "possibleTypes": [
-                    "int"
-                ],
+                "possibleTypes": ["int"],
+                "dataType": "int",
                 "autoCaptured": True
             },
             {
                 "id": "sf_3",
                 "name": schemas.FilterType.UTM_SOURCE,
                 "displayName": "UTM Source",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_4",
                 "name": schemas.FilterType.UTM_MEDIUM,
                 "displayName": "UTM Medium",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_5",
                 "name": schemas.FilterType.UTM_CAMPAIGN,
                 "displayName": "UTM Campaign",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_6",
                 "name": schemas.FilterType.USER_COUNTRY,
                 "displayName": "Country",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_7",
                 "name": schemas.FilterType.USER_CITY,
                 "displayName": "City",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_8",
                 "name": schemas.FilterType.USER_STATE,
                 "displayName": "State / Province",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_9",
                 "name": schemas.FilterType.USER_OS,
                 "displayName": "OS",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_10",
                 "name": schemas.FilterType.USER_BROWSER,
                 "displayName": "Browser",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_11",
                 "name": schemas.FilterType.USER_DEVICE,
                 "displayName": "Device",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_12",
                 "name": schemas.FilterType.PLATFORM,
                 "displayName": "Platform",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             },
             {
                 "id": "sf_13",
                 "name": schemas.FilterType.REV_ID,
                 "displayName": "Version ID",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": True
             }
         ]
@@ -136,18 +123,16 @@ def get_users_filters(project_id: int):
                 "id": "uf_1",
                 "name": schemas.FilterType.USER_ID,
                 "displayName": "User ID",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": False
             },
             {
                 "id": "uf_2",
                 "name": schemas.FilterType.USER_ANONYMOUS_ID,
                 "displayName": "User Anonymous ID",
-                "possibleTypes": [
-                    "string"
-                ],
+                "possibleTypes": ["string"],
+                "dataType": "string",
                 "autoCaptured": False
             }
         ]}
