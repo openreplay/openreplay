c49c931e3c9806a87c3d7136b713cbd62dfada61
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-07-16 12:45:19 +0200

    refactor(gapi): check CH connexion on startup

--- backend/pkg/db/clickhouse/connection.go
+++ backend/pkg/db/clickhouse/connection.go
@@ -1,9 +1,10 @@
 package clickhouse
 
 import (
+	"context"
+	"fmt"
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
-
 	"openreplay/backend/internal/config/common"
 )
 
@@ -25,5 +26,16 @@ func NewConnection(cfg common.Clickhouse) (driver.Conn, error) {
 			Method: clickhouse.CompressionLZ4,
 		}
 	}
-	return clickhouse.Open(opts)
+	var conn driver.Conn
+	var err error
+	if conn, err = clickhouse.Open(opts); err != nil {
+		return nil, err
+	}
+	if err := conn.Ping(context.Background()); err != nil {
+		if exception, ok := err.(*clickhouse.Exception); ok {
+			fmt.Printf("Exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+		}
+		return nil, err
+	}
+	return conn, err
 }
