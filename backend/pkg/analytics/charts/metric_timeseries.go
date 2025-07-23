e4c335c51634949d93fd8c516eb89f80698054cc
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-07-23 18:11:26 +0200

    refactor(gapi): refactored timeseries query

--- backend/pkg/analytics/charts/metric_timeseries.go
+++ backend/pkg/analytics/charts/metric_timeseries.go
@@ -3,45 +3,74 @@ package charts
 import (
 	"context"
 	"fmt"
-	"log"
-	"sort"
-	"strings"
-
+	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"log"
 	"openreplay/backend/pkg/analytics/model"
+	"reflect"
+	//"sort"
+	//"database/sql"
+	"strings"
 )
 
-type TimeSeriesQueryBuilder struct{}
+func isSlice(v interface{}) bool {
+	return reflect.TypeOf(v).Kind() == reflect.Slice
+}
+func convertParams(params map[string]any) []interface{} {
+	chParams := make([]interface{}, 0, len(params))
+	for k, v := range params {
+		//if isSlice(v) {
+		//	stringSlice := v.([]string)
+		//	if len(stringSlice) == 0 {
+		//		v = 0
+		//		continue
+		//	}
+		//	v = "['" + strings.Join(stringSlice, "', '") + "']"
+		//} else {
+		//	v = fmt.Sprintf("%v", v) // Convert non-slice values to string
+		//}
+		//chParams = append(chParams, clickhouse.Named(k, v.(string)))
+		chParams = append(chParams, clickhouse.Named(k, v))
+	}
+	return chParams
+
+}
+
+type TimeSeriesQueryBuilder struct {
+	conn *clickhouse.Conn
+}
 
 func (t *TimeSeriesQueryBuilder) Execute(p *Payload, conn driver.Conn) (interface{}, error) {
 	data := make(map[uint64]map[string]uint64)
 	for _, series := range p.Series {
-		query, err := t.buildQuery(p, series)
+		query, params, err := t.buildQuery(p, series)
 		if err != nil {
 			log.Printf("buildQuery %s: %v", series.Name, err)
 			return nil, fmt.Errorf("series %s: %v", series.Name, err)
 		}
 
 		var pts []DataPoint
-		if err = conn.Select(context.Background(), &pts, query); err != nil {
+		//if err = conn.Select(context.Background(), &pts, query, convertParams(params)...); err != nil {
+		if err = conn.Select(context.Background(), &pts, query, convertParams(params)...); err != nil {
 			log.Panicf("exec %s: %v", series.Name, err)
 			return nil, err
 		}
 
-		filled := FillMissingDataPoints(p.StartTimestamp, p.EndTimestamp, p.Density, DataPoint{}, pts, 1000)
-		for _, dp := range filled {
+		//filled := FillMissingDataPoints(p.StartTimestamp, p.EndTimestamp, p.Density, DataPoint{}, pts, 1000)
+		for _, dp := range pts {
 			if data[dp.Timestamp] == nil {
 				data[dp.Timestamp] = map[string]uint64{}
 			}
 			data[dp.Timestamp][series.Name] = dp.Count
 		}
 	}
+	log.Printf("---- %+v", data)
 
 	var timestamps []uint64
 	for ts := range data {
 		timestamps = append(timestamps, ts)
 	}
-	sort.Slice(timestamps, func(i, j int) bool { return timestamps[i] < timestamps[j] })
+	//sort.Slice(timestamps, func(i, j int) bool { return timestamps[i] < timestamps[j] })
 
 	var result []map[string]interface{}
 	for _, ts := range timestamps {
@@ -54,33 +83,50 @@ func (t *TimeSeriesQueryBuilder) Execute(p *Payload, conn driver.Conn) (interfac
 	return result, nil
 }
 
-func (t *TimeSeriesQueryBuilder) buildQuery(p *Payload, s model.Series) (string, error) {
+func (t *TimeSeriesQueryBuilder) buildQuery(p *Payload, s model.Series) (string, map[string]any, error) {
 	switch p.MetricOf {
 	case "sessionCount":
-		return t.buildTimeSeriesQuery(p, s, "sessionCount", "session_id"), nil
+		return t.buildTimeSeriesQuery(p, s, "sessionCount", "session_id")
 	case "userCount":
-		return t.buildTimeSeriesQuery(p, s, "userCount", "user_id"), nil
+		return t.buildTimeSeriesQuery(p, s, "userCount", "user_id")
 	default:
-		return "", fmt.Errorf("unsupported metric %q", p.MetricOf)
+		return "", nil, fmt.Errorf("unsupported metric %q", p.MetricOf)
 	}
 }
 
-func (t *TimeSeriesQueryBuilder) buildTimeSeriesQuery(p *Payload, s model.Series, metric, idField string) string {
+func (t *TimeSeriesQueryBuilder) buildTimeSeriesQuery(p *Payload, s model.Series, metric, idField string) (string, map[string]any, error) {
 	sub := t.buildSubQuery(p, s, metric)
 	step := getStepSize(p.StartTimestamp, p.EndTimestamp, p.Density, 1)
-
-	query := fmt.Sprintf(
-		`SELECT gs.generate_series AS timestamp, COALESCE(COUNT(DISTINCT ps.%s),0) AS count
-				FROM generate_series(%d,%d,%d) AS gs
+	//query := fmt.Sprintf(
+	//	`SELECT gs.generate_series AS timestamp, COALESCE(COUNT(DISTINCT ps.%s),0) AS count
+	//			FROM generate_series(%d,%d,%d) AS gs
+	//				LEFT JOIN (%s) AS ps ON TRUE
+	//			WHERE ps.datetime >= toDateTime(timestamp/1000)
+	//				AND ps.datetime < toDateTime((timestamp+%d)/1000)
+	//			GROUP BY timestamp ORDER BY timestamp;`,
+	//	idField, p.StartTimestamp, p.EndTimestamp, step, sub, step,
+	//)
+	//query := fmt.Sprintf(`SELECT gs.generate_series AS timestamp, COALESCE(COUNT(DISTINCT ps.{idField:Identifier}),0) AS count
+	//			FROM generate_series({startTimestamp:int},{endTimestamp:int},{step:int}) AS gs
+	//					LEFT JOIN (%s) AS ps ON TRUE
+	//			WHERE ps.datetime >= toDateTime(timestamp/1000)
+	//				AND ps.datetime < toDateTime((timestamp+{step:int})/1000)
+	//			GROUP BY timestamp ORDER BY timestamp;`, sub)
+	query := fmt.Sprintf(`SELECT gs.generate_series AS timestamp, COALESCE(COUNT(DISTINCT ps.%s),0) AS count
+				FROM generate_series(@startTimestamp,@endTimestamp,@step) AS gs
 						LEFT JOIN (%s) AS ps ON TRUE
-				WHERE ps.datetime >= toDateTime(timestamp/1000) 
-					AND ps.datetime < toDateTime((timestamp+%d)/1000)
-				GROUP BY timestamp ORDER BY timestamp;`,
-		idField, p.StartTimestamp, p.EndTimestamp, step, sub, step,
-	)
+				WHERE ps.datetime >= toDateTime(timestamp/1000)
+					AND ps.datetime < toDateTime((timestamp+@step)/1000)
+				GROUP BY timestamp ORDER BY timestamp;`, idField, sub)
+	params := map[string]any{
+		"startTimestamp": p.StartTimestamp,
+		"endTimestamp":   p.EndTimestamp,
+		"step":           step,
+		"project_id":     p.ProjectId,
+	}
 
-	logQuery(fmt.Sprintf("TimeSeriesQueryBuilder.buildQuery: %s", query))
-	return query
+	//logQuery(fmt.Sprintf("TimeSeriesQueryBuilder.buildQuery: %s", query))
+	return query, params, nil
 }
 
 func (t *TimeSeriesQueryBuilder) buildSubQuery(p *Payload, s model.Series, metric string) string {
@@ -101,19 +147,19 @@ func (t *TimeSeriesQueryBuilder) buildSubQuery(p *Payload, s model.Series, metri
 	if len(otherConds) > 0 {
 		whereParts = append(whereParts, strings.Join(otherConds, " AND "))
 	}
-	whereClause := strings.Join(whereParts, " AND ")
 
 	// build subquery with GROUP BY always
 	var sb strings.Builder
 	sb.WriteString(fmt.Sprintf(
-		`SELECT main.session_id, 
+		`SELECT main.session_id,
 					   MIN(main.created_at) AS first_event_ts, 
 					   MAX(main.created_at) AS last_event_ts
 				FROM product_analytics.events AS main
-				WHERE %s`,
-		whereClause,
+				WHERE %s
+				GROUP BY main.session_id`,
+		strings.Join(whereParts, " AND "),
 	))
-	sb.WriteString("GROUP BY main.session_id\n")
+	//sb.WriteString("GROUP BY main.session_id\n")
 
 	// append HAVING based on EventsOrder
 	switch eo {
@@ -154,8 +200,13 @@ func (t *TimeSeriesQueryBuilder) buildSubQuery(p *Payload, s model.Series, metri
 	}[metric] + ", s.datetime AS datetime"
 
 	return fmt.Sprintf(
-		"SELECT %s FROM (\n%s\n) AS evt "+
-			"INNER JOIN experimental.sessions AS s ON s.session_id = evt.session_id",
+		`SELECT %s 
+				FROM (%s) AS evt 
+				INNER JOIN (SELECT session_id, datetime 
+						 FROM experimental.sessions
+						 WHERE project_id=@project_id
+							AND datetime >= toDateTime(@startTimestamp/1000)
+							AND datetime <= toDateTime(@endTimestamp/1000)) AS s ON s.session_id = evt.session_id`,
 		proj, sub,
 	)
 }
