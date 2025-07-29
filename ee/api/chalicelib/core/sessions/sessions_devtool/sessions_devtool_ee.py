a3f21ae1592328c43f71f68f0691133bdad6f03f
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-07-29 17:09:21 +0200

    fix(chalice): fixed service account permission check for devtools
    (cherry picked from commit 351e12b8f61d815fb1f00b0de870dac5333c70ce)

--- ee/api/chalicelib/core/sessions/sessions_devtool/sessions_devtool_ee.py
+++ ee/api/chalicelib/core/sessions/sessions_devtool/sessions_devtool_ee.py
@@ -5,9 +5,11 @@ from chalicelib.core import permissions
 from .sessions_devtool import get_urls as _get_urls
 
 SCOPES = SecurityScopes([schemas.Permissions.DEV_TOOLS])
+SERVICE_SCOPES = SecurityScopes([schemas.ServicePermissions.DEV_TOOLS])
 
 
 def get_urls(session_id, project_id, context: schemas.CurrentContext, check_existence: bool = True):
-    if not permissions.check(security_scopes=SCOPES, context=context):
+    if not permissions.check(security_scopes=SCOPES, context=context) \
+            and not permissions.check(security_scopes=SERVICE_SCOPES, context=context):
         return []
     return _get_urls(session_id=session_id, project_id=project_id, context=context, check_existence=check_existence)
