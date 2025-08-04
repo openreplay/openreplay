3fddf69977a1dbc08a4631176da6b6fa78a2ac0b
Shekar Siri <sshekarsiri@gmail.com>
2025-08-04 14:23:35 +0200

    change(api): search code cleanup and improvements

--- backend/pkg/analytics/search/search.go
+++ backend/pkg/analytics/search/search.go
@@ -56,22 +56,57 @@ var sortOptions = map[string]string{
 	"eventsCount": "s.events_count",
 }
 
+const (
+	sessionsQuery = `
+SELECT
+	toString(s.session_id)                    AS session_id,
+	s.project_id,
+	toUInt64(toUnixTimestamp(s.datetime)*1000) AS start_ts,
+	s.duration,
+	s.platform,
+	s.timezone,
+	s.user_id,
+	s.user_uuid,
+	s.user_anonymous_id,
+	s.user_browser,
+	s.user_city,
+	s.user_country,
+	s.user_device,
+	s.user_device_type,
+	s.user_os,
+	s.user_state,
+	s.events_count,
+	toUInt8(viewed_sessions.session_id>0)      AS viewed,
+	count() OVER()                            AS total
+FROM experimental.sessions AS s
+	%s
+	%s
+WHERE %s
+ORDER BY %s %s
+LIMIT ? OFFSET ?
+`
+	viewedSessionsJoinTemplate = `ANY LEFT JOIN (
+	SELECT DISTINCT session_id
+	FROM experimental.user_viewed_sessions
+	WHERE user_id    = %d
+	  AND project_id = %d
+	  AND _timestamp >= toDateTime(%d)
+) AS viewed_sessions USING (session_id)`
+)
+
 func (s *searchImpl) GetAll(projectId int, userId uint64, req *model.SessionsSearchRequest) (*model.GetSessionsResponse, error) {
 	offset := (req.Page - 1) * req.Limit
-	conds := []string{}
 	eventsWhere, filtersWhere, sessionsWhere := buildWhere(req)
-	eventsInnerJoin := ""
+	sessionsWhere = append([]string{fmt.Sprintf("s.project_id = %d", projectId),
+		fmt.Sprintf("s.datetime BETWEEN toDateTime(%d) AND toDateTime(%d)", req.StartDate/1000, req.EndDate/1000),
+	}, sessionsWhere...)
 
+	var eventsInnerJoin string
+	conds := make([]string, 0, len(req.Filters)+2)
 	if req.EventsOrder == "then" && len(eventsWhere) == 1 {
 		conds = append(conds, eventsWhere[0])
 	}
-
-	if len(eventsWhere) == 0 && len(filtersWhere) == 0 {
-		sessionsWhere = append([]string{
-			fmt.Sprintf("s.project_id = %d", projectId),
-			fmt.Sprintf("s.datetime BETWEEN toDateTime(%d) AND toDateTime(%d)", req.StartDate/1000, req.EndDate/1000),
-		}, sessionsWhere...)
-	} else {
+	if len(eventsWhere) > 0 || len(filtersWhere) > 0 {
 		conds = append([]string{
 			fmt.Sprintf("e.project_id = %d", projectId),
 			fmt.Sprintf("e.created_at BETWEEN toDateTime(%d) AND toDateTime(%d)", req.StartDate/1000, req.EndDate/1000),
@@ -80,15 +115,16 @@ func (s *searchImpl) GetAll(projectId int, userId uint64, req *model.SessionsSea
 
 		joinClause := buildJoinClause(req.EventsOrder, eventsWhere)
 		eventsInnerJoin = fmt.Sprintf(`ANY INNER JOIN (
-		    SELECT DISTINCT session_id
-		    FROM product_analytics.events AS e
-		    %s
-		    %s
-		) AS fs USING (session_id)`, joinClause, "PREWHERE "+strings.Join(conds, " AND "))
+		SELECT DISTINCT session_id
+		FROM product_analytics.events AS e
+		%s
+		PREWHERE %s
+	) AS fs USING (session_id)`,
+			joinClause, strings.Join(conds, " AND "))
 	}
 
-	sortField, ok := sortOptions[req.Sort]
-	if !ok {
+	sortField := sortOptions[req.Sort]
+	if sortField == "" {
 		sortField = sortOptions["datetime"]
 	}
 	sortOrder := "DESC"
@@ -96,67 +132,29 @@ func (s *searchImpl) GetAll(projectId int, userId uint64, req *model.SessionsSea
 		sortOrder = "ASC"
 	}
 
-	viewedSessionsJoin := fmt.Sprintf(`ANY LEFT JOIN (
-    SELECT DISTINCT session_id
-    FROM experimental.user_viewed_sessions
-    WHERE user_id    = %d
-      AND project_id = %d
-      AND _timestamp >= toDateTime(%d)
-) AS viewed_sessions USING (session_id)`, userId, projectId, req.StartDate/1000)
+	viewedJoin := fmt.Sprintf(viewedSessionsJoinTemplate, userId, projectId, req.StartDate/1000)
 
-	dataQ := fmt.Sprintf(`
-SELECT
-    toString(s.session_id)                    AS session_id,
-    s.project_id,
-    toUInt64(toUnixTimestamp(s.datetime)*1000) AS start_ts,
-    s.duration,
-    s.platform,
-    s.timezone,
-    s.user_id,
-    s.user_uuid,
-    s.user_anonymous_id,
-    s.user_browser,
-    s.user_city,
-    s.user_country,
-    s.user_device,
-    s.user_device_type,
-    s.user_os,
-    s.user_state,
-    s.events_count,
-    toUInt8(viewed_sessions.session_id > 0)  AS viewed,
-    count() OVER ()                          AS total
-FROM experimental.sessions AS s
-	%s
-	%s
-WHERE
-    %s
-ORDER BY %s %s
-LIMIT %d OFFSET %d
-`,
+	query := fmt.Sprintf(sessionsQuery,
 		eventsInnerJoin,
-		viewedSessionsJoin,
+		viewedJoin,
 		strings.Join(sessionsWhere, " AND "),
-		sortField, sortOrder,
-		req.Limit,
-		offset,
+		sortField,
+		sortOrder,
 	)
 
-	log.Printf("Sessions Search Query: %s\n", dataQ)
+	log.Printf("Sessions Search Query: %s", query)
 
-	rows, err := s.chConn.Query(context.Background(), dataQ)
+	rows, err := s.chConn.Query(context.Background(), query, req.Limit, offset)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	resp := &model.GetSessionsResponse{
-		Sessions: make([]model.Session, 0, req.Limit),
-	}
-	first := true
+	resp := &model.GetSessionsResponse{Sessions: make([]model.Session, 0, req.Limit)}
+	var total uint64
 	for rows.Next() {
 		var sess model.Session
-		var total uint64
-		if err := rows.Scan(
+		err = rows.Scan(
 			&sess.SessionId,
 			&sess.ProjectId,
 			&sess.StartTs,
@@ -176,12 +174,12 @@ LIMIT %d OFFSET %d
 			&sess.EventsCount,
 			&sess.Viewed,
 			&total,
-		); err != nil {
+		)
+		if err != nil {
 			return nil, err
 		}
-		if first {
+		if resp.Total == 0 {
 			resp.Total = total
-			first = false
 		}
 		resp.Sessions = append(resp.Sessions, sess)
 	}
