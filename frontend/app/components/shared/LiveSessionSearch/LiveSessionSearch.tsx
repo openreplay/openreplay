2c41547de09d8404b18ddf6fc84f5fff1d9ce503
nick-delirium <nikita@openreplay.com>
2025-08-18 16:07:24 +0200

    ui: support new filters in assist search

--- frontend/app/components/shared/LiveSessionSearch/LiveSessionSearch.tsx
+++ frontend/app/components/shared/LiveSessionSearch/LiveSessionSearch.tsx
@@ -1,18 +1,48 @@
 import React, { useEffect } from 'react';
-import { FilterList } from 'Shared/Filters/FilterList';
+import UnifiedFilterList from 'Shared/Filters/FilterList/UnifiedFilterList';
+import FilterListHeader from 'Shared/Filters/FilterList/FilterListHeader';
+import FilterSelection from 'Shared/Filters/FilterSelection';
+import { Button, Card } from 'antd';
+import { Plus } from 'lucide-react';
 import { observer } from 'mobx-react-lite';
 import { useStore } from 'App/mstore';
 
+const liveFilterKeys = [
+  'metadata',
+  'userid',
+  'usercountry',
+  'usercity',
+  'userstate',
+  'useranonymousid',
+  'userbrowser',
+  'useros',
+  'userdevice',
+  'platform',
+  'utm_medium',
+  'utm_source',
+  'utm_campaign',
+];
+
 function LiveSessionSearch() {
-  const { projectsStore, searchStoreLive } = useStore();
+  const { projectsStore, searchStoreLive, filterStore } = useStore();
   const saveRequestPayloads = projectsStore.active?.saveRequestPayloads;
   const appliedFilter = searchStoreLive.instance;
+  const allFilterOptions = filterStore.getCurrentProjectFilters();
+  const propertyOptions = allFilterOptions
+    .filter((i) => !i.isEvent)
+    .filter(
+      (i) =>
+        liveFilterKeys.includes(i.name.toLowerCase()) ||
+        i.name.startsWith('metadata'),
+    );
+
+  const activeFilters = appliedFilter.filters.map((f) => f.name);
 
   useEffect(() => {
     if (projectsStore.activeSiteId) {
       void searchStoreLive.fetchSessions(true);
     }
-  }, [projectsStore.activeSiteId])
+  }, [projectsStore.activeSiteId]);
 
   const onAddFilter = (filter: any) => {
     filter.autoOpen = true;
@@ -32,23 +62,34 @@ function LiveSessionSearch() {
       filters: newFilters,
     });
   };
-
-  const onChangeEventsOrder = (e: any, { name, value }: any) => {
-    searchStoreLive.edit({
-      eventsOrder: value,
-    });
-  };
-
   return (
-    <FilterList
-      filter={appliedFilter}
-      onAddFilter={onAddFilter}
-      onUpdateFilter={onUpdateFilter}
-      onRemoveFilter={onRemoveFilter}
-      onChangeEventsOrder={onChangeEventsOrder}
-      saveRequestPayloads={saveRequestPayloads}
-      isLive
-    />
+    <Card className="rounded-lg" classNames={{ body: '!p-4' }}>
+      <FilterListHeader
+        title="Filters"
+        filterSelection={
+          <FilterSelection
+            filters={propertyOptions}
+            activeFilters={activeFilters}
+            onFilterClick={onAddFilter}
+          >
+            <Button type="default" size="small">
+              <div className="flex items-center gap-1">
+                <Plus size={16} strokeWidth={1} />
+                <span>Add</span>
+              </div>
+            </Button>
+          </FilterSelection>
+        }
+      />
+      <UnifiedFilterList
+        title="Filters"
+        handleAdd={onAddFilter}
+        handleUpdate={onUpdateFilter}
+        handleRemove={onRemoveFilter}
+        saveRequestPayloads={saveRequestPayloads}
+        filters={appliedFilter.filters}
+      />
+    </Card>
   );
 }
 
