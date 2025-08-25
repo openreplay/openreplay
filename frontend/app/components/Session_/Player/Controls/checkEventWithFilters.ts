0f1218f9a94571eccc88bd93c9d88aa2c9aa25f3
nick-delirium <nikita@openreplay.com>
2025-08-25 17:24:27 +0200

    ui: fix basic event highlight after search

--- frontend/app/components/Session_/Player/Controls/checkEventWithFilters.ts
+++ frontend/app/components/Session_/Player/Controls/checkEventWithFilters.ts
@@ -3,16 +3,54 @@ import FilterItem from '@/mstore/types/filterItem';
 export const checkEventWithFilters = (event: Event, filters: FilterItem[]) => {
   let result = false;
   filters.forEach((filter) => {
-    if (filter.key.toUpperCase() === event.type.toUpperCase()) {
+    if (filter.name.toUpperCase() === event.type.toUpperCase()) {
       if (filter.operator) {
         const operator = operators[filter.operator];
         if (operator) {
-            result = !!operator(event.label, filter.value);
+          let usedValue = filter.value;
+          if (filter.name === 'CLICK') {
+            usedValue = handleClick(filter);
+            return !!operator(event.label, usedValue);
+          }
+          if (filter.name === 'LOCATION') {
+            usedValue = handleLocation(filter);
+            const pathname = event.url ? new URL(event.url).pathname : '';
+            return !!operator(pathname, usedValue);
+          }
+          if (filter.name === 'TAG_TRIGGER') {
+            usedValue = handleTag(filter);
+            return !!operator(event.name, usedValue);
+          }
+          if (filter.name === 'INPUT') {
+            usedValue = handleInput(filter);
+            return !!operator(event.label, usedValue);
+          }
+          result = !!operator(event.label, usedValue);
         }
       }
     }
   });
-  return result
+  return result;
+};
+
+const handleClick = (filter: FilterItem) => {
+  const value = filter.filters?.find((f) => f.name === 'label')?.value ?? [];
+  return value;
+};
+
+const handleLocation = (filter: FilterItem) => {
+  const value = filter.filters?.find((f) => f.name === 'url_path')?.value ?? [];
+  return value;
+};
+
+const handleTag = (filter: FilterItem) => {
+  const value = filter.filters?.find((f) => f.name === 'tag_id')?.value ?? [];
+  return value;
+};
+
+const handleInput = (filter: FilterItem) => {
+  const value = filter.filters?.find((f) => f.name === 'label')?.value ?? [];
+  return value;
 };
 
 const operators = {
@@ -36,7 +74,8 @@ const operators = {
   notOn: (val: string, target: string[]) =>
     !target.some((t) => val.includes(t)),
   onAny: () => true,
-  selectorIs: (val: string, target: string[]) => target.some((t) => val.includes(t)),
+  selectorIs: (val: string, target: string[]) =>
+    target.some((t) => val.includes(t)),
   selectorIsAny: () => true,
   selectorIsNot: (val: string, target: string[]) =>
     !target.some((t) => val.includes(t)),
