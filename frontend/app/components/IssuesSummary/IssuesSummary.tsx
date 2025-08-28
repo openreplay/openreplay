09954cd7ccc15828df715b6ff7a54d1037d094a1
nick-delirium <nikita@openreplay.com>
2025-08-28 15:53:26 +0200

    ui: adopt new params for granular issue search

--- frontend/app/components/IssuesSummary/IssuesSummary.tsx
+++ frontend/app/components/IssuesSummary/IssuesSummary.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Card, Tag, Select } from 'antd';
+import { ChevronDown, ChevronRight } from 'lucide-react';
 import { useModal } from '../Modal';
 import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
-import { client } from 'App/mstore';
 import { useRouteMatch } from 'react-router-dom';
-import Session from '@/types/session';
-import SessionItem from '../shared/SessionItem';
-
-interface Data {
-  issueName: string;
-  count: number;
-  sessions: any[];
-  labels: string[];
-}
+import { Data } from './types'
+import { getIssues, getTagLabels } from './api'
+import IssueSessions from './IssueSessionsModal'
 
 function getTagColor(label: string): string {
   const safeLabel = label.toLowerCase();
@@ -24,38 +18,17 @@ function getTagColor(label: string): string {
   return 'blue';
 }
 
-async function getTagLabels(projectId: string) {
-  const response = await client.get(`/kai/${projectId}/smart_labels`);
-  const json = await response.json();
-  const options = (json.data ?? []).map((label: string) => ({
-    label,
-    value: label,
-  }));
-  return options;
-}
-
-async function getIssues(
-  projectId: string,
-  usedLabels?: string[],
-): Promise<Data[]> {
-  const options = {
-    issues_limit: 20,
-    sessions_limit: 20,
-    labels: usedLabels,
-  };
-  const edp = `/kai/${projectId}/smart_alerts`;
-  const response = await client.post(edp, options);
-  const json = await response.json();
-  return json.data ?? [];
-}
-
 function IssuesSummary() {
+  const [showOther, setShowOther] = React.useState(false);
   const [usedLabels, setUsedLabels] = React.useState<string[]>([]);
   const match = useRouteMatch<{ siteId: string }>();
   const projectId = match.params.siteId;
   const { showModal } = useModal();
   const { t } = useTranslation();
-  const { data = [], isPending } = useQuery<Data[]>({
+  const { data = { critical: [], other: [] }, isPending } = useQuery<{
+    critical: Data[];
+    other: Data[];
+  }>({
     queryKey: ['issuesSummary', projectId, usedLabels],
     queryFn: () => getIssues(projectId, usedLabels),
   });
@@ -68,18 +41,19 @@ function IssuesSummary() {
 
   const onIssueClick = async (issue: {
     issueName: string;
-    sessions: any[];
+    labels: { name: string }[];
   }) => {
-    const sessions: { session: Session; issue: string }[] = issue.sessions.map(
-      (session: any) => ({
-        session: new Session(session),
-        issue: session.issueDescription,
-      }),
+    showModal(
+      <IssueSessions
+        issueName={issue.issueName}
+        labels={issue.labels.map(l => l.name)}
+        projectId={projectId}
+      />,
+      {
+        right: true,
+        width: 720,
+      },
     );
-    showModal(<IssueSessions sessions={sessions} />, {
-      right: true,
-      width: 720,
-    });
   };
 
   const handleChange = (value: string[]) => {
@@ -102,43 +76,66 @@ function IssuesSummary() {
       }
     >
       <div className="flex flex-col gap-2">
-        {data.map((issue, index) => (
-          <div
-            className="w-full gap-4 flex items-center p-2 hover:bg-active-blue rounded-lg cursor-pointer"
+        <div className="flex items-center gap-4 px-2">
+          <Tag>Impact</Tag>
+          <div>Issues</div>
+        </div>
+        {data.critical.map((issue, index) => (
+          <Issue
             key={index}
-            onClick={() => onIssueClick(issue)}
-          >
-            <Tag>{issue.count}</Tag>
-            <div className="font-semibold">{issue.issueName}</div>
-            <div className="flex items-center flex-wrap">
-              {issue.labels.map((label, idx) => (
-                <Tag key={idx} color={getTagColor(label)}>
-                  {label}
-                </Tag>
-              ))}
-            </div>
-          </div>
+            issue={issue}
+            index={index}
+            onIssueClick={onIssueClick}
+          />
         ))}
+        <div
+          className="cursor-pointer p-2 rounded-lg font-semibold flex items-center gap-2 hover:bg-active-blue"
+          onClick={() => setShowOther(!showOther)}
+        >
+          <div>
+            {t('Other issues')} ({data.other.length})
+          </div>
+          {showOther ? <ChevronDown size={16} /> : <ChevronRight size={16} />}
+        </div>
+        {showOther
+          ? data.other.map((issue, index) => (
+              <Issue
+                key={index}
+                issue={issue}
+                index={index}
+                onIssueClick={onIssueClick}
+              />
+            ))
+          : null}
       </div>
     </Card>
   );
 }
 
-function IssueSessions({
-  sessions,
+function Issue({
+  issue,
+  index,
+  onIssueClick,
 }: {
-  sessions: { session: Session; issue: string }[];
+  issue: Data;
+  index: number;
+  onIssueClick: (issue: Data) => void;
 }) {
   return (
-    <div className="flex flex-col gap-2 h-screen overflow-y-auto bg-white">
-      {sessions.map((session, index) => (
-        <div>
-          <SessionItem key={index} session={session.session} />
-          <div className="text-gray-500 text-sm px-4">
-            {session.issue || 'No issue description available'}
-          </div>
-        </div>
-      ))}
+    <div
+      className="w-full gap-4 flex items-center p-2 hover:bg-active-blue rounded-lg cursor-pointer"
+      key={index}
+      onClick={() => onIssueClick(issue)}
+    >
+      <Tag>{issue.impact}%</Tag>
+      <div className="font-semibold">{issue.issueName}</div>
+      <div className="flex items-center flex-wrap">
+        {issue.labels.map((label, idx) => (
+          <Tag key={idx} color={getTagColor(label.name)}>
+            {label.name}
+          </Tag>
+        ))}
+      </div>
     </div>
   );
 }
