22ef81c960866fdda18129c7d269a029c0bb806c
Shekar Siri <sshekarsiri@gmail.com>
2025-09-12 17:33:44 +0200

    feat(api): session videos - revert the util wrapper

--- backend/pkg/analytics/utils/response_utils.go
+++ backend/pkg/analytics/utils/response_utils.go
@@ -26,6 +26,14 @@ func InitRequestContext(r *http.Request) *RequestContext {
 	}
 }
 
+func HandleError(log logger.Logger, responser *api.Responser, ctx *RequestContext, w http.ResponseWriter, r *http.Request, statusCode int, err error) {
+	responser.ResponseWithError(log, r.Context(), w, statusCode, err, ctx.StartTime, ctx.Path, ctx.BodySize)
+}
+
+func HandleSuccess(log logger.Logger, responser *api.Responser, ctx *RequestContext, w http.ResponseWriter, r *http.Request, data interface{}) {
+	responser.ResponseWithJSON(log, r.Context(), w, map[string]interface{}{"data": data}, ctx.StartTime, ctx.Path, ctx.BodySize)
+}
+
 func GetCurrentUser(r *http.Request) *user.User {
 	return r.Context().Value("userData").(*user.User)
 }
@@ -33,18 +41,18 @@ func GetCurrentUser(r *http.Request) *user.User {
 func ParseJSONBody(log logger.Logger, responser *api.Responser, validator *validator.Validate, ctx *RequestContext, w http.ResponseWriter, r *http.Request, jsonSizeLimit int64, dest interface{}) error {
 	bodyBytes, err := api.ReadBody(log, w, r, jsonSizeLimit)
 	if err != nil {
-		responser.ResponseWithError(log, r.Context(), w, http.StatusRequestEntityTooLarge, err, ctx.StartTime, ctx.Path, ctx.BodySize)
+		HandleError(log, responser, ctx, w, r, http.StatusRequestEntityTooLarge, err)
 		return err
 	}
 	ctx.BodySize = len(bodyBytes)
 
 	if err := json.Unmarshal(bodyBytes, dest); err != nil {
-		responser.ResponseWithError(log, r.Context(), w, http.StatusBadRequest, err, ctx.StartTime, ctx.Path, ctx.BodySize)
+		HandleError(log, responser, ctx, w, r, http.StatusBadRequest, err)
 		return err
 	}
 
 	if err := validator.Struct(dest); err != nil {
-		responser.ResponseWithError(log, r.Context(), w, http.StatusBadRequest, err, ctx.StartTime, ctx.Path, ctx.BodySize)
+		HandleError(log, responser, ctx, w, r, http.StatusBadRequest, err)
 		return err
 	}
 
