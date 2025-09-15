05550f4c8d3bed91d55fc63b52e010b99fb45aeb
nick-delirium <nikita@openreplay.com>
2025-09-15 14:22:52 +0200

    ui: fix table chart modal prefetch

--- frontend/app/components/Dashboard/Widgets/CardSessionsByList.tsx
+++ frontend/app/components/Dashboard/Widgets/CardSessionsByList.tsx
@@ -50,6 +50,10 @@ function CardSessionsByList({
     setLoading(false);
   };
 
+  React.useEffect(() => {
+    if (!data.length) loadData(1);
+  }, [])
+
   return (
     <div className="flex flex-col h-full">
       <div className="flex-1 overflow-y-auto">
