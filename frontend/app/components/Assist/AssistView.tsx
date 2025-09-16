01d556a15a82b76e1677b2b64d0428b879fb54c9
Vincent Chen <vincent.chen@aven.com>
2025-09-16 10:31:46 -0700

    fix metadata querys

--- frontend/app/components/Assist/AssistView.tsx
+++ frontend/app/components/Assist/AssistView.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import LiveSessionList from 'Shared/LiveSessionList';
 import LiveSessionSearch from 'Shared/LiveSessionSearch';
 import AssistSearchField from './AssistSearchField';
+import { connect } from 'react-redux';
+import { fetchListActive as fetchMetadata } from 'Duck/customField';
 
-function AssistView() {
+interface Props {
+  fetchMetadata: () => void;
+  metaLoading: boolean;
+}
+
+function AssistView(props: Props) {
+  React.useEffect(() => {
+    // Ensure custom fields are loaded for metadata filters
+    props.fetchMetadata();
+  }, []);
+
   return (
     <div className="w-full mx-auto" style={{ maxWidth: '1300px'}}>
       <AssistSearchField />
@@ -14,4 +26,6 @@ function AssistView() {
   )
 }
 
-export default AssistView;
+export default connect((state: any) => ({
+  metaLoading: state.getIn(['customFields', 'fetchRequestActive', 'loading']),
+}), { fetchMetadata })(AssistView);
