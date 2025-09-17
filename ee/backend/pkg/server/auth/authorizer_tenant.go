fe0caf01686527156efb19c077e6605ad2a39350
Shekar Siri <sshekarsiri@gmail.com>
2025-09-17 17:39:27 +0200

    feat(api): api key auth - ee changes

---
+++ ee/backend/pkg/server/auth/authorizer_tenant.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"fmt"
+	"openreplay/backend/pkg/server/tenant"
+)
+
+func (a *authImpl) isAuthorizedApiKey(authHeader string, projectKey string) (*tenant.Tenant, error) {
+	if a.tenants == nil {
+		return nil, fmt.Errorf("tenants service is not configured")
+	}
+	if a.projects == nil {
+		return nil, fmt.Errorf("projects service is not configured")
+	}
+
+	apiKey, err := getTokenString(authHeader)
+	if err != nil {
+		return nil, err
+	}
+
+	dbTenant, err := (*a.tenants).GetTenantByApiKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = (*a.projects).GetProjectByKey(projectKey, dbTenant.TenantID)
+	if err != nil {
+		a.log.Warn(nil, "Unauthorized request, wrong api key: %s", a)
+		return nil, err
+	}
+
+	return dbTenant, nil
+}
