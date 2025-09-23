50963dd1b219880a01cc67ccaf6ccbc81878c8e3
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-09-23 15:25:35 +0100

    fix(chalice): changed EE env vars

--- ee/api/entrypoint.sh
+++ ee/api/entrypoint.sh
@@ -5,7 +5,5 @@ source /tmp/.env.override
 
 #uvicorn app:app --host 0.0.0.0 --port $LISTEN_PORT --proxy-headers
 NB_WORKERS="${NB_WORKERS:=4}"
-# TODO: remove this before release
-ENABLE_SSO='false'
 gunicorn app:app --workers $NB_WORKERS --worker-class uvicorn.workers.UvicornWorker \
                  --bind 0.0.0.0:$LISTEN_PORT  --log-level ${S_LOGLEVEL:-warning}
