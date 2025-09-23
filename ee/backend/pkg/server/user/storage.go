8d3c55f15a74ddde89f141d3a10aa9cb1419a8ae
Shekar Siri <sshekarsiri@gmail.com>
2025-09-23 14:06:50 +0200

    feat(api): service_account check for auth and fix for permission check

--- ee/backend/pkg/server/user/storage.go
+++ ee/backend/pkg/server/user/storage.go
@@ -8,12 +8,12 @@ import (
 
 func getUserFromDB(conn pool.Pool, userID, tenantID int, tokenType TokenType) (*User, error) {
 	sql := fmt.Sprintf(`
-		SELECT user_id, users.tenant_id, users.name, email, EXTRACT(epoch FROM %s)::BIGINT AS jwt_iat, roles.permissions, users.service_account
+		SELECT user_id, users.tenant_id, users.name, email, EXTRACT(epoch FROM %s)::BIGINT AS jwt_iat, roles.permissions,
+		       (users.service_account = true AND NOT EXISTS (SELECT 1 FROM public.basic_authentication WHERE user_id = users.user_id)) AS service_account
 	   	FROM public.users as users
 	   	JOIN tenants on users.tenant_id = tenants.tenant_id
 		JOIN roles on users.role_id = roles.role_id
-	   	WHERE users.user_id = $1 AND users.tenant_id = $2 AND users.deleted_at IS NULL
-		AND (users.service_account = true AND NOT EXISTS (SELECT 1 FROM public.basic_authentication WHERE user_id = users.user_id));`, tokenType)
+	   	WHERE users.user_id = $1 AND users.tenant_id = $2 AND users.deleted_at IS NULL`, tokenType)
 	user := &User{AuthMethod: "jwt"}
 	var permissions []string
 	if err := conn.QueryRow(sql, userID, tenantID).
