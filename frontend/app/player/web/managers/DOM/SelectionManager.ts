3515f53bb74779b6ebe9f36d150b9e192958354d
nick-delirium <nikita@openreplay.com>
2025-10-27 10:43:19 +0100

    ui: fix selection style, add padding and rounding

--- frontend/app/player/web/managers/DOM/SelectionManager.ts
+++ frontend/app/player/web/managers/DOM/SelectionManager.ts
@@ -59,35 +59,55 @@ export default class SelectionManager extends ListWalker<SelectionChange> {
 
       for (const el of this.highlightRange) el.remove();
       this.highlightRange = [];
+      const takenRanges: { start: number; end: number }[] = [];
       const rects = range.getClientRects();
       const rectLength = rects.length;
       for (let i = 0; i < rectLength; i += 1) {
         const r = rects[i];
-        const hl = document.createElement('div');
-        let top, left, width, height;
-        top = `${r.top + window.scrollY - 3}px`;
-        left = `${r.left + window.scrollX}px`;
-        width = `${r.width}px`;
-        height = `${r.height + 6}px`;
+        const insideRange = takenRanges.some((range) => {
+          return range.start <= r.left && range.end >= r.left + r.width;
+        });
+        if (!insideRange) {
+          takenRanges.push({
+            start: r.left,
+            end: r.left + r.width,
+          });
+          const hl = document.createElement('div');
 
+          const left = `${r.left + window.scrollX}px`;
+          const width = `${r.width}px`;
+          const top = `${r.top + window.scrollY - 3}px`;
+          const height = `${r.height + 6}px`;
 
-        Object.assign(hl.style, {
-          position: 'absolute',
-          top,
-          left,
-          width,
-          height,
-          background: 'rgba(173, 216, 230, 0.45)',
-          borderRadius: '6px',
-          pointerEvents: 'none',
-          zIndex: '100',
-          borderTopLeftRadius: i === 0 ? '6px' : '0px',
-          borderBottomLeftRadius: i === 0 ? '6px' : '0px',
-          borderTopRightRadius: i === rectLength - 1 ? '6px' : '0px',
-          borderBottomRightRadius: i === rectLength - 1 ? '6px' : '0px',
-        } as CSSStyleDeclaration);
-        this.highlightRange.push(hl);
+          Object.assign(hl.style, {
+            position: 'absolute',
+            top,
+            left,
+            width,
+            height,
+            background: 'rgba(173, 216, 230, 0.45)',
+            borderRadius: '6px',
+            pointerEvents: 'none',
+            zIndex: '100',
+          } as CSSStyleDeclaration);
+          this.highlightRange.push(hl);
+        }
       }
+      const hlRangeLength = this.highlightRange.length;
+      this.highlightRange.forEach((el: HTMLDivElement, i) => {
+        const isStart = i === 0;
+        const isEnd = i === hlRangeLength - 1;
+        const elLeft = parseFloat(el.style.left);
+        const elWidth = parseFloat(el.style.width);
+        Object.assign(el.style, {
+          borderTopLeftRadius: isStart ? '6px' : '0px',
+          borderBottomLeftRadius: isStart ? '6px' : '0px',
+          borderTopRightRadius: isEnd ? '6px' : '0px',
+          borderBottomRightRadius: isEnd ? '6px' : '0px',
+          left: isStart ? `${elLeft - 6}px` : el.style.left,
+          width: isStart || isEnd ? `${elWidth + 6}px` : el.style.width,
+        });
+      });
       this.screen.createSelection(this.highlightRange);
 
       this.selected = [
