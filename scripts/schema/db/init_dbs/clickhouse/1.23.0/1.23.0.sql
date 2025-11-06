d01ea4ca4a048924022dc0530a0b7cfe38937a01
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-11-06 14:45:30 +0100

    refactor(DB): support incident issues

--- scripts/schema/db/init_dbs/clickhouse/1.23.0/1.23.0.sql
+++ scripts/schema/db/init_dbs/clickhouse/1.23.0/1.23.0.sql
@@ -455,3 +455,12 @@ CREATE TABLE IF NOT EXISTS experimental.parsed_errors
 
 ALTER TABLE experimental.sessions
     DROP COLUMN IF EXISTS issue_score;
+
+ALTER TABLE experimental.events
+    MODIFY COLUMN issue_type Nullable(Enum8('click_rage'=1,'dead_click'=2,'excessive_scrolling'=3,'bad_request'=4,'missing_resource'=5,'memory'=6,'cpu'=7,'slow_resource'=8,'slow_page_load'=9,'crash'=10,'ml_cpu'=11,'ml_memory'=12,'ml_dead_click'=13,'ml_click_rage'=14,'ml_mouse_thrashing'=15,'ml_excessive_scrolling'=16,'ml_slow_resources'=17,'custom'=18,'js_exception'=19,'mouse_thrashing'=20,'app_crash'=21,'incident'=22));
+
+ALTER TABLE experimental.issues
+    MODIFY COLUMN type Enum8('click_rage'=1,'dead_click'=2,'excessive_scrolling'=3,'bad_request'=4,'missing_resource'=5,'memory'=6,'cpu'=7,'slow_resource'=8,'slow_page_load'=9,'crash'=10,'ml_cpu'=11,'ml_memory'=12,'ml_dead_click'=13,'ml_click_rage'=14,'ml_mouse_thrashing'=15,'ml_excessive_scrolling'=16,'ml_slow_resources'=17,'custom'=18,'js_exception'=19,'mouse_thrashing'=20,'app_crash'=21,'incident'=22);
+
+ALTER TABLE product_analytics.events
+    MODIFY COLUMN issue_type Enum8(''=0,'click_rage'=1,'dead_click'=2,'excessive_scrolling'=3,'bad_request'=4,'missing_resource'=5,'memory'=6,'cpu'=7,'slow_resource'=8,'slow_page_load'=9,'crash'=10,'ml_cpu'=11,'ml_memory'=12,'ml_dead_click'=13,'ml_click_rage'=14,'ml_mouse_thrashing'=15,'ml_excessive_scrolling'=16,'ml_slow_resources'=17,'custom'=18,'js_exception'=19,'mouse_thrashing'=20,'app_crash'=21,'incident'=22) DEFAULT '';
