7e2f693657c54d761638ca4525e04d3552e540cc
nick-delirium <nikita@openreplay.com>
2025-11-14 18:51:25 +0100

    tracker: typo

--- tracker/tracker-assist/src/Assist.ts
+++ tracker/tracker-assist/src/Assist.ts
@@ -100,6 +100,7 @@ export default class Assist {
   constructor(
     private readonly app: App,
     options?: Partial<Options>,
+    private readonly noSecureMode: boolean = false,
   ) {
     // @ts-ignore
     window.__OR_ASSIST_VERSION = this.version;
