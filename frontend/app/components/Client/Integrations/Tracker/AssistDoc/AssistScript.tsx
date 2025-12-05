3c8bad09f85b1bfad434fcae64484b375c6447bf
nick-delirium <nikita@openreplay.com>
2025-12-05 16:23:23 +0100

    ui: fix assist script mapping

--- frontend/app/components/Client/Integrations/Tracker/AssistDoc/AssistScript.tsx
+++ frontend/app/components/Client/Integrations/Tracker/AssistDoc/AssistScript.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { CodeBlock } from 'UI';
-import ENV from '../../../../../../ENV';;
+import ENV from '../../../../../../env';
 
 function AssistScript(props) {
   const { t } = useTranslation();
