5041df607375c53b52bbd6bc5c16cab982c76999
Taha Yassine Kraiem <tahayk2@gmail.com>
2025-12-09 17:48:53 +0100

    refactor(DB): DB changes

--- scripts/schema/db/init_dbs/clickhouse/1.24.0/1.24.0.sql
+++ scripts/schema/db/init_dbs/clickhouse/1.24.0/1.24.0.sql
@@ -376,3 +376,31 @@ DROP TABLE IF EXISTS product_analytics.property_dvalues_sampler_mv;
 DROP TABLE IF EXISTS product_analytics.property_values_sampler_mv;
 DROP TABLE IF EXISTS product_analytics.event_dproperties_extractor_mv;
 DROP TABLE IF EXISTS product_analytics.autocomplete_event_properties_grouped_mv;
+
+DROP TABLE IF EXISTS product_analytics.event_dproperties_extractor_mv SYNC;
+CREATE MATERIALIZED VIEW IF NOT EXISTS product_analytics.event_dproperties_extractor_mv
+    TO product_analytics.event_properties AS
+SELECT project_id,
+       `$event_name`    AS event_name,
+       a.1              AS property_name,
+       a.2              AS value_type,
+       `$auto_captured` AS auto_captured_event,
+       TRUE             AS auto_captured_property,
+       created_at
+FROM product_analytics.events
+         ARRAY JOIN JSONAllPathsWithTypes(`$properties`) AS a
+GROUP BY ALL;
+
+DROP TABLE IF EXISTS product_analytics.event_properties_extractor_mv SYNC;
+CREATE MATERIALIZED VIEW IF NOT EXISTS product_analytics.event_properties_extractor_mv
+    TO product_analytics.event_properties AS
+SELECT project_id,
+       `$event_name`    AS event_name,
+       a.1              AS property_name,
+       a.2              AS value_type,
+       `$auto_captured` AS auto_captured_event,
+       FALSE            AS auto_captured_property,
+       created_at
+FROM product_analytics.events
+         ARRAY JOIN JSONAllPathsWithTypes(`properties`) AS a
+GROUP BY ALL;
