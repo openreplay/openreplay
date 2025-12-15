53acfda987bdb9c3c82f52de298c3abd67b5aea7
nick-delirium <nikita@openreplay.com>
2025-12-15 17:38:39 +0100

    ui: remove v2 for github/jira issue req

--- frontend/app/api_client.ts
+++ frontend/app/api_client.ts
@@ -50,6 +50,7 @@ const except = [
   'integrations/slack/notify',
   'integrations/msteams/notify',
   '/events/search',
+  'assign/projects',
 ];
 const useNewApi = localStorage.getItem('__old_api') !== 'true';
 
