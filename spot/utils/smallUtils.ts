92a9bf454098d9a4facf468022c65683ea33fdd0
nick-delirium <nikita@openreplay.com>
2026-01-19 16:19:34 +0100

    fix url wrapper for spot

--- spot/utils/smallUtils.ts
+++ spot/utils/smallUtils.ts
@@ -1,12 +1,12 @@
 export function safeApiUrl(url: string) {
   let str = url;
-  if (str.endsWith("/")) {
-    str = str.slice(0, -1);
-  }
   const urlObj = new URL(str);
   if (urlObj.hostname === "app.openreplay.com") {
     urlObj.hostname = "api.openreplay.com";
     str = urlObj.toString();
+  }
+  if (str.endsWith("/")) {
+    str = str.slice(0, -1);
   }
   return str;
 }
