f989f2063dce15f9ab0596b11ad30b207bf9828d
Taha Yassine Kraiem <tahayk2@gmail.com>
2026-01-19 16:48:41 +0100

    refactor(DB): removed unused autocomplete

--- ee/scripts/schema/db/init_dbs/clickhouse/create/init_schema.sql
+++ ee/scripts/schema/db/init_dbs/clickhouse/create/init_schema.sql
@@ -1,17 +1,6 @@
 CREATE OR REPLACE FUNCTION openreplay_version AS() -> 'v1.25.0-ee';
 CREATE DATABASE IF NOT EXISTS experimental;
 
-CREATE TABLE IF NOT EXISTS experimental.autocomplete
-(
-    project_id UInt16,
-    type       LowCardinality(String),
-    value      String,
-    _timestamp DateTime DEFAULT now()
-) ENGINE = ReplacingMergeTree(_timestamp)
-      PARTITION BY toYYYYMM(_timestamp)
-      ORDER BY (project_id, type, value);
-
-
 CREATE TABLE IF NOT EXISTS experimental.sessions
 (
     session_id           UInt64,
