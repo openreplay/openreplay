f532d3af5b2cd1657399d65c832ecd5e8cc64c30
Taha Yassine Kraiem <tahayk2@gmail.com>
2026-01-19 16:48:41 +0100

    refactor(DB): removed unused autocomplete

--- scripts/schema/db/init_dbs/clickhouse/1.25.0/1.25.0.sql
+++ scripts/schema/db/init_dbs/clickhouse/1.25.0/1.25.0.sql
@@ -74,3 +74,5 @@ ALTER TABLE product_analytics.autocomplete_events_grouped
 
 ALTER TABLE product_analytics.users_distinct_id
     MODIFY TTL _deleted_at WHERE _deleted_at != '1970-01-01 00:00:00';
+
+DROP TABLE IF EXISTS experimental.autocomplete;
