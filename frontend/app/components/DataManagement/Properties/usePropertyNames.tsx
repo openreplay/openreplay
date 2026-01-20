359dbc8c03fb23827ef73f735de45e3825cdd6e5
nick-delirium <nikita@openreplay.com>
2026-01-20 11:48:44 +0100

    ui: support hidden lexicon for prop names

--- frontend/app/components/DataManagement/Properties/usePropertyNames.tsx
+++ frontend/app/components/DataManagement/Properties/usePropertyNames.tsx
@@ -3,6 +3,8 @@ import { fetchList } from './api';
 import { useQuery } from '@tanstack/react-query';
 import { TextEllipsis } from 'UI';
 import { Loader } from 'lucide-react';
+import { menuHidden } from '@/utils/split-utils';
+
 /**
  * hook that will get all properties and then return a method to grab displayname by property name
  */
@@ -10,6 +12,7 @@ function usePropertyNames(source: 'events' | 'users') {
   const { data = { properties: [] }, isPending } = useQuery({
     queryKey: ['props-list', source],
     queryFn: () => fetchList(source),
+    enabled: !menuHidden.lexicon,
   });
 
   const getDisplayNameStr = (propName: string) => {
@@ -20,11 +23,13 @@ function usePropertyNames(source: 'events' | 'users') {
   };
 
   const getDisplayName = (propName: string, options?: Record<string, any>) => {
+    if (menuHidden.lexicon)
+      return <TextEllipsis {...options} text={propName} />;
     if (isPending)
       return (
         <div className="flex items-center gap-1">
-          <TextEllipsis {...options} text={propName} />
           <Loader className="animate-spin" size={12} />
+          <TextEllipsis {...options} text={propName} />
         </div>
       );
     const prop = data.properties.find((p) => p.name === propName);
