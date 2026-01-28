ddde65a1a5a200c8e9385fe2d218773a8023828e
Taha Yassine Kraiem <tahayk2@gmail.com>
2026-01-28 15:17:24 +0100

    refactor(DB): changed users&events simple autocomplete

--- scripts/schema/db/init_dbs/clickhouse/create/init_schema.sql
+++ scripts/schema/db/init_dbs/clickhouse/create/init_schema.sql
@@ -890,17 +890,17 @@ SELECT project_id,
 FROM product_analytics.events
          ARRAY JOIN
      [--(name,column,auto-captured)
-         ('user_id', "$user_id", FALSE),
-         ('sdk_edition', "$sdk_edition", TRUE),
-         ('sdk_version', "$sdk_version", TRUE),
-         ('current_url', "$current_url", TRUE),
-         ('current_path', "$current_path", TRUE),
-         ('initial_referrer', "$initial_referrer", TRUE),
-         ('referring_domain', "$referring_domain", TRUE),
-         ('country', "$country", TRUE),
-         ('state', "$state", TRUE),
-         ('city', "$city", TRUE),
-         ('or_api_endpoint', "$or_api_endpoint", TRUE)
+         ('$user_id', "$user_id", FALSE),
+         ('$sdk_edition', "$sdk_edition", TRUE),
+         ('$sdk_version', "$sdk_version", TRUE),
+         ('$current_url', "$current_url", TRUE),
+         ('$current_path', "$current_path", TRUE),
+         ('$initial_referrer', "$initial_referrer", TRUE),
+         ('$referring_domain', "$referring_domain", TRUE),
+         ('$country', "$country", TRUE),
+         ('$state', "$state", TRUE),
+         ('$city', "$city", TRUE),
+         ('$or_api_endpoint', "$or_api_endpoint", TRUE)
          ] AS t
 WHERE isNotNull(t.2)
   AND notEmpty(toString(t.2))
@@ -918,25 +918,25 @@ SELECT project_id,
 FROM product_analytics.users
          ARRAY JOIN
      [--(name,column,auto-captured)
-         ('user_id', "$user_id", FALSE),
-         ('email', "$email", FALSE),
-         ('name', "$name", FALSE),
-         ('first_name', "$first_name", FALSE),
-         ('last_name', "$last_name", FALSE),
-         ('phone', "$phone", FALSE),
-         ('sdk_edition', "$sdk_edition", TRUE),
-         ('sdk_version', "$sdk_version", TRUE),
-         ('current_url', "$current_url", TRUE),
-         ('current_path', "$current_path", TRUE),
-         ('initial_referrer', "$initial_referrer", TRUE),
-         ('referring_domain', "$referring_domain", TRUE),
+         ('$user_id', "$user_id", FALSE),
+         ('$email', "$email", FALSE),
+         ('$name', "$name", FALSE),
+         ('$first_name', "$first_name", FALSE),
+         ('$last_name', "$last_name", FALSE),
+         ('$phone', "$phone", FALSE),
+         ('$sdk_edition', "$sdk_edition", TRUE),
+         ('$sdk_version', "$sdk_version", TRUE),
+         ('$current_url', "$current_url", TRUE),
+         ('$current_path', "$current_path", TRUE),
+         ('$initial_referrer', "$initial_referrer", TRUE),
+         ('$referring_domain', "$referring_domain", TRUE),
          ('initial_utm_source', initial_utm_source, TRUE),
          ('initial_utm_medium', initial_utm_medium, TRUE),
          ('initial_utm_campaign', initial_utm_campaign, TRUE),
-         ('country', "$country", TRUE),
-         ('state', "$state", TRUE),
-         ('city', "$city", TRUE),
-         ('or_api_endpoint', "$or_api_endpoint", TRUE)
+         ('$country', "$country", TRUE),
+         ('$state', "$state", TRUE),
+         ('$city', "$city", TRUE),
+         ('$or_api_endpoint', "$or_api_endpoint", TRUE)
          ] AS t
 WHERE isNotNull(t.2)
   AND notEmpty(toString(t.2))
