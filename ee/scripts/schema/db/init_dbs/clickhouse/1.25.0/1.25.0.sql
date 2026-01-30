633335915494b8be49563bd97a13f0a926ac903b
Taha Yassine Kraiem <tahayk2@gmail.com>
2026-01-30 16:14:19 +0100

    refactor(DB): changed properties extraction MVs

--- ee/scripts/schema/db/init_dbs/clickhouse/1.25.0/1.25.0.sql
+++ ee/scripts/schema/db/init_dbs/clickhouse/1.25.0/1.25.0.sql
@@ -189,3 +189,42 @@ CREATE TABLE IF NOT EXISTS product_analytics.all_properties_customized
     _timestamp    DateTime               DEFAULT now()
 ) ENGINE = ReplacingMergeTree(_timestamp, _is_deleted)
       ORDER BY (project_id, source, property_name, auto_captured);
+
+DROP TABLE IF EXISTS product_analytics.events_all_properties_extractor_mv;
+CREATE MATERIALIZED VIEW IF NOT EXISTS product_analytics.events_all_properties_extractor_mv
+    TO product_analytics.all_properties AS
+SELECT project_id,
+       'events'                    AS source,
+       property_name,
+       TRUE                        AS is_event_property,
+       auto_captured_property      AS auto_captured,
+       0                           AS data_count,
+       0                           AS query_count,
+       event_properties.created_at AS created_at,
+       FALSE                       AS _edited_by_user
+FROM product_analytics.event_properties;
+
+DROP TABLE IF EXISTS product_analytics.users_all_properties_extractor_mv;
+CREATE MATERIALIZED VIEW IF NOT EXISTS product_analytics.users_all_properties_extractor_mv
+    TO product_analytics.all_properties AS
+SELECT project_id,
+       'users'                AS source,
+       property_name,
+       FALSE                  AS is_event_property,
+       auto_captured_property AS auto_captured,
+       0                      AS data_count,
+       0                      AS query_count,
+       _timestamp             AS created_at,
+       FALSE                  AS _edited_by_user
+FROM product_analytics.user_properties;
+
+DROP TABLE IF EXISTS product_analytics.all_events_extractor_mv;
+CREATE MATERIALIZED VIEW IF NOT EXISTS product_analytics.all_events_extractor_mv
+    TO product_analytics.all_events AS
+SELECT project_id,
+       `$auto_captured` AS auto_captured,
+       `$event_name`    AS event_name,
+       created_at       AS created_at,
+       FALSE            AS _edited_by_user
+FROM product_analytics.events
+GROUP BY ALL;
