71a5484ba9192889bbcb4f87f1772257437c2c66
nick-delirium <nikita@openreplay.com>
2026-01-30 17:22:02 +0100

    ui: auditinput classnames fix

--- frontend/app/components/Client/Audit/AuditSearchField/AuditSearchField.tsx
+++ frontend/app/components/Client/Audit/AuditSearchField/AuditSearchField.tsx
@@ -26,7 +26,7 @@ function AuditSearchField(props: Props) {
       />
       <Input
         name="searchQuery"
-        // className="bg-white p-2 border border-gray-light rounded-sm w-full pl-10"
+        className="bg-white! p-2! border! border-gray-light! rounded-sm! w-full! pl-10!"
         placeholder="Filter by name"
         onChange={write}
         icon="search"
