8ceb34072f199db6e3dd5efc5b9195aaf6a43e42
Shekar Siri <sshekarsiri@gmail.com>
2026-02-02 13:20:43 +0100

    change(api): cherry-pick audit and perssmissions changes

--- backend/pkg/analytics/events/api/handlers.go
+++ backend/pkg/analytics/events/api/handlers.go
@@ -31,8 +31,8 @@ func NewHandlers(log logger.Logger, jsonSizeLimit int64, events events.Events, r
 
 func (h *handlersImpl) GetAll() []*api.Description {
 	return []*api.Description{
-		{"/{project}/events", "POST", req.HandleWithBody(h.eventsSearch), []string{api.DATA_MANAGEMENT}, api.DoNotTrack},
-        {"/{project}/events/{eventId}", "GET", req.Handle(h.getEvent), []string{api.DATA_MANAGEMENT}, "get_event_by_id"},
+		{"/{project}/events", "POST", api.AutoRespondContextWithBody(h, h.eventsSearch), []string{api.DATA_MANAGEMENT}, api.DoNotTrack},
+        {"/{project}/events/{eventId}", "GET", api.AutoRespondContext(h, h.getEvent), []string{api.DATA_MANAGEMENT}, "get_event_by_id"},
 	}
 }
 
