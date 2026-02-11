f93520ad02ab54f0ad595689fcdd745b206bd814
nick-delirium <nikita@openreplay.com>
2026-02-11 17:24:17 +0100

    ui: fix ssa time format

--- frontend/app/utils/intlUtils.ts
+++ frontend/app/utils/intlUtils.ts
@@ -1,8 +1,8 @@
-export function getLocalHourFormat() {
+export function getLocalHourFormat(withSeconds = true): string {
   const date = new Date();
   const timeString = date.toLocaleTimeString();
   const hasAmPm =
     timeString.toLowerCase().includes('am') ||
     timeString.toLowerCase().includes('pm');
-  return hasAmPm ? 'hh:mm:ss a' : 'HH:mm:ss';
+  return hasAmPm ? `hh:mm${withSeconds ? ':ss' : ''}a` : `HH:mm${withSeconds ? ':ss' : ''}`;
 }
