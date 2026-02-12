ace9d6f52e09bcaaa9f3eef37f6bab664f40ead7
Shekar Siri <sshekarsiri@gmail.com>
2026-02-12 14:19:21 +0100

    feat(api): actions - review and updates

--- backend/pkg/analytics/lexicon/model/action.go
+++ backend/pkg/analytics/lexicon/model/action.go
@@ -4,8 +4,8 @@ import "openreplay/backend/pkg/analytics/filters"
 
 type Action struct {
 	ActionID    string           `json:"actionId" db:"action_id"`
-	ProjectID   uint             `json:"projectId" db:"project_id" validate:"required"`
-	UserID      uint             `json:"userId" db:"user_id"`
+	ProjectID   uint64           `json:"projectId" db:"project_id" validate:"required"`
+	UserID      uint64           `json:"userId" db:"user_id"`
 	Name        string           `json:"name" db:"name" validate:"required,min=1,max=255"`
 	Description string           `json:"description" db:"description" validate:"max=1024"`
 	Filters     []filters.Filter `json:"filters" db:"filters" validate:"omitempty,dive"`
@@ -15,23 +15,20 @@ type Action struct {
 }
 
 type UpdateActionRequest struct {
-	Name        string           `json:"name,omitempty" validate:"omitempty,min=1,max=255"`
-	Description string           `json:"description,omitempty" validate:"omitempty,max=1024"`
-	Filters     []filters.Filter `json:"filters,omitempty" validate:"omitempty,dive"`
-	IsPublic    *bool            `json:"isPublic,omitempty"`
+	Name        *string           `json:"name" validate:"omitempty,min=1,max=255"`
+	Description *string           `json:"description" validate:"omitempty,max=1024"`
+	Filters     *[]filters.Filter `json:"filters" validate:"omitempty,dive"`
 }
 
 type CreateActionRequest struct {
 	Name        string           `json:"name" validate:"required,min=1,max=255"`
 	Description string           `json:"description" validate:"max=1024"`
 	Filters     []filters.Filter `json:"filters" validate:"omitempty,dive"`
-	IsPublic    bool             `json:"isPublic"`
 }
 
 type SearchActionRequest struct {
-	Name     string `json:"name,omitempty"`
-	UserID   *uint  `json:"userId,omitempty"`
-	IsPublic *bool  `json:"isPublic,omitempty"`
+	Name   string  `json:"name,omitempty"`
+	UserID *uint64 `json:"userId,omitempty"`
 
 	SortBy    string `json:"sortBy,omitempty" validate:"omitempty,oneof=name createdAt updatedAt"`
 	SortOrder string `json:"sortOrder,omitempty" validate:"omitempty,oneof=asc desc"`
