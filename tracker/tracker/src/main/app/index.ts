13645b790fe205078bf6666cbd6a4370e7c0dc12
nick-delirium <nikita@openreplay.com>
2026-04-14 13:04:00 +0200

    tracker: check proto version when continuing the session

--- tracker/tracker/src/main/app/index.ts
+++ tracker/tracker/src/main/app/index.ts
@@ -1167,10 +1167,15 @@ export default class App {
   singleBuffer = false
 
   private checkSessionToken(forceNew?: boolean) {
-    const lsReset = this.sessionStorage.getItem(this.options.session_reset_key) !== null
-    const needNewSessionID = forceNew || lsReset
+    const PROTO_VERSION = "2";
+    const needReset = this.sessionStorage.getItem(this.options.session_reset_key) !== null
+    let needNewSessionID = forceNew || needReset
     const sessionToken = this.session.getSessionToken(this.projectKey)
-
+    if (sessionToken) {
+      const storedVersion = this.sessionStorage.getItem(`${this.options.session_token_key}_version`)
+      needNewSessionID = !storedVersion || storedVersion !== PROTO_VERSION
+      this.sessionStorage.setItem(`${this.options.session_token_key}_version`, PROTO_VERSION)
+    }
     return needNewSessionID || !sessionToken
   }
 
