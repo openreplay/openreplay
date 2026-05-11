122a5d5b7e723c6cbaaa26da990e4896fa9149c2
KRAIEM Taha Yassine <tahay@openreplay.com>
2026-05-11 11:58:12 +0200

    refactor(DB): removed unused table and changed DB migration refactor(chalice): removed unused table

--- ee/scripts/schema/db/init_dbs/clickhouse/1.23.0/2.sql
+++ ee/scripts/schema/db/init_dbs/clickhouse/1.23.0/2.sql
@@ -3,4 +3,6 @@ SELECT throwIf((SELECT openreplay_migration_state()) != 1, 'Previous step is not
 ALTER TABLE experimental.sessions
     MODIFY COLUMN platform Enum8('web'=1,'mobile'=2) DEFAULT 'web';
 
+DROP TABLE IF EXISTS experimental.sessions_l7d_mv;
+
 CREATE OR REPLACE FUNCTION openreplay_migration_state AS() -> 2;
--- ee/scripts/schema/db/init_dbs/clickhouse/1.25.0/1.sql
+++ ee/scripts/schema/db/init_dbs/clickhouse/1.25.0/1.sql
@@ -1,4 +1,5 @@
 SELECT throwIf((SELECT openreplay_migration_state()) != 0, 'Previous step is not done') AS check;
+DROP TABLE IF EXISTS experimental.sessions_l7d_mv;
 ALTER TABLE product_analytics.events
     ADD COLUMN IF NOT EXISTS "$current_path" String MATERIALIZED path("$current_url") AFTER "$current_url"
     SETTINGS max_execution_time = 0;
