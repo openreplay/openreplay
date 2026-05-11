b2ef774d639ef37de6cff07e7cb6e3ca58bfda91
KRAIEM Taha Yassine <tahay@openreplay.com>
2026-05-11 14:03:55 +0200

    refactor(DB): CH prepare for v1.28

---
+++ ee/scripts/schema/db/init_dbs/clickhouse/1.28.0/0.sql
@@ -0,0 +1,10 @@
+SELECT throwIf((SELECT openreplay_version()) != 'v1.27.0-ee', 'Previous version does not match v1.27.0-ee') AS check;
+CREATE FUNCTION IF NOT EXISTS openreplay_migration_state AS() ->
+    -1;
+SELECT throwIf((SELECT openreplay_migration_state()) > -1,
+               'Previous migration did not finish[' || (SELECT openreplay_version()) || ' step:' ||
+               (SELECT openreplay_migration_state()) || ']') AS check;
+
+CREATE OR REPLACE FUNCTION openreplay_version AS() -> 'v1.28.0-ee';
+DROP TABLE IF EXISTS experimental.sessions_l7d_mv;
+CREATE OR REPLACE FUNCTION openreplay_migration_state AS() -> -1;
