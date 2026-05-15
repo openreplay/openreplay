9c3b6721b574ca8f8c8820d4fe27cb279661682f
nick-delirium <nikita@openreplay.com>
2026-05-15 17:39:54 +0200

    ui: make dbip smaller

--- frontend/app/components/ui/CountryFlag/DbIPNotice.tsx
+++ frontend/app/components/ui/CountryFlag/DbIPNotice.tsx
@@ -7,26 +7,23 @@ interface Props {
   className?: string;
 }
 
-const DbIPNotice = () => null;
-
-// saas only for now
-// const DbIPNotice: React.FC<Props> = ({ size = 14, className = '' }) => (
-//   <Tooltip
-//     title={
-//       <a
-//         href="https://db-ip.com"
-//         target="_blank"
-//         rel="noopener noreferrer"
-//         style={{ color: 'inherit', textDecoration: 'underline' }}
-//       >
-//         Geolocation by DB-IP
-//       </a>
-//     }
-//   >
-//     <span className={`inline-flex items-center ${className}`}>
-//       <Icon name="info-circle" size={size} />
-//     </span>
-//   </Tooltip>
-// );
+const DbIPNotice: React.FC<Props> = ({ size = 8, className = '' }) => (
+  <Tooltip
+    title={
+      <a
+        href="https://db-ip.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: 'inherit', textDecoration: 'underline' }}
+      >
+        Geolocation by DB-IP
+      </a>
+    }
+  >
+    <span className={`inline-flex items-center ${className}`}>
+      <Icon name="info-circle" size={size} />
+    </span>
+  </Tooltip>
+);
 
 export default DbIPNotice;
