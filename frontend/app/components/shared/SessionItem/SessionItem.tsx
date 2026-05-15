b15c04df7a58148af586740d051963c4634b2ea9
nick-delirium <nikita@openreplay.com>
2026-05-15 17:53:50 +0200

    ui: remove note from session list

--- frontend/app/components/shared/SessionItem/SessionItem.tsx
+++ frontend/app/components/shared/SessionItem/SessionItem.tsx
@@ -1,8 +1,9 @@
+import { Tooltip } from 'antd';
 import cn from 'classnames';
 import { Duration } from 'luxon';
 import { observer } from 'mobx-react-lite';
-import React, { useState, useCallback, useMemo } from 'react';
-import { RouteComponentProps, useHistory, withRouter } from 'App/routing';
+import React, { useCallback, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { durationFormatted, formatTimeOrDate } from 'App/date';
 import { useStore } from 'App/mstore';
@@ -12,16 +13,15 @@ import {
   liveSession,
   sessions as sessionsRoute,
 } from 'App/routes';
+import { RouteComponentProps, useHistory, withRouter } from 'App/routing';
 import { capitalize } from 'App/utils';
-import { Avatar, CountryFlag, DbIPNotice, Icon, Label, TextEllipsis } from 'UI';
+import { Avatar, CountryFlag, Icon, Label, TextEllipsis } from 'UI';
 
 import Counter from './Counter';
 import ErrorBars from './ErrorBars';
 import PlayLink from './PlayLink';
 import SessionMetaList from './SessionMetaList';
 import stl from './sessionItem.module.css';
-import { useTranslation } from 'react-i18next';
-import { Tooltip } from 'antd';
 
 const ASSIST_ROUTE = assistRoute();
 const ASSIST_LIVE_SESSION = liveSession();
@@ -405,7 +405,6 @@ function SessionItem(props: RouteComponentProps & Props) {
                   country={userCountry}
                   showLabel={true}
                 />
-                <DbIPNotice className="ml-1" />
               </div>
               <div className="flex items-center text-sm text-neutral-500">
                 {userBrowser && (
