f9f5f6d02beeb8a1c384d908d3f7234d67e4e486
alexander <zavorotynskiy@pm.me>
2026-05-20 15:24:21 +0200

    fix(storage): removed unnecessary go call

--- backend/cmd/storage/main.go
+++ backend/cmd/storage/main.go
@@ -100,7 +100,7 @@ func main() {
 			consumer.Close()
 			os.Exit(0)
 		case <-counterTick:
-			go log.Info(ctx, "count: %d", counter)
+			log.Info(ctx, "count: %d", counter)
 			counter = 0
 			srv.Wait()
 			if err := consumer.Commit(); err != nil {
