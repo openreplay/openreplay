541fc467df6e61e084e3d84585dc73a705c83340
alexander <zavorotynskiy@pm.me>
2026-08-10 14:30:55 +0200

    feat(backend): process session's data even when the project is inactive

--- backend/cmd/sink/main.go
+++ backend/cmd/sink/main.go
@@ -78,7 +78,7 @@ func main() {
 	})
 
 	projManager := projects.New(log, pgConn, redisClient, dbMetric)
-	sessManager := sessions.New(log, pgConn, projManager, redisClient, dbMetric, sessions.DoNotIgnoreInactiveProjects)
+	sessManager := sessions.New(log, pgConn, projManager, redisClient, dbMetric, sessions.IgnoreInactiveProjects)
 
 	filePool := sessionwriter.NewFilePool(log, int(cfg.FsUlimit), cfg.FileBuffer, cfg.MaxFileSize, cfg.SyncWorkers)
 	mobWriter := sessionwriter.NewMobWriter(log, sessManager, filePool, cfg.FsDir, cfg.FileSplitTime)
